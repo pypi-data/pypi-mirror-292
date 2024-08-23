0486b1056cca34fbdb873753710dcc8a9b42232f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:47 +0000

    Add package RunwayLib/0.1.3/runwaylib-0.1.3.tar.gz

---
+++ packages/RunwayLib/runwaylib-0.1.3.tar.gz/runwaylib-0.1.3/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RunwayLib",
+    version="0.1.3",
+    author="Parsa",
+    author_email="your.email@example.com",  # Add your email here
+    description="A lib for a server",  # Update this with a brief description of your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/runway",  # Add your project's homepage URL here
+    py_modules=["runway"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Specify the license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],  # Add dependencies here if your package has any
+)
