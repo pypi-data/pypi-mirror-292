ba65e63daf27a87960820b58a97b0df89ae1f8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:29 +0000

    Add package telometer/0.92/telometer-0.92-py3-none-any.whl

---
+++ packages/telometer/telometer-0.92-py3-none-any.whl/telometer/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = '0.92'
+__author__ = 'Santiago E Sanchez'
+__email__ = 'santy.esanchez@gmail.com'
+__license__ = 'MIT'
+
+import logging
+from .telometer import (
+    process_bam_file,
+    measure_telomere_length,
+    plot_telomere_regions
+)
+
+
