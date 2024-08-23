135e8a3239902ca694219a110d4b55d0de7d91b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:20 +0000

    Add package rms-pdsfile/0.0.9/rms_pdsfile-0.0.9-py3-none-any.whl

---
+++ packages/rms-pdsfile/rms_pdsfile-0.0.9-py3-none-any.whl/pdsfile/pds3file/tests/helper.py
@@ -0,0 +1,36 @@
+##########################################################################################
+# pds3file/tests/helper.py
+#
+# Helper functions for tests on pds3file
+##########################################################################################
+
+import os
+import pdsfile.pds3file as pds3file
+
+try:
+    PDS3_HOLDINGS_DIR = os.environ['PDS3_HOLDINGS_DIR']
+except KeyError: # pragma: no cover
+    raise KeyError("'PDS3_HOLDINGS_DIR' environment variable not set")
+
+def instantiate_target_pdsfile(path, is_abspath=True):
+    if is_abspath:
+        TESTFILE_PATH = PDS3_HOLDINGS_DIR + '/' + path
+        target_pdsfile = pds3file.Pds3File.from_abspath(TESTFILE_PATH)
+    else:
+        TESTFILE_PATH = path
+        target_pdsfile = pds3file.Pds3File.from_logical_path(TESTFILE_PATH)
+    return target_pdsfile
+
+def get_pdsfiles(paths, is_abspath=True):
+    pdsfiles_arr = []
+    if is_abspath:
+        for path in paths:
+            TESTFILE_PATH = PDS3_HOLDINGS_DIR + '/' +  path
+            target_pdsfile = pds3file.Pds3File.from_abspath(TESTFILE_PATH)
+            pdsfiles_arr.append(target_pdsfile)
+    else:
+        for path in paths:
+            TESTFILE_PATH = path
+            target_pdsfile = pds3file.Pds3File.from_logical_path(TESTFILE_PATH)
+            pdsfiles_arr.append(target_pdsfile)
+    return pdsfiles_arr
