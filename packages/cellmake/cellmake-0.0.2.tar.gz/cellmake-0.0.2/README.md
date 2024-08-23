1e3d4e53ea918c354af6e9d85e51f7c4a5e6737c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:51 +0000

    Add package cellmake/0.0.2/cellmake-0.0.2.tar.gz

---
+++ packages/cellmake/cellmake-0.0.2.tar.gz/cellmake-0.0.2/README.md
@@ -0,0 +1,3 @@
+# cellmake
+
+cellmake is a single-cell RNA-sequencing processing pipeline.
---
+++ packages/cellmake/cellmake-0.0.2.tar.gz/cellmake-0.0.2/cellmake/__init__.py
@@ -0,0 +1,3 @@
+"""A Python package for single-cell RNA-sequencing data analysis."""
+# Semantic Versioning: https://semver.org/
+__version__ = "0.0.2"
