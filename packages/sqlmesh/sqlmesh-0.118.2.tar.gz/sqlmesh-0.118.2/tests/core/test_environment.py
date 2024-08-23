839d39c252f265940aa0da2e1338af1766d85028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:03:06 +0000

    Add package sqlmesh/0.118.2/sqlmesh-0.118.2.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.118.2.tar.gz/sqlmesh-0.118.2/tests/core/test_environment.py
@@ -0,0 +1,44 @@
+import pytest
+
+from sqlmesh.core.environment import Environment, EnvironmentNamingInfo
+
+
+def test_sanitize_name():
+    assert Environment.sanitize_name("12foo!#%@") == "12foo____"
+    assert Environment.sanitize_name("__test@$@%") == "__test____"
+    assert Environment.sanitize_name("test") == "test"
+    assert Environment.sanitize_name("-test_") == "_test_"
+    with pytest.raises(TypeError, match="Expected str or Environment, got int"):
+        Environment.sanitize_name(123)
+
+
+@pytest.mark.parametrize(
+    "mapping, name, expected",
+    [
+        # Match the first pattern
+        ({"^prod$": "prod_catalog", "^dev$": "dev_catalog"}, "prod", "prod_catalog"),
+        # Match the second pattern
+        ({"^prod$": "prod_catalog", "^dev$": "dev_catalog"}, "dev", "dev_catalog"),
+        # Match no pattern
+        (
+            {"^prod$": "prod_catalog", "^dev$": "dev_catalog"},
+            "develop",
+            None,
+        ),
+        # Match both patterns but take the first
+        (
+            {".*": "catchall", "^prod$": "will_never_happen"},
+            "prod",
+            "catchall",
+        ),
+        # Don't need to test invalid regex pattern since regex patterns are validated when the config is parsed
+    ],
+)
+def test_from_environment_catalog_mapping(mapping, name, expected):
+    assert (
+        EnvironmentNamingInfo.from_environment_catalog_mapping(
+            mapping,
+            name,
+        ).catalog_name_override
+        == expected
+    )
