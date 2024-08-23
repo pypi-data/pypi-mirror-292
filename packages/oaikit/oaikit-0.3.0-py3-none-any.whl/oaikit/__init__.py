e50c3f2aa4a75be8f013e765130c1679b2619bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:54 +0000

    Add package oaikit/0.3.0/oaikit-0.3.0-py3-none-any.whl

---
+++ packages/oaikit/oaikit-0.3.0-py3-none-any.whl/oaikit/__init__.py
@@ -0,0 +1,8 @@
+from oaikit.oai import OAI, T_OAI
+from oaikit.msg import OAIMsg
+from oaikit.models import OAIModels, T_OAIModels, GPT_4O, GPT_4O_MINI, GPT_4O_2024_08_06, GPT_4O_MINI_2024_07_18
+from oaikit.role import SYSTEM, USER, ASSISTANT, OAIRole
+from oaikit.content.text import ContentText
+from oaikit.content.image import ContentImage
+from oaikit.fn_call.model import OAISchemaParams, FnCallOAI, OAISchema
+from oaikit.completion import ChatCompletionHandler
