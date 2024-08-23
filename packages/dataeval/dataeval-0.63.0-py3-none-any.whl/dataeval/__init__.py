5becabdbe326f6a61710b466b6b0216755bbdbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:20 +0000

    Add package dataeval/0.63.0/dataeval-0.63.0-py3-none-any.whl

---
+++ packages/dataeval/dataeval-0.63.0-py3-none-any.whl/dataeval/__init__.py
@@ -0,0 +1,18 @@
+from importlib.util import find_spec
+
+from . import detectors, flags, metrics
+
+__version__ = "0.63.0"
+
+__all__ = ["detectors", "flags", "metrics"]
+
+if find_spec("torch") is not None:  # pragma: no cover
+    from . import models, workflows
+
+    __all__ += ["models", "workflows"]
+elif find_spec("tensorflow") is not None:  # pragma: no cover
+    from . import models
+
+    __all__ += ["models"]
+
+del find_spec
