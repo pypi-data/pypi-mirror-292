2908839ca8b2f04690a247ad91d0a62523bccac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:03:31 +0000

    Add package MeUtils/2024.8.22.21.22.18/meutils-2024.8.22.21.22.18.tar.gz

---
+++ packages/MeUtils/meutils-2024.8.22.21.22.18.tar.gz/meutils-2024.8.22.21.22.18/meutils/schemas/image_types.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : image_types
+# @Time         : 2024/8/21 14:18
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+ASPECT_RATIOS = {
+    "1:1": "1024x1024",
+
+    "1:2": "512x1024",
+
+    '2:3': "768x512",
+    '3:2': "512x768",
+
+    "4:3": "1280x960",  # "1024x768"
+    "3:4": "960x1280",
+
+    "5:4": "1280x960",
+    "4:5": "960x1280",
+
+    "16:9": "1366x768",  # "1024x576"
+    "9:16": "768x1366",
+
+    "21:9": "1344x576",
+}
