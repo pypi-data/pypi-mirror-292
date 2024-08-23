2671df47c0bb30e9d0b3089f902bacb8ca14fe42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 17:03:45 +0000

    Add package ascmhl/1.0.4/ascmhl-1.0.4-py3-none-any.whl

---
+++ packages/ascmhl/ascmhl-1.0.4-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,18 @@
+from packaging import version
+
+from ascmhl.utils import datetime_now_filename_string
+import datetime
+import os, time
+
+
+def test_time():
+    datetime_now_filename = datetime_now_filename_string()
+    datetime_now_filename_reference = datetime.datetime.strftime(
+        datetime.datetime.now(datetime.UTC), "%Y-%m-%d_%H%M%SZ"
+    )
+
+    print("datetime_now_filename:           " + datetime_now_filename)
+    print("datetime_now_filename_reference: " + datetime_now_filename_reference)
+
+    # seems always to be working, as tests run in UTC ?
+    assert datetime_now_filename == datetime_now_filename_reference
