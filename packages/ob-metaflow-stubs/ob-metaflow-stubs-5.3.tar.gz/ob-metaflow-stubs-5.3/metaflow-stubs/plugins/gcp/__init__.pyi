ed236634aab4f836afdbdc6304ce0e1152763061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:31 +0000

    Add package ob-metaflow-stubs/5.3/ob-metaflow-stubs-5.3.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.3.tar.gz/ob-metaflow-stubs-5.3/metaflow-stubs/plugins/gcp/__init__.pyi
@@ -0,0 +1,12 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.15.1+ob(v1)                                                   #
+# Generated on 2024-08-22T21:03:01.672405                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def get_credentials(scopes, *args, **kwargs):
+    ...
+
