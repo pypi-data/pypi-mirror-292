a2774f02efb21288bd18d16779483aa9eb09b6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:02:53 +0000

    Add package geometricjax-experimental/0.3.0/geometricjax_experimental-0.3.0.tar.gz

---
+++ packages/geometricjax-experimental/geometricjax_experimental-0.3.0.tar.gz/geometricjax_experimental-0.3.0/geometricjax_experimental/pg.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("hello world 2")
+print("hello world 3")
+print("hello with new tomlver")
+print("hello wolrd4")
+print("hello world5")
+
+
+def hi():
+    return "hello world"
+
+
+def func2():
+    return "hello world 2"
