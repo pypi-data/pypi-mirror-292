2d53175fe2d82fcd588535ee5967b6b5a2121437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:04:38 +0000

    Add package griffe-endstone/0.1.0/griffe_endstone-0.1.0-py2.py3-none-any.whl

---
+++ packages/griffe-endstone/griffe_endstone-0.1.0-py2.py3-none-any.whl/griffe_endstone/extension.py
@@ -0,0 +1,39 @@
+import ast
+
+from griffe import Extension, ObjectNode, Function, Class, Kind, typing_overload
+
+
+class EndstoneExtension(Extension):
+
+    def on_function_instance(self, *, node: ast.AST | ObjectNode, func: Function) -> None:
+        """
+        Fix overloads with implementations in stub files not detected
+        by adding a dummy function to class members
+
+        For original issue, please see: https://github.com/mkdocstrings/griffe/issues/116
+        """
+
+        if isinstance(node, ObjectNode):
+            return
+
+        if func.parent.kind != Kind.CLASS:
+            return
+
+        overload = False
+        for decorator in func.decorators:
+            overload |= decorator.callable_path in typing_overload
+
+        if not overload:
+            return
+
+        parent_class: Class = func.parent
+        try:
+            function = parent_class.members[func.name]
+        except KeyError:
+            function = Function(name=func.name)
+            parent_class.set_member(func.name, function)
+
+        if function.overloads is None:
+            function.overloads = []
+
+        function.overloads.append(func)
