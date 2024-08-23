aabf2b6c1b06d53bce2a1d3f05c358dc7b457e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:28 +0000

    Add package marimo/0.8.2/marimo-0.8.2-py3-none-any.whl

---
+++ packages/marimo/marimo-0.8.2-py3-none-any.whl/marimo/_config/settings.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class GlobalSettings:
+    DEVELOPMENT_MODE: bool = False
+    QUIET: bool = False
+    CHECK_STATUS_UPDATE: bool = False
+    TRACING: bool = os.getenv("MARIMO_TRACING", "false") in ("true", "1")
+    PROFILE_DIR: str | None = None
+
+
+GLOBAL_SETTINGS = GlobalSettings()
