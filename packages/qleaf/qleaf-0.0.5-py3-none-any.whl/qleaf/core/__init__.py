ddb5e972f56307cc22381922568bd273665266df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:49 +0000

    Add package qleaf/0.0.5/qleaf-0.0.5-py3-none-any.whl

---
+++ packages/qleaf/qleaf-0.0.5-py3-none-any.whl/qleaf/core/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2023 Jaehak Lee
+
+from . import main_window
+from . import abstract_comp
+from . import image
+from . import prop
+
+from .main_window import show_status
+import os, shutil
+
+CURRENT_SOURCE_CODE_FILE_PATH = os.path.relpath(__file__)
+
+def cout(*args):
+    show_status(" ".join([str(arg) for arg in args]))
+
+def setStyle(widget, style_name):    
+    sshFile=os.path.dirname(CURRENT_SOURCE_CODE_FILE_PATH)+"/../style/"+style_name+"/"+style_name+".qss"
+    print("sshFile Path:",sshFile)
+    with open(sshFile,"r") as fh:
+        widget.setStyleSheet(fh.read())
