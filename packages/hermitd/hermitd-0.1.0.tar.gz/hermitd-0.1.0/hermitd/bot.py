f7a2fd873f1b95516f0d5a34de0409edf67b6b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:04:21 +0000

    Add package hermitd/0.1.0/hermitd-0.1.0.tar.gz

---
+++ packages/hermitd/hermitd-0.1.0.tar.gz/hermitd-0.1.0/hermitd/bot.py
@@ -0,0 +1,25 @@
+import textwrap
+from hermitd.llm import LLM
+from hermitd.context import Context
+import hermitd.messages as messages
+
+
+class Bot:
+    GEN_CMD_PROMPT = textwrap.dedent(
+        """You are an assistant for our user using a posix shell. 
+        Your job is to generate a shell command satisfying the USER's prompt. \n
+        """
+    )
+
+    def __init__(self, user: str, session_id: int, llm: LLM) -> None:
+        self.user: str = user
+        self.session_id: int = session_id
+        self.llm: LLM = llm
+        self.context: Context = Context()
+
+    def generate_command(self, user_request: str) -> str:
+        prompt = Bot.GEN_CMD_PROMPT + self.context.get_context_prompt()
+        return self.llm.generate(user_request, header=prompt)
+
+    def save_context(self, context_type: messages.ShellOutputType, context: str):
+        self.context.save_shell_context(context_type, context)
