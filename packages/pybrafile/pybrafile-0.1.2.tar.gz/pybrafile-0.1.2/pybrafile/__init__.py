89b7f535e4f37e0839f9cf0010f59cf9ac1d2b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:04:00 +0000

    Add package pybrafile/0.1.2/pybrafile-0.1.2.tar.gz

---
+++ packages/pybrafile/pybrafile-0.1.2.tar.gz/pybrafile-0.1.2/pybrafile/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.2"
+
+def version()->str:
+    """Return the version of the lib
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    str
+        The string with version
+    """
+    return __version__
+
+# str functions
+from .file import FileUtils
+
+__all__ = [version, FileUtils]
