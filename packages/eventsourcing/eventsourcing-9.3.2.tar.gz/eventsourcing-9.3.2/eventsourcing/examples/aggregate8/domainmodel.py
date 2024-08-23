8005fcc17c9170447bf48599ef97f4ce557a26d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:42 +0000

    Add package eventsourcing/9.3.2/eventsourcing-9.3.2.tar.gz

---
+++ packages/eventsourcing/eventsourcing-9.3.2.tar.gz/eventsourcing-9.3.2/eventsourcing/examples/aggregate8/domainmodel.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from datetime import datetime  # noqa: TCH003
+from typing import List
+from uuid import UUID  # noqa: TCH003
+
+from pydantic import BaseModel, Extra
+
+from eventsourcing.domain import (
+    Aggregate as BaseAggregate,
+    CanInitAggregate,
+    CanMutateAggregate,
+    CanSnapshotAggregate,
+    event,
+)
+
+
+class DomainEvent(BaseModel):
+    originator_id: UUID
+    originator_version: int
+    timestamp: datetime
+
+    class Config:
+        frozen = True
+
+
+class Aggregate(BaseAggregate):
+    class Event(DomainEvent, CanMutateAggregate):
+        pass
+
+    class Created(Event, CanInitAggregate):
+        originator_topic: str
+
+
+class SnapshotState(BaseModel):
+    class Config:
+        extra = Extra.allow
+
+
+class AggregateSnapshot(DomainEvent, CanSnapshotAggregate):
+    topic: str
+    state: SnapshotState
+
+
+class Trick(BaseModel):
+    name: str
+
+
+class DogState(SnapshotState):
+    name: str
+    tricks: List[Trick]
+
+
+class Dog(Aggregate):
+    class Snapshot(AggregateSnapshot):
+        state: DogState
+
+    @event("Registered")
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.tricks: List[Trick] = []
+
+    @event("TrickAdded")
+    def add_trick(self, trick: Trick) -> None:
+        self.tricks.append(trick)
