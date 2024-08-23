0e8717a8a3ec362318936d24aa460b2fd7d130ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:49 +0000

    Add package changelog_gen/0.11.15/changelog_gen-0.11.15-py3-none-any.whl

---
+++ packages/changelog_gen/changelog_gen-0.11.15-py3-none-any.whl/changelog_gen/config.py
@@ -0,0 +1,282 @@
+from __future__ import annotations
+
+import dataclasses
+import json
+import re
+import string
+import typing as t
+from pathlib import Path
+
+import rtoml
+
+from changelog_gen import errors
+from changelog_gen.util import timer
+
+
+@dataclasses.dataclass
+class CommitType:
+    """Represent a supported commit_type."""
+
+    header: str
+    semver: str = "patch"
+
+
+SUPPORTED_TYPES = {
+    "feat": CommitType(
+        header="Features and Improvements",
+        semver="minor",
+    ),
+    "fix": CommitType(
+        header="Bug fixes",
+    ),
+    "bug": CommitType(
+        header="Bug fixes",
+    ),
+    "docs": CommitType(
+        header="Documentation",
+    ),
+    "chore": CommitType(
+        header="Miscellaneous",
+    ),
+    "ci": CommitType(
+        header="Miscellaneous",
+    ),
+    "perf": CommitType(
+        header="Miscellaneous",
+    ),
+    "refactor": CommitType(
+        header="Miscellaneous",
+    ),
+    "revert": CommitType(
+        header="Miscellaneous",
+    ),
+    "style": CommitType(
+        header="Miscellaneous",
+    ),
+    "test": CommitType(
+        header="Miscellaneous",
+    ),
+}
+
+
+@dataclasses.dataclass
+class PostProcessConfig:
+    """Post Processor configuration options."""
+
+    url: str | None = None
+    verb: str = "POST"
+    # The body to send as a post-processing command,
+    # can have the entries: ::issue_ref::, ::version::
+    body: str = '{"body": "Released on ::version::"}'
+    auth_type: str = "basic"  # future proof config
+    headers: dict | None = None
+    # Name of an environment variable to use as HTTP Basic Auth parameters.
+    # The variable should contain "{user}:{api_key}"
+    auth_env: str | None = None
+
+
+STRICT_VALIDATOR = re.compile(
+    r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?$",
+)
+
+
+@dataclasses.dataclass
+class Config:
+    """Changelog configuration options."""
+
+    current_version: str = ""
+    parser: t.Pattern = r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
+    serialisers: list[str] = dataclasses.field(default_factory=lambda: ["{major}.{minor}.{patch}"])
+    parts: dict[str, list[str]] = dataclasses.field(default_factory=dict)
+    files: dict = dataclasses.field(default_factory=dict)
+    strict: bool = False
+
+    verbose: int = 0
+
+    issue_link: str | None = None
+    commit_link: str | None = None
+    date_format: str | None = None
+    version_string: str = "v{new_version}"
+
+    allowed_branches: list[str] = dataclasses.field(default_factory=list)
+    commit_types: dict[str, CommitType] = dataclasses.field(default_factory=lambda: SUPPORTED_TYPES)
+
+    hooks: list[str] = dataclasses.field(default_factory=list)
+
+    interactive: bool = True
+    release: bool = True
+    commit: bool = True
+    tag: bool = True
+    allow_dirty: bool = False
+    allow_missing: bool = False
+    reject_empty: bool = False
+
+    pre_release: bool = False
+    pre_release_components: list[str] | None = None
+
+    post_process: PostProcessConfig | None = None
+
+    custom: dict = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self: t.Self) -> None:
+        """Process parser and validate if strict check enabled."""
+        self.parser = re.compile(self.parser)
+        if self.commit_types != SUPPORTED_TYPES:
+            commit_types = {}
+            for k, v in self.commit_types.items():
+                value = json.loads(v) if isinstance(v, str) else v
+                ct = CommitType(**value) if isinstance(value, dict) else value
+                commit_types[k.lower()] = ct
+            self.commit_types = commit_types
+
+        if self.strict:
+            parts = {component: self.parts.get(component, [0])[0] for component in self.parser.groupindex}
+            # Validate major, minor, patch in regex
+            configured_keys = list(parts.keys())
+
+            if {"major", "minor", "patch"} - set(configured_keys):
+                msg = "major.minor.patch, pattern required at minimum."
+                raise errors.UnsupportedParserError(msg)
+
+            if configured_keys[:3] != ["major", "minor", "patch"]:
+                msg = "major.minor.patch, pattern order required."
+                raise errors.UnsupportedParserError(msg)
+
+            serialised_keys = set()
+
+            for serialiser in self.serialisers:
+                version = serialiser.format(**parts)
+                # validate that version string fits SemVer 2.0.0
+                m = STRICT_VALIDATOR.match(version)
+                if m is None:
+                    msg = f"{serialiser} generates non SemVer 2.0.0 version string."
+                    raise errors.UnsupportedSerialiserError(msg)
+                serialised_keys.update([i[1] for i in string.Formatter().parse(serialiser)])
+
+            # Validate all components covered by at least one serialiser
+            missed_keys = set(configured_keys) - serialised_keys
+            if missed_keys:
+                msg = f"Not all parsed components handled by a serialiser, missing {missed_keys}."
+                raise errors.UnsupportedSerialiserError(msg)
+
+    @property
+    def semver_mappings(self: t.Self) -> dict[str, str]:
+        """Generate `type: semver` mapping from commit types."""
+        return {ct: c.semver for ct, c in self.commit_types.items()}
+
+    @property
+    def type_headers(self: t.Self) -> dict[str, str]:
+        """Generate `type: header` mapping from commit types."""
+        return {ct: c.header for ct, c in self.commit_types.items()}
+
+    def to_dict(self: Config) -> dict:
+        """Convert a Config object to a dictionary of key value pairs."""
+        data = dataclasses.asdict(self)
+        data["parser"] = data["parser"].pattern
+        ret = {k: v for k, v in data.items() if k != "files"}
+        ret["files"] = data.get("files")
+
+        return ret
+
+
+@timer
+def _process_overrides(overrides: dict) -> tuple[dict, PostProcessConfig | None]:
+    """Process provided overrides.
+
+    Remove any unsupplied values (None).
+    """
+    post_process_url = overrides.pop("post_process_url", "")
+    post_process_auth_env = overrides.pop("post_process_auth_env", None)
+
+    post_process = None
+    if post_process_url or post_process_auth_env:
+        post_process = PostProcessConfig(
+            url=post_process_url,
+            auth_env=post_process_auth_env,
+        )
+
+    overrides = {k: v for k, v in overrides.items() if v is not None}
+
+    return overrides, post_process
+
+
+@timer
+def _process_pyproject(pyproject: Path) -> dict:
+    cfg = {}
+    with pyproject.open() as f:
+        data = rtoml.load(f)
+
+        if "tool" not in data or "changelog_gen" not in data["tool"]:
+            return cfg
+
+        commit_types = SUPPORTED_TYPES.copy()
+        commit_types.update(data["tool"]["changelog_gen"].get("commit_types", {}))
+        data["tool"]["changelog_gen"]["commit_types"] = commit_types
+        return data["tool"]["changelog_gen"]
+
+
+@timer
+def check_deprecations(cfg: dict) -> None:  # noqa: ARG001
+    """Check parsed configuration dict for deprecated features."""
+    # No current deprecations
+    return
+
+
+@timer
+def read(path: str = "pyproject.toml", **kwargs) -> Config:
+    """Read configuration from local environment.
+
+    Supported configuration locations (checked in order):
+    * pyproject.toml
+    """
+    overrides, post_process = _process_overrides(kwargs)
+    cfg = {}
+
+    pyproject = Path(path)
+
+    if pyproject.exists():
+        # parse pyproject
+        cfg = _process_pyproject(pyproject)
+
+    if "post_process" not in cfg and post_process:
+        cfg["post_process"] = {
+            "url": post_process.url,
+            "auth_env": post_process.auth_env,
+        }
+
+    if "post_process" in cfg and post_process:
+        cfg["post_process"]["url"] = post_process.url or cfg["post_process"].get("url")
+        cfg["post_process"]["auth_env"] = post_process.auth_env or cfg["post_process"].get("auth_env")
+
+    cfg.update(overrides)
+
+    check_deprecations(cfg)  # pragma: no mutate
+
+    for replace_key_path in [
+        ("issue_link",),
+        ("commit_link",),
+        ("post_process", "url"),
+        ("post_process", "body"),
+    ]:
+        data, value = cfg, None
+        for key in replace_key_path:
+            value = data.get(key)
+            if key in data:
+                data = data[key]
+
+        # check for non supported replace keys
+        supported = {"::issue_ref::", "::version::", "::commit_hash::"}
+        unsupported = sorted(set(re.findall(r"(::.*?::)", str(value)) or []) - supported)
+        if unsupported:
+            msg = f"""Replace string(s) ('{"', '".join(unsupported)}') not supported."""
+            raise errors.UnsupportedReplaceError(msg)
+
+    if cfg.get("post_process"):
+        pp = cfg["post_process"]
+        try:
+            cfg["post_process"] = PostProcessConfig(**pp)
+        except Exception as e:
+            msg = f"Failed to create post_process: {e!s}"
+            raise RuntimeError(msg) from e
+
+    return Config(**cfg)
---
+++ packages/changelog_gen/changelog_gen-0.11.15-py3-none-any.whl/changelog_gen/extractor.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+import dataclasses
+import re
+import typing as t
+from collections import defaultdict
+
+from changelog_gen.util import timer
+
+if t.TYPE_CHECKING:
+    from changelog_gen.context import Context
+    from changelog_gen.vcs import Git
+
+
+@dataclasses.dataclass
+class Change:  # noqa: D101
+    issue_ref: str
+    description: str
+    commit_type: str
+
+    short_hash: str = ""
+    commit_hash: str = ""
+    authors: str = ""
+    scope: str = ""
+    breaking: bool = False
+
+    def __lt__(self: t.Self, other: Change) -> bool:  # noqa: D105
+        s = (not self.breaking, self.scope.lower() if self.scope else "zzz", self.issue_ref.lower())
+        o = (not other.breaking, other.scope.lower() if other.scope else "zzz", other.issue_ref.lower())
+        return s < o
+
+
+SectionDict = dict[str, dict[str, Change]]
+
+
+class ChangeExtractor:
+    """Parse commit logs and generate section dictionaries."""
+
+    @timer
+    def __init__(
+        self: t.Self,
+        context: Context,
+        git: Git,
+        *,
+        dry_run: bool = False,
+        include_all: bool = False,
+    ) -> None:
+        self.dry_run = dry_run
+        self.include_all = include_all
+        self.type_headers = context.config.type_headers
+        if self.include_all:
+            self.type_headers["_misc"] = "Miscellaneous"
+        self.git = git
+        self.context = context
+
+    @timer
+    def _extract_commit_logs(
+        self: t.Self,
+        sections: dict[str, dict],
+        current_version: str,
+    ) -> None:
+        # find tag from current version
+        tag = self.git.find_tag(current_version)
+        logs = self.git.get_logs(tag)
+
+        # Build a conventional commit regex based on configured sections
+        #   ^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test){1}(\([\w\-\.]+\))?(!)?: ([\w ])+([\s\S]*)
+        types = "|".join(self.type_headers.keys())
+        reg = re.compile(rf"^({types})(\([\w\-\.]+\))?(!)?: (.*)([\s\S]*)", re.IGNORECASE)
+        self.context.warning("Extracting commit log changes.")
+
+        for i, (short_hash, commit_hash, log) in enumerate(logs):
+            m = reg.match(log)
+            if m:
+                self.context.debug("  Parsing commit log: %s", log.strip())
+                commit_type = m[1].lower()
+                scope = (m[2] or "").replace("(", "(`").replace(")", "`)")
+                breaking = m[3] is not None
+                description = m[4].strip()
+                # Strip githubs additional link information from description.
+                description = re.sub(r" \(#\d+\)$", "", description)
+                details = m[5] or ""
+
+                # Handle missing refs in commit message, skip link generation in writer
+                issue_ref = f"__{i}__"
+                breaking = breaking or "BREAKING CHANGE" in details
+
+                self.context.info("  commit_type: '%s'", commit_type)
+                self.context.info("  scope: '%s'", scope)
+                self.context.info("  breaking: %s", breaking)
+                self.context.info("  description: '%s'", description)
+                self.context.info("  details: '%s'", details)
+
+                if breaking:
+                    self.context.info("  Breaking change detected:\n    %s: %s", commit_type, description)
+
+                change = Change(
+                    description=description,
+                    issue_ref=issue_ref,
+                    breaking=breaking,
+                    scope=scope,
+                    short_hash=short_hash,
+                    commit_hash=commit_hash,
+                    commit_type=commit_type,
+                )
+
+                for line in details.split("\n"):
+                    for target, pattern in [
+                        ("issue_ref", r"Refs: #?([\w-]+)"),
+                        ("issue_ref", r"closes #([\w-]+)"),  # support github closes footer
+                        ("issue_ref", r"fixes #([\w-]+)"),  # support github fixes footer
+                        ("authors", r"Authors: (.*)"),
+                    ]:
+                        m = re.match(pattern, line, re.IGNORECASE)
+                        if m:
+                            self.context.info("  '%s' footer extracted '%s'", target, m[1])
+                            setattr(change, target, m[1])
+
+                header = self.type_headers.get(commit_type, commit_type)
+                sections[header][change.issue_ref] = change
+            elif self.include_all:
+                self.context.debug("  Including non-conventional commit log (include-all): %s", log.strip())
+                issue_ref = f"__{i}__"
+                change = Change(
+                    description=log.strip().split("\n")[0],
+                    issue_ref=issue_ref,
+                    breaking=False,
+                    scope="",
+                    short_hash=short_hash,
+                    commit_hash=commit_hash,
+                    commit_type="_misc",
+                )
+                header = self.type_headers.get(change.commit_type, change.commit_type)
+                sections[header][change.issue_ref] = change
+
+            else:
+                self.context.debug("  Skipping commit log (not conventional): %s", log.strip())
+
+    @timer
+    def extract(self: t.Self, current_version: str) -> SectionDict:
+        """Iterate over release note files extracting sections and issues."""
+        sections = defaultdict(dict)
+
+        self._extract_commit_logs(sections, current_version)
+
+        return sections
+
+    @timer
+    def unique_issues(self: t.Self, sections: SectionDict) -> list[str]:
+        """Generate unique list of issue references."""
+        issue_refs = set()
+        issue_refs = {
+            issue.issue_ref
+            for issues in sections.values()
+            for issue in issues.values()
+            if issue.commit_type in self.type_headers
+        }
+        return sorted(issue_refs)
+
+
+@timer
+def extract_semver(
+    sections: SectionDict,
+    context: Context,
+    current: str,
+) -> str:
+    """Extract detected semver from commit logs.
+
+    Breaking changes: major
+    Feature releases: minor
+    Bugs/Fixes: patch
+
+    """
+    context.warning("Detecting semver from changes.")
+    semver_mapping = context.config.semver_mappings
+
+    context.indent()
+    semvers = ["patch", "minor", "major"]
+    semver = "patch"
+    for section_issues in sections.values():
+        for issue in section_issues.values():
+            if semvers.index(semver) < semvers.index(semver_mapping.get(issue.commit_type, "patch")):
+                semver = semver_mapping.get(issue.commit_type, "patch")
+                context.info("'%s' change detected from commit_type '%s'", semver, issue.commit_type)
+            if issue.breaking and semver != "major":
+                semver = "major"
+                context.info("'%s' change detected from breaking issue '%s'", semver, issue.commit_type)
+
+    if current.startswith("0.") and semver != "patch":
+        # If currently on 0.X releases, downgrade semver by one, major -> minor etc.
+        idx = semvers.index(semver)
+        new_ = semvers[max(idx - 1, 0)]
+        context.info("'%s' change downgraded to '%s' for 0.x release.", semver, new_)
+        semver = new_
+
+    context.reset()
+    return semver
