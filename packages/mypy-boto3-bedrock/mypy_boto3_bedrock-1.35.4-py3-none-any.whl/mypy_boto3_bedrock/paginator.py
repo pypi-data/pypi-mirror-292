7cc9002e7f8320a3fe58129f1bca305c994df42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:20 +0000

    Add package mypy-boto3-bedrock/1.35.4/mypy_boto3_bedrock-1.35.4-py3-none-any.whl

---
+++ packages/mypy-boto3-bedrock/mypy_boto3_bedrock-1.35.4-py3-none-any.whl/mypy_boto3_bedrock/paginator.py
@@ -0,0 +1,294 @@
+"""
+Type annotations for bedrock service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_bedrock.client import BedrockClient
+    from mypy_boto3_bedrock.paginator import (
+        ListCustomModelsPaginator,
+        ListEvaluationJobsPaginator,
+        ListGuardrailsPaginator,
+        ListImportedModelsPaginator,
+        ListModelCopyJobsPaginator,
+        ListModelCustomizationJobsPaginator,
+        ListModelImportJobsPaginator,
+        ListModelInvocationJobsPaginator,
+        ListProvisionedModelThroughputsPaginator,
+    )
+
+    session = Session()
+    client: BedrockClient = session.client("bedrock")
+
+    list_custom_models_paginator: ListCustomModelsPaginator = client.get_paginator("list_custom_models")
+    list_evaluation_jobs_paginator: ListEvaluationJobsPaginator = client.get_paginator("list_evaluation_jobs")
+    list_guardrails_paginator: ListGuardrailsPaginator = client.get_paginator("list_guardrails")
+    list_imported_models_paginator: ListImportedModelsPaginator = client.get_paginator("list_imported_models")
+    list_model_copy_jobs_paginator: ListModelCopyJobsPaginator = client.get_paginator("list_model_copy_jobs")
+    list_model_customization_jobs_paginator: ListModelCustomizationJobsPaginator = client.get_paginator("list_model_customization_jobs")
+    list_model_import_jobs_paginator: ListModelImportJobsPaginator = client.get_paginator("list_model_import_jobs")
+    list_model_invocation_jobs_paginator: ListModelInvocationJobsPaginator = client.get_paginator("list_model_invocation_jobs")
+    list_provisioned_model_throughputs_paginator: ListProvisionedModelThroughputsPaginator = client.get_paginator("list_provisioned_model_throughputs")
+    ```
+"""
+
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import (
+    EvaluationJobStatusType,
+    FineTuningJobStatusType,
+    ModelCopyJobStatusType,
+    ModelImportJobStatusType,
+    ModelInvocationJobStatusType,
+    ProvisionedModelStatusType,
+    SortOrderType,
+)
+from .type_defs import (
+    ListCustomModelsResponseTypeDef,
+    ListEvaluationJobsResponseTypeDef,
+    ListGuardrailsResponseTypeDef,
+    ListImportedModelsResponseTypeDef,
+    ListModelCopyJobsResponseTypeDef,
+    ListModelCustomizationJobsResponseTypeDef,
+    ListModelImportJobsResponseTypeDef,
+    ListModelInvocationJobsResponseTypeDef,
+    ListProvisionedModelThroughputsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    TimestampTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = (
+    "ListCustomModelsPaginator",
+    "ListEvaluationJobsPaginator",
+    "ListGuardrailsPaginator",
+    "ListImportedModelsPaginator",
+    "ListModelCopyJobsPaginator",
+    "ListModelCustomizationJobsPaginator",
+    "ListModelImportJobsPaginator",
+    "ListModelInvocationJobsPaginator",
+    "ListProvisionedModelThroughputsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListCustomModelsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListCustomModels)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listcustommodelspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeBefore: TimestampTypeDef = ...,
+        creationTimeAfter: TimestampTypeDef = ...,
+        nameContains: str = ...,
+        baseModelArnEquals: str = ...,
+        foundationModelArnEquals: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        isOwned: bool = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListCustomModelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListCustomModels.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listcustommodelspaginator)
+        """
+
+
+class ListEvaluationJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListEvaluationJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listevaluationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: EvaluationJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListEvaluationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListEvaluationJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listevaluationjobspaginator)
+        """
+
+
+class ListGuardrailsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListGuardrails)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listguardrailspaginator)
+    """
+
+    def paginate(
+        self, *, guardrailIdentifier: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListGuardrailsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListGuardrails.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listguardrailspaginator)
+        """
+
+
+class ListImportedModelsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListImportedModels)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listimportedmodelspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeBefore: TimestampTypeDef = ...,
+        creationTimeAfter: TimestampTypeDef = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListImportedModelsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListImportedModels.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listimportedmodelspaginator)
+        """
+
+
+class ListModelCopyJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCopyJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelcopyjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelCopyJobStatusType = ...,
+        sourceAccountEquals: str = ...,
+        sourceModelArnEquals: str = ...,
+        targetModelNameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListModelCopyJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCopyJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelcopyjobspaginator)
+        """
+
+
+class ListModelCustomizationJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCustomizationJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelcustomizationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: FineTuningJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListModelCustomizationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelCustomizationJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelcustomizationjobspaginator)
+        """
+
+
+class ListModelImportJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelImportJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelimportjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelImportJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListModelImportJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelImportJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelimportjobspaginator)
+        """
+
+
+class ListModelInvocationJobsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelInvocationJobs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelinvocationjobspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        submitTimeAfter: TimestampTypeDef = ...,
+        submitTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ModelInvocationJobStatusType = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListModelInvocationJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListModelInvocationJobs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listmodelinvocationjobspaginator)
+        """
+
+
+class ListProvisionedModelThroughputsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListProvisionedModelThroughputs)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listprovisionedmodelthroughputspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        creationTimeAfter: TimestampTypeDef = ...,
+        creationTimeBefore: TimestampTypeDef = ...,
+        statusEquals: ProvisionedModelStatusType = ...,
+        modelArnEquals: str = ...,
+        nameContains: str = ...,
+        sortBy: Literal["CreationTime"] = ...,
+        sortOrder: SortOrderType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> _PageIterator[ListProvisionedModelThroughputsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock.html#Bedrock.Paginator.ListProvisionedModelThroughputs.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_bedrock/paginators/#listprovisionedmodelthroughputspaginator)
+        """
