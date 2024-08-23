bc5e2311faf92d62d4e98f06a0d7802d8c0a0a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:54 +0000

    Add package json-exception-logger/3.0.0/json_exception_logger-3.0.0.tar.gz

---
+++ packages/json-exception-logger/json_exception_logger-3.0.0.tar.gz/json_exception_logger-3.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='json_exception_logger',  # Updated name
+    version='3.0.0',
+    author='Aayush Chaudhary',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+    ],
+    python_requires='>=3.6',
+)
