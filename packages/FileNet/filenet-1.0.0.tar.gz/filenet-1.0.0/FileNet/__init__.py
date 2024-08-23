6dc517c6f47c7174344226da150ea464fb7d0494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:04:24 +0000

    Add package FileNet/1.0.0/filenet-1.0.0.tar.gz

---
+++ packages/FileNet/filenet-1.0.0.tar.gz/filenet-1.0.0/FileNet/__init__.py
@@ -0,0 +1,2 @@
+from .main import draw2D
+from .main import draw3D
---
+++ packages/FileNet/filenet-1.0.0.tar.gz/filenet-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup( 
+	name='FileNet', 
+	version='1.0.0', 
+    packages=find_packages(),
+	description='A Python package for creating 2D, 3D vizulization of directory and files with their hierarchical relations.', 
+	author='Aditya Narayan', 
+	author_email='adityaan277@gmail.com', 
+	install_requires=[  
+		'networkx', 
+        'matplotlib',
+        'plotly'
+	], 
+) 
