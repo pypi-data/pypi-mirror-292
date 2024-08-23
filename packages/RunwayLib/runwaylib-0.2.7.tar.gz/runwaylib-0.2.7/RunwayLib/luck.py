709ef402f516040e34fb6201c909a8639406539e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:23 +0000

    Add package RunwayLib/0.2.7/runwaylib-0.2.7.tar.gz

---
+++ packages/RunwayLib/runwaylib-0.2.7.tar.gz/runwaylib-0.2.7/RunwayLib/luck.py
@@ -0,0 +1,8 @@
+import random
+
+
+def Luck(Number1, Number2):
+    number = random.randint(Number1, Number2)
+    randomnumhald =  number / 2
+    if number > randomnumhald:
+        return(f"Your Are Lucky luck:{number} from {Number2}")
