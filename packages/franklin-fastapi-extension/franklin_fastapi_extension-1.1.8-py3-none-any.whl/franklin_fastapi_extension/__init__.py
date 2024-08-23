d278f075cd859bbe2ad34ec0f464ab3adadeee65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:21 +0000

    Add package franklin-fastapi-extension/1.1.8/franklin_fastapi_extension-1.1.8-py3-none-any.whl

---
+++ packages/franklin-fastapi-extension/franklin_fastapi_extension-1.1.8-py3-none-any.whl/franklin_fastapi_extension/__init__.py
@@ -0,0 +1,17 @@
+from models import Base
+from schemas import Response
+from utils import Database, Repository
+
+
+def API(response_class=None, exception_handlers: dict=None, **kwargs):
+    from config import validation_error_handler, JsonResponse, HTTPException_handler
+    from fastapi import FastAPI, HTTPException
+    from fastapi.exceptions import RequestValidationError
+    return FastAPI(
+        default_response_class=response_class or JsonResponse,
+        exception_handlers=exception_handlers or {
+            RequestValidationError: validation_error_handler,
+            HTTPException: HTTPException_handler
+        },
+        **kwargs
+    )
