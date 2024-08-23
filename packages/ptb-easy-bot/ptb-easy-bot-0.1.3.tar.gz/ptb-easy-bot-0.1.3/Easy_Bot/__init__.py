dcc9506354afd033f6c207d48bff8cfb5863fe9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:20 +0000

    Add package ptb-easy-bot/0.1.3/ptb-easy-bot-0.1.3.tar.gz

---
+++ packages/ptb-easy-bot/ptb-easy-bot-0.1.3.tar.gz/ptb-easy-bot-0.1.3/Easy_Bot/__init__.py
@@ -0,0 +1,11 @@
+from ._keyboard import InlineReplyMarkup
+from ._easyBot import update , bot
+from ._task import do_task
+
+__author__ = "@pamod_madubashana"
+__all__ = (
+    "do_task",
+    "update",
+    "bot",
+    "InlineReplyMarkup",
+)
