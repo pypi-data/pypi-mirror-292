a6210c5bac81d91b9cc5531ee02fc5111c67c030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:50 +0000

    Add package connectors-to-databases/1.1.8/connectors_to_databases-1.1.8-py3-none-any.whl

---
+++ packages/connectors-to-databases/connectors_to_databases-1.1.8-py3-none-any.whl/connectors_to_databases/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: k0rsakov.
+:license: Apache License, Version 2.0, see LICENSE file.
+:copyright: (c) 2022 k0rsakov.
+"""  # noqa: D205
+
+
+from .ClickHouse import ClickHouse  # noqa: F401
+from .MariaDB import MariaDB  # noqa: F401
+from .MSSQL import MSSQL  # noqa: F401
+from .MySQL import MySQL  # noqa: F401
+from .PostgreSQL import PostgreSQL  # noqa: F401
+from .TypeHinting import SQLQuery  # noqa: F401
