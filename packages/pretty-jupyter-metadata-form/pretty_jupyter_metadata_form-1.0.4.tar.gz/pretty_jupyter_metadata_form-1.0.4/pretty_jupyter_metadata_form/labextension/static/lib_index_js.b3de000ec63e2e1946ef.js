1a2d914354afeece561acdb1b0569b17882d8823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:03:44 +0000

    Add package pretty-jupyter-metadata-form/1.0.4/pretty_jupyter_metadata_form-1.0.4.tar.gz

---
+++ packages/pretty-jupyter-metadata-form/pretty_jupyter_metadata_form-1.0.4.tar.gz/pretty_jupyter_metadata_form-1.0.4/pretty_jupyter_metadata_form/labextension/static/lib_index_js.b3de000ec63e2e1946ef.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunk_jans_code_pretty_jupyter_metadata_form"] = self["webpackChunk_jans_code_pretty_jupyter_metadata_form"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const pjmdf = {
+    id: '@jans-code/pretty_jupyter_metadata_form:pjmdf',
+    autoStart: true,
+    activate: (app) => {
+        console.log('Pretty Jupyter metadata-form activated');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ([pjmdf]);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.b3de000ec63e2e1946ef.js.map
