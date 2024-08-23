a51d819516003663990e9ee704e8b8f9942edb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:33 +0000

    Add package patchwork-cli/0.0.49/patchwork_cli-0.0.49-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.49-py3-none-any.whl/patchwork/steps/ModifyCodePB/ModifyCodePB.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from patchwork.step import Step
+from patchwork.steps import ModifyCode
+from patchwork.steps.ModifyCodePB.typed import ModifyCodePBInputs, ModifyCodePBOutputs
+
+
+class ModifyCodePB(Step, input_class=ModifyCodePBInputs, output_class=ModifyCodePBOutputs):
+    def __init__(self, inputs: dict):
+        super().__init__(inputs)
+        self.file_path = inputs["file_path"]
+        self.start_line = inputs["start_line"]
+        self.end_line = inputs["end_line"]
+        self.patch = inputs["new_code"]
+
+    def run(self) -> dict:
+        modify_code = ModifyCode(
+            {
+                "files_to_patch": [
+                    dict(
+                        uri=self.file_path,
+                        startLine=self.start_line,
+                        endLine=self.end_line,
+                    )
+                ],
+                "extracted_responses": [
+                    dict(
+                        patch=self.patch,
+                    )
+                ],
+            }
+        )
+        modified_code_files = modify_code.run()
+        return modified_code_files.get("modified_code_files", [{}])[0]
