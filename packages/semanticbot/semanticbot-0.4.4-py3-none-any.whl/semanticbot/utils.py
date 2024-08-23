4f33281835c048717609c08081e903b34ffd482b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:05:01 +0000

    Add package semanticbot/0.4.4/semanticbot-0.4.4-py3-none-any.whl

---
+++ packages/semanticbot/semanticbot-0.4.4-py3-none-any.whl/semanticbot/utils.py
@@ -0,0 +1,17 @@
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+import numpy as np
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def load_embeddings():
+    return HuggingFaceBgeEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+def chunk_text(text, chunk_size=1024, chunk_overlap=80):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+    )
+    return text_splitter.split_text(text)
