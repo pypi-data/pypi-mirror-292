7d4c384a1160a701be25e6fe1c01f3cb471bb4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:02:45 +0000

    Add package pytximport/0.8.0/pytximport-0.8.0.tar.gz

---
+++ packages/pytximport/pytximport-0.8.0.tar.gz/pytximport-0.8.0/pytximport/__init__.py
@@ -0,0 +1,22 @@
+"""pytximport: Convert transcript-level expression to gene-level expression.
+
+The `pytximport` package provides a Python implementation of the `tximport` R package, which converts transcript-level
+expression to gene-level count estimates.
+
+.. note::
+    Please also cite the original `tximport` R package when using `pytximport`.
+    DOI: https://doi.org/10.12688/f1000research.7563.1
+
+.. warning::
+    The `pytximport` package is still in development and currently does not support alevin quantification files,
+    unlike the `tximport` R package. Default parameters, argument names and return types may also differ between the
+    implementations.
+"""
+
+from . import definitions, importers, utils
+from ._cli import cli
+from ._version import __version__
+from .core import tximport
+
+# allow users to import the tximport function as pytximport as well
+pytximport = tximport
