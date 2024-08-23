4110fed7fb6b86bdd3c1e78e9f817c473dbee35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:34 +0000

    Add package ciervo/2024.8.25/ciervo-2024.8.25-py3-none-any.whl

---
+++ packages/ciervo/ciervo-2024.8.25-py3-none-any.whl/ciervo/io/load.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import numpy as np
+import importlib
+from scipy import signal
+
+def load_csv(file_path):
+    # df : Elapsed Time,Isquio,Cuadriceps,GLMedio,AductorLargo,Angle
+    # Load csv
+    df = pd.read_csv(file_path)
+    # Set dtypes
+    df = df.astype(float)
+    return df
+
+
+def load_data(path):
+    files = os.listdir(path)
+    files = [f for f in files if f.endswith(".csv")]
+    files.sort()
+    return [load_csv(os.path.join(path, f)) for f in files]
+
+
