b91a1938ddb0ea1e0349ba8432f04b746e2a1145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:30 +0000

    Add package metaflow-stubs/2.12.14/metaflow_stubs-2.12.14-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.12.14-py2.py3-none-any.whl/metaflow-stubs/plugins/gcp/__init__.pyi
@@ -0,0 +1,12 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.14                                                            #
+# Generated on 2024-08-22T15:17:51.849594                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def get_credentials(scopes, *args, **kwargs):
+    ...
+
