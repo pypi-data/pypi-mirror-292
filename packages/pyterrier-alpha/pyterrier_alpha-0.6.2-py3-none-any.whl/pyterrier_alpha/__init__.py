7e88c7d96dcbacedf057c3215aaee9b185281269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 17:04:11 +0000

    Add package pyterrier-alpha/0.6.2/pyterrier_alpha-0.6.2-py3-none-any.whl

---
+++ packages/pyterrier-alpha/pyterrier_alpha-0.6.2-py3-none-any.whl/pyterrier_alpha/__init__.py
@@ -0,0 +1,10 @@
+from pyterrier_alpha import validate
+from pyterrier_alpha.dataframe_builder import DataFrameBuilder
+from pyterrier_alpha import io
+from pyterrier_alpha import artifact
+from pyterrier_alpha.artifact import Artifact
+from pyterrier_alpha.rbo import RBO, rbo
+from pyterrier_alpha.transformer_repr import transformer_repr
+from pyterrier_alpha import utils
+
+__version__ = '0.6.2'
---
+++ packages/pyterrier-alpha/pyterrier_alpha-0.6.2-py3-none-any.whl/pyterrier_alpha/dataframe_builder.py
@@ -0,0 +1,41 @@
+from typing import Optional
+import pandas as pd
+import numpy as np
+
+class DataFrameBuilder:
+    def __init__(self, columns):
+        self._data = {c: [] for c in columns}
+
+    def extend(self, values):
+        assert all(c in values.keys() for c in self._data), f"all columns must be provided: {list(self._data)}"
+        lens = {k: len(v) for k, v in values.items() if hasattr(v, '__len__') and not isinstance(v, str)}
+        if any(lens):
+            first_len = list(lens.values())[0]
+        else:
+            first_len = 1 # if nothing has a len, everything is gien a length of 1
+        assert all(l == first_len for l in lens.values()), f"all values must have the same length {lens}"
+        for k, v in values.items():
+            if k not in lens:
+                self._data[k].append([v] * first_len)
+            elif isinstance(v, pd.Series):
+                self._data[k].append(v.values)
+            else:
+                self._data[k].append(v)
+
+    def to_df(self, merge_on_index: Optional[pd.DataFrame] = None):
+        result = pd.DataFrame({
+            k: np.concatenate(v)
+            for k, v in self._data.items()
+        })
+        if merge_on_index is not None:
+            assert '_index' in result
+            merge_on_index = merge_on_index.reset_index(drop=True)
+            result = result.assign(**{
+                col: merge_on_index[col].iloc[result['_index']].values
+                for col in merge_on_index.columns
+                if col not in result.columns
+            }).drop(columns=['_index'])
+            merge_columns = set(merge_on_index.columns)
+            column_order = list(merge_on_index.columns) + [c for c in result.columns if c not in merge_columns]
+            result = result[column_order]
+        return result
