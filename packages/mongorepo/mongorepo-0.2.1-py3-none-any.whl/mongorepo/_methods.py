ce5d96734bc93cb20e6c2a8fbdc6cbd2cbac250d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:03:56 +0000

    Add package mongorepo/0.2.1/mongorepo-0.2.1-py3-none-any.whl

---
+++ packages/mongorepo/mongorepo-0.2.1-py3-none-any.whl/mongorepo/_methods.py
@@ -0,0 +1,190 @@
+from dataclasses import asdict, is_dataclass
+from typing import Any, Callable, Iterable
+
+from bson import ObjectId
+from pymongo.collection import Collection
+from mongorepo.utils import _get_converter, get_dataclass_fields
+from mongorepo import DTO, exceptions
+from mongorepo.base import _DTOField
+
+
+def _add_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None,
+) -> Callable:
+
+    def add(self, dto: DTO) -> DTO:
+        collection.insert_one(asdict(dto))
+        return dto
+
+    def add_return_with_id(self, dto: DTO) -> DTO:
+        object_id = ObjectId()
+        dto.__dict__[id_field] = str(object_id)  # type: ignore
+        collection.insert_one({**asdict(dto), '_id': object_id})
+        return dto
+
+    if not id_field:
+        return add
+    return add_return_with_id
+
+
+def _get_list_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None,
+):
+    to_dto = _get_converter(dto_type, id_field=id_field)
+
+    def get_list(self, offset: int = 0, limit: int = 20) -> list[DTO]:
+        cursor = collection.find().skip(offset).limit(limit)
+        return [to_dto(dto_type, doc) for doc in cursor]
+    return get_list
+
+
+def _get_all_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None
+) -> Callable:
+    to_dto = _get_converter(dto_type=dto_type, id_field=id_field)
+
+    def get_all(self, **filters: Any) -> Iterable[DTO]:
+        cursor = collection.find(filters)
+        for dct in cursor:
+            yield to_dto(dto_type, dct)
+    return get_all
+
+
+def _update_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None,
+) -> Callable:
+    to_dto = _get_converter(dto_type, id_field=id_field)
+
+    def update(self, dto: DTO, **filters: Any) -> DTO | None:
+        data: dict[str, dict[str, Any]] = {'$set': {}}
+        for field, value in asdict(dto).items():
+            if isinstance(value, (int, bool, float)):
+                data['$set'][field] = value
+            elif not value:
+                continue
+            else:
+                data['$set'][field] = value
+        updated_document: dict[str, Any] | None = collection.find_one_and_update(
+            filter=filters, update=data, return_document=True,
+        )
+        return to_dto(dto_type, updated_document) if updated_document else None
+    return update
+
+
+def _delete_method(dto_type: type[DTO], collection: Collection) -> Callable:
+    def delete(self, **filters: Any) -> bool:
+        deleted = collection.find_one_and_delete(filters)
+        return True if deleted else False
+    return delete
+
+
+def _get_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None,
+) -> Callable:
+    to_dto = _get_converter(dto_type, id_field=id_field)
+
+    def get(self, **filters: Any) -> DTO | None:
+        result = collection.find_one(filters)
+        return to_dto(dto_type, result) if result else None
+    return get
+
+
+def _update_field_method(
+    dto_type: type[DTO],
+    collection: Collection,
+    id_field: str | None = None,
+) -> Callable:
+    to_dto = _get_converter(dto_type, id_field=id_field)
+
+    def update_field(self, field_name: str, value: Any, **filters) -> DTO | None:
+        if field_name not in dto_type.__dict__['__annotations__']:
+            raise exceptions.MongoRepoException(
+                message=f'{dto_type} does not have field "{field_name}"'
+            )
+        result = collection.find_one_and_update(
+            filter=filters, update={'$set': {field_name: value}}, return_document=True,
+        )
+        return to_dto(dto_type, result) if result else None
+    return update_field
+
+
+def _update_integer_field_method(
+    dto_type: type[DTO], collection: Collection, field_name: str, _weight: int = 1,
+) -> Callable:
+    def update_interger_field(self, weight: int | None = None, **filters) -> None:
+        w = weight if weight is not None else _weight
+        collection.update_one(
+            filter=filters, update={'$inc': {field_name: w}}
+        )
+    return update_interger_field
+
+
+def _update_list_field_method(
+    dto_type: type[DTO], collection: Collection, field_name: str, command: str = '$push',
+) -> Callable:
+    def update_list(self, value: Any, **filters) -> None:
+        collection.update_one(
+            filter=filters, update={command: {field_name: value}}
+        )
+    return update_list
+
+
+def _get_list_of_field_values_method(
+    dto_type: type[DTO], collection: Collection, field_name: str,
+) -> Callable:
+    dataclass_fields = get_dataclass_fields(dto_type=dto_type, only_dto_types=True)
+    field_type = dataclass_fields.get(field_name, None)
+
+    def get_list_dto(
+        self, offset: int, limit: int, **filters
+    ) -> list[_DTOField]:  # type: ignore
+        document = collection.find_one(
+            filters, {field_name: {'$slice': [offset, limit]}},
+        )
+        return [to_dto(field_type, d) for d in document[field_name]] if document else []
+
+    def get_list(
+        self, offset: int, limit: int, **filters
+    ) -> list[Any]:
+        document = collection.find_one(
+            filters, {field_name: {'$slice': [offset, limit]}},
+        )
+        return document[field_name] if document else []
+
+    if is_dataclass(field_type):
+        to_dto = _get_converter(dataclass_fields[field_name])
+        return get_list_dto
+    return get_list
+
+
+def _pop_list_method(dto_type: type[DTO], collection: Collection, field_name: str) -> Callable:
+    def pop_list(self, **filters) -> Any | None:
+        document = collection.find_one_and_update(
+            filter=filters, update={'$pop': {field_name: 1}},
+        )
+        return document[field_name][-1] if document else None
+    return pop_list
+
+
+METHOD_NAME__CALLABLE: dict[str, Callable] = {
+    'get': _get_method,
+    'add': _add_method,
+    'update': _update_method,
+    'delete': _delete_method,
+    'get_list': _get_list_method,
+    'get_all': _get_all_method,
+    'pop': _pop_list_method,
+    'update_field': _update_field_method,
+    'update_list': _update_list_field_method,
+    'update_integer': _update_integer_field_method,
+}
