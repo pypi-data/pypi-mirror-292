fe6aee9bd5de8795b51638ae24ffdbbe03a42d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:04:17 +0000

    Add package scann/1.3.4/scann-1.3.4-cp39-cp39-manylinux_2_27_x86_64.whl

---
+++ packages/scann/scann-1.3.4-cp39-cp39-manylinux_2_27_x86_64.whl/scann/__init__.py
@@ -0,0 +1,4 @@
+from scann.scann_ops.py.scann_builder import ReorderType
+from scann.scann_ops.py.scann_builder import ScannBuilder
+from scann.scann_ops.py import scann_ops
+from scann.scann_ops.py import scann_ops_pybind
