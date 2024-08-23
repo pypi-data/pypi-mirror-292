54aa14805acdc513165021d46019cb8f01c1d5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:03:02 +0000

    Add package Spark-df-Cleaner/0.0.3/Spark-df-Cleaner-0.0.3.tar.gz

---
+++ packages/Spark-df-Cleaner/Spark-df-Cleaner-0.0.3.tar.gz/Spark-df-Cleaner-0.0.3/SparkCleaner/Strategies/drop_missing_values.py
@@ -0,0 +1,17 @@
+from SparkCleaner.Strategies.base import CleaningStrategy, DataFrame
+
+class DropMissingValuesStrategy(CleaningStrategy):
+    def clean(self, df: DataFrame) -> DataFrame:
+        initial_count = df.count()
+        df_cleaned = df.dropna(subset=self.columns)
+        final_count = df_cleaned.count()
+        
+        if initial_count > final_count:
+            for column in self.columns:
+                missing = df.filter(df[column].isNull())
+                for row in missing.collect():
+                    self.logger.log_error(row['__index'], column, f'missing_value: {row[column]}')
+        
+        return df_cleaned
+    
+    
