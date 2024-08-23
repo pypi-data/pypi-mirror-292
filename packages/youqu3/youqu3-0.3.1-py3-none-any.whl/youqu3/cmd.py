00cb832b3e41cb74db8c55182488f5f66a0e51a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:53 +0000

    Add package youqu3/0.3.1/youqu3-0.3.1-py3-none-any.whl

---
+++ packages/youqu3/youqu3-0.3.1-py3-none-any.whl/youqu3/cmd.py
@@ -0,0 +1,169 @@
+import os
+import subprocess
+import sys
+
+from youqu3 import exceptions
+from youqu3 import logger
+from youqu3 import setting
+
+
+class Cmd:
+
+    @staticmethod
+    def _run(command, _input=None, timeout=None, check=False, executable=None, **kwargs):
+        with subprocess.Popen(command, **kwargs) as process:
+            try:
+                stdout, stderr = process.communicate(_input, timeout=timeout)
+            except:
+                process.kill()
+                raise
+            retcode = process.poll()
+            if check and retcode:
+                raise subprocess.CalledProcessError(
+                    retcode, process.args, output=stdout, stderr=stderr
+                )
+        return subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
+
+    @classmethod
+    def _getstatusoutput(cls, command, timeout, executable):
+        kwargs = {
+            "shell": True,
+            "stderr": subprocess.STDOUT,
+            "stdout": subprocess.PIPE,
+            "timeout": timeout,
+            "executable": executable,
+        }
+        try:
+            if sys.version_info >= (3, 7):
+                kwargs["text"] = True
+            result = cls._run(command, **kwargs)
+            data = result.stdout
+            if isinstance(data, bytes):
+                data = data.decode("utf-8")
+            exitcode = result.returncode
+        except subprocess.CalledProcessError as ex:
+            data = ex.output
+            exitcode = ex.returncode
+        except subprocess.TimeoutExpired as ex:
+            data = ex.__str__()
+            exitcode = -1
+        if data[-1:] == "\n":
+            data = data[:-1]
+        return exitcode, data
+
+    @classmethod
+    def run(
+            cls,
+            command: str,
+            interrupt: bool = False,
+            timeout: [None, int] = 25,
+            print_log: bool = True,
+            command_log: bool = True,
+            return_code: bool = False,
+            executable: str = "/bin/bash",
+    ):
+        """
+         执行shell命令
+        :param command: shell 命令
+        :param interrupt: 命令异常时是否中断
+        :param timeout: 命令执行超时
+        :param out_debug_flag: 命令返回信息输出日志
+        :param command_log: 执行的命令字符串日志
+        :return: 返回终端输出
+        """
+        exitcode, stdout = cls._getstatusoutput(command, timeout=timeout, executable=executable)
+        if command_log:
+            logger.debug(command)
+        if exitcode != 0 and interrupt:
+            raise exceptions.ShellExecutionFailed(stdout)
+        if print_log and stdout:
+            logger.debug(stdout)
+        if return_code:
+            return stdout, exitcode
+        return stdout
+
+    @staticmethod
+    def expect_run(
+            cmd: str,
+            events: dict,
+            return_code=False,
+            timeout: int = 30
+    ):
+        """
+        expect_run(
+            "ssh username@machine_ip 'ls -l'",
+            events={'password':'secret\n'}
+        )
+        如果 return_code=True，返回 (stdout, return_code)
+        """
+        import pexpect
+        res = pexpect.run(
+            cmd,
+            events=events,
+            withexitstatus=return_code,
+            timeout=timeout
+        )
+        if return_code is False:
+            return res.decode("utf-8")
+        stdout, return_code = res
+        return stdout.decode("utf-8"), return_code
+
+    @classmethod
+    def sudo_run(
+            cls,
+            command,
+            password: str = None,
+            workdir: str = None,
+            interrupt: bool = False,
+            timeout: int = 25,
+            print_log: bool = True,
+            command_log: bool = True,
+            return_code: bool = False
+    ):
+        if password is None:
+            password = setting.PASSWORD
+        wd = ""
+        if workdir:
+            if not os.path.exists(workdir):
+                raise FileNotFoundError
+            wd = f"cd {workdir} && "
+        res = cls.run(
+            f"{wd}echo '{password}' | sudo -S {command}",
+            interrupt=interrupt,
+            timeout=timeout,
+            print_log=print_log,
+            command_log=command_log,
+            return_code=return_code
+        )
+        if return_code is False:
+            return res.lstrip("请输入密码●")
+        res = list(res)
+        res[0] = res[0].lstrip("请输入密码●")
+        return res
+
+
+class RemoteCmd:
+
+    def __init__(self, user: str, ip: str, password: str):
+        self.user = user
+        self.ip = ip
+        self.password = password
+
+    def remote_run(self, cmd: str, return_code: bool = False, timeout: int = None):
+        res = Cmd.expect_run(
+            f'ssh -o StrictHostKeyChecking=no {self.user}@{self.ip} "{cmd}"',
+            events={'password': f'{self.password}\n'},
+            return_code=return_code,
+            timeout=timeout
+        )
+        return res
+
+    def remote_sudo_run(self, cmd: str, workdir: str = None, return_code: bool = False):
+        wd = ""
+        if workdir is not None:
+            _, code = self.remote_run(f"ls {workdir}", return_code=True)
+            if code == 0:
+                wd = workdir
+            else:
+                raise FileNotFoundError(workdir)
+        return self.remote_run(f"{wd}echo '{self.password}' | sudo -S {cmd}", return_code=return_code)
---
+++ packages/youqu3/youqu3-0.3.1-py3-none-any.whl/youqu3/driver/run.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-License-Identifier: GPL-2.0-only
+import os.path
+import pathlib
+import pytest
+import random
+import re
+import sys
+
+from youqu3 import logger, setting
+
+
+class Run:
+    __author__ = "mikigo <huangmingqiang@uniontech.com>"
+
+    def __init__(
+            self,
+            workdir=None,
+            path=None,
+            keywords=None,
+            tags=None,
+            setup_plan=None,
+            slaves=None,
+            txt=None,
+            reruns=None,
+            job_start=None,
+            job_end=None,
+            pytest_opt=None,
+            record_failed_num=None,
+            **kwargs,
+    ):
+        logger("INFO")
+
+        self.workdir = workdir
+        self.path = path
+        self.keywords = keywords
+        self.tags = tags
+        self.setup_plan = setup_plan
+        self.slaves = slaves
+        self.txt = txt
+        self.reruns = reruns
+        self.job_start = job_start
+        self.job_end = job_end
+        self.pytest_opt = pytest_opt
+        self.record_failed_num = record_failed_num
+
+        self.rootdir = pathlib.Path(".").absolute()
+        self.report_path = self.rootdir / "report"
+        self.html_report_path = self.report_path / "html"
+        self.allure_data_path = self.html_report_path / "_data"
+        self.allure_html_path = self.html_report_path / "html"
+        self.json_report_path = self.report_path / "json"
+
+        from funnylog2.config import config as funnylog2_config
+
+        funnylog2_config.LOG_FILE_PATH = self.report_path
+
+    @staticmethod
+    def makedirs(dirs):
+        pathlib.Path(dirs).mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def set_recursion_limit(strings):
+        len_tags = len(re.split("or |and |not ", strings))
+        if len_tags >= 999:
+            sys.setrecursionlimit(len_tags + 100)
+
+    def read_tags_txt(self):
+        youqu_tags_file = os.path.join(self.rootdir, "youqu-tags.txt")
+        if os.path.exists(youqu_tags_file):
+            with open(youqu_tags_file, "r", encoding="utf-8") as f:
+                tags_txt = f.readlines()[0]
+                return tags_txt
+        return None
+
+    def read_keywords_txt(self):
+        youqu_keywords_file = os.path.join(self.rootdir, "youqu-keywords.txt")
+        if os.path.exists(youqu_keywords_file):
+            with open(youqu_keywords_file, "r", encoding="utf-8") as f:
+                keywords_txt = f.readlines()[0]
+                return keywords_txt
+        return None
+
+    def generate_cmd(self):
+        cmd = ["pytest"]
+
+        if self.path:
+            cmd.append(self.path)
+
+        keywords_txt = self.read_keywords_txt()
+        if self.keywords:
+            self.set_recursion_limit(self.keywords)
+            cmd.extend(["-k", f"'{self.keywords}'"])
+        elif self.txt and keywords_txt is not None:
+            self.set_recursion_limit(keywords_txt)
+            cmd.extend(["-k", f"'{keywords_txt}'"])
+
+        tags_txt = self.read_tags_txt()
+        if self.tags:
+            self.set_recursion_limit(self.tags)
+            cmd.extend(["-m", f"'{self.tags}'"])
+        elif self.txt and tags_txt is not None:
+            self.set_recursion_limit(tags_txt)
+            cmd.extend(["-m", f"'{tags_txt}'"])
+
+        if self.slaves:
+            cmd.extend(["--slaves", f"'{self.slaves}'"])
+        if self.record_failed_num or setting.RECORD_FAILED_NUM:
+            cmd.extend(["--record_failed_num", f"{self.record_failed_num or setting.RECORD_FAILED_NUM}"])
+        if self.pytest_opt:
+            cmd.extend([i.strip() for i in self.pytest_opt])
+
+        cmd.extend([
+            f"--reruns={self.reruns or setting.RERUNS}",
+            f"--timeout={setting.TIMEOUT}",
+        ])
+
+        if self.setup_plan:
+            cmd.append("--setup-plan")
+        else:
+            cmd.extend([
+                "--json-report",
+                "--json-report-indent=2",
+                "--json-report-omit",
+                "collectors",
+                "log",
+                "keywords",
+                f"--json-report-file={self.json_report_path / f'report_{setting.TIME_STRING}.json'}",
+                f"--alluredir={self.allure_data_path}",
+                "--clean-alluredir",
+                "-s",
+                "--no-header",
+            ])
+
+        return cmd
+
+    def job_start_driver(self):
+        from youqu3.cmd import Cmd
+        if self.job_start:
+            Cmd.run(self.job_start, timeout=3600)
+        for file in os.listdir(self.rootdir):
+            if file == "job_start.py":
+                Cmd.run(f"cd {self.rootdir} && {sys.executable} {file}", timeout=600)
+            if file == "job_start.sh":
+                Cmd.run(f"cd {self.rootdir} && /bin/bash {file}", timeout=600)
+
+    def job_end_driver(self):
+        from youqu3.cmd import Cmd
+        if self.job_end:
+            Cmd.run(self.job_end, timeout=3600)
+        for file in os.listdir(self.rootdir):
+            if file == "job_end.py":
+                Cmd.run(f"cd {self.rootdir} && {sys.executable} {file}", timeout=600)
+            if file == "job_end.sh":
+                Cmd.run(f"cd {self.rootdir} && /bin/bash {file}", timeout=600)
+
+    def run(self):
+        if not self.setup_plan:
+            self.job_start_driver()
+        print(" ".join(self.generate_cmd()))
+        if self.workdir:
+            if os.path.exists(self.workdir):
+                os.chdir(self.workdir)
+            else:
+                raise FileNotFoundError(f"workdir not found: {self.workdir}")
+        pytest.main(
+            [i.strip("'") for i in self.generate_cmd()[1:]]
+        )
+        if self.setup_plan:
+            return
+        self.gen_html()
+        self.job_end_driver()
+
+    def gen_html(self):
+        os.makedirs(self.allure_html_path, exist_ok=True)
+        try:
+            from youqu_html import YouQuHtml
+            YouQuHtml.gen(str(self.allure_data_path), str(self.allure_html_path), clean=True)
+        except ImportError:
+            try:
+                from youqu3.cmd import Cmd
+                from youqu_html_rpc import YouQuHtmlRpc
+                from youqu_html_rpc.environment import environment
+                from youqu_html_rpc.config import config as html_rpc_config
+                log_server = servers = [i.strip() for i in setting.REPORT_SERVER_IP.split("/") if i]
+                while servers:
+                    html_rpc_config.SERVER_IP = random.choice(servers)
+                    if YouQuHtmlRpc.check_connected() is False:
+                        servers.remove(html_rpc_config.SERVER_IP)
+                        html_rpc_config.SERVER_IP = None
+                    else:
+                        break
+                if html_rpc_config.SERVER_IP is None:
+                    raise EnvironmentError(f"所有REPORT SERVER不可用: {log_server}")
+                report_dirname = f"{setting.TIME_STRING}_{setting.HOST_IP}_{self.rootdir.name}"
+                report_server_path = f"{setting.REPORT_BASE_PATH}/{report_dirname}"
+                report_server_data_path = f"{report_server_path}/data"
+                report_server_html_path = f"{report_server_path}/html"
+                YouQuHtmlRpc.makedirs(report_server_data_path)
+                rs_user, rs_password = YouQuHtmlRpc.server_user_password()
+                environment(self.allure_data_path)
+                logger.info(f"send data to report server: {html_rpc_config.SERVER_IP}")
+                rsync = 'rsync -av -e "ssh -o StrictHostKeyChecking=no"'
+                Cmd.expect_run(
+                    f"/bin/bash -c '{rsync} {str(self.allure_data_path)}/ {rs_user}@{html_rpc_config.SERVER_IP}:{report_server_data_path}/'",
+                    events={'password': f'{rs_password}\n'},
+                    return_code=True,
+                    timeout=6000,
+                )
+                YouQuHtmlRpc.gen(report_server_data_path, report_server_html_path, report_dirname)
+                report_server_url = f"http://{html_rpc_config.SERVER_IP}/{report_dirname}"
+                logger.info(f"html report url: {report_server_url}")
+                with open(f"{self.allure_html_path}/{report_dirname}.txt", "w", encoding="utf-8") as f:
+                    f.write(report_server_url)
+            except ImportError:
+                print(f"only json report")
+
+
+if __name__ == "__main__":
+    Run().run()
