4dda94c396bf5adb797d0e1e785ade75b82e494a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:03:13 +0000

    Add package pandass-plus/0.0.3/pandass_plus-0.0.3.tar.gz

---
+++ packages/pandass-plus/pandass_plus-0.0.3.tar.gz/pandass_plus-0.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pandass_plus',
+      version='0.0.3',
+      description='Taa',
+      packages=['pandass_plus'],
+      author_email='serezha.belikov.05@mail.ru',
+      zip_safe=False)
