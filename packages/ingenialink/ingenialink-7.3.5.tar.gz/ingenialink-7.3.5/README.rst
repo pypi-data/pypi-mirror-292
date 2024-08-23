b0dc38731bbc540e8c77f7ea42afa37aa940fb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:04:14 +0000

    Add package ingenialink/7.3.5/ingenialink-7.3.5.tar.gz

---
+++ packages/ingenialink/ingenialink-7.3.5.tar.gz/ingenialink-7.3.5/README.rst
@@ -0,0 +1,49 @@
+==================
+ingenialink-python
+==================
+
+.. image:: https://img.shields.io/pypi/v/ingenialink.svg
+    :target: https://pypi.python.org/pypi/ingenialink
+    :alt: PyPI Version
+
+.. image:: https://img.shields.io/badge/License-CC%20BY--NC--ND%204.0-lightgrey.svg
+   :alt: CC by-nc-nd
+   :target: https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+ingenialink-python is a Python library for simple motion control tasks and communication with Ingenia drives.
+
+.. image:: https://github.com/ingeniamc/ingenialink-python/blob/master/docs/_static/images/main_image.png?raw=true
+     :target: http://www.ingeniamc.com
+     :alt: Ingenia Servodrives
+
+Requirements
+------------
+
+* Python 3.9 or higher
+* WinPcap_ 4.1.3
+
+.. _WinPcap: https://www.winpcap.org/install/
+
+Development
+^^^^^^^^^^^
+
+In case that library will be use from source code the FoE application will be missing.
+It must be added to ``ingenialink/bin/FoE/win_64x`` in order to use FOE feature.
+
+Installation
+------------
+
+Installation is done by using ``pip``, i.e::
+
+    pip install ingenialink
+
+
+
+
+Documentation
+-------------
+
+For further details you can read the documentation_ where you will find
+simple usage examples, the API docs, etc.
+
+.. _documentation: https://distext.ingeniamc.com/doc/ingenialink-python/latest/
---
+++ packages/ingenialink/ingenialink-7.3.5.tar.gz/ingenialink-7.3.5/ingenialink/exceptions.py
@@ -0,0 +1,200 @@
+class ILError(Exception):
+    """IngeniaLink generic error."""
+
+    pass
+
+
+class ILConfigurationError(ILError):
+    """IngeniaLink IPB protocol configuration error."""
+
+    pass
+
+
+class ILUDPException(Exception):
+    """Ingenialink exception on UDP action."""
+
+    pass
+
+
+class ILFirmwareLoadError(ILError):
+    """IngeniaLink error while loading a firmware."""
+
+    pass
+
+
+class ILValueError(ILError):
+    """IngeniaLink value error."""
+
+    pass
+
+
+class ILTimeoutError(ILError):
+    """IngeniaLink timeout error."""
+
+    pass
+
+
+class ILAlreadyInitializedError(ILError):
+    """IngeniaLink already initialized error."""
+
+    pass
+
+
+class ILMemoryError(ILError):
+    """IngeniaLink memory error."""
+
+    pass
+
+
+class ILDisconnectionError(ILError):
+    """IngeniaLink disconnection error."""
+
+    pass
+
+
+class ILAccessError(ILError):
+    """IngeniaLink access error."""
+
+    pass
+
+
+class ILStateError(ILError):
+    """IngeniaLink state error."""
+
+    pass
+
+
+class ILIOError(ILError):
+    """IngeniaLink I/O error."""
+
+    pass
+
+
+class ILNotSupportedError(ILError):
+    """IngeniaLink Not supported error."""
+
+    pass
+
+
+class ILWrongRegisterError(ILError):
+    """IngeniaLink Wrong register error."""
+
+    pass
+
+
+class ILRegisterNotFoundError(ILError):
+    """IngeniaLink register not found in dictionary."""
+
+    pass
+
+
+class ILWrongCRCError(ILError):
+    """IngeniaLink Wrong CRC error."""
+
+    pass
+
+
+class ILNACKError(ILError):
+    """IngeniaLink NACK error."""
+
+    def __init__(self, err_code: int):
+        self.error_code = err_code
+        super().__init__(f"Communications error (NACK -> 0x{err_code:08X})")
+
+
+class ILDictionaryParseError(ILError):
+    """IngeniaLink dictionary parse error."""
+
+    pass
+
+
+# Configuration error
+class ILIncorrectAccessType(ILConfigurationError):
+    """Incorrect access type configuration error."""
+
+    pass
+
+
+class ILObjectNotExist(ILConfigurationError):
+    """Object doesn't exist configuration error."""
+
+    pass
+
+
+class ILObjectNotCyclicMappable(ILConfigurationError):
+    """Object isn't cyclic mappable as requested configuration error."""
+
+    pass
+
+
+class ILCyclicMappingTooLarge(ILConfigurationError):
+    """Cyclic mapping is too large configuration error."""
+
+    pass
+
+
+class ILWrongCyclicKey(ILConfigurationError):
+    """Cyclic mapping key is wrong configuration error."""
+
+    pass
+
+
+class ILWrongCyclicRegisterSize(ILConfigurationError):
+    """Mapped cyclic register size is wrong configuration error."""
+
+    pass
+
+
+class ILCommunicationStateUnreachable(ILConfigurationError):
+    """Communication state is unreachable configuration error."""
+
+    pass
+
+
+class ILCommunicationNotModifiable(ILConfigurationError):
+    """Communication setting is not modifiable in the
+    current state configuration"""
+
+    pass
+
+
+class ILUnsupportedRegisterValue(ILConfigurationError):
+    """Unsupported value introduced in register configuration error."""
+
+    pass
+
+
+class ILInvalidCommand(ILConfigurationError):
+    """Invalid command configuration error."""
+
+    pass
+
+
+class ILCRCError(ILConfigurationError):
+    """CRC error configuration error."""
+
+    pass
+
+
+class ILUnsupportedSynchronization(ILConfigurationError):
+    """Unsupported synchronization method configuration error."""
+
+    pass
+
+
+class ILActiveFeedbacksHigherThanAllowed(ILConfigurationError):
+    """Number of active feedbacks is higher than allowed configuration error."""
+
+    pass
+
+
+class ILCOMKITTimeout(ILConfigurationError):
+    """COMKIT Timeout. CORE device is not properly connected configuration error."""
+
+    pass
+
+
+class ILWrongWorkingCount(ILError):
+    """PDOs process data working count expected and received differ."""
+
+    pass
