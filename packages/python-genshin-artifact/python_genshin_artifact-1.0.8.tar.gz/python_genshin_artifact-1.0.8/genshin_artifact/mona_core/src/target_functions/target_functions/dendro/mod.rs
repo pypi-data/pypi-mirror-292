d6457cb5a84ec933af6d678eb6acd74865767365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:22 +0000

    Add package python-genshin-artifact/1.0.8/python_genshin_artifact-1.0.8.tar.gz

---
+++ packages/python-genshin-artifact/python_genshin_artifact-1.0.8.tar.gz/python_genshin_artifact-1.0.8/genshin_artifact/mona_core/src/target_functions/target_functions/dendro/mod.rs
@@ -0,0 +1,9 @@
+mod tighnari_default;
+mod nahida_default;
+mod alhaitham_default;
+mod emilie_default;
+
+pub use tighnari_default::TighnariDefaultTargetFunction;
+pub use nahida_default::NahidaDefaultTargetFunction;
+pub use alhaitham_default::AlhaithamDefaultTargetFunction;
+pub use emilie_default::EmilieDefaultTargetFunction;
