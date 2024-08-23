4693be91aadf1a5d10ba03d4d9571883219008fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:37 +0000

    Add package qleaf/0.0.6/qleaf-0.0.6.tar.gz

---
+++ packages/qleaf/qleaf-0.0.6.tar.gz/qleaf-0.0.6/qleaf/core/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2023 Jaehak Lee
+
+from . import main_window
+from . import abstract_comp
+from . import image
+from . import prop
+
+from .main_window import show_status
+import os, shutil
+
+CURRENT_SOURCE_CODE_FILE_PATH = os.path.abspath(__file__)
+
+def cout(*args):
+    show_status(" ".join([str(arg) for arg in args]))
+
+def setStyle(widget, style_name):
+    dir_path = os.path.dirname(CURRENT_SOURCE_CODE_FILE_PATH)
+    parent_dir = os.path.dirname(dir_path)
+    sshFile=parent_dir +'/style/"+style_name+"/"+style_name+".qss'
+    print("sshFile Path:",sshFile)
+    with open(sshFile,"r") as fh:
+        widget.setStyleSheet(fh.read())
