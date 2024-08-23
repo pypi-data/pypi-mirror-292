996f82281857ebb43601ba76798bf650df3ef2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:48 +0000

    Add package elyes-printer/0.2/elyes_printer-0.2-py3-none-any.whl

---
+++ packages/elyes-printer/elyes_printer-0.2-py3-none-any.whl/modules/print-hello/main.py
@@ -0,0 +1,6 @@
+def elyes_print(message: any) -> str:
+    """New function to print messages
+    :param message: message to print
+    :return message with prefix ELYES
+    """
+    return f"ELYES {message}"
