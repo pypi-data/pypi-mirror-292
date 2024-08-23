3f216409058a252504ad4b27f085d50eeaf767ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:52 +0000

    Add package softwrdwrangler/0.0.6/softwrdwrangler-0.0.6.tar.gz

---
+++ packages/softwrdwrangler/softwrdwrangler-0.0.6.tar.gz/softwrdwrangler-0.0.6/softwrdwrangler/__init__.py
@@ -0,0 +1,1 @@
+from .s3 import read_pickle, write_pickle, read_json, to_json
---
+++ packages/softwrdwrangler/softwrdwrangler-0.0.6.tar.gz/softwrdwrangler-0.0.6/softwrdwrangler/s3/__init__.py
@@ -0,0 +1,2 @@
+from .__pickle_operation import read_pickle, write_pickle
+from .__json_operation import read_json, to_json
