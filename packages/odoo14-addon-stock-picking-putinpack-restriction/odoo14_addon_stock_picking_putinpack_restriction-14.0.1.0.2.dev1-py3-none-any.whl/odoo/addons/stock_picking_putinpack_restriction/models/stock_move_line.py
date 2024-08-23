4fbec51dd139330b3c118dd2268b75d6ab9f0981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:38 +0000

    Add package odoo14-addon-stock-picking-putinpack-restriction/14.0.1.0.2.dev1/odoo14_addon_stock_picking_putinpack_restriction-14.0.1.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-stock-picking-putinpack-restriction/odoo14_addon_stock_picking_putinpack_restriction-14.0.1.0.2.dev1-py3-none-any.whl/odoo/addons/stock_picking_putinpack_restriction/models/stock_move_line.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Camptocamp (https://www.camptocamp.com)
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    def _action_done(self):
+        res = super()._action_done()
+        for line in self.exists():
+            picking_type = line.move_id.picking_type_id
+            restriction = picking_type.put_in_pack_restriction
+            if not restriction:
+                continue
+            line_has_package = bool(line.result_package_id)
+            if restriction == "no_package" and line_has_package:
+                raise ValidationError(
+                    _("Using a package on transfer type %s is not allowed.")
+                    % picking_type.name
+                )
+            if restriction == "with_package" and not line_has_package:
+                raise ValidationError(
+                    _("A package is required for transfer type %s.") % picking_type.name
+                )
+        return res
