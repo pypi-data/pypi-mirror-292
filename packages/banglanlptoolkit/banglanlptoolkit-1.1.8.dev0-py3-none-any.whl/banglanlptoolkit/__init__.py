c4e593e4df8c273ded01c4df5162585dbe41ab9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:48 +0000

    Add package banglanlptoolkit/1.1.8.dev0/banglanlptoolkit-1.1.8.dev0-py3-none-any.whl

---
+++ packages/banglanlptoolkit/banglanlptoolkit-1.1.8.dev0-py3-none-any.whl/banglanlptoolkit/__init__.py
@@ -0,0 +1,9 @@
+from .bnnlpnormalizer import BnNLPNormalizer
+from .seq2seqaug import Seq2SeqAug
+from .sequenceclassificationaug import SequenceClassificationAug
+from .banglaaugmentation import AugmentationBangla
+from .banglapunctuation import BanglaPunctuation
+from .banglatextmentation import *
+from .utils import *
+from .bnnlpnormalizerplus import *
+from .bntokenizers import sent_tokenize, word_tokenize
