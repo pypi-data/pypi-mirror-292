c29120d5f849a78a6728329470062285ca3b173d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:02:45 +0000

    Add package mul-umn/0.0.1/mul_umn-0.0.1-py3-none-any.whl

---
+++ packages/mul-umn/mul_umn-0.0.1-py3-none-any.whl/mul_umn/addition_umn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name      : Add 
+# Purpose   : Add two numbers
+# Author    : Umesh.N
+# Date      : 23-12-2023
+# Status    : Working Fine
+#-------------------------------------------------------------------------------
+def add_two_numbers(x,y):
+    return x+y
