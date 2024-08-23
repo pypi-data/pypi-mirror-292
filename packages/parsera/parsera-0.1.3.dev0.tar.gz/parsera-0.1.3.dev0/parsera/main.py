94f722ae6a6995db6f8f1a41e7844cc4460fbdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 17:03:53 +0000

    Add package parsera/0.1.3.dev0/parsera-0.1.3.dev0.tar.gz

---
+++ packages/parsera/parsera-0.1.3.dev0.tar.gz/parsera-0.1.3.dev0/parsera/main.py
@@ -0,0 +1,44 @@
+import asyncio
+from langchain_core.language_models import BaseChatModel
+import enum
+from parsera.engine.model import GPT4oMiniModel
+from parsera.engine.simple_extractor import TabularExtractor, ListExtractor, ItemExtractor
+from parsera.page import fetch_page_content
+
+class Parsera:
+    class ExtractorType(enum.Enum):
+        LIST = ListExtractor
+        TABULAR = TabularExtractor
+        ITEM = ItemExtractor
+
+    def __init__(self, model: BaseChatModel | None = None, extractor: ExtractorType = ExtractorType.TABULAR):
+        if model is None:
+            self.model = GPT4oMiniModel()
+        else:
+            self.model = model
+        self.extractor = extractor
+
+    async def _run(
+        self, url: str, elements: dict, proxy_settings: dict | None = None
+    ) -> dict:
+        if proxy_settings:
+            content = await fetch_page_content(url=url, proxy_settings=proxy_settings)
+        else:
+            content = await fetch_page_content(url=url)
+        extractor_instance = self.extractor.value(
+            elements=elements, model=self.model, content=content
+        )
+        result = await extractor_instance.run()
+        return result
+
+    def run(self, url: str, elements: dict, proxy_settings: dict | None = None) -> dict:
+        return asyncio.run(
+            self._run(url=url, elements=elements, proxy_settings=proxy_settings)
+        )
+
+    async def arun(
+        self, url: str, elements: dict, proxy_settings: dict | None = None
+    ) -> dict:
+        return await self._run(
+            url=url, elements=elements, proxy_settings=proxy_settings
+        )
