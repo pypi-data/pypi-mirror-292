802709a84f5bcbcba56746dfafb88ce882d5c19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:47 +0000

    Add package dataset-viber/0.2.4/dataset_viber-0.2.4-py3-none-any.whl

---
+++ packages/dataset-viber/dataset_viber-0.2.4-py3-none-any.whl/dataset_viber/_constants.py
@@ -0,0 +1,29 @@
+# Copyright 2024-present, David Berenstein, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEFAULT_EMBEDDING_MODEL = "Snowflake/snowflake-arctic-embed-xs"
+COLORS = [
+    "#a6cee3",
+    "#1f78b4",
+    "#b2df8a",
+    "#33a02c",
+    "#fb9a99",
+    "#e31a1c",
+    "#fdbf6f",
+    "#ff7f00",
+    "#cab2d6",
+    "#6a3d9a",
+    "#ffff99",
+    "#b15928",
+]
