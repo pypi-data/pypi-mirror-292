c5a92c5442a6035ff00ee4d6b25a822aef2c2382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:45 +0000

    Add package RunwayLib/0.2.2/runwaylib-0.2.2.tar.gz

---
+++ packages/RunwayLib/runwaylib-0.2.2.tar.gz/runwaylib-0.2.2/RunwayLib/server.py
@@ -0,0 +1,11 @@
+# runwaylib/server.py
+
+class Server:
+    def start(self):
+        print("Server started")
+
+def run_server():
+    print("Running the server")
+
+def hello():
+    return("hey")
