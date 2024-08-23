cc9455cdbc2f2c83ad75341141159f115f9f85dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:45 +0000

    Add package print-hello-tarmiz/0.1.1/print_hello_tarmiz-0.1.1-py3-none-any.whl

---
+++ packages/print-hello-tarmiz/print_hello_tarmiz-0.1.1-py3-none-any.whl/print_hello/__init__.py
@@ -0,0 +1,2 @@
+from .main import elyes_print
+
---
+++ packages/print-hello-tarmiz/print_hello_tarmiz-0.1.1-py3-none-any.whl/print_hello/main.py
@@ -0,0 +1,6 @@
+def elyes_print(message: any):
+    """New function to print messages
+    :param message: message to print
+    :return None
+    """
+    print(f"ELYES {message}")
