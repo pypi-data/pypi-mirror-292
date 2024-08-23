675af38d02ca8b994abb5c6fa1a0739de05866f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:20 +0000

    Add package utz/0.7.2/utz-0.7.2.tar.gz

---
+++ packages/utz/utz-0.7.2.tar.gz/utz-0.7.2/setup.py
@@ -0,0 +1,42 @@
+from utz.setup import setup
+
+extras_require = {
+    'dt': [
+        'click',
+        'python-dateutil==2.8.2',
+        'pytz',
+    ],
+    'git': [
+        'click',
+        'GitPython',
+        'PyGithub',
+    ],
+    'pd': [
+        'numpy',
+        'pandas',
+    ],
+    'pdf': [
+        'reportlab',
+        'pyPDF2',
+    ],
+    'plot': [
+        'kaleido',
+        'plotly',
+    ],
+    'setup': [
+        'setuptools',
+    ],
+    'test': [
+        'pytest',
+        'pytest-mock',
+    ],
+}
+extras_require['all'] = sum(extras_require.values(), [ 'pyyaml', ])
+
+setup(
+    name="utz",
+    version="0.7.2",
+    install_requires=open('requirements.txt', 'r').read(),
+    extras_require=extras_require,
+    url="https://github.com/runsascoded/utz",
+)
