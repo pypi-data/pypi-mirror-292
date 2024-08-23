b42f326f1057dbe2c25dbaad0f7a54c25f9156f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:56 +0000

    Add package scrapy-cookies-fixed/0.3.3/scrapy-cookies-fixed-0.3.3.tar.gz

---
+++ packages/scrapy-cookies-fixed/scrapy-cookies-fixed-0.3.3.tar.gz/scrapy-cookies-fixed-0.3.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    description = f.read()
+
+setup(
+    name='scrapy-cookies-fixed',  # Название вашего пакета (оно должно быть уникальным на PyPI)
+    version='0.3.3',  # Версия пакета
+    description='A fixed version of scrapy-cookie with updated imports',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='aveaxii',
+    author_email='kimalex2004dev@gmail.com',
+    url='https://github.com/aveaxii',
+    packages=find_packages(include=['scrapy_cookies', 'scrapy_cookies.*']),    
+    install_requires=[],  # Укажите зависимости, если есть
+    python_requires='>=3.9',  # Укажите минимальную версию Python
+)
