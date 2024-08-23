d9c0b5ef088295e03dfc7ff081c4ae099753354a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:04:18 +0000

    Add package backup-tracker-micro/0.1.6/backup_tracker_micro-0.1.6.tar.gz

---
+++ packages/backup-tracker-micro/backup_tracker_micro-0.1.6.tar.gz/backup_tracker_micro-0.1.6/main.py
@@ -0,0 +1,9 @@
+from src.backup_tracker_micro import BackupTracker, BackupEvent
+
+b = BackupTracker("localhost", "Test", "http://localhost:5000/")
+
+b.backup_event(BackupEvent.STARTED)
+b.backup_status(100.01)
+b.backup_event(BackupEvent.FINISHED)
+res = b.get_backup_events(100)
+print(res)
