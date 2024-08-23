1801d05e1cfc3b973b9dbf19edc539e271791eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:31 +0000

    Add package ob-metaflow-stubs/5.3/ob_metaflow_stubs-5.3-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-5.3-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/production_token.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.15.1+ob(v1)                                                   #
+# Generated on 2024-08-22T21:03:01.722165                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def new_token(token_prefix, prev_token = None):
+    ...
+
+def load_token(token_prefix):
+    ...
+
+def store_token(token_prefix, token):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-5.3-py2.py3-none-any.whl/metaflow-stubs/plugins/resources_decorator.pyi
@@ -0,0 +1,15 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.15.1+ob(v1)                                                   #
+# Generated on 2024-08-22T21:03:01.668402                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ResourcesDecorator(metaflow.decorators.StepDecorator, metaclass=type):
+    ...
+
