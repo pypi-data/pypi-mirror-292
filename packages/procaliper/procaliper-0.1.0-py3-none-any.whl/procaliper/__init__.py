4b6494c0257caf1501a9ad14e72e6288f6681d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:04:09 +0000

    Add package procaliper/0.1.0/procaliper-0.1.0-py3-none-any.whl

---
+++ packages/procaliper/procaliper-0.1.0-py3-none-any.whl/procaliper/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for procaliper."""
+
+__author__ = """AlphaMeter"""
+__email__ = "song.feng@pnnl.gov"
+__version__ = "0.1.0"
+
+# from procaliper.protein import Protein as Protein
+
+# __all__ = ["Protein"]
