60812592da738862ff5b0b6188ec46a5f058f093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:03:23 +0000

    Add package kedro/0.19.8/kedro-0.19.8-py3-none-any.whl

---
+++ packages/kedro/kedro-0.19.8-py3-none-any.whl/kedro/io/__init__.py
@@ -0,0 +1,33 @@
+"""``kedro.io`` provides functionality to read and write to a
+number of data sets. At the core of the library is the ``AbstractDataset`` class.
+"""
+
+from __future__ import annotations
+
+from .cached_dataset import CachedDataset
+from .core import (
+    AbstractDataset,
+    AbstractVersionedDataset,
+    DatasetAlreadyExistsError,
+    DatasetError,
+    DatasetNotFoundError,
+    Version,
+)
+from .data_catalog import DataCatalog
+from .lambda_dataset import LambdaDataset
+from .memory_dataset import MemoryDataset
+from .shared_memory_dataset import SharedMemoryDataset
+
+__all__ = [
+    "AbstractDataset",
+    "AbstractVersionedDataset",
+    "CachedDataset",
+    "DataCatalog",
+    "DatasetAlreadyExistsError",
+    "DatasetError",
+    "DatasetNotFoundError",
+    "LambdaDataset",
+    "MemoryDataset",
+    "SharedMemoryDataset",
+    "Version",
+]
