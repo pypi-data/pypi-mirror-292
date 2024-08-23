dec08bfcc7b3c19c4b3fae11a55fb07b4a581fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 14:03:29 +0000

    Add package polar-patch/0.0.6/polar_patch-0.0.6.tar.gz

---
+++ packages/polar-patch/polar_patch-0.0.6.tar.gz/polar_patch-0.0.6/polar_patch/__cli__.py
@@ -0,0 +1,25 @@
+import typer
+
+app = typer.Typer(name="pp", pretty_exceptions_show_locals=False, pretty_exceptions_short=True)
+
+
+@app.command(name="mount")
+def mount() -> None:
+  """
+  Mount your plugins type hints
+  """
+  import asyncio
+
+  from polar_patch import mount_plugins
+
+  asyncio.run(mount_plugins())
+
+
+@app.command()
+def unmount() -> None:
+  """
+  Unmount your plugins type hints
+  """
+  from polar_patch import unmount_plugins
+
+  unmount_plugins()
