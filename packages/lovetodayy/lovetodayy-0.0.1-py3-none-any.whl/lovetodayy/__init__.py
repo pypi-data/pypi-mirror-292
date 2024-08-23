4502365f6cf80f82fb827214c38a2c713de19ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:03:51 +0000

    Add package lovetodayy/0.0.1/lovetodayy-0.0.1-py3-none-any.whl

---
+++ packages/lovetodayy/lovetodayy-0.0.1-py3-none-any.whl/lovetodayy/__init__.py
@@ -0,0 +1,16 @@
+import random
+def monika_lover():
+    lst = ['RS','Vijeth','Naveen','Kumaran','Annapoorna','Sibi','Dinesh','Ramanan']
+    ans = random.choice(lst)
+    return "Monika is going to marry " + ans
+
+def nasrin_lover():
+    lst = ['RS','Vijeth','Naveen','Kumaran','Annapoorna','Sibi','Dinesh','Ramanan', 'Jaffer']
+    ans = random.choice(lst)
+    return "Nasrin is going to run with " + ans
+
+def dhar_lover():
+    lst = ['Sunny leone', 'Mia Khalifa', 'Kumaran', 'Miruna', 'Dharaneesh', 'Reema', 'Nivya']
+    ans = "Idiot! Dharaneesh will loves only roadside paid chicks and run with them one day"
+    return ans
+
