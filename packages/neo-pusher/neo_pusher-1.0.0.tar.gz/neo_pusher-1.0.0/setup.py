0e134fe375f9a992f2792391b2c2e636ade0d474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:02:53 +0000

    Add package neo-pusher/1.0.0/neo_pusher-1.0.0.tar.gz

---
+++ packages/neo-pusher/neo_pusher-1.0.0.tar.gz/neo_pusher-1.0.0/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="neo-pusher",
+    version="1.0.0",
+    description="A package for Neo4j data ingestion using an AI agent.",
+    author="Siddharth Choudhury",
+    author_email="siddharthc@mindfiresolutions.com",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # or "text/x-rst" if using reStructuredText
+    install_requires=[
+        "langchain",
+        "langchain_experimental",
+        "langchain_openai",
+        "pandas",
+        "neo4j"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
