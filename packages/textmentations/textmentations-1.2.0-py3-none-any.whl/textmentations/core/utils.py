ab5ff7cae1a22154517036b631714dad6b87706f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:04:12 +0000

    Add package textmentations/1.2.0/textmentations-1.2.0-py3-none-any.whl

---
+++ packages/textmentations/textmentations-1.2.0-py3-none-any.whl/textmentations/core/utils.py
@@ -0,0 +1,50 @@
+from typing import Dict
+
+from ..augmentations.utils import extract_first_sentence, remove_first_sentence, wrap_text_with_sentences
+from ..corpora.types import Sentence, Text
+
+
+def extract_first_sentence_by_key(key2text: Dict[str, Text]) -> Dict[str, Sentence]:
+    """Extracts the first sentence from a dictionary of text inputs.
+
+    Args:
+        key2text: A dictionary with string keys and Text values.
+
+    Returns:
+        A dictionary with the same keys as `key2text`, but with the values replaced by their first sentence.
+    """
+    return {key: extract_first_sentence(text) for key, text in key2text.items()}
+
+
+def remove_first_sentence_by_key(key2text: Dict[str, Text]) -> Dict[str, Text]:
+    """Removes the first sentence from the dictionary of text inputs.
+
+    Args:
+        key2text: The dictionary with string keys and Text values.
+
+    Returns:
+        A dictionary with the same keys as `key2text`,
+            but with the values replaced by the input text with the first sentence removed.
+    """
+    return {key: remove_first_sentence(text) for key, text in key2text.items()}
+
+
+def wrap_text_with_first_sentence_by_key(
+    key2text_without_first_sentence: Dict[str, Text], key2first_sentence: Dict[str, Sentence]
+) -> Dict[str, Text]:
+    """Wraps text inputs with their corresponding first sentence.
+
+    Args:
+        key2text_without_first_sentence: The dictionary with string keys and Text values that
+            represent text inputs with their first sentences removed.
+        key2first_sentence: The dictionary with string keys and Sentence values that
+            represent the first sentences of the original text inputs.
+
+    Returns:
+        A dictionary with the same keys as `key2text_without_first_sentence`,
+            but with the values wrapped with their corresponding first sentence as a prefix.
+    """
+    return {
+        key: wrap_text_with_sentences(text_without_first_sentence, prefix_sentences=[key2first_sentence[key]])
+        for key, text_without_first_sentence in key2text_without_first_sentence.items()
+    }
