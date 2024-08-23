59c94b9beb05767b670625895c17645ead98ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:03:04 +0000

    Add package cubedpandas/0.2.21/cubedpandas-0.2.21.tar.gz

---
+++ packages/cubedpandas/cubedpandas-0.2.21.tar.gz/cubedpandas-0.2.21/README.md
@@ -0,0 +1,126 @@
+# CubedPandas 
+
+## OLAP comfort meets Pandas power!
+
+![PyPI version](https://img.shields.io/pypi/v/cubedpandas)
+![PyPI Downloads](https://img.shields.io/pypi/dm/cubedpandas.svg?label=PyPI%20downloads)
+![GitHub last commit](https://img.shields.io/github/last-commit/Zeutschler/cubedpandas)
+![CI - Test](https://github.com/pandas-dev/pandas/actions/workflows/unit-tests.yml/badge.svg)
+![build](https://img.shields.io/github/actions/workflow/status/zeutschler/cubedpandas/python-package.yml)
+![codecov](https://codecov.io/github/Zeutschler/cubedpandas/graph/badge.svg?token=B12O0B6F10)
+![Python versions](https://img.shields.io/pypi/pyversions/cubedpandas)
+![GitHub license](https://img.shields.io/github/license/Zeutschler/cubedpandas)
+![sponsor](https://img.shields.io/github/sponsors/zeutschler)
+
+-----------------
+
+***Remark:*** *CubedPandas is in an early stage of development. Features are likely subject to change. 
+Anyhow, your [Ideas, Issues](https://github.com/Zeutschler/cubedpandas/issues) and [Feedback](https://github.com/Zeutschler/cubedpandas/discussions) are very welcome!
+
+CubedPandas aims to offer an ***easy, fast & natural approach to work with Pandas dataframes***. 
+To achieve this, CubedPandas wraps your dataframe into a lightweight, virtual multi-dimensional OLAP cube, 
+that can be analysed and used in a very natural, compact and readable and natural manner. 
+
+For novice users, CubedPandas can be a great help to get started with Pandas, as it hides some
+of the complexity and verbosity of Pandas dataframes. For experienced users, CubedPandas
+can be a great productivity booster, as it allows to write more compact, readable and
+maintainable code. Just to give you a first idea, this Pandas code:
+
+```python
+value = df.loc[(df['make'] == 'Audi') & (df['engine'] == 'hybrid'), 'price'].sum()
+```
+
+can be turned into this CubedPandas code:
+
+```python
+value = df.cubed.Audi.hybrid.price
+```
+
+As CubedPandas does not duplicate data or modifies the underlying dataframe and does not add 
+any performance penalty - but in some cases can even boost performance by factors - it can be 
+used in production use cases without any concerns and should be of great help in many use cases. 
+
+And in [Jupyter notebooks](https://jupyter.org), CubedPandas will really 
+start to shine. For further information, please visit the 
+[CubedPandas Documentation](https://zeutschler.github.io/cubedpandas/).
+
+
+### Installation and Getting Started
+
+After installing CubedPandas using `pip install cubedpandas`, you are ready to go. 
+*'Cubing'* a Pandas dataframe is as simple as this:
+
+```python
+import pandas as pd
+from cubedpandas import cubed
+
+# Create a dataframe with some sales data
+df = pd.DataFrame({"product":  ["Apple",  "Pear",   "Banana", "Apple",  "Pear",   "Banana"],
+                   "channel":  ["Online", "Online", "Online", "Retail", "Retail", "Retail"],
+                   "customer": ["Peter",  "Peter",  "Paul",   "Paul",   "Mary",   "Mary"  ],
+                   "mailing":  [True,     False,    True,     False,    True,     False   ],
+                   "revenue":  [100,      150,      300,      200,      250,      350     ],
+                   "cost":     [50,       90,       150,      100,      150,      175     ]})
+
+cdf = cubed(df)  # Wrapp your dataframe into a cube and start cubing!
+```
+
+CubedPandas **automatically infers a multi-dimensional schema** from your Pandas dataframe which 
+defines a virtual **Cube** over the dataframe. By default, numeric columns of the dataframe 
+are considered as **Measures** - *the numeric values to analyse & aggregate* - all other columns are 
+considered as **Dimensions** - *to filter, navigate and view the data*. The individual values in a 
+dimension column are called the **Members** of the dimension. In the example above, column `channel` 
+becomes a dimension with the two members `Online` and `Retail`, revenue and cost are the measures.
+
+Although rarely required, you can also define your own schema. Schemas are quite powerful and flexible, 
+as they will allow you to define dimensions and measures, aliases and (planned for upcoming releases) 
+also custom aggregations, business logic, number formating, linked cubes and much more. 
+
+### Give me a Context, so I will give you a Cell!
+One key feature of CubePandas is an easy & intuitive access to individual **Data Cells** in 
+multi-dimensional data space. To do so, you'll need to define a multi-dimensional **Context** and 
+CubedPandas will evaluate, aggregate (`sum` by default) and return the requested value from 
+the underlying dataframe.
+
+**Context objects behave like numbers** (float, int), so you can use them directly in arithmetic 
+operations. In the following examples, all addresses will refer to the exactly same rows from the dataframe
+and thereby all return the same value of `100`. 
+
+```python
+# Let Pandas set the scene...
+a = df.loc[(df["product"] == "Apple") & (df["channel"] == "Online") & (df["customer"] == "Peter"), "revenue"].sum()
+
+# Can we do better with CubedPandas? 
+b = cdf.product["Apple"].channel["Online"].customer["Peter"].revenue  # explicit, readable, flexible and fast  
+c = cdf.product.Apple.channel.Online.customer.Peter.revenue  # identical, if member names are Python-compliant
+
+# If there are no ambiguities in your data - can be easily checked - then you can use this shorthand forms:
+d = cdf["Online", "Apple", "Peter", "revenue"]
+e = cdf.Online.Apple.Peter.revenue
+f = cdf.Online.Apple.Peter  # if 'revenue' is the default measure (it is), so it can be omitted
+
+assert a == b == c == d == e == f == 100
+```
+
+Context objects act as filters on the underlying dataframe. So you can use also CubedPandas for 
+fast and easy filtering only, e.g. like this:
+
+```python   
+df = df.cubed.product["Apple"].channel["Online"].df
+```
+
+For further information, samples and a complete feature list as well as valuable tips and tricks,
+please visit the [CubedPandas Documentation](https://zeutschler.github.io/cubedpandas/).
+
+
+### Your feedback, ideas and support are very welcome!
+Please help improve and extend CubedPandas with **your feedback & ideas** and use the 
+[CubedPandas GitHub Issues](https://github.com/Zeutschler/cubedpandas/issues) to request new features and report bugs. 
+For general questions, discussions and feedback, please use the 
+[CubedPandas GitHub Discussions](https://github.com/Zeutschler/cubedpandas/discussions).
+
+If you have fallen in love with CubedPandas or find it otherwise valuable, 
+please consider to [become a sponsor of the CubedPandas project](https://github.com/sponsors/Zeutschler) so we 
+can push the project forward faster and make CubePandas even more awesome.
+
+*...happy cubing!*
---
+++ packages/cubedpandas/cubedpandas-0.2.21.tar.gz/cubedpandas-0.2.21/cubedpandas/schema.py
@@ -0,0 +1,230 @@
+# CubedPandas - Copyright (c)2024, Thomas Zeutschler, see LICENSE file
+from __future__ import annotations
+import json
+from typing import Any
+
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+
+from cubedpandas.settings import CachingStrategy
+from cubedpandas.dimension import Dimension
+from cubedpandas.dimension_collection import DimensionCollection
+from cubedpandas.measure import Measure
+from cubedpandas.measure_collection import MeasureCollection
+
+
+class Schema:
+    """
+    Defines a multidimensional schema, for cell-based data access to a Pandas dataframe using an Cube.
+
+    The schema defines the dimensions and measures of the cube and can be either inferred from the underlying
+    Pandas dataframe automatically or defined explicitly. The schema can be validated against the Pandas dataframe
+    to ensure the schema is valid for the table.
+    """
+
+    def __init__(self, df: pd.DataFrame | None = None, schema: Any = None,
+                 caching: CachingStrategy = CachingStrategy.LAZY):
+        """
+        Initializes a new schema for a Cube upon a given Pandas dataframe. If the dataframe is not provided,
+        the schema needs to be built manually and can also not be validated against the Pandas dataframe.
+
+        For building a schema manually, you can either create a new schema from scratch or you can load, extend
+        and modify an existing schema as defined by parameter `schema`. The parameter `schema` can either be
+        another Schema object, a Python dictionary containing valid schema information, a json string containing
+        valid schema information or a file name or path to a json file containing valid schema information.
+
+        :param df: (optional) the Pandas dataframe to build the schema from or for.
+        :param schema: (optional) a schema to initialize the Schema with. The parameter `schema` can either be
+                another Schema object, a Python dictionary containing valid schema information, a json string
+                containing valid schema information or a file name or path to a json file containing valid schema
+                information.
+        :param caching: The caching strategy to be used for the Cube. Default is `CachingStrategy.LAZY`. Please refer to
+                the documentation of 'CachingStrategy' for more information.
+        """
+        self._df: pd.DataFrame | None = df
+        self._schema: Schema | Any = schema
+        self._dimensions: DimensionCollection = DimensionCollection()
+        self._measures: MeasureCollection = MeasureCollection()
+        self._validation_message: str = ""
+        self._caching: CachingStrategy = caching
+
+        if schema is not None:
+            if not self.validate(self._df):
+                raise ValueError(self._validation_message)
+
+    def validate(self, df: pd.DataFrame) -> bool:
+        """
+        Validates the schema against an existing Pandas dataframe.
+
+        If returned True, the schema is valid for the given Pandas dataframe and can be used to access its data.
+        Otherwise, the schema is not valid and will or may lead to errors when accessing its data.
+
+        :param df: The Pandas dataframe to validate the schema against.
+
+        :return: Returns True if the schema is valid for the given Pandas dataframe, otherwise False.
+        """
+        self._dimensions = DimensionCollection()
+        for dimension in self._schema["dimensions"]:
+            if "column" in dimension:
+                column = dimension["column"]
+                if column not in df.columns:
+                    self._validation_message = f"Dimension column '{column}' not found in dataframe."
+                    return False
+                self._dimensions.add(Dimension(df, column, self._caching))
+            else:
+                if isinstance(dimension, str) or isinstance(dimension, int):
+                    if dimension not in df.columns:
+                        self._validation_message = f"Dimension column '{dimension}' not found in dataframe."
+                        return False
+                    self._dimensions.add(Dimension(df, dimension, self._caching))
+                else:
+                    self._validation_message = "Dimension column not found in schema."
+                    return False
+
+        self._measures = MeasureCollection()
+        for measure in self._schema["measures"]:
+            if "column" in measure:
+                column = measure["column"]
+                if column not in df.columns:
+                    self._validation_message = f"Measure column '{column}' not found in dataframe."
+                    return False
+                self._measures.add(Measure(df, column))
+            else:
+                if isinstance(measure, str) or isinstance(measure, int):
+                    if measure not in df.columns:
+                        self._validation_message = f"Measure column '{measure}' not found in dataframe."
+                        return False
+                    self._measures.add(Measure(df, measure))
+                else:
+                    self._validation_message = "Measure column not found in schema."
+                    return False
+        return True
+
+    @property
+    def dimensions(self) -> DimensionCollection:
+        """ Returns the dimensions of the schema."""
+        return self._dimensions
+
+    @property
+    def measures(self) -> MeasureCollection:
+        """ Returns the measures of the schema."""
+        return self._measures
+
+    def infer_schema(self, exclude: str | list | tuple | None = None) -> Schema:
+        """
+        Infers a multidimensional schema from the Pandas dataframe of the Schema or another Pandas dataframe by
+        analyzing the columns of the table and their contents.
+
+        This process can be time-consuming for large tables. For such cases, it is recommended to
+        infer the schema only from a sample of the records by setting parameter 'sample_records' to True.
+        By default, the schema is inferred from and validated against all records.
+
+        The inference process tries to identify the dimensions and their hierarchies of the cube as
+        well as the measures of the cube. If no schema cannot be inferred, an exception is raised.
+
+        By default, string, datetime and boolean columns are assumed to be measure columns and
+        numerical columns are assumed to be measures for cube computations. By default, all columns
+        of the Pandas dataframe will be used to infer the schema. However, a subset of columns can be
+        specified to infer the schema from. The subset needs to contain at least two columns, one
+        for a single dimensions and one for a single measures.
+
+        For more complex tables it is possible or even likely that the resulting schema does not match your
+        expectations or requirements. For such cases, you will need to build your schema manually.
+        Please refer the documentation for further details on how to build a schema manually.
+
+        :param exclude: (optional) a list of either column names or ordinal column ids to exclude when
+            inferring the schema.
+
+        :return: Returns the inferred schema.
+        """
+        df = self._df
+        schema = {"dimensions": [], "measures": []}
+        self._dimensions = DimensionCollection()
+        self._measures = MeasureCollection()
+
+        if exclude is not None:
+            if isinstance(exclude, str) or isinstance(exclude, int):
+                exclude = [exclude,]
+        else:
+            exclude = []
+
+        for column_name, dtype in df.dtypes.items():
+            if column_name not in exclude:
+                if is_numeric_dtype(df[column_name]):
+                    schema["measures"].append({"column": column_name})
+                    self._measures.add(Measure(df, column_name))
+                else:
+                    schema["dimensions"].append({"column": column_name})
+                    self._dimensions.add(Dimension(df, column_name, self._caching))
+
+        return self
+
+    # region Serialization
+    @classmethod
+    def from_dict(cls, dictionary: dict) -> Schema:
+        """
+        Creates a new schema from a dictionary containing schema information for a Cube.
+        Please refer to the documentation for further details on valid schema definitions.
+
+        :param dictionary: The dictionary containing the schema information.
+        :return: Returns a new schema object.
+        :exception: Raises an exception if the schema information is not valid or incomplete.
+        """
+        return cls(schema=dictionary)
+
+    @classmethod
+    def from_json(cls, json_string: str) -> Schema:
+        """
+        Creates a new schema from a json string containing schema information for a Cube.
+        If the json string is not valid and does refer to a file that contains a valid schema in json format,
+        an exception is raised.
+        Please refer to the documentation for further details on valid schema definitions.
+
+        :param json_string: The json string containing the schema information.
+        :return: Returns a new schema object.
+        :exception: Raises an exception if the schema information is not valid or incomplete.
+        """
+        return cls(schema=json_string)
+
+    def to_dict(self) -> dict:
+        """
+        Converts the schema into a dictionary containing schema information for an Cube.
+
+        :return: Returns a dictionary containing the schema information.
+        """
+        schema = {"dimensions": [], "measures": []}
+        for measure in self._measures:
+            schema["measures"].append({"column": measure.column})
+        for dimension in self._dimensions:
+            schema["dimensions"].append({"column": dimension.column})
+        return schema
+
+    def to_json(self) -> str:
+        """
+        Converts the schema into a dictionary containing schema information for an Cube.
+
+        :return: Returns a dictionary containing the schema information.
+        """
+        return json.dumps(self.to_dict)
+
+    def save_as_json(self, file_name: str):
+        """
+        Saves the schema as a json file.
+
+        :param file_name: The name of the file to save the schema to.
+        """
+        with open(file_name, 'w') as file:
+            json.dump(self.to_dict(), file)
+
+    # endregion
+
+    def __str__(self) -> str:
+        return self.to_json()
+
+    def __repr__(self) -> str:
+        return self.to_json()
+
+    def __len__(self):
+        """ Returns the number of dimensions of the schema."""
+        return len(self.dimensions)
+
