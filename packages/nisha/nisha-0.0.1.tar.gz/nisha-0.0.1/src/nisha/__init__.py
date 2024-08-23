f00be5d9c59f0fd540ce9a10acd4686da4a05b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:20 +0000

    Add package nisha/0.0.1/nisha-0.0.1.tar.gz

---
+++ packages/nisha/nisha-0.0.1.tar.gz/nisha-0.0.1/src/nisha/__init__.py
@@ -0,0 +1,31 @@
+import random
+def look():
+    return "Nisha look so beautiful. Even girls will sight her" 
+
+def talks():
+    return "Nisha talks so nice. But need to tolerate the mokka jokes."
+
+def style():
+    return "Nisha dress style is unmatchable. No one can come even near to her"
+
+def lookwithoutmkup():
+    return "Nisha look different without makeup. Sometimes hard to see. You will be afraid"
+
+def character():
+    return "Nisha has a very beautiful character. Very calm but makes noice when her R is with her"
+
+def knowledge():
+    return "Nisha has good amount knowledge to crack UPSC but no knowledge in INTL control report"
+
+def weight():
+    return "Nisha's weight is approximately 100 kgs"
+
+def height():
+    return "Nisha's height is 150cm"
+
+def BarathMomtalk():
+    input("Can you please tell What did you talk with Barath's Mom")
+    return "Thanks"
+
+def regret():
+    return "Thanks for telling mistakes"
