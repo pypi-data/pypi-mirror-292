c4cf1802a2d158963616ca8cb8c274d195a7012a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:34 +0000

    Add package accuweather-client/0.0.1/accuweather_client-0.0.1.tar.gz

---
+++ packages/accuweather-client/accuweather_client-0.0.1.tar.gz/accuweather_client-0.0.1/src/accuweather_client/models/location.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class LocationModelCity(BaseModel):
+    Version: int
+    Key: str
+    Type: str
+    Rank: int
+    LocalizedName: str
+    EnglishName: str
+    PrimaryPostalCode: str
+    Region: dict
+    Country: dict
+    AdministrativeArea: dict
+    TimeZone: dict
+    GeoPosition: dict
+    IsAlias: bool
+    SupplementalAdminAreas: list
+    DataSets: list
+
+
+class LocationModel(BaseModel):
+    response: list[LocationModelCity]
+
+    def get_location_key(self):
+        return self.response[0].Key
