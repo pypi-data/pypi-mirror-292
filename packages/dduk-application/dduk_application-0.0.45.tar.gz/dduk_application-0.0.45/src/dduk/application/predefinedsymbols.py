f11c0b42dd68baabcc3f10af18f5d05e4aa2821c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:03:56 +0000

    Add package dduk-application/0.0.45/dduk_application-0.0.45.tar.gz

---
+++ packages/dduk-application/dduk_application-0.0.45.tar.gz/dduk_application-0.0.45/src/dduk/application/predefinedsymbols.py
@@ -0,0 +1,13 @@
+#--------------------------------------------------------------------------------
+# 참조 모듈 목록.
+#--------------------------------------------------------------------------------
+from __future__ import annotations
+from typing import Any, Final, Callable, Iterator, Optional, Type, TypeVar, Union, Tuple, List, Dict, Set, cast
+import builtins
+
+
+#--------------------------------------------------------------------------------
+# 전역 상수 목록.
+#--------------------------------------------------------------------------------
+SYMBOL_SUBPROCESS : str = "SUBPROCESS"
+SYMBOL_LOG : str = "LOG"
