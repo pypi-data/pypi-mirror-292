89700ee363ece9601d21c4ecaf838f3875ede6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:22 +0000

    Add package natlinkcore/5.4.1/natlinkcore-5.4.1-py3-none-any.whl

---
+++ packages/natlinkcore/natlinkcore-5.4.1-py3-none-any.whl/natlinkcore/__init__.py
@@ -0,0 +1,27 @@
+'''Python portion of Natlink, a compatibility module for Dragon Naturally Speaking
+The python stuff including test modules'''
+
+__version__="5.4.1"
+#pylint:disable=
+from pathlib import Path
+
+def getThisDir(fileOfModule):
+    """get directory of calling module
+    """
+    return Path(fileOfModule).parent
+def logname():
+    """ Returns the name of the logger module, which is simply 'natlink'.  An entry point is defined for this in pyproject.toml.
+    """
+    return "natlink"
+# warningTexts = []
+# def warning(text):
+#     """print warning only once, if warnings is set!
+#     
+#     warnings can be set in the calling functions above...
+#     """
+#     textForeward = text.replace("\\", "/")
+#     if textForeward in warningTexts:
+#         return
+#     warningTexts.append(textForeward)
+#     print(text)
+#     
