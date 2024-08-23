13c15acf4b96c33367d426b194ede7921f07083f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:34 +0000

    Add package couchbase-sqlalchemy/0.1.8/couchbase_sqlalchemy-0.1.8.tar.gz

---
+++ packages/couchbase-sqlalchemy/couchbase_sqlalchemy-0.1.8.tar.gz/couchbase_sqlalchemy-0.1.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='couchbase-sqlalchemy',
+    version='0.1.8',
+    author='Ayush Tripathi',
+    author_email='ayush.tripathi@couchbase.com',
+    description='A SQLAlchemy dialect for Couchbase Analytics/Columnar.',
+    packages=find_packages(),
+    install_requires=[
+        'sqlalchemy>=1.3.0,<2.0',
+        'couchbase>=4.0.0',
+    ],
+    entry_points={
+        'sqlalchemy.dialects': [
+            'couchbase = couchbaseSqlalchemy.dialect.couchbase_dialect:CouchbaseDialect',
+        ],
+    },
+)
