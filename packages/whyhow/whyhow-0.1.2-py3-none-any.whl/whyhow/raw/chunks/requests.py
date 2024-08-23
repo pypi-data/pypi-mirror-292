54c327e360ab3aac75a503c6dafab69115619a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:09 +0000

    Add package whyhow/0.1.2/whyhow-0.1.2-py3-none-any.whl

---
+++ packages/whyhow/whyhow-0.1.2-py3-none-any.whl/whyhow/raw/chunks/requests.py
@@ -0,0 +1,128 @@
+"""Chunk requests."""
+
+from typing import Literal
+
+from httpx import AsyncClient, Client
+
+from whyhow.raw.base import ErrorReturnType, SuccessReturnType, asend, send
+from whyhow.raw.chunks.schemas import (
+    GetAllChunksQueryParameters,
+    GetAllChunksResponseBody,
+    GetChunkPathParameters,
+    GetChunkQueryParameters,
+    GetChunkResponseBody,
+)
+
+
+# sync functions
+def get_chunk(
+    client: Client,
+    chunk_id: str,
+    include_embeddings: bool | None = None,
+) -> SuccessReturnType[GetChunkResponseBody] | ErrorReturnType:
+    """Get chunk by ID."""
+    url = f"/chunks/{chunk_id}"
+    path_parameters = GetChunkPathParameters(
+        chunk_id=chunk_id,
+    )
+    query_parameters = GetChunkQueryParameters(
+        include_embeddings=include_embeddings,
+    )
+    return send(
+        client=client,
+        method="get",
+        url=url,
+        path_parameters=path_parameters,
+        query_parameters=query_parameters,
+        response_body_schema=GetChunkResponseBody,
+    )
+
+
+def get_all_chunks(
+    client: Client,
+    skip: int | None = None,
+    limit: int | None = None,
+    workspace_id: str | None = None,
+    workspace_name: str | None = None,
+    document_id: str | None = None,
+    document_filename: str | None = None,
+    include_embeddings: bool | None = None,
+    order: Literal["ascending", "descending"] | None = None,
+) -> SuccessReturnType[GetAllChunksResponseBody] | ErrorReturnType:
+    """Get all chunks."""
+    url = "/chunks"
+    query_parameters = GetAllChunksQueryParameters(
+        skip=skip,
+        limit=limit,
+        workspace_id=workspace_id,
+        workspace_name=workspace_name,
+        document_id=document_id,
+        document_filename=document_filename,
+        include_embeddings=include_embeddings,
+        order=order,
+    )
+
+    return send(
+        client=client,
+        method="get",
+        url=url,
+        query_parameters=query_parameters,
+        response_body_schema=GetAllChunksResponseBody,
+    )
+
+
+# async functions
+async def aget_chunk(
+    client: AsyncClient,
+    chunk_id: str,
+    include_embeddings: bool | None = None,
+) -> SuccessReturnType[GetChunkResponseBody] | ErrorReturnType:
+    """Get chunk by ID."""
+    url = f"/chunks/{chunk_id}"
+    path_parameters = GetChunkPathParameters(
+        chunk_id=chunk_id,
+    )
+    query_parameters = GetChunkQueryParameters(
+        include_embeddings=include_embeddings,
+    )
+    return await asend(
+        client=client,
+        method="get",
+        url=url,
+        path_parameters=path_parameters,
+        query_parameters=query_parameters,
+        response_body_schema=GetChunkResponseBody,
+    )
+
+
+async def aget_all_chunks(
+    client: AsyncClient,
+    skip: int | None = None,
+    limit: int | None = None,
+    workspace_id: str | None = None,
+    workspace_name: str | None = None,
+    document_id: str | None = None,
+    document_filename: str | None = None,
+    include_embeddings: bool | None = None,
+    order: Literal["ascending", "descending"] | None = None,
+) -> SuccessReturnType[GetAllChunksResponseBody] | ErrorReturnType:
+    """Get all chunks."""
+    url = "/chunks"
+    query_parameters = GetAllChunksQueryParameters(
+        skip=skip,
+        limit=limit,
+        workspace_id=workspace_id,
+        workspace_name=workspace_name,
+        document_id=document_id,
+        document_filename=document_filename,
+        include_embeddings=include_embeddings,
+        order=order,
+    )
+
+    return await asend(
+        client=client,
+        method="get",
+        url=url,
+        query_parameters=query_parameters,
+        response_body_schema=GetAllChunksResponseBody,
+    )
