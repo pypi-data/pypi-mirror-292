b7fdcd78eabfabcd98ad21c9afc6224bbf643478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 03:03:18 +0000

    Add package deeporigin/2.1.0/deeporigin-2.1.0.tar.gz

---
+++ packages/deeporigin/deeporigin-2.1.0.tar.gz/deeporigin-2.1.0/src/__init__.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+import subprocess
+from pathlib import Path
+
+from deeporigin.data_hub.dataframe import DataFrame
+
+__all__ = ["__version__", "DataFrame"]
+
+SRC_DIR = pathlib.Path(__file__).parent
+
+version_filename = os.path.join(SRC_DIR, "VERSION")
+
+with open(version_filename, "r") as file:
+    __version__ = file.read()
+
+    if __version__ == "0.0.0.dev0":
+        # in dev mode. we use git to get a "version number"
+        # that will change with tags and commits, if possible
+        try:
+            process = subprocess.run(
+                ["git", "describe", "--tags", "--dirty", "--long"],
+                capture_output=True,
+                universal_newlines=True,
+                cwd=Path(__file__).parent.parent,
+            )
+            if process.returncode == 0:
+                __version__ = process.stdout.strip()
+        except Exception:
+            pass
