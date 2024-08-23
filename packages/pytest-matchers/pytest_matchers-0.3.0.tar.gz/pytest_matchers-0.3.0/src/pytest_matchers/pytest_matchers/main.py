b64334a62bc1fb2130421b0c97f1e1f2d7248cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 06:03:27 +0000

    Add package pytest-matchers/0.3.0/pytest_matchers-0.3.0.tar.gz

---
+++ packages/pytest-matchers/pytest_matchers-0.3.0.tar.gz/pytest_matchers-0.3.0/src/pytest_matchers/pytest_matchers/main.py
@@ -0,0 +1,145 @@
+from typing import Any, Callable, Type
+
+from pytest_matchers.matchers import (
+    Anything,
+    Between,
+    Case,
+    Datetime,
+    DatetimeString,
+    Dict,
+    DifferentValue,
+    HasAttribute,
+    If,
+    IsInstance,
+    List,
+    Number,
+    String,
+    JSON,
+    Matcher,
+    Or,
+    SameValue,
+    StrictDict,
+    UUID,
+)
+
+
+def anything() -> Anything:
+    return Anything()
+
+
+def is_instance(match_type: Type) -> IsInstance:
+    return IsInstance(match_type)
+
+
+def is_list(match_type: Type = None, **kwargs) -> List | IsInstance:
+    if match_type is None and not kwargs:
+        return is_instance(list)
+    return List(match_type, **kwargs)
+
+
+def is_string(**kwargs) -> String:
+    return String(**kwargs)
+
+
+def is_number(match_type: Type = None, **kwargs) -> Number:
+    return Number(match_type, **kwargs)
+
+
+def one_of(*values: Matcher | Any) -> Or:
+    return Or(*values)
+
+
+def has_attribute(attribute_name: str, expected_value: Any = None) -> HasAttribute:
+    return HasAttribute(attribute_name, expected_value)
+
+
+def between(
+    min_value: Any,
+    max_value: Any,
+    *,
+    inclusive: bool = None,
+    min_inclusive: bool = None,
+    max_inclusive: bool = None,
+) -> Between:
+    return Between(min_value, max_value, inclusive, min_inclusive, max_inclusive)
+
+
+def is_datetime(
+    min_value: Any = None,
+    max_value: Any = None,
+    year: int = None,
+    month: int = None,
+    day: int = None,
+    hour: int = None,
+    minute: int = None,
+    second: int = None,
+) -> Datetime:
+    return Datetime(min_value, max_value, year, month, day, hour, minute, second)
+
+
+def is_datetime_string(
+    expected_format: str,
+    *,
+    min_value: Any = None,
+    max_value: Any = None,
+) -> DatetimeString:
+    return DatetimeString(expected_format, min_value=min_value, max_value=max_value)
+
+
+def same_value() -> SameValue:
+    return SameValue()
+
+
+def different_value() -> DifferentValue:
+    return DifferentValue()
+
+
+def if_true(
+    condition: Callable | bool | Any,
+    then: Matcher | Any = None,
+    or_else: Matcher | Any = None,
+) -> If:
+    return If(condition, then, or_else)
+
+
+def if_false(
+    condition: Callable | bool | Any,
+    then: Matcher | Any = None,
+    or_else: Matcher | Any = None,
+) -> If:
+    return if_true(condition, or_else, then)  # pylint: disable=arguments-out-of-order
+
+
+def case(
+    case_value: Any,
+    expectations: dict[Any, Matcher | Any],
+    default_expectation: Matcher | Any | None = None,
+) -> Case:
+    return Case(case_value, expectations, default_expectation)
+
+
+def is_dict(matching: dict = None, *, exclude: list[Any] = None) -> Dict:
+    return Dict(matching, exclude=exclude)
+
+
+def is_strict_dict(
+    matching: dict,
+    *,
+    extra_condition: bool = None,
+    when_true: dict = None,
+    when_false: dict = None,
+) -> StrictDict:
+    return StrictDict(
+        matching,
+        extra_condition=extra_condition,
+        when_true=when_true,
+        when_false=when_false,
+    )
+
+
+def is_json(matching: dict = None, *, exclude: list[Any] = None) -> JSON:
+    return JSON(matching, exclude=exclude)
+
+
+def is_uuid(matching_type: Type = None, *, version: int | Matcher = None) -> UUID:
+    return UUID(matching_type, version=version)
---
+++ packages/pytest-matchers/pytest_matchers-0.3.0.tar.gz/pytest_matchers-0.3.0/src/pytest_matchers/pytest_matchers/utils/repr_utils.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+from pytest_matchers.matchers import Matcher
+
+
+def _repr(extra_repr: Any | Matcher | None) -> str:
+    if isinstance(extra_repr, Matcher):
+        return extra_repr.concatenated_repr()
+    return str(extra_repr) if extra_repr is not None else ""
+
+
+def concat_matcher_repr(matcher: Matcher | None) -> str:
+    return _repr(matcher)
+
+
+def concat_reprs(
+    base_repr: str | Matcher | None,
+    *extra_reprs: str | Matcher | None,
+    separator: str = "and",
+) -> str:
+    base_repr = str(base_repr) if base_repr else ""
+    extra_repr = f" {separator} ".join(
+        [_repr(extra_repr) for extra_repr in extra_reprs if extra_repr]
+    )
+    if not base_repr and extra_repr:
+        return capitalized(extra_repr)
+    if extra_repr:
+        return f"{base_repr} {extra_repr}"
+    return base_repr
+
+
+def capitalized(string: str):
+    if not string:
+        return string
+    return string[0].upper() + string[1:]
+
+
+def non_capitalized(string: str):
+    if not string:
+        return string
+    return string[0].lower() + string[1:]
