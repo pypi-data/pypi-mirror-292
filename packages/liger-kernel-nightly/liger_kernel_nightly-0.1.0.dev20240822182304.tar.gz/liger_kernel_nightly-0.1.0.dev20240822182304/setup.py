0c8fd5ab46eeab53c4dbb1e0202da44c8573b927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:03:59 +0000

    Add package liger-kernel-nightly/0.1.0.dev20240822182304/liger_kernel_nightly-0.1.0.dev20240822182304.tar.gz

---
+++ packages/liger-kernel-nightly/liger_kernel_nightly-0.1.0.dev20240822182304.tar.gz/liger_kernel_nightly-0.1.0.dev20240822182304/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_namespace_packages, setup
+
+__version__ = "0.1.0.dev20240822182304"
+
+setup(
+    name="liger_kernel_nightly",
+    version=__version__,
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src"),
+    include_package_data=True,
+    install_requires=[
+        "torch>=2.1.2",
+        "triton>=2.3.0",
+        "transformers>=4.40.1",
+    ],
+    extras_require={
+        "dev": [
+            "matplotlib>=3.7.2",
+            "flake8>=4.0.1.1",
+            "black>=24.4.2",
+            "isort>=5.13.2",
+            "pre-commit>=3.7.1",
+            "torch-tb-profiler>=0.4.1",
+        ]
+    },
+)
