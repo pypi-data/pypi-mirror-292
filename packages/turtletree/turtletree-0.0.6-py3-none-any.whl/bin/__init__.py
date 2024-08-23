1ef9a68c304cf1b2add4c21ba7a9280d862f7cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:41 +0000

    Add package turtletree/0.0.6/turtletree-0.0.6-py3-none-any.whl

---
+++ packages/turtletree/turtletree-0.0.6-py3-none-any.whl/bin/__init__.py
@@ -0,0 +1,8 @@
+# QUTAT - Multiphysics Simulation Platform
+# Copyright (C) 2023 Jaehak Lee
+# SPDX-License-Identifier: GPL-3.0-only
+
+__version__='0.0.6'
+
+from . import array
+from . import database
