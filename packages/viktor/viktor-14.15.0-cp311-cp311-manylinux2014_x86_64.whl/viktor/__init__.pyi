813b99c1d0d470b3c643862ce0aeab4203481664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:04:12 +0000

    Add package viktor/14.15.0/viktor-14.15.0-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/viktor/viktor-14.15.0-cp311-cp311-manylinux2014_x86_64.whl/viktor/__init__.pyi
@@ -0,0 +1,9 @@
+from .core import *
+from .errors import *
+from .geometry import *
+from .parametrization import *
+from .result import *
+from .utils import *
+from .views import *
+from .__version__ import __version__ as __version__
+from .external import axisvm as axisvm, dfoundations as dfoundations, dgeostability as dgeostability, dsettlement as dsettlement, dsheetpiling as dsheetpiling, dstability as dstability, dynamo as dynamo, excel as excel, grasshopper as grasshopper, grlweap as grlweap, idea_rcs as idea_rcs, rfem as rfem, robot as robot, scia as scia, spreadsheet as spreadsheet, word as word
