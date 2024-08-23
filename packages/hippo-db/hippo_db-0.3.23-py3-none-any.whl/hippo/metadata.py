3fdb225f243097af54df465500c2a2708d5fb129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:22 +0000

    Add package hippo-db/0.3.23/hippo_db-0.3.23-py3-none-any.whl

---
+++ packages/hippo-db/hippo_db-0.3.23-py3-none-any.whl/hippo/metadata.py
@@ -0,0 +1,59 @@
+
+from collections import UserDict
+from typing import Mapping
+
+class MetaData(UserDict):
+
+	"""Metadata dictionary linked to a compound or pose in the database"""
+
+	def __init__(self, 
+		__dict: Mapping[str, str] | None
+	) -> None:
+
+		super().__init__()
+		if __dict:
+			for key, value in __dict.items():
+				super().__setitem__(key, value)
+
+		self._db = None
+		self._table: str = None
+		self._id: str = None
+
+	def _update_db(self, commit=True):
+		"""
+
+		:param commit:  (Default value = True)
+
+		"""
+		self._db.insert_metadata(table=self._table, id=self._id, payload=self.data, commit=commit)
+
+	def __setitem__(self, key: str, item: str) -> None:
+		self.data.__setitem__(key, item)
+		self._update_db()
+
+	def __delitem__(self, key: str) -> None:
+		self.data.__delitem__(key)
+		self._update_db()
+
+	def update(self, data, commit=True):
+		"""Wrapper for dict.update()
+
+		:param data: 
+		:param commit:  (Default value = True)
+
+		"""
+		self.data.update(data)
+		self._update_db(commit=commit)
+
+	def append(self, key, value):
+		"""Create or append to a list-like value with given key
+
+		:param key: 
+		:param value: 
+
+		"""
+		if key not in self:
+			self.data[key] = []
+		if value not in self.data[key]:
+			self.data[key].append(value)
+		self._update_db()
---
+++ packages/hippo-db/hippo_db-0.3.23-py3-none-any.whl/hippo/target.py
@@ -0,0 +1,81 @@
+
+import mcol
+
+class Target:
+	"""Object representing a protein target"""
+
+	def __init__(self, db, id, name):
+
+		self._db = db
+		self._id = id
+		self._name = name
+
+	@property
+	def db(self):
+		""" """
+		return self._db
+
+	@property
+	def id(self):
+		"""Returns the target's ID"""
+		return self._id
+
+	@property
+	def name(self):
+		"""Returns the target's name"""
+		return self._name
+
+	@property
+	def feature_ids(self):
+		"""Returns the target's feature ID's"""
+		feature_ids = self.db.select_where(
+			query='feature_id', 
+			table='feature', 
+			key='target', 
+			value=self.id, 
+			none=False, 
+			multiple=True,
+			sort='feature_chain_name, feature_residue_number'
+		)
+
+		if feature_ids:
+			feature_ids = [v for v, in feature_ids]
+
+		return feature_ids
+
+	@property
+	def features(self):
+		"""Returns the target's features"""
+		if feature_ids := self.feature_ids:
+			return [self.db.get_feature(id=i) for i in feature_ids]
+		return None
+
+	def __str__(self):
+		return f'T{self.id}'
+
+	def __repr__(self):
+		return f'{mcol.bold}{mcol.underline}{self} "{self.name}"{mcol.unbold}{mcol.ununderline}'
+
+	def calculate_features(self, protein):
+		"""Calculate features from a protein system
+
+		:param protein: 
+
+		"""
+
+		features = protein.get_protein_features()
+
+		for f in features:
+			self.db.insert_feature(
+				family=f.family,
+				target=self.id,
+				atom_names=[a.name for a in f.atoms],
+				residue_name=f.res_name,
+				residue_number=f.res_number,
+				chain_name=f.res_chain,
+				commit=False,
+			)
+
+		self.db.commit()
+
+		return self.features
