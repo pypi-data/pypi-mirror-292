996b50b4f36a68eb77b73bf0ecf5d5e58115f2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:22 +0000

    Add package MolParse/0.0.27/molparse-0.0.27-py3-none-any.whl

---
+++ packages/MolParse/molparse-0.0.27-py3-none-any.whl/molparse/rdkit/__init__.py
@@ -0,0 +1,7 @@
+from .features import features_from_group, Feature, features_from_mol
+
+from .draw import *
+from .mol import *
+from .fingerprint import get_similar
+from .tools import sdf_align
+from .sucos import SuCOS_score
