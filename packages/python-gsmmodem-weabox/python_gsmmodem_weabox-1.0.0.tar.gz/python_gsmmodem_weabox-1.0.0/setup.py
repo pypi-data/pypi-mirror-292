a9c1f85ebc006242fc34233eb9541e486edbbdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 17:04:14 +0000

    Add package python-gsmmodem-weabox/1.0.0/python_gsmmodem_weabox-1.0.0.tar.gz

---
+++ packages/python-gsmmodem-weabox/python_gsmmodem_weabox-1.0.0.tar.gz/python_gsmmodem_weabox-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='python-gsmmodem_weabox',
+    version='1.0.0',
+    author='PT Thanh',
+    author_email='ptthanh@example.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.3',
+)
