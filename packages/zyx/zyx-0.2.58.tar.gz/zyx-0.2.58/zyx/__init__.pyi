619bb25a2510905d592479c009f649d9f8d39290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:04:05 +0000

    Add package zyx/0.2.58/zyx-0.2.58.tar.gz

---
+++ packages/zyx/zyx-0.2.58.tar.gz/zyx-0.2.58/zyx/__init__.pyi
@@ -0,0 +1,42 @@
+__all__ = [
+    "BaseModel",
+    "Field",
+    "hf",
+    "logger",
+    "app",
+    "agents",
+    "cli",
+    "chainofthought",
+    "classify",
+    "completion",
+    "embedding",
+    "image",
+    "code",
+    "delegate",
+    "extract",
+    "function",
+    "generate",
+    "rag",
+    "zyxModuleLoader",
+]
+
+# --- zyx ----------------------------------------------------------------
+
+from .core.ext import BaseModel, Field, zyxModuleLoader
+from .client.agents import Agents as agents
+from .client.app import cli, app
+from .client.main import completion
+from .client.fn import (
+    classify,
+    chainofthought,
+    delegate,
+    code,
+    extract,
+    function,
+    generate,
+)
+from .client.rag import Qdrant as rag
+from .image.main import image
+from litellm.main import embedding
+from huggingface_hub.inference._client import InferenceClient as hf
+from loguru import logger
