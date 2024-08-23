91ea46b8f84fae8a7cb46f0d7e49b04c001a5f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 15:03:27 +0000

    Add package Dirlin/0.1.16/dirlin-0.1.16-py3-none-any.whl

---
+++ packages/Dirlin/dirlin-0.1.16-py3-none-any.whl/dirlin/__init__.py
@@ -0,0 +1,7 @@
+"""Dirlin (Directory Handler), set of tools to help process local files."""
+
+__version__ = "0.1.3"
+
+from .src.base import Folder as Folder
+from .src.base import Document as Document
+from .src.base import Path as Path
