32e2d48a30a6ae0d78a9f0966501c4d57040761a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 05:02:45 +0000

    Add package Flask-WebTest/0.1.5/flask_webtest-0.1.5.tar.gz

---
+++ packages/Flask-WebTest/flask_webtest-0.1.5.tar.gz/flask_webtest-0.1.5/README.rst
@@ -0,0 +1,24 @@
+Flask-WebTest
+=============
+
+.. image:: https://dl.circleci.com/status-badge/img/gh/level12/flask-webtest/tree/master.svg?style=svg
+        :target: https://dl.circleci.com/status-badge/redirect/gh/level12/flask-webtest/tree/master
+
+Flask-WebTest provides a set of utilities to ease testing Flask applications with WebTest.
+
+As a small-in-scope project and relatively mature/stable, lack of recent commits does not indicate
+it is abandoned. Occasionally, Flask or another dependency will deprecate imports. Most updates
+here will be to resolve those breakages when they occur.
+
+
+Installation
+------------
+
+``pip install flask-webtest``
+
+Documentation
+-------------
+
+Documentation is available on `Read the Docs`_.
+
+.. _Read the Docs: https://flask-webtest.readthedocs.org/en/latest/
---
+++ packages/Flask-WebTest/flask_webtest-0.1.5.tar.gz/flask_webtest-0.1.5/setup.py
@@ -0,0 +1,52 @@
+"""
+Flask-WebTest
+-------------
+
+Provides a set of utilities to ease testing Flask applications with WebTest.
+
+Links
+`````
+
+* `documentation <http://flask-webtest.readthedocs.org/en/latest/>`_
+* `development version
+  <http://github.com/level12/flask-webtest/zipball/master#egg=Flask-WebTest-dev>`_
+"""
+import os.path as osp
+from setuptools import setup
+
+cdir = osp.abspath(osp.dirname(__file__))
+version_fpath = osp.join(cdir, 'version.py')
+version_globals = {}
+with open(version_fpath) as fo:
+    exec(fo.read(), version_globals)
+
+setup(
+    name='Flask-WebTest',
+    version=version_globals['VERSION'],
+    url='https://github.com/level12/flask-webtest',
+    license='BSD',
+    description = 'Utilities for testing Flask applications with WebTest.',
+    long_description=__doc__,
+    author='Anton Romanovich',
+    author_email='anthony.romanovich@gmail.com',
+    include_package_data=True,
+    py_modules=['flask_webtest'],
+    zip_safe=False,
+    install_requires=[
+        'Flask>=1.1.0',
+        'WebTest',
+        'blinker',
+    ],
+    extras_require={
+        'tests': [
+            'flask-sqlalchemy',
+            'greenlet',
+        ],
+    },
+    classifiers=[
+        'Topic :: Software Development :: Testing',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+)
