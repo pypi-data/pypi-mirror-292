cbfde8118581882d85bd126377bb3d57df8201ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 16:03:52 +0000

    Add package m3u8-analyzer/1.0.2.1/m3u8_analyzer-1.0.2.1.tar.gz

---
+++ packages/m3u8-analyzer/m3u8_analyzer-1.0.2.1.tar.gz/m3u8_analyzer-1.0.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Lê o conteúdo do README.md
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name="m3u8-analyzer",
+    version="1.0.2.1",
+    description="Análise de dados de HLS m3u8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="PauloCesar0073-dev404",
+    author_email="paulocesar0073dev404@gmail.com",
+    url='https://paulocesar-dev404.github.io/M3u8_Analyzer/',
+    license="MIT",
+    keywords=["hls", "m3u8", "m3u8_analyzer", "M3u8Analyzer"],
+    packages=find_packages(),  # Inclui todos os pacotes encontrados automaticamente
+    install_requires=['cryptography', 'requests'],
+    include_package_data=True,  # Inclui dados adicionais do pacote, como README.md
+)
