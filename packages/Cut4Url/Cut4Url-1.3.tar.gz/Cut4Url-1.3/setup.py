86cbbe789ff60add7a551c3b491572c89e08d4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-23 04:03:16 +0000

    Add package Cut4Url/1.3/Cut4Url-1.3.tar.gz

---
+++ packages/Cut4Url/Cut4Url-1.3.tar.gz/Cut4Url-1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cut4Url",
+    version="1.3",
+    description="A simple library for shortening links",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
