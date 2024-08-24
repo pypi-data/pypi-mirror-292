c16f98e33593b5b3e8f920dd47e96d95219cee84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:39 +0000

    Add package klabmodels/0.1.0/klabmodels-0.1.0.tar.gz

---
+++ packages/klabmodels/klabmodels-0.1.0.tar.gz/klabmodels-0.1.0/src/klabmodels/kopi/models.py
@@ -0,0 +1,20 @@
+from typing import Dict, List, Optional
+from redis_om import JsonModel, EmbeddedJsonModel, Field
+from pydantic.v1 import BaseModel, Extra, ValidationError
+from enum import Enum
+
+
+
+
+
+# This will later be part of Kopi
+class User(JsonModel):
+    username: str  
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
