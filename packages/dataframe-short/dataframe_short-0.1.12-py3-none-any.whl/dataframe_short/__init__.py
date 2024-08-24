3721c33ca7191ff3426237c0b698d25da1892b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:05:19 +0000

    Add package dataframe-short/0.1.12/dataframe_short-0.1.12-py3-none-any.whl

---
+++ packages/dataframe-short/dataframe_short-0.1.12-py3-none-any.whl/dataframe_short/__init__.py
@@ -0,0 +1,10 @@
+from dataframe_short.func_24_Feb import *
+from dataframe_short.utils_ds import *
+from dataframe_short.sandbox1_ds import *
+from dataframe_short.convert_types import *
+import dataframe_short.display # as dsd
+import dataframe_short.move_column # as mc
+__version__ = "0.1.12"
+
+
+
