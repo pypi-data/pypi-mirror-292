eaeebdd671c59dfc1b29e453e57203957196b1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:04:30 +0000

    Add package metaphor-connectors/0.14.86/metaphor_connectors-0.14.86.tar.gz

---
+++ packages/metaphor-connectors/metaphor_connectors-0.14.86.tar.gz/metaphor_connectors-0.14.86/metaphor/common/sql/process_query/config.py
@@ -0,0 +1,57 @@
+from dataclasses import field
+
+from pydantic.dataclasses import dataclass
+
+from metaphor.common.base_config import ConnectorConfig
+
+
+@dataclass(config=ConnectorConfig)
+class RedactPIILiteralsConfig:
+    """
+    Config to control whether we want to redact literal values. Useful if you want to remove PII from your queries.
+    """
+
+    where_clauses: bool = False
+    """
+    Whether to redact literal values in WHERE clauses. If set to `True`, all literal values will be redacted to a predefined string value.
+    """
+
+    case_clauses: bool = False
+    """
+    Whether to redact literal values in CASE clauses. If set to `True`, all literal values will be redacted to a predefined string value.
+    """
+
+    when_not_matched_insert_clauses: bool = False
+    """
+    Whether to redact literal values in WHEN NOT MATCHED INSERT clauses. If set to `True`, all literal values will be redacted to a predefined string value.
+    """
+
+    placeholder_literal: str = "<REDACTED>"
+
+
+@dataclass(config=ConnectorConfig)
+class ProcessQueryConfig:
+    """
+    Config to control what to do when processing the parsed SQL queries.
+    """
+
+    redact_literals: RedactPIILiteralsConfig = field(
+        default_factory=lambda: RedactPIILiteralsConfig()
+    )
+
+    ignore_insert_values_into: bool = False
+    """
+    Ignore `INSERT INTO ... VALUES` expressions. These expressions don't have any
+    lineage information, and are often very large in size.
+    """
+
+    @property
+    def should_process(self) -> bool:
+        """
+        Whether we should run the processing method at all.
+        """
+        return (
+            self.redact_literals.where_clauses
+            or self.redact_literals.when_not_matched_insert_clauses
+            or self.ignore_insert_values_into
+        )
