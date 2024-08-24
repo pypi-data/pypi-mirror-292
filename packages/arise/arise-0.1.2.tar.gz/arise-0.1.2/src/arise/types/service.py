381973e8128d5243197b0b6945f4c55fec174f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:48 +0000

    Add package arise/0.1.2/arise-0.1.2.tar.gz

---
+++ packages/arise/arise-0.1.2.tar.gz/arise-0.1.2/src/arise/types/service.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3.8
+# coding:utf-8
+# Copyright (C) 2024 All rights reserved.
+# FILENAME:    ~~/src/arise/types/service.py
+# VERSION:     0.1.2
+# CREATED:     2024-07-07 16:11
+# AUTHOR:      Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from typing import Dict, List, Literal
+from typing_extensions import Annotated
+
+### Third-party packages ###
+from pydantic import BaseModel, Field, StrictStr
+
+PortMapping = Annotated[StrictStr, Field(pattern=r"^\d{1,5}:\d{1,5}$")]
+
+
+class Service(BaseModel):
+  command: Dict[int, StrictStr] = {}
+  env_vars: List[StrictStr] = []
+  image: StrictStr
+  ports: List[PortMapping]
+
+
+ServiceName = Literal[
+  "arise-bitcoind", "arise-mainnet", "arise-signet", "arise-testnet", "arise-testnet4"
+]
+
+__all__ = ("PortMapping", "Service", "ServiceName")
