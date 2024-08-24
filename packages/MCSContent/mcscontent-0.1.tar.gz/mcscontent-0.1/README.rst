e53c0e3061519323d4e74e1894ce42dc5531eaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:30 +0000

    Add package MCSContent/0.1/mcscontent-0.1.tar.gz

---
+++ packages/MCSContent/mcscontent-0.1.tar.gz/mcscontent-0.1/README.rst
@@ -0,0 +1,1 @@
+This is a module used to simplify what is returned by the MCStutas module
---
+++ packages/MCSContent/mcscontent-0.1.tar.gz/mcscontent-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MCSContent',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='Ray',
+    author_email='fengrui@fengrui.asia',
+    description='This is a module used to simplify what is returned by the MCStutas module',
+    long_description=open('README.rst').read(),
+    url='https://fengrui.link',
+    license='LICENSE'
+)
