c2e06b667c3253fa2c490bf2adabd2d1f35cb174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:04:28 +0000

    Add package frplib/0.1.25/frplib-0.1.25-py3-none-any.whl

---
+++ packages/frplib/frplib-0.1.25-py3-none-any.whl/frplib/examples/circle_points.py
@@ -0,0 +1,73 @@
+#
+# Random Points in a Circle Example Chapter 0, Section 4.3
+# Points in a Circle, With Constraints Example Chapter 0, Section 5
+#
+from math              import floor
+from typing            import Literal, Union
+
+from frplib.frps       import FRP, frp
+from frplib.kinds      import (conditional_kind,
+                               constant, uniform, weighted_by)
+from frplib.utils      import compose, dim, irange
+
+# ATTN! Need to update for new interfaces, e.g., domain, codim
+# This def is temporary along these lines
+from frplib.vec_tuples import as_vec_tuple
+from frplib.exceptions import MismatchedDomain
+
+def value_set(*vals):
+    if len(vals) == 1 and hasattr(vals[0], '__next__'):
+        # We have been given a single iterator/generator, use it
+        vals = vals[0]
+    vs = set([as_vec_tuple(v) for v in vals])
+    dims = set(map(dim, vs))
+    if len(dims) != 1:
+        raise MismatchedDomain(f'Value set {vals} have different dimensions.')
+    return vs
+
+
+#
+# Helpers
+#
+
+def y_points(x: int, radius: Union[int, float] = 5) -> list[int]:
+    r = abs(radius)
+    r_lo = floor(r)
+    return [y for y in irange(-r_lo, r_lo) if y * y <= r * r - x * x]
+
+num_y_points = compose(len, y_points)   # len `after` y_points
+
+def points_inside(radius: Union[int, float] = 5) -> list[tuple[int, int]]:
+    "Returns list of integer points within circle of given radius >= 0"
+    r = abs(radius)
+    r_lo = floor(r)
+    vals = list(irange(-r_lo, r_lo))
+    return [(x, y) for x in vals for y in vals
+            if x * x + y * y <= r * r]
+
+
+#
+# FRP Factories
+#
+
+def circle_points(
+        radius: Union[int, float] = 5,
+        method: Literal["mixture", "jointly"] = "mixture"
+) -> FRP:
+    "Returns FRP for uniform random point inside circle of a given radius."
+    if method == "jointly":
+        return frp(uniform(points_inside(radius)))
+
+    # Construct as a mixture
+    r = abs(radius)
+    r_lo = floor(r)
+    if r == 0:
+        return frp(constant(0, 0))
+
+    x_kind = weighted_by(-r_lo, -r_lo + 1, ..., r_lo, weight_by=num_y_points)
+
+    @conditional_kind(domain=value_set(irange(-r_lo, r_lo)))   # ATTN: remove value_set
+    def y_kind(x):
+        return uniform(y_points(x))
+
+    return frp(x_kind >> y_kind)
---
+++ packages/frplib/frplib-0.1.25-py3-none-any.whl/frplib/expectations.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from collections.abc   import Iterable
+from functools         import wraps
+
+from frplib.exceptions import ComplexExpectationWarning
+from frplib.frps       import FRP, ConditionalFRP
+from frplib.kinds      import Kind, ConditionalKind
+from frplib.output     import in_panel
+from frplib.protocols  import SupportsExpectation, SupportsApproxExpectation, SupportsForcedExpectation
+from frplib.quantity   import show_qtuple
+from frplib.statistics import Statistic
+from frplib.vec_tuples import VecTuple, as_vec_tuple
+
+
+class Expectation(VecTuple):
+    def __init__(self, contents: Iterable):
+        self.label = ''
+
+    def __str__(self) -> str:
+        return show_qtuple(self)
+
+    def __frplib_repr__(self):
+        return in_panel(str(self), title=self.label or None)
+
+    # ATTN: needed?
+    @property
+    def raw(self):
+        "Expectation as a raw scalar or vector tuple."
+        if len(self) == 1:
+            return self[0]
+        return VecTuple(self)
+
+def E(x, force_kind=False, allow_approx=True, tolerance=0.01):
+    """Computes and returns the expectation of a given object.
+
+    If `x` is an FRP or kind, its expectation is computed directly,
+    unless doing so seems computationally inadvisable. In this case,
+    the expectation is forced if `forced_kind` is True and otherwise
+    is approximated, with specified `tolerance`, if `allow_approx`
+    is True.
+
+    In this case, returns a quantity wrapping the expectation that
+    allows convenient display at the repl; the actual value is in
+    the .this property of the returned object.
+
+    If `x` is a ConditionalKind or ConditionalFRP, then returns
+    a *function* from domain values in the conditional to
+    expectations.
+
+    """
+    if isinstance(x, (ConditionalKind, ConditionalFRP)):
+        f = x.expectation()
+
+        @wraps(f)
+        def c_expectation(*xs):
+            return Expectation(as_vec_tuple(f(*xs)))
+        if getattr(f, 'dim') is not None:
+            label = f'dimension {getattr(f, "dim")} values'
+        else:
+            label = 'values'
+        setattr(c_expectation, '__frplib_repr__',
+                lambda: f'A conditional expectation as a function of {label}.')
+        return c_expectation
+
+    if isinstance(x, SupportsExpectation):
+        label = ''
+        try:
+            expect = x.expectation()
+        except ComplexExpectationWarning as e:
+            if force_kind and isinstance(x, SupportsForcedExpectation):
+                expect = x.forced_expectation()
+            elif isinstance(x, SupportsApproxExpectation):
+                expect = x.approximate_expectation(tolerance)
+                label = (f'Computing approximation (tolerance {tolerance}) '
+                         f'as exact calculation may be costly')  # ': {str(e)}\n'
+            else:
+                raise e
+        expect = Expectation(as_vec_tuple(expect))
+        if label:
+            expect.label = label
+        return expect
+    return None
+
+def D_(X: FRP | Kind):
+    """The distribution operator for an FRP or kind.
+
+    When passed an FRP or kind, this returns a function
+    that maps any compatible statistic to the expectation
+    of the transformed FRP or kind.
+
+    """
+    def probe(psi: Statistic):
+        # ATTN: Check compatibility here
+        return E(psi(X))
+    return probe
+
+
+#
+# Info tags
+#
+
+setattr(E, '__info__', 'actions')
+setattr(D_, '__info__', 'actions')
