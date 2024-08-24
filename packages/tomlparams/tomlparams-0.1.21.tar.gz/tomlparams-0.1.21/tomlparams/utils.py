e58a3cf2d96f35932f663bb6f5324d44f1b65f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:05:07 +0000

    Add package tomlparams/0.1.21/tomlparams-0.1.21.tar.gz

---
+++ packages/tomlparams/tomlparams-0.1.21.tar.gz/tomlparams-0.1.21/tomlparams/utils.py
@@ -0,0 +1,99 @@
+"""
+Utils
+=====
+"""
+
+import sys
+from typing import Any, Generator, NoReturn
+
+import tomli
+
+
+def error(*msg, exit_code=1) -> NoReturn:
+    print("*** ERROR:", *msg, file=sys.stderr)
+    sys.exit(exit_code)
+    # raise Exception
+
+
+def warn(*msg):
+    print("*** WARNING:", *msg, file=sys.stderr)
+
+
+def nvl(v, default):
+    return default if v is None else v
+
+
+def load_toml(path):
+    """
+    Protected TOML load using tomli that reports what the file
+    was if parsing fails (and then re-raises the exception).
+    """
+    with open(path, 'rb') as f:
+        try:
+            return tomli.load(f)
+        except Exception:
+            print(
+                f'\n***\n*** Problem parsing {path}:\n***\n', file=sys.stderr
+            )
+            raise
+
+
+def concatenate_keys(
+    d: dict, sep='.'
+) -> Generator[tuple[str, Any], None, None]:
+    """
+    Concatenate keys in a nested dict, e.g.:
+            >>> d = {'a': {'b': 1, 'c': 2}, 'd': 3}
+            >>> dict(concat_keys(d))
+            {'a.b': 1, 'a.c': 2, 'd': 3}
+    Args:
+        d: dict
+        sep: separator between keys
+    Returns:
+        generator of (key, value) pairs
+    """
+    for key1, value1 in d.items():
+        if isinstance(value1, dict):
+            for key2, value2 in dict(
+                concatenate_keys(value1, sep=sep)
+            ).items():
+                yield key1 + sep + key2, value2
+        else:
+            yield key1, value1
+
+
+def concatenate_keys_with_list(
+    d: dict, sep='.'
+) -> Generator[tuple[str, Any], None, None]:
+    """
+    Concatenate keys in a nested dict, e.g.:
+            >>> d = {'a': {'b': 1, 'c': 2}, 'd': 3}
+            >>> dict(concat_keys(d))
+            {'a.b': 1, 'a.c': 2, 'd': 3}
+    Special when values is a list:
+            >>> d = {'a': {'b': 1, 'c': 2}, 'd': [3, {'e': 4}]}
+            >>> dict(concat_keys(d))
+            {'a.b': 1, 'a.c': 2, 'd.0': 3, 'd.1.e': 4}
+    Args:
+        d: dict
+        sep: separator between keys
+    Returns:
+        generator of (key, value) pairs
+    """
+    for key1, value1 in d.items():
+        if isinstance(value1, dict):
+            for key2, value2 in dict(
+                concatenate_keys_with_list(value1, sep=sep)
+            ).items():
+                yield key1 + sep + key2, value2
+        elif isinstance(value1, list):
+            for list_index, list_item in enumerate(value1):
+                if isinstance(list_item, dict):
+                    for key3, value3 in concatenate_keys_with_list(
+                        list_item, sep=sep
+                    ):
+                        yield f'{key1}{sep}{list_index}{sep}{key3}', value3
+                else:
+                    yield f'{key1}{sep}{list_index}', list_item
+        else:
+            yield key1, value1
