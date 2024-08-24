0ffcef2fb9d92e41a8b3ccf889e93381ff7bf5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:03:53 +0000

    Add package franklin-fastapi-extension/1.2.1/franklin_fastapi_extension-1.2.1.tar.gz

---
+++ packages/franklin-fastapi-extension/franklin_fastapi_extension-1.2.1.tar.gz/franklin_fastapi_extension-1.2.1/franklin_fastapi_extension/config/__init__.py
@@ -0,0 +1,9 @@
+from .response_config import *
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=" %(levelname)s \t-\t %(message)s",
+)
+logger = logging.getLogger(__name__)
