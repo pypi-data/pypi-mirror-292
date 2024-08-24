a879b9bc5fb9742c303309ff5286faf5e34947d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:04:50 +0000

    Add package mytoolsID/0.5.3.1.2/mytoolsid-0.5.3.1.2.tar.gz

---
+++ packages/mytoolsID/mytoolsid-0.5.3.1.2.tar.gz/mytoolsid-0.5.3.1.2/mytools/__init__.py
@@ -0,0 +1,1 @@
+from .ende import FARNET
---
+++ packages/mytoolsID/mytoolsid-0.5.3.1.2.tar.gz/mytoolsid-0.5.3.1.2/mytools/ende.py
@@ -0,0 +1,36 @@
+import base64
+import hashlib
+import random
+import textwrap
+
+from cryptography import fernet
+
+
+class FARNET:
+    def __init__(self, key):
+        self.key = hashlib.sha256(key.encode()).digest()
+        self.cipher_suite = fernet.Fernet(base64.urlsafe_b64encode(self.key))
+
+    def en(self, data):
+        serialized_data = textwrap.dedent(data).encode("utf-8")
+        encrypted_data = self.cipher_suite.encrypt(serialized_data)
+        return encrypted_data
+
+    def de(self, encrypted_data):
+        try:
+            decrypted_data = self.cipher_suite.decrypt(encrypted_data)
+            data = decrypted_data.decode("utf-8")
+            return data
+        except fernet.InvalidToken:
+            raise Exception(f"[ERROR]: KUNCI - [{self.key}] - TIDAK COCOK")
+
+    def logs(self, text):
+        random_color = random.choice(["\033[91m", "\033[92m", "\033[93m", "\033[94m", "\033[95m", "\033[96m"])
+        reset_color = "\033[0m"
+        print(f"{random_color}{text}{reset_color}")
+
+    def run(self, decrypted_data):
+        try:
+            exec(self.de(decrypted_data))
+        except Exception as error:
+            self.logs(error)
