0fd6700df82f7f1c22004caebf31f1009855356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:05:07 +0000

    Add package wisecube/1.0.3/wisecube-1.0.3-py3-none-any.whl

---
+++ packages/wisecube/wisecube-1.0.3-py3-none-any.whl/wisecube_sdk/string_query.py
@@ -0,0 +1,188 @@
+qa = """
+query($query: String!) {
+  summaryInsights(engineID: "23343", searchInput: {query: $query, type: [QA]}) {
+    data {
+      __typename
+      ... on QAInsight {
+        question
+        answers {
+          answer
+          document {
+            id
+            title
+            abs
+            source
+            index_name
+            __typename
+          }
+          took
+          context
+          statistics
+          probability
+          __typename
+        }
+        __typename
+      }
+    }
+    __typename
+  }
+}
+"""
+
+documents = """
+query($query: String!){
+  summaryInsights(engineID: "23343", searchInput: {query: $query, type: [DOCUMENTS]}) {
+    data {
+      __typename
+      ... on QAInsight {
+        question
+        answers {
+          document {
+            id
+            title
+            abs
+            source
+            index_name
+            __typename
+          }
+          took
+          context
+          statistics
+          probability
+          __typename
+        }
+        __typename
+      }
+    }
+    __typename
+  }
+}
+"""
+
+search_graph = """
+query graphInsights($startNode: ID, $startNodeName: String, $nodeTypes: [NodeType], $maxNeighbors: Int) {
+  graphInsights(
+    engineID: "23343"
+    graphInput: {startNode: $startNode, startNodeName: $startNodeName, nodeTypes: $nodeTypes, maxNeighbors: $maxNeighbors}
+  ) {
+    description
+    data {
+      __typename
+      ... on SubGraph {
+        description
+        rootNode{
+            id
+            name
+        }
+        nodes {
+          id
+          name
+          type
+          properties {
+            name
+            values
+            __typename
+          }
+          __typename
+        }
+        edges {
+          id
+          start {
+            id
+            __typename
+          }
+          end {
+            id
+            __typename
+          }
+          __typename
+        }
+        __typename
+      }
+    }
+    __typename
+  }
+}
+"""
+
+search_text = """
+query($query: String!){
+  searchAsYouType(
+    searchInput: { searchText: $query, type: SEARCH_LABEL }
+  ) {
+    description
+    type
+    data {
+      __typename
+      ... on SearchLabels {
+        searchLabels {
+          qid
+          text
+          nodeTypes
+        }
+      }
+    }
+  }
+}
+"""
+
+execute_score_function = """
+query($triples: [[String!]]) { executeScoreFunction(triples: $triples
+  )
+}
+"""
+
+execute_vector_function = """
+query($graphIds: [String!]) { executeVectorFunction(graphIds: $graphIds)  
+}
+"""
+
+execute_nl2sparql = """
+query executeNl2Sparql($question: String!) {
+  executeNl2Sparql(question: $question) 
+}
+"""
+
+get_predicates = """
+query($label: String!){getPredicates(label: $label)}
+"""
+
+get_admet_prediction = """
+query ($smiles: [String!], $modelName: String){ admetPredict(smiles: $smiles, modelName: $modelName)
+}
+
+"""
+
+advanced_search_query = """
+query advancedSearchQuery($query: String) {
+  advancedSearchGraph(query: $query) {
+    head
+    results
+    __typename
+  }
+}
+"""
+
+
+ask_pythia = """
+query askPythia($reference: [String!], $response: String!, $question: String, $includeDefaultValidators: Boolean) {
+  askPythia(reference: $reference, response: $response, question: $question, includeDefaultValidators: $includeDefaultValidators) 
+}
+"""
+
+
+search_qids = """
+query getQids($question: String!) { getQids(question: $question) }
+"""
+
+search_by_type = """
+query searchByType($qid: String) { searchByType(qid: $qid) }
+"""
+
+search_by_relationship="""
+query searchByRelationship($predicate: String, $qids: [String]) { searchByRelationship(predicate: $predicate, qids: $qids) }
+"""
+
+search_by_qualifier = """
+query searchByQualifier($triples: [[String]], $predicate: String) { searchByQualifier(triples:$triples, predicate: $predicate) }
+"""
