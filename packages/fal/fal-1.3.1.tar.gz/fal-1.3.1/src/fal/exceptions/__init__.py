e7b716165b9ae1cd508aa46be40d2bf04e2738dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:32 +0000

    Add package fal/1.3.1/fal-1.3.1.tar.gz

---
+++ packages/fal/fal-1.3.1.tar.gz/fal-1.3.1/src/fal/exceptions/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+from ._base import AppException, FalServerlessException, FieldException  # noqa: F401
+from ._cuda import CUDAOutOfMemoryException  # noqa: F401
