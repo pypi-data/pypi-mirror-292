2517ac1c20e3c7e614f9d400e9f9ce970e93ddaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:04:30 +0000

    Add package codeforlife-portal/7.1.2/codeforlife_portal-7.1.2-py2.py3-none-any.whl

---
+++ packages/codeforlife-portal/codeforlife_portal-7.1.2-py2.py3-none-any.whl/example_project/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path
+from game import urls as game_urls
+
+from portal import urls as portal_urls
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r"^", include(portal_urls)),
+    path("administration/", admin.site.urls),
+    url(r"^rapidrouter/", include(game_urls)),
+]
