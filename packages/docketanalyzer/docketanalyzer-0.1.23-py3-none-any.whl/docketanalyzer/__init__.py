9a3e44fc2c8f58f4bea46cb0cb4116cecd6c6d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:34 +0000

    Add package docketanalyzer/0.1.23/docketanalyzer-0.1.23-py3-none-any.whl

---
+++ packages/docketanalyzer/docketanalyzer-0.1.23-py3-none-any.whl/docketanalyzer/__init__.py
@@ -0,0 +1,40 @@
+from docketanalyzer._version import __version__
+from docketanalyzer.config import EnvConfig, ConfigKey
+import docketanalyzer.utils as utils
+from docketanalyzer.tools.registry import Registry
+
+import docketanalyzer.choices as choices
+from docketanalyzer.tools.s3 import S3Utility
+from docketanalyzer.tools.websearch import WebSearch
+
+from docketanalyzer.cli import cli
+
+extensions = {
+    'core': dict(
+        names=[
+            'CoreDataset', 'load_dataset',
+            'DocketBatch', 'DocketIndex', 'load_docket_index', 'DocketManager',
+            'load_elastic', 'JuriscraperUtility',
+            'Task', 'DocketTask', 'task_registry', 'register_task', 'load_tasks', 'load_task',
+        ],
+    ),
+    'pipelines': dict(
+        names=[
+            'parallel_inference', 
+            'Pipeline', 'pipeline', 'remote_pipeline',
+            'Routine', 'training_routine',
+        ],
+    ),
+}
+
+
+from docketanalyzer.utils import lazy_load
+
+__all__ = ['EnvConfig', 'ConfigKey', 'utils', 'Registry', 'choices']
+
+for extension_name, extension in extensions.items():
+    names = extension['names']
+    module = f"docketanalyzer_{extension_name}"
+    for name in extension['names']:
+        globals()[name] = lazy_load(module, name, extension_name)
+        __all__.append(name)
