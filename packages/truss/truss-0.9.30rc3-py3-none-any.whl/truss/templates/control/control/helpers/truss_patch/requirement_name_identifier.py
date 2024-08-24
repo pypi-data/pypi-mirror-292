872b1c1915c386105f5f20b1208bf7bbc34b2fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:52 +0000

    Add package truss/0.9.30rc3/truss-0.9.30rc3-py3-none-any.whl

---
+++ packages/truss/truss-0.9.30rc3-py3-none-any.whl/truss/templates/control/control/helpers/truss_patch/requirement_name_identifier.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+from packaging import requirements  # type: ignore
+
+
+def identify_requirement_name(req: str) -> str:
+    try:
+        parsed_req = requirements.Requirement(req)
+        return parsed_req.name  # type: ignore
+    except (requirements.InvalidRequirement, ValueError):
+        # default to the whole line if we can't parse it.
+        return req.strip()
+
+
+def reqs_by_name(reqs: List[str]) -> Dict[str, str]:
+    return {identify_requirement_name(req): req for req in reqs if req.strip()}
