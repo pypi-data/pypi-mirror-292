1247597b183742c5f457907c9ea417d964ce9743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:29 +0000

    Add package pysics-lab/0.1.0/pysics_lab-0.1.0-py3-none-any.whl

---
+++ packages/pysics-lab/pysics_lab-0.1.0-py3-none-any.whl/pysics_lab/__init__.py
@@ -0,0 +1,17 @@
+from .objects import Measure, Line, VALUE, ERROR, Unit
+from .reader import load
+from .constants import *
+from .units import *
+
+from . import fit
+from . import aprox
+from . import reader
+from . import statistic
+from . import func
+from . import objects
+from . import plot
+from . import tables
+from . import calculos
+from . import constants
+from . import prelude
+from . import units
