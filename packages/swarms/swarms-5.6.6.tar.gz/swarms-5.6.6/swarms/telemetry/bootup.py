f11477aea2afc9bba96b265a21cf031f7c75e613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:55 +0000

    Add package swarms/5.6.6/swarms-5.6.6.tar.gz

---
+++ packages/swarms/swarms-5.6.6.tar.gz/swarms-5.6.6/swarms/telemetry/bootup.py
@@ -0,0 +1,16 @@
+import os
+import logging
+import warnings
+
+from swarms.telemetry.auto_upgrade_swarms import auto_update
+from swarms.utils.disable_logging import disable_logging
+
+
+def bootup():
+    """Bootup swarms"""
+    disable_logging()
+    logging.disable(logging.CRITICAL)
+    os.environ["WANDB_SILENT"] = "true"
+    os.environ["WORKSPACE_DIR"] = "agent_workspace"
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    auto_update()
