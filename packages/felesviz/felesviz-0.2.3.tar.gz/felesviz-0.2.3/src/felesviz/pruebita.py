f7e564c0149f063787a3bcdeb9a9f21ede940439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:05:10 +0000

    Add package felesviz/0.2.3/felesviz-0.2.3.tar.gz

---
+++ packages/felesviz/felesviz-0.2.3.tar.gz/felesviz-0.2.3/src/felesviz/pruebita.py
@@ -0,0 +1,8 @@
+def saludo():
+    return "hola profe Germán"
+
+def hola():
+    return "Hola Danna"
+
+def cumple_majito():
+    return "Felizzzzz cumpleeee Majitoooo, que te vayaaa muy bienn hoy y te den muchosss regalosss."
