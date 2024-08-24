615e54210fe273ed1942a2c25c7288c2fdfa0225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:54 +0000

    Add package anc/0.2.3/anc-0.2.3-py3-none-any.whl

---
+++ packages/anc/anc-0.2.3-py3-none-any.whl/anc/conf/remote.py
@@ -0,0 +1,4 @@
+remote_server = "http://198.18.36.94:5000"
+remote_storage_prefix = "/mnt/weka"
+repo_path = "/mnt/weka/xug/dvc_repo/dataset-registry"
+repo_url = "https://github.com/a7n-global/dataset-registry.git"
