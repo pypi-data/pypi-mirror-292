875ac8298b21bb0cebda37a77e3d626abbad530a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:37 +0000

    Add package voir/0.2.19/voir-0.2.19.tar.gz

---
+++ packages/voir/voir-0.2.19.tar.gz/voir-0.2.19/voir/instruments/gpu/cpu.py
@@ -0,0 +1,18 @@
+def is_installed():
+    return True
+
+
+class DeviceSMI:
+    def get_gpus_info(self, selection=None):
+        return {}
+
+    @property
+    def arch(self):
+        return "cpu"
+
+    @property
+    def visible_devices(self):
+        return ""
+
+    def close(self):
+        pass
