88e7871e1d8402f18617e0aa8131f069832f5286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:43 +0000

    Add package jdif/1.0.6/jdif-1.0.6.tar.gz

---
+++ packages/jdif/jdif-1.0.6.tar.gz/jdif-1.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name="jdif",
+    version="1.0.6",
+    description="To find json differences",
+    author="shivani",
+    entry_points={
+        'console_scripts': [
+            'jdif = jdif:start',
+        ]
+    },
+    url="https://github.com/Shivani-20/json-diff-cli",
+    package_dir={"": "src"},
+    packages=["jdif"],
+    install_requires=[
+        'termcolor>=2.4.0',
+        'deepdiff>=7.0.1',
+        'terminaltables>=3.1.10'
+    ])
