9136665263eb24ea7c2673ecdeffb13f562693bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:04:35 +0000

    Add package PayPaython-mobile/1.8.6/PayPaython-mobile-1.8.6.tar.gz

---
+++ packages/PayPaython-mobile/PayPaython-mobile-1.8.6.tar.gz/PayPaython-mobile-1.8.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+def load_readme() -> str:
+    with open("README.md",encoding="utf-8_sig") as fin:
+        return fin.read()
+setup(
+    name='PayPaython-mobile',
+    version='1.8.6',
+    keywords = "paypay",
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    author='taka4602',
+    author_email='shun4602@gmail.com',
+    url='https://github.com/taka-4602/Akinator-python',
+    description='An API wrapper for the PayPayAPI',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        "requests",
+        "bs4",
+        "pkce"
+    ],
+    python_requires='>=3.6',
+)
