e3de35d45c6a716787d2f56ceeb54f9b57b9c801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:04:43 +0000

    Add package docketanalyzer-pipelines/0.0.7/docketanalyzer_pipelines-0.0.7-py3-none-any.whl

---
+++ packages/docketanalyzer-pipelines/docketanalyzer_pipelines-0.0.7-py3-none-any.whl/docketanalyzer_pipelines/__init__.py
@@ -0,0 +1,4 @@
+from .parallel_inference import parallel_inference
+from .pipelines import Pipeline, pipeline, pipeline_registry
+from .remote_pipeline import remote_pipeline
+from .routines import Routine, training_routine
