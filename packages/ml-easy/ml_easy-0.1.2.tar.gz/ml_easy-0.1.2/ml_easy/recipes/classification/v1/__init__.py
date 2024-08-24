f8c9c471d50e1a4e4c8cac57ff483390ef57fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:34 +0000

    Add package ml-easy/0.1.2/ml_easy-0.1.2.tar.gz

---
+++ packages/ml-easy/ml_easy-0.1.2.tar.gz/ml_easy-0.1.2/ml_easy/recipes/classification/v1/__init__.py
@@ -0,0 +1,6 @@
+from ml_easy._recipes._classification.v1.config import (
+    ClassificationRecipeConfig as ConfigImpl,
+)
+from ml_easy._recipes._classification.v1.recipe import ClassificationRecipe as RecipeImpl
+
+__all__ = ['RecipeImpl', 'ConfigImpl']
