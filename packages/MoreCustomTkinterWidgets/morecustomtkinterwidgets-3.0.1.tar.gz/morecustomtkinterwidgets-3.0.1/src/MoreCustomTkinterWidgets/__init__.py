eb7964ca9806a824e8b1ec40ee128eeb6aac2c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:48 +0000

    Add package MoreCustomTkinterWidgets/3.0.1/morecustomtkinterwidgets-3.0.1.tar.gz

---
+++ packages/MoreCustomTkinterWidgets/morecustomtkinterwidgets-3.0.1.tar.gz/morecustomtkinterwidgets-3.0.1/src/MoreCustomTkinterWidgets/__init__.py
@@ -0,0 +1,11 @@
+from .AskValue import AskValue, askstring, askinteger, askfloat
+from .AskDialog import AskDialog, askyesno
+from .Message import Message, showinfo, showwarning, showerror
+from .FileExplorer import FileExplorer, Filedialog, askfile, askdir
+from .Selector import Selector
+from .SmoothFrame import SmoothFrame, get_coordinates_from_grid
+from .BetterCTkImage import BetterCTkImage
+from .AnimatedImage import AnimatedImage
+
+
+_version = "3.0.1"
