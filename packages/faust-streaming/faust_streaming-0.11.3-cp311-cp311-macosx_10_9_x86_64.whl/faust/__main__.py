8f8ec7f2f650c3baae4b5968164e03d5ce7465bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:03:52 +0000

    Add package faust-streaming/0.11.3/faust_streaming-0.11.3-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/faust-streaming/faust_streaming-0.11.3-cp311-cp311-macosx_10_9_x86_64.whl/faust/__main__.py
@@ -0,0 +1,6 @@
+"""Command-line entry point."""
+
+# pragma: no cover
+from faust.cli.faust import cli
+
+cli()
---
+++ packages/faust-streaming/faust_streaming-0.11.3-cp311-cp311-macosx_10_9_x86_64.whl/faust/sensors/__init__.py
@@ -0,0 +1,11 @@
+"""Sensors."""
+
+from .base import Sensor, SensorDelegate
+from .monitor import Monitor, TableState
+
+__all__ = [
+    "Monitor",
+    "Sensor",
+    "SensorDelegate",
+    "TableState",
+]
