b8fc94d06678a5db7bf2e64d7408faf694157aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:32 +0000

    Add package kb-bot.core/0.1.0/kb_bot_core-0.1.0.tar.gz

---
+++ packages/kb-bot.core/kb_bot_core-0.1.0.tar.gz/kb_bot_core-0.1.0/chunkers/semantic.py
@@ -0,0 +1,21 @@
+from llama_index.core import Document
+from llama_index.core.node_parser import (
+    SemanticSplitterNodeParser,
+)
+
+from chunkers import BaseChunker
+from embedding import OpenAIEmbedClient
+
+class SemanticChunker(BaseChunker):
+    def __init__(self, buffer_size=1, breakpoint_percentile_threshold=95):
+        embed_model = OpenAIEmbedClient()
+        self.node_parser = SemanticSplitterNodeParser(
+            buffer_size=buffer_size, breakpoint_percentile_threshold=breakpoint_percentile_threshold, embed_model=embed_model
+        )
+    
+    def get_chunks(self, text):
+        nodes = self.node_parser.get_nodes_from_documents([Document(text=text)])
+        chunks = []
+        for node in nodes:
+            chunks.append(node.get_content())
+        return chunks
---
+++ packages/kb-bot.core/kb_bot_core-0.1.0.tar.gz/kb_bot_core-0.1.0/scraper/__init__.py
@@ -0,0 +1,2 @@
+from scraper.file import FileScraper
+from scraper.web import WebScraper
