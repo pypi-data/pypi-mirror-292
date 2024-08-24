57769c4aa8be835dc61df43168c125a086a8f3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:35 +0000

    Add package lambdaext/1.0.1/lambdaext-1.0.1.tar.gz

---
+++ packages/lambdaext/lambdaext-1.0.1.tar.gz/lambdaext-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdaext",
+    version="1.0.1",
+    author="SoulCodingYanhun",
+    author_email="zhuzhishengzhu6@outlook.com",
+    description="A brief description of lambdaext",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SoulCodingYanhun/lambdaext",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
