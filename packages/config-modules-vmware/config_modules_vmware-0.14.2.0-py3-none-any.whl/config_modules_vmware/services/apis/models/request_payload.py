d44adf02ea50f53fc85b430c60ce97f5723d8e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:58 +0000

    Add package config-modules-vmware/0.14.2.0/config_modules_vmware-0.14.2.0-py3-none-any.whl

---
+++ packages/config-modules-vmware/config_modules_vmware-0.14.2.0-py3-none-any.whl/config_modules_vmware/services/apis/models/request_payload.py
@@ -0,0 +1,25 @@
+# Copyright 2024 Broadcom. All Rights Reserved.
+from pydantic import BaseModel
+from pydantic import Field
+
+from config_modules_vmware.services.apis.models.target_model import RequestTarget
+
+
+class GetSchema(BaseModel):
+    """Class to represent the request format of a get schema API call."""
+
+    target: RequestTarget = Field(description="The product target information.")
+
+
+class GetConfigurationRequest(BaseModel):
+    """Class to represent the request format of a get configuration API call."""
+
+    target: RequestTarget = Field(description="The product target information.")
+    template: dict = Field(default=None, description="Filter spec to filter, based on the product schema.")
+
+
+class ScanDriftsRequest(BaseModel):
+    """Class to represent the request format of a scan drifts API call."""
+
+    target: RequestTarget = Field(description="The product target information.")
+    input_spec: dict = Field(description="Desired state input spec, based on the product schema.")
