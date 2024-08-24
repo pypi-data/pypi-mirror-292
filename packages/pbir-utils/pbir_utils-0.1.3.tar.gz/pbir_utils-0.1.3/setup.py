f1e817c604426a7f737938d21e6254297513629d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:21 +0000

    Add package pbir-utils/0.1.3/pbir_utils-0.1.3.tar.gz

---
+++ packages/pbir-utils/pbir_utils-0.1.3.tar.gz/pbir_utils-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pbir-utils', 
+    version='0.1.3',
+    description='A tool for managing Power BI Enhanced Report Format (PBIR) projects',
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown',  
+    url='https://github.com/akhilannan/PBIR-Utils',  
+    author='Akhil Ashok',
+    license='MIT',  
+    packages=find_packages(where='src'), 
+    package_dir={'':'src'},
+    install_requires=[ 
+        'dash', 'plotly', 
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+    'console_scripts': [
+        'batch_update_pbir_project=src.pbir_processor:batch_update_pbir_project',
+        'export_pbir_metadata_to_csv=src.metadata_extractor:export_pbir_metadata_to_csv',
+        'display_report_wireframes=src.report_wireframe_visualizer:display_report_wireframes',
+        'disable_visual_interactions=src.visual_interactions_utils:disable_visual_interactions',
+        ],
+    },
+
+)
