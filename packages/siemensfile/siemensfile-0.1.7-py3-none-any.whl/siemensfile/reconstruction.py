1dba16a567c95e6af69745458c6106e7e4dbf1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:36 +0000

    Add package siemensfile/0.1.7/siemensfile-0.1.7-py3-none-any.whl

---
+++ packages/siemensfile/siemensfile-0.1.7-py3-none-any.whl/siemensfile/reconstruction.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from .utils import ifftnd, rms_comb
+
+def reconstruct_image(rawdata, method="Cartesiana"):
+    if method.lower() == "cartesiana":
+        fig, axs = plt.subplots(len(rawdata), 2, figsize=(10, 5*len(rawdata)))
+        for i, kspace in enumerate(rawdata):
+            # Espacio K
+            axs[i, 0].imshow(np.abs(kspace[:, 0])**0.2, cmap='gray')
+            axs[i, 0].set_title(f'Espacio K - Scan {i+1}')
+            
+            # Reconstrucción IFFT
+            image_ifft = ifftnd(kspace, [0, -1])
+            image_ifft = rms_comb(image_ifft)
+            axs[i, 1].imshow(np.abs(image_ifft), cmap='gray')
+            axs[i, 1].set_title(f'Reconstrucción IFFT - Scan {i+1}')
+
+        plt.tight_layout()
+        plt.show()
+    else:
+        print(f"Método de reconstrucción '{method}' no implementado.")
