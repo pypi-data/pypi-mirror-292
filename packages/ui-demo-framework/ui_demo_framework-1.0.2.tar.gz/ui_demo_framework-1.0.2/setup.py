91a0765e26d241d83cbe11d7e56021fa1df42d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:05:07 +0000

    Add package ui-demo-framework/1.0.2/ui_demo_framework-1.0.2.tar.gz

---
+++ packages/ui-demo-framework/ui_demo_framework-1.0.2.tar.gz/ui_demo_framework-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Reading the requirements.txt file
+requirements_path = this_directory / "requirements.txt"
+with open(requirements_path) as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='ui_demo_framework',
+    version='1.0.2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
+        '': ['*.ini', '*.txt', '*.md', 'pytest.ini'],
+    },
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
