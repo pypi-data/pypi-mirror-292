c63aafc8e850de42802d5a7a8cfa7ce7088a8ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:05:00 +0000

    Add package open-space-toolkit-astrodynamics/11.1.0/open_space_toolkit_astrodynamics-11.1.0-py38-none-manylinux2014_x86_64.whl

---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-11.1.0-py38-none-manylinux2014_x86_64.whl/ostk/astrodynamics/converters.py
@@ -0,0 +1,104 @@
+# Apache License 2.0
+
+from datetime import datetime, timezone
+
+from ostk.physics.time import Instant
+from ostk.physics.time import Interval
+from ostk.physics.time import Scale
+
+
+def coerce_to_datetime(value: Instant | datetime | str) -> datetime:
+    """
+    Return datetime from value.
+
+    Args:
+        value (Instant | datetime | str): A value to coerce.
+
+    Returns:
+        datetime: The coerced datetime.
+    """
+
+    if isinstance(value, datetime):
+        return value
+
+    if isinstance(value, Instant):
+        return value.get_date_time(Scale.UTC).replace(tzinfo=timezone.utc)
+
+    if isinstance(value, str):
+        if value.endswith("Z"):
+            value = value.replace("Z", "+00:00")
+
+        return datetime.fromisoformat(value)
+
+    raise TypeError("Argument must be a datetime, an Instant, or a str.")
+
+
+def coerce_to_instant(value: Instant | datetime | str) -> Instant:
+    """
+    Return Instant from value.
+
+    Args:
+        value (Instant | datetime | str): A value to coerce.
+
+    Returns:
+        Instant: The coerced Instant.
+    """
+
+    if isinstance(value, Instant):
+        return value
+
+    if isinstance(value, datetime):
+        return Instant.date_time(value.astimezone(tz=timezone.utc), Scale.UTC)
+
+    if isinstance(value, str):
+        return coerce_to_instant(coerce_to_datetime(value))
+
+    raise TypeError("Argument must be a datetime, an Instant, or a str.")
+
+
+def coerce_to_iso(value: Instant | datetime | str, timespec: str = "microseconds") -> str:
+    """
+    Return an ISO string from value.
+
+    Args:
+        value (Instant | datetime | str): A value to coerce.
+        timespec (str): A time resolution. Defaults to "microseconds".
+
+    Returns:
+        str: The coerced ISO string.
+    """
+
+    if isinstance(value, str):
+        return coerce_to_iso(coerce_to_datetime(value), timespec=timespec)
+
+    if isinstance(value, datetime):
+        return value.isoformat(timespec=timespec)
+
+    if isinstance(value, Instant):
+        return coerce_to_iso(coerce_to_datetime(value), timespec=timespec)
+
+    raise TypeError("Argument must be a datetime, an Instant, or a str.")
+
+
+def coerce_to_interval(
+    value: (
+        Interval | tuple[Instant, Instant] | tuple[datetime, datetime] | tuple[str, str]
+    ),
+) -> Interval:
+    """
+    Return Interval from value.
+
+    Args:
+        value (Interval | tuple[Instant, Instant] | tuple[datetime, datetime] | tuple[str, str]): A value to coerce.
+
+    Returns:
+        Interval: The coerced Interval.
+    """
+
+    if isinstance(value, Interval):
+        return value
+
+    return Interval.closed(
+        start_instant=coerce_to_instant(value[0]),
+        end_instant=coerce_to_instant(value[1]),
+    )
---
+++ packages/open-space-toolkit-astrodynamics/open_space_toolkit_astrodynamics-11.1.0-py38-none-manylinux2014_x86_64.whl/ostk/astrodynamics/utilities.py
@@ -0,0 +1,158 @@
+# Apache License 2.0
+
+from .OpenSpaceToolkitAstrodynamicsPy import *
+
+from ostk.physics import Environment
+from ostk.physics.time import Scale
+from ostk.physics.time import Instant
+from ostk.physics.time import Duration
+from ostk.physics.time import Interval
+from ostk.physics.coordinate.spherical import LLA
+from ostk.physics.coordinate.spherical import AER
+from ostk.physics.coordinate import Position
+from ostk.physics.coordinate import Frame
+from ostk.physics.environment.object.celestial import Earth
+from ostk.physics.environment.gravitational import Earth as EarthGravitationalModel
+
+
+def lla_from_state(state: trajectory.State) -> tuple[float, float, float]:
+    """
+    Return latitude (degrees), longitude (degrees), altitude (meters) float list from a state.
+    """
+
+    lla: LLA = lla_from_position(state.get_position(), state.get_instant())
+
+    return (
+        float(lla.get_latitude().in_degrees()),
+        float(lla.get_longitude().in_degrees()),
+        float(lla.get_altitude().in_meters()),
+    )
+
+
+def lla_from_position(
+    position: Position,
+    instant: Instant | None = None,
+) -> LLA:
+    """
+    Return LLA from position and instant.
+    """
+
+    if position.access_frame() != Frame.ITRF():
+        if instant is None:
+            raise ValueError(
+                "Instant must be provided if position is not expressed in ECEF."
+            )
+        position: Position = position.in_frame(Frame.ITRF(), instant)
+
+    return LLA.cartesian(
+        position.get_coordinates(),
+        EarthGravitationalModel.EGM2008.equatorial_radius,
+        EarthGravitationalModel.EGM2008.flattening,
+    )
+
+
+def position_from_lla(lla: LLA) -> Position:
+    """
+    Return position from lla.
+    """
+
+    return Position.meters(
+        lla.to_cartesian(
+            EarthGravitationalModel.EGM2008.equatorial_radius,
+            EarthGravitationalModel.EGM2008.flattening,
+        ),
+        Frame.ITRF(),
+    )
+
+
+def compute_aer(
+    instant: Instant,
+    from_position: Position,
+    to_position: Position,
+    environment: Environment,
+) -> tuple[float, float, float]:
+    """
+    Return [azimuth (degrees), elevation (degrees), range (meters)] from Instant and Positions (observer, target).
+    """
+
+    from_lla: LLA = lla_from_position(from_position, instant)
+
+    earth: Earth = environment.access_celestial_object_with_name("Earth")
+    ned_frame: Frame = earth.get_frame_at(from_lla, Earth.FrameType.NED)
+
+    from_position_NED: Position = from_position.in_frame(ned_frame, instant)
+    to_position_NED: Position = to_position.in_frame(ned_frame, instant)
+
+    aer: AER = AER.from_position_to_position(from_position_NED, to_position_NED, True)
+
+    return (
+        float(aer.get_azimuth().in_degrees()),
+        float(aer.get_elevation().in_degrees()),
+        float(aer.get_range().in_meters()),
+    )
+
+
+def compute_time_lla_aer_state(
+    state: trajectory.State,
+    from_position: Position,
+    environment: Environment,
+) -> tuple[Instant, float, float, float, float, float, float]:
+    """
+    Return [instant, latitude, longitude, altitude, azimuth, elevation, range] from State and observer Position.
+    """
+
+    instant: Instant = state.get_instant()
+
+    lla: tuple[float, float, float] = lla_from_state(state)
+    aer: AER = compute_aer(
+        instant,
+        from_position,
+        state.get_position().in_frame(Frame.ITRF(), state.get_instant()),
+        environment,
+    )
+
+    return (instant, lla[0], lla[1], lla[2], aer[0], aer[1], aer[2])
+
+
+def compute_trajectory_geometry(
+    trajectory: Trajectory,
+    interval: Interval,
+) -> list[tuple[float, float, float]]:
+    """
+    Return [latitude (degrees), longitude (degrees), altitude (meters)] values along a Trajectory during Interval.
+    """
+
+    return [
+        lla_from_state(state)
+        for state in trajectory.get_states_at(
+            interval.generate_grid(Duration.minutes(1.0))
+        )
+    ]
+
+
+def convert_state(
+    state: trajectory.State,
+) -> tuple[str, float, float, float, float, float, float, float, float, float]:
+    """
+    Convert a State into dataframe-ready values.
+    """
+
+    lla: LLA = LLA.cartesian(
+        state.get_position()
+        .in_frame(Frame.ITRF(), state.get_instant())
+        .get_coordinates(),
+        EarthGravitationalModel.EGM2008.equatorial_radius,
+        EarthGravitationalModel.EGM2008.flattening,
+    )
+
+    instant: Instant = state.get_instant()
+
+    return (
+        repr(instant),
+        float(instant.get_modified_julian_date(Scale.UTC)),
+        *state.get_position().get_coordinates().transpose().tolist(),
+        *state.get_velocity().get_coordinates().transpose().tolist(),
+        float(lla.get_latitude().in_degrees()),
+        float(lla.get_longitude().in_degrees()),
+        float(lla.get_altitude().in_meters()),
+    )
