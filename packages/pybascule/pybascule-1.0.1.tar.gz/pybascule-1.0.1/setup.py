aac39729f5953969b26a37b7662420f8682dce17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:24 +0000

    Add package pybascule/1.0.1/pybascule-1.0.1.tar.gz

---
+++ packages/pybascule/pybascule-1.0.1.tar.gz/pybascule-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "pybascule",
+    version = "1.0.1",
+    description = "Bayesian NMF signatures deconvolution and DP clustering.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/caravagnalab/pybascule",
+    packages = ["pybascule"],
+    python_requires = ">=3.8",
+    install_requires = [
+        "pandas>=1.4.2",
+        "pyro-api==0.1.2",
+        "pyro-ppl==1.8.0",
+        "numpy==1.*",
+        "torch==1.*",
+        "tqdm",
+        "statsmodels",
+        "scikit-learn",
+        "scipy"
+        ],
+)
