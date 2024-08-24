7ac68fd2072e89e76316caf4484126b43d8d0161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:04:34 +0000

    Add package Social-LIWC/1.1.4/Social_LIWC-1.1.4.tar.gz

---
+++ packages/Social-LIWC/Social_LIWC-1.1.4.tar.gz/Social_LIWC-1.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='Social_LIWC',
+    version='1.1.4',
+    license='MIT License',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=['fastapi', 'liwc', 'pandas'],
+    packages=['Social_LIWC/api'],
+    include_package_data=True,
+    package_data={
+        'Social_LIWC': ['dados/*'],
+    }
+)
