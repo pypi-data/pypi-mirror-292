37f96bc978040c0db5c2e000f53c1fae5c433d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:41 +0000

    Add package swarmauri/0.4.1/swarmauri-0.4.1-py3-none-any.whl

---
+++ packages/swarmauri/swarmauri-0.4.1-py3-none-any.whl/swarmauri/standard/tools/concrete/AdditionTool.py
@@ -0,0 +1,39 @@
+from typing import List, Literal
+from pydantic import Field
+from swarmauri.standard.tools.base.ToolBase import ToolBase 
+from swarmauri.standard.tools.concrete.Parameter import Parameter
+
+class AdditionTool(ToolBase):
+    version: str = "0.0.1"
+    parameters: List[Parameter] = Field(default_factory=lambda: [
+            Parameter(
+                name="x",
+                type="integer",
+                description="The left operand",
+                required=True
+            ),
+            Parameter(
+                name="y",
+                type="integer",
+                description="The right operand",
+                required=True
+            )
+        ])
+
+    name: str = 'AdditionTool'
+    description: str = "This tool has two numbers together"
+    type: Literal['AdditionTool'] = 'AdditionTool'
+
+
+    def __call__(self, x: int, y: int) -> int:
+        """
+        Add two numbers x and y and return the sum.
+
+        Parameters:
+        - x (int): The first number.
+        - y (int): The second number.
+
+        Returns:
+        - str: The sum of x and y.
+        """
+        return str(x + y)
---
+++ packages/swarmauri/swarmauri-0.4.1-py3-none-any.whl/swarmauri/standard/tools/concrete/WeatherTool.py
@@ -0,0 +1,31 @@
+from typing import List, Literal
+from pydantic import Field
+from swarmauri.standard.tools.base.ToolBase import ToolBase 
+from swarmauri.standard.tools.concrete.Parameter import Parameter 
+
+class WeatherTool(ToolBase):
+    version: str = "0.1.dev1"
+    parameters: List[Parameter] = Field(default_factory=lambda: [
+        Parameter(
+            name="location",
+            type="string",
+            description="The location for which to fetch weather information",
+            required=True
+        ),
+        Parameter(
+            name="unit",
+            type="string",
+            description="The unit for temperature ('fahrenheit' or 'celsius')",
+            required=True,
+            enum=["fahrenheit", "celsius"]
+        )
+    ])
+    name: str = 'WeatherTool'
+    description: str = "Fetch current weather info for a location"
+    type: Literal['WeatherTool'] = 'WeatherTool'
+
+    def __call__(self, location, unit="fahrenheit") -> str:
+        weather_info = (location, unit)
+        # Here you would implement the actual logic for fetching the weather information.
+        # For demonstration, let's just return the parameters as a string.
+        return f"Weather Info: {weather_info}\n"
