6204742530b6a5c841d5f4578cbdc2b73845a3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:12 +0000

    Add package zarr/3.0.0a1/zarr-3.0.0a1.tar.gz

---
+++ packages/zarr/zarr-3.0.0a1.tar.gz/zarr-3.0.0a1/src/zarr/testing/utils.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from zarr.core.buffer import Buffer
+from zarr.core.common import BytesLike
+
+__all__ = ["assert_bytes_equal"]
+
+
+def assert_bytes_equal(b1: Buffer | BytesLike | None, b2: Buffer | BytesLike | None) -> None:
+    """Help function to assert if two bytes-like or Buffers are equal
+
+    Warnings
+    --------
+    Always copies data, only use for testing and debugging
+    """
+    if isinstance(b1, Buffer):
+        b1 = b1.to_bytes()
+    if isinstance(b2, Buffer):
+        b2 = b2.to_bytes()
+    assert b1 == b2
