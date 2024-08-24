aedb0dc14a45bbec3819b7b71da2e3dec1b4a798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:05 +0000

    Add package meta-cleaner/0.2.0/meta_cleaner-0.2.0.tar.gz

---
+++ packages/meta-cleaner/meta_cleaner-0.2.0.tar.gz/meta_cleaner-0.2.0/meta_cleaner/cleaner.py
@@ -0,0 +1,56 @@
+from .model import NERModel
+
+
+class TextCleaner:
+    def __init__(self, model_name_or_path, confidence_threshold=0.25, max_length=512, stride=256, device=None):
+        self.model = NERModel(model_name_or_path, device=device)
+        self.confidence_threshold = confidence_threshold
+        self.max_length = max_length
+        self.stride = stride
+
+    def clean_text(self, text):
+        full_tokens = self.model.tokenizer.tokenize(text)
+
+        if len(full_tokens) <= self.max_length:
+            return self._clean_single_chunk(text)
+        else:
+            return self._clean_long_text(full_tokens)
+
+    def _clean_single_chunk(self, text):
+        predictions = self.model.predict(text)
+        cleaned_tokens = []
+        skip_space = False
+
+        for token, pred, conf in predictions:
+            if pred in [1, 2] and conf >= self.confidence_threshold:
+                skip_space = True
+                continue
+
+            if token.startswith("▁"):
+                if not skip_space:
+                    cleaned_tokens.append(" ")
+                cleaned_tokens.append(token.lstrip("▁"))
+            else:
+                cleaned_tokens.append(token)
+
+            skip_space = False
+
+        cleaned_text = "".join(cleaned_tokens).strip()
+        cleaned_text = cleaned_text.replace("<s>", "").replace("</s>", "").strip()
+
+        return cleaned_text
+
+    def _clean_long_text(self, full_tokens):
+        cleaned_text = []
+
+        for i in range(0, len(full_tokens), self.stride):
+            chunk_tokens = full_tokens[i:i + self.max_length]
+            chunk_text = self.model.tokenizer.convert_tokens_to_string(chunk_tokens)
+
+            cleaned_chunk = self._clean_single_chunk(chunk_text)
+
+            cleaned_text.append(cleaned_chunk)
+
+        final_cleaned_text = " ".join(cleaned_text).strip()
+
+        return final_cleaned_text
---
+++ packages/meta-cleaner/meta_cleaner-0.2.0.tar.gz/meta_cleaner-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meta_cleaner',
+    version='0.2.0',
+    description='A Python package to clean text from META tags using a BERT NER model.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tim Isbister',
+    author_email='tim.isbister@pirr.me',
+    url='https://github.com/pirr-me/meta_cleaner',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
