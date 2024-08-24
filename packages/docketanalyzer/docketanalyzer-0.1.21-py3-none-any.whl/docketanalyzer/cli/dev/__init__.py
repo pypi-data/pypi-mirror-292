1f825055e8cda11530fd78cede5091e68cc42262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:05:07 +0000

    Add package docketanalyzer/0.1.21/docketanalyzer-0.1.21-py3-none-any.whl

---
+++ packages/docketanalyzer/docketanalyzer-0.1.21-py3-none-any.whl/docketanalyzer/cli/dev/__init__.py
@@ -0,0 +1,12 @@
+import click
+from .build_extension import build_extension
+from .install_extension import install_extension
+
+
+@click.group('dev')
+def dev_cli():
+    pass
+
+
+dev_cli.add_command(build_extension)
+dev_cli.add_command(install_extension)
