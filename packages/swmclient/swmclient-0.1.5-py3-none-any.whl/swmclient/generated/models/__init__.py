adfb030905f3d4b0847810c967071162e9f5548c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:05:11 +0000

    Add package swmclient/0.1.5/swmclient-0.1.5-py3-none-any.whl

---
+++ packages/swmclient/swmclient-0.1.5-py3-none-any.whl/swmclient/generated/models/__init__.py
@@ -0,0 +1,25 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .flavor import Flavor
+from .job import Job
+from .job_state import JobState
+from .node import Node
+from .node_role import NodeRole
+from .node_state_alloc import NodeStateAlloc
+from .node_state_power import NodeStatePower
+from .post_user_job_multipart_data import PostUserJobMultipartData
+from .remote_site import RemoteSite
+from .resource import Resource
+
+__all__ = (
+    "Flavor",
+    "Job",
+    "JobState",
+    "Node",
+    "NodeRole",
+    "NodeStateAlloc",
+    "NodeStatePower",
+    "PostUserJobMultipartData",
+    "RemoteSite",
+    "Resource",
+)
