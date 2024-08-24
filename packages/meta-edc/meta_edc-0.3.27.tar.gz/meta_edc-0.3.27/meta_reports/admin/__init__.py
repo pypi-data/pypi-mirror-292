97b4f885ecd4f4c9763b02aeed64ca2931c9e83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:05:02 +0000

    Add package meta-edc/0.3.27/meta_edc-0.3.27.tar.gz

---
+++ packages/meta-edc/meta_edc-0.3.27.tar.gz/meta_edc-0.3.27/meta_reports/admin/__init__.py
@@ -0,0 +1,11 @@
+from .endpoints_admin import EndpointsAdmin
+from .endpoints_all_admin import EndpointsAllAdmin
+from .unmanaged import (
+    GlucoseSummaryAdmin,
+    MissingOgttNoteModelAdmin,
+    MissingScreeningOgttAdmin,
+    PatientHistoryMissingBaselineCd4Admin,
+    UnattendedThreeInRow2Admin,
+    UnattendedThreeInRowAdmin,
+    UnattendedTwoInRowAdmin,
+)
