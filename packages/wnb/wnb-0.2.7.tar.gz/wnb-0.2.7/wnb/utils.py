0cb8d8fae30f38ee49390a7766becefacfe387c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 14:05:02 +0000

    Add package wnb/0.2.7/wnb-0.2.7.tar.gz

---
+++ packages/wnb/wnb-0.2.7.tar.gz/wnb-0.2.7/wnb/utils.py
@@ -0,0 +1,38 @@
+import contextlib
+from typing import Optional
+
+from ._typing import DistibutionLike
+from .base import DistMixin
+from .dist import AllDistributions
+from .enums import Distribution
+
+__all__ = ["is_dist_supported", "get_dist_class"]
+
+
+def is_dist_supported(dist: DistibutionLike) -> bool:
+    with contextlib.suppress(TypeError):
+        return issubclass(dist, DistMixin)
+
+    if (
+        isinstance(dist, Distribution)
+        or dist in Distribution.__members__.values()
+        or all(hasattr(dist, attr_name) for attr_name in ["from_data", "support", "__call__"])
+    ):
+        return True
+
+    return False
+
+
+def get_dist_class(name_or_type: DistibutionLike) -> Optional[DistMixin]:
+    with contextlib.suppress(TypeError):
+        if issubclass(name_or_type, DistMixin):
+            return name_or_type
+
+    if isinstance(name_or_type, Distribution) or name_or_type in Distribution.__members__.values():
+        return AllDistributions[name_or_type]
+    elif isinstance(name_or_type, str) and name_or_type.upper() in [d.name for d in Distribution]:
+        return AllDistributions[Distribution.__members__[name_or_type.upper()]]
+    elif isinstance(name_or_type, str) and name_or_type.title() in [d.value for d in Distribution]:
+        return AllDistributions[Distribution(name_or_type.title())]
+    else:
+        return
