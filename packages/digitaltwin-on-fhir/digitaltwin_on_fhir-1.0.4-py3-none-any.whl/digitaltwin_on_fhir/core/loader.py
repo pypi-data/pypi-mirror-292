669ec123dbe6542ed97641d1e11294a02d63732d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:05:06 +0000

    Add package digitaltwin-on-fhir/1.0.4/digitaltwin_on_fhir-1.0.4-py3-none-any.whl

---
+++ packages/digitaltwin-on-fhir/digitaltwin_on_fhir-1.0.4-py3-none-any.whl/digitaltwin_on_fhir/core/loader.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+import sys
+from pathlib import Path
+import json
+from .digital_twins import Measurements
+
+
+class AbstractLoader(ABC):
+    core = None
+
+    def __init__(self, core, operator):
+        self.core = core
+        self.operator = operator
+
+    @abstractmethod
+    def load_fhir_bundle(self, dataset_path):
+        pass
+
+    @abstractmethod
+    def load_sparc_dataset_primary_measurements(self):
+        pass
+
+
+class Loader(AbstractLoader, ABC):
+    measurements_class = Measurements
+
+    def __init__(self, core, operator):
+        super().__init__(core, operator)
+
+    def load_sparc_dataset_primary_measurements(self):
+        return self.measurements_class(self.operator)
+
+    async def load_fhir_bundle(self, dataset_path):
+        sys.stdout.write("Import progress: 0%   \r")
+
+        dataset_root = Path(dataset_path)
+
+        filenames = [
+            filename.name for filename in dataset_root.iterdir()
+            if filename.name.endswith('.json')]
+
+        total_count = len(filenames)
+        for index, filename in enumerate(filenames):
+            await self._import_bundle(dataset_root / filename)
+            progress = int(float(index + 1) / float(total_count) * 100)
+            sys.stdout.write("Import progress: %d%%   \r" % progress)
+            sys.stdout.flush()
+        sys.stdout.write("Import progress: 100%\n")
+        sys.stdout.write("{0} bundles imported".format(total_count))
+
+    async def _import_bundle(self, filename):
+        with open(filename, encoding='utf-8') as fd:
+            patient_json = json.load(fd)
+        bundle = self.core.async_client.resource('Bundle')
+        bundle['type'] = 'transaction'
+        bundle['entry'] = patient_json['entry']
+        await bundle.save()
