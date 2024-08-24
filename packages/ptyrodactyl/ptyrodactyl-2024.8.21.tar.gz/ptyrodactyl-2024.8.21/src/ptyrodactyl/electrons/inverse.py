a619d774d37ded3a9be3fad7f49a9c55351b40fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:04 +0000

    Add package ptyrodactyl/2024.8.21/ptyrodactyl-2024.8.21.tar.gz

---
+++ packages/ptyrodactyl/ptyrodactyl-2024.8.21.tar.gz/ptyrodactyl-2024.8.21/src/ptyrodactyl/electrons/inverse.py
@@ -0,0 +1,8 @@
+from typing import Any, NamedTuple
+
+import jax
+import jax.numpy as jnp
+from jax import Array
+from jaxtyping import Complex, Float, Int, Shaped
+
+import ptyrodactyl.electrons as pte
