1ef4a5a9b9a17f6b4f51dfc7713192a2c7b2863b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:34 +0000

    Add package spkit/0.0.9.7/spkit-0.0.9.7-py3-none-any.whl

---
+++ packages/spkit/spkit-0.0.9.7-py3-none-any.whl/spkit/ml/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function
+
+name = "Signal Processing toolkit | ML"
+
+__version__ = '0.0.9'
+__author__ = 'Nikesh Bajaj'
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+
+#from .DeepNet import DeepNet
+from .Logistic import LR, LogisticRegression
+from .Probabilistic import NaiveBayes
+from .Trees import ClassificationTree, RegressionTree, DecisionTree
+
+__all__ = [ 'LR','LogisticRegression','NaiveBayes', 'ClassificationTree', 'RegressionTree']
