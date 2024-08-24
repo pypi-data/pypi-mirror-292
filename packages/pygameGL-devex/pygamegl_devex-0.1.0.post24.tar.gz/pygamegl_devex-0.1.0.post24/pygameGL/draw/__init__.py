09804251bdc87f459a8d4de52cb975af104fc6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 03:04:40 +0000

    Add package pygameGL-devex/0.1.0.post24/pygamegl_devex-0.1.0.post24.tar.gz

---
+++ packages/pygameGL-devex/pygamegl_devex-0.1.0.post24.tar.gz/pygamegl_devex-0.1.0.post24/pygameGL/draw/__init__.py
@@ -0,0 +1,3 @@
+SOGL=None
+
+
---
+++ packages/pygameGL-devex/pygamegl_devex-0.1.0.post24.tar.gz/pygamegl_devex-0.1.0.post24/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygameGL-devex',
+    version='0.1.0-24',
+    description='A packaged version of the pygameGL devex repository branch containing developmental and experimental features.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Izaiyah Stokes',
+    author_email='zeroth.bat@gmail.com',
+    url='https://github.com/stozak/pygameGL',
+    packages=find_packages(),
+    package_data={"pygameGL": ['assets/*', 'ext/SoGL/*']},
+    install_requires=[
+        "ipdb",
+        "GLFW",
+        "Numba",
+        "Numpy",
+        "PyGLM",
+        "PyOpenGL",
+        "ModernGL",
+        "objgraph",
+        "Swarm-ECS",
+        "memory_profiler"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+# setup(
+#     name='pygameGL',
+#     version='0.1.0',
+#     description='',
+#     long_description=open('README.md').read(),
+#     long_description_content_type='text/markdown',
+#     author='Izaiyah Stokes',
+#     author_email='zeroth.bat@gmail.com',
+#     url='https://github.com/stozak/pygameGL',
+#     packages=find_packages(),
+#     package_data={"pygameGL": ['assets/*']},
+#     install_requires=[
+#         "ipdb",
+#         "GLFW",
+#         "Numba",
+#         "Numpy",
+#         "PyGLM",
+#         "PyOpenGL",
+#         "ModernGL",
+#         "objgraph",
+#         "Swarm-ECS",
+#         "memory_profiler"
+#     ],
+#     classifiers=[
+#         'Programming Language :: Python :: 3.12',
+#         'License :: OSI Approved :: MIT License',
+#         'Operating System :: OS Independent',
+#     ],
+# )
+
