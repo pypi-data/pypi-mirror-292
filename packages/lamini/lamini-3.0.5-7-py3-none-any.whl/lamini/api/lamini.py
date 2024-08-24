77be0d3dc4b81dd2f3b574e502e3b5621d43c73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:46 +0000

    Add package lamini/3.0.5/lamini-3.0.5-7-py3-none-any.whl

---
+++ packages/lamini/lamini-3.0.5-7-py3-none-any.whl/lamini/api/lamini.py
@@ -0,0 +1,293 @@
+import json
+import jsonlines
+import logging
+import os
+import pandas as pd
+import time
+
+from lamini.api.lamini_config import get_config
+from lamini.api.rest_requests import get_version
+from lamini.api.train import Train
+from lamini.api.utils.completion import Completion
+from lamini.api.utils.upload_client import upload_to_blob
+from lamini.error.error import (
+    DownloadingModelError,
+)
+from typing import Dict, Iterable, List, Optional, Union
+
+logger = logging.getLogger(__name__)
+
+
+class Lamini:
+    def __init__(
+        self,
+        model_name: str,
+        api_key: Optional[str] = None,
+        api_url: Optional[str] = None,
+    ):
+        self.config = get_config()
+        self.model_name = model_name
+        self.api_key = api_key
+        self.api_url = api_url
+        self.completion = Completion(api_key, api_url)
+        self.trainer = Train(api_key, api_url)
+        self.upload_file_path = None
+        self.upload_base_path = None
+
+    def version(self):
+        return get_version(self.api_key, self.api_url, self.config)
+
+    def generate(
+        self,
+        prompt: Union[str, List[str]],
+        model_name: Optional[str] = None,
+        output_type: Optional[dict] = None,
+        max_tokens: Optional[int] = None,
+        max_new_tokens: Optional[int] = None,
+    ):
+        result = None
+        try:
+            result = self.completion.generate(
+                prompt=prompt,
+                model_name=model_name or self.model_name,
+                output_type=output_type,
+                max_tokens=max_tokens,
+                max_new_tokens=max_new_tokens,
+            )
+        except DownloadingModelError as e:
+            return e
+        if output_type is None:
+            if isinstance(prompt, list):
+                result = [single_result["output"] for single_result in result]
+            else:
+                result = result["output"]
+        return result
+
+    async def async_generate(
+        self,
+        prompt: Union[str, List[str]],
+        model_name: Optional[str] = None,
+        output_type: Optional[dict] = None,
+        max_tokens: Optional[int] = None,
+        max_new_tokens: Optional[int] = None,
+    ):
+        req_data = self.completion.make_llm_req_map(
+            prompt=prompt,
+            model_name=model_name or self.model_name,
+            output_type=output_type,
+            max_tokens=max_tokens,
+            max_new_tokens=max_new_tokens,
+        )
+        result = await self.completion.async_generate(req_data)
+        if output_type is None:
+            if isinstance(prompt, list):
+                result = [single_result["output"] for single_result in result]
+            else:
+                result = result["output"]
+        return result
+
+    def upload_data(
+        self,
+        data: Iterable[Dict[str, Union[int, float, str, bool, Dict, List]]],
+        is_public: Optional[bool] = None,
+    ):
+        num_datapoints = 0
+
+        def get_data_str(d):
+            nonlocal num_datapoints
+            for item in d:
+                num_datapoints += 1
+                yield json.dumps(item) + "\n"
+
+        if not data:
+            raise ValueError("Data pairs cannot be empty.")
+
+        output = self.trainer.get_upload_base_path()
+        self.upload_base_path = output["upload_base_path"]
+
+        try:
+            if self.upload_base_path == "azure":
+                data_str = get_data_str(data)
+                response = self.trainer.create_blob_dataset_location(
+                    self.upload_base_path, is_public
+                )
+                self.upload_file_path = response["dataset_location"]
+                upload_to_blob(data_str, self.upload_file_path)
+                self.trainer.update_blob_dataset_num_datapoints(
+                    response["dataset_id"], num_datapoints
+                )
+                print("Data pairs uploaded to blob.")
+            else:
+                response = self.trainer.upload_dataset_locally(
+                    self.upload_base_path, is_public, data
+                )
+                self.upload_file_path = response["dataset_location"]
+                print("Data pairs uploaded to local.")
+
+            print(
+                f"\nYour dataset id is: {response['dataset_id']} . Consider using this in the future to train using the same data. \nEg: "
+                f"llm.train(data_or_dataset_id='{response['dataset_id']}')"
+            )
+
+        except Exception as e:
+            print(f"Error uploading data pairs: {e}")
+            raise e
+
+        return response["dataset_id"]
+
+    def upload_file(
+        self, file_path: str, input_key: str = "input", output_key: str = "output"
+    ):
+        items = self._upload_file_impl(file_path, input_key, output_key)
+        try:
+            dataset_id = self.upload_data(items)
+            return dataset_id
+        except Exception as e:
+            print(f"Error reading data file: {e}")
+            raise e
+
+    def _upload_file_impl(
+        self, file_path: str, input_key: str = "input", output_key: str = "output"
+    ):
+        if os.path.getsize(file_path) > 1e10:
+            raise Exception("File size is too large, please upload file less than 10GB")
+
+        # Convert file records to appropriate format before uploading file
+        items = []
+        if file_path.endswith(".jsonl") or file_path.endswith(".jsonlines"):
+            with open(file_path) as dataset_file:
+
+                for row in jsonlines.Reader(dataset_file):
+                    yield {"input": row[input_key], "output": row.get(output_key, "")}
+
+        elif file_path.endswith(".csv"):
+            df = pd.read_csv(file_path).fillna("")
+            data_keys = df.columns
+            if input_key not in data_keys:
+                raise ValueError(
+                    f"File must have input_key={input_key} as a column (and optionally output_key={output_key}). You "
+                    "can pass in different input_key and output_keys."
+                )
+
+            try:
+                for _, row in df.iterrows():
+                    yield {
+                        "input": row[input_key],
+                        "output": row.get(output_key, ""),
+                    }
+            except KeyError:
+                raise ValueError("Each object must have 'input' and 'output' as keys")
+
+        else:
+            raise Exception(
+                "Upload of only csv and jsonlines file supported at the moment."
+            )
+        return items
+
+    def train(
+        self,
+        data_or_dataset_id: Union[
+            str, Iterable[Dict[str, Union[int, float, str, bool, Dict, List]]]
+        ],
+        finetune_args: Optional[dict] = None,
+        gpu_config: Optional[dict] = None,
+        is_public: Optional[bool] = None,
+        **kwargs,
+    ):
+        if isinstance(data_or_dataset_id, str):
+            dataset_id = data_or_dataset_id
+        else:
+            dataset_id = self.upload_data(data_or_dataset_id, is_public=is_public)
+        assert dataset_id is not None
+        base_path = self.trainer.get_upload_base_path()
+        self.upload_base_path = base_path["upload_base_path"]
+        existing_dataset = self.trainer.get_existing_dataset(
+            dataset_id, self.upload_base_path
+        )
+        self.upload_file_path = existing_dataset["dataset_location"]
+
+        job = self.trainer.train(
+            model_name=self.model_name,
+            dataset_id=dataset_id,
+            upload_file_path=self.upload_file_path,
+            finetune_args=finetune_args,
+            gpu_config=gpu_config,
+            is_public=is_public,
+        )
+        job["dataset_id"] = dataset_id
+        return job
+
+    # Add alias for tune
+    tune = train
+
+    # continuously poll until the job is completed
+    def train_and_wait(
+        self,
+        data_or_dataset_id: Union[
+            str, Iterable[Dict[str, Union[int, float, str, bool, Dict, List]]]
+        ],
+        finetune_args: Optional[dict] = None,
+        gpu_config: Optional[dict] = None,
+        is_public: Optional[bool] = None,
+        **kwargs,
+    ):
+        job = self.train(
+            data_or_dataset_id,
+            finetune_args=finetune_args,
+            gpu_config=gpu_config,
+            is_public=is_public,
+        )
+
+        try:
+            status = self.check_job_status(job["job_id"])
+            if status["status"] == "FAILED":
+                print(f"Job failed: {status}")
+                return status
+
+            while status["status"] not in (
+                "COMPLETED",
+                "PARTIALLY COMPLETED",
+                "FAILED",
+                "CANCELLED",
+            ):
+                if kwargs.get("verbose", False):
+                    print(f"job not done. waiting... {status}")
+                time.sleep(30)
+                status = self.check_job_status(job["job_id"])
+                if status["status"] == "FAILED":
+                    print(f"Job failed: {status}")
+                    return status
+                elif status["status"] == "CANCELLED":
+                    print(f"Job canceled: {status}")
+                    return status
+            print(
+                f"Finetuning process completed, model name is: {status['model_name']}"
+            )
+        except KeyboardInterrupt as e:
+            print("Cancelling job")
+            return self.cancel_job(job["job_id"])
+
+        return status
+
+    # Add alias for tune
+    tune_and_wait = train_and_wait
+
+    def cancel_job(self, job_id=None):
+        return self.trainer.cancel_job(job_id)
+
+    def cancel_all_jobs(
+        self,
+    ):
+        return self.trainer.cancel_all_jobs()
+
+    def resume_job(self, job_id=None):
+        return self.trainer.resume_job(job_id)
+
+    def check_job_status(self, job_id=None):
+        return self.trainer.check_job_status(job_id)
+
+    def get_jobs(self):
+        return self.trainer.get_jobs()
+
+    def evaluate(self, job_id=None):
+        return self.trainer.evaluate(job_id)
---
+++ packages/lamini/lamini-3.0.5-7-py3-none-any.whl/lamini/api/rest_requests.py
@@ -0,0 +1,226 @@
+import asyncio
+import importlib.metadata
+import logging
+
+import aiohttp
+import requests
+from lamini.api.lamini_config import get_config, get_configured_key, get_configured_url
+from lamini.error.error import (
+    APIError,
+    APIUnprocessableContentError,
+    AuthenticationError,
+    DownloadingModelError,
+    ModelNotFound,
+    RateLimitError,
+    RequestTimeoutError,
+    UnavailableResourceError,
+    UserError,
+)
+
+logger = logging.getLogger(__name__)
+
+warn_once = False
+
+
+def get_version(key, url, config):
+    api_key = key or get_configured_key(config)
+    api_url = url or get_configured_url(config)
+    return make_web_request(api_key, api_url + "/v1/version", "get", None)
+
+
+def check_version(resp):
+    global warn_once
+    if not warn_once:
+        if resp.headers is not None and "X-Warning" in resp.headers:
+            warn_once = True
+            print(resp.headers["X-Warning"])
+
+
+async def make_async_web_request(client, key, url, http_method, json=None):
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + key,
+        }
+    except:
+        raise AuthenticationError("Missing API Key")
+    try:
+        version = importlib.metadata.version("lamini")
+        headers["Lamini-Version"] = version
+    except:
+        pass
+    assert http_method == "post" or http_method == "get"
+    logger.debug(f"Making {http_method} request to {url} with payload {json}")
+    try:
+        if http_method == "post":
+            async with client.post(
+                url,
+                headers=headers,
+                json=json,
+            ) as resp:
+                check_version(resp)
+                if resp.status == 200:
+                    json_response = await resp.json()
+                    logger.debug("api response: " + str(json_response))
+                else:
+                    await handle_error(resp)
+        elif http_method == "get":
+            async with client.get(url, headers=headers) as resp:
+                check_version(resp)
+                if resp.status == 200:
+                    json_response = await resp.json()
+                else:
+                    handle_error(resp)
+    except asyncio.TimeoutError:
+        raise APIError(
+            "Request Timeout: The server did not respond in time.",
+        )
+
+    return json_response
+
+
+async def handle_error(resp: aiohttp.ClientResponse):
+    if resp.status == 594:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise ModelNotFound(json_response.get("detail", "ModelNotFound"))
+    if resp.status == 429:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise RateLimitError(json_response.get("detail", "RateLimitError"))
+    if resp.status == 401:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise AuthenticationError(json_response.get("detail", "AuthenticationError"))
+    if resp.status == 400:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise UserError(json_response.get("detail", "UserError"))
+    if resp.status == 422:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise APIUnprocessableContentError(
+            "The API has returned a 422 Error. This typically happens when the python package is outdated. Please consider updating the lamini python package version with `pip install --upgrade --force-reinstall lamini`"
+        )
+    if resp.status == 503:
+        try:
+            json_response = await resp.json()
+        except Exception:
+            json_response = {}
+        raise UnavailableResourceError(
+            json_response.get("detail", "UnavailableResourceError")
+        )
+    if resp.status != 200:
+        try:
+            description = await resp.json()
+        except BaseException:
+            description = resp.status
+        finally:
+            if description == {"detail": ""}:
+                raise APIError("500 Internal Server Error")
+            raise APIError(f"API error {description}")
+
+
+def make_web_request(key, url, http_method, json=None):
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + key,
+        }
+    except:
+        raise AuthenticationError("Missing API Key")
+    try:
+        version = importlib.metadata.version("lamini")
+        headers["Lamini-Version"] = version
+    except:
+        pass
+    if http_method == "post":
+        resp = requests.post(url=url, headers=headers, json=json)
+    elif http_method == "get":
+        resp = requests.get(url=url, headers=headers)
+    else:
+        raise Exception("http_method must be 'post' or 'get'")
+    try:
+        check_version(resp)
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if resp.status_code == 594:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise ModelNotFound(json_response.get("detail", "ModelNameError"))
+        if resp.status_code == 429:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise RateLimitError(json_response.get("detail", "RateLimitError"))
+        if resp.status_code == 401:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise AuthenticationError(
+                json_response.get("detail", "AuthenticationError")
+            )
+        if resp.status_code == 400:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise UserError(json_response.get("detail", "UserError"))
+        if resp.status_code == 422:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise UserError(json_response.get("detail", "UserError"))
+        if resp.status_code == 503:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise UnavailableResourceError(
+                json_response.get("detail", "UnavailableResourceError")
+            )
+        if resp.status_code == 513:
+            message = ""
+            try:
+                json_response = resp.json()
+                message = json_response.get('detail')
+                message = message.split('Downloading', 1)[1].join(['Downloding', ''])
+            except Exception:
+                json_response = {}
+            raise DownloadingModelError(
+                message
+            )
+        if resp.status_code == 524:
+            try:
+                json_response = resp.json()
+            except Exception:
+                json_response = {}
+            raise RequestTimeoutError(
+                json_response.get("detail", "RequestTimeoutError")
+            )
+        if resp.status_code != 200:
+            try:
+                description = resp.json()
+            except BaseException:
+                description = resp.status_code
+            finally:
+                if description == {"detail": ""}:
+                    raise APIError("500 Internal Server Error")
+                raise APIError(f"API error {description}")
+
+    return resp.json()
