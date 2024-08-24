51618fca45d1bf8521fd70691746a939a9085417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:04:26 +0000

    Add package ics-query/0.3.0b0/ics_query-0.3.0b0.tar.gz

---
+++ packages/ics-query/ics_query-0.3.0b0.tar.gz/ics_query-0.3.0b0/ics_query/__init__.py
@@ -0,0 +1,12 @@
+from .cli import cli, main
+from .version import __version__, __version_tuple__, version, version_tuple
+
+__all__ = [
+    "cli",
+    "app",
+    "__version__",
+    "version",
+    "__version_tuple__",
+    "version_tuple",
+    "main",
+]
