619556386a7bc27001795019c4efa3d963d3ca9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:40 +0000

    Add package qandle/0.0.12/qandle-0.0.12-py3-none-any.whl

---
+++ packages/qandle/qandle-0.0.12-py3-none-any.whl/qandle/ansaetze/__init__.py
@@ -0,0 +1,7 @@
+# ruff: noqa: F403 F401
+
+from .stronglyentangling import StronglyEntanglingLayer
+from .twolocal import TwoLocal
+from .specialunitary import SpecialUnitary
+
+SU = SpecialUnitary  # alias
