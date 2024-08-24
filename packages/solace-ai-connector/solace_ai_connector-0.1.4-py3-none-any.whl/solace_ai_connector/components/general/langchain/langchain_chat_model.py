8553eeed6ef36dd4c60ace0974c74f92ab1e8b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 06:04:25 +0000

    Add package solace-ai-connector/0.1.4/solace_ai_connector-0.1.4-py3-none-any.whl

---
+++ packages/solace-ai-connector/solace_ai_connector-0.1.4-py3-none-any.whl/solace_ai_connector/components/general/langchain/langchain_chat_model.py
@@ -0,0 +1,19 @@
+# This is a wrapper around all the LangChain chat models
+# The configuration will control dynamic loading of the chat models
+from copy import deepcopy
+from .langchain_chat_model_base import (
+    LangChainChatModelBase,
+    info_base,
+)
+
+# Deepcopy info_base
+info = deepcopy(info_base)
+info["class_name"] = "LangChainChatModel"
+
+
+class LangChainChatModel(LangChainChatModelBase):
+
+    def invoke_model(
+        self, input_message, messages, session_id=None, clear_history=False
+    ):
+        return self.component.invoke(messages)
