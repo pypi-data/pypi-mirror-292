2c194339394f600c7d624de3dc1b0a1e05c28cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:23 +0000

    Add package mypackage-muzili/0.1/mypackage_muzili-0.1.tar.gz

---
+++ packages/mypackage-muzili/mypackage_muzili-0.1.tar.gz/mypackage_muzili-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage-muzili',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项都在这里列出
+    ],
+    author='Your Name',
+    author_email='youremail@example.com',
+    description='A brief description of your package',
+    license='MIT',
+    keywords='sample setuptools development',
+    url='https://github.com/yourusername/mypackage'
+)
