c78d270076019ff76e33bfc46644ac7a534e3cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:56 +0000

    Add package comotion-sdk/0.0.30/comotion_sdk-0.0.30.tar.gz

---
+++ packages/comotion-sdk/comotion_sdk-0.0.30.tar.gz/comotion_sdk-0.0.30/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+
+if __name__ == "__main__":
+
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    # with open("VERSION", "r", encoding="utf-8") as vfh:
+        # version = vfh.read()
+
+    setuptools.setup(
+        name="comotion-sdk",
+        # version=version,
+        author="Comotion",
+        author_email="tim@comotion.us",
+        description="SDK for interacting with the Comotion APIs",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/ComotionLabs/comotion-sdk",
+        project_urls={
+            "Bug Tracker": "https://github.com/ComotionLabs/comotion-sdk/issues"  # noqa E501
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src"),
+        python_requires=">=3.8",
+        entry_points={
+        'console_scripts': [
+            'comotion = comotion.cli:safe_entry_point',
+            ],
+        },
+        install_requires=[
+            'requests>=2.25.0',
+            'pandas>=1.2.0',
+            'click>=8.0.3',
+            'keyring>=23.2.1',
+            'pyjwt',
+            'urllib3>=1.25.3',
+            'sqlalchemy>=1.4.36',
+            'tqdm>=4.64.0',
+            'aiohttp',
+            'python-dateutil',
+            'pydantic>2',
+            'awswrangler>=3.4.2'
+        ],
+        tests_require=[
+            'mock',
+            'pytest'
+        ]
+    )
