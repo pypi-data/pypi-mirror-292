dc459c1540c48316a8189770b1cc5c16a8b61c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 05:03:54 +0000

    Add package aiomultithreading/0.1.1/aiomultithreading-0.1.1-py3-none-any.whl

---
+++ packages/aiomultithreading/aiomultithreading-0.1.1-py3-none-any.whl/aiomultithreading/__init__.py
@@ -0,0 +1,8 @@
+"""A MultiProccesingThreadPool Made to combine multiprocessing + threading + asyncio 
+all together to deliver the highest power possible
+"""
+
+__author__ = "Vizonex"
+
+from .__version__ import __version__
+from .pool import MultiPool, ProcessWorker
