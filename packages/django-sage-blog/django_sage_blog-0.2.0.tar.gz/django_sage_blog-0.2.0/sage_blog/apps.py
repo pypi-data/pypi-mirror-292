a7a9ce8cc873b404d8453af1bf386a13af1d1a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 04:03:54 +0000

    Add package django-sage-blog/0.2.0/django_sage_blog-0.2.0.tar.gz

---
+++ packages/django-sage-blog/django_sage_blog-0.2.0.tar.gz/django_sage_blog-0.2.0/sage_blog/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class SageBlogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sage_blog"
+    verbose_name = _("Blog")
+
+    def ready(self) -> None:
+        import sage_blog.settings.check
