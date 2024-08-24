317d6c1a280ce0c87b8d800cc83e93257216f358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-24 15:05:34 +0000

    Add package llamaverify/0.1.6/llamaverify-0.1.6-py3-none-any.whl

---
+++ packages/llamaverify/llamaverify-0.1.6-py3-none-any.whl/llamaverify/__init__.py
@@ -0,0 +1,8 @@
+# llamaverify/__init__.py
+
+from .client import Client
+
+# The API key can be handled directly by passing it during Client instantiation.
+
+
+__all__ = ['client', 'callDehallucinateEndpoint']
