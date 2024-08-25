eb726c49f8cf693d47179b457de9aa5f9aa5f0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:04:51 +0000

    Add package achatbot/0.0.5.22/achatbot-0.0.5.22.tar.gz

---
+++ packages/achatbot/achatbot-0.0.5.22.tar.gz/achatbot-0.0.5.22/pypi_build/app/achatbot/cmd/bots/dummy_bot.py
@@ -0,0 +1,14 @@
+import os
+import logging
+import asyncio
+
+from .base import DailyRoomBot, register_daily_room_bots
+
+
+@register_daily_room_bots.register
+class DummyBot(DailyRoomBot):
+    async def arun(self):
+        logging.info(f"Starting bot,env: {os.environ}")
+        logging.info(f"dummy bot run with args: {self.args}")
+        await asyncio.sleep(10)
+        logging.info("dummy bot over")
