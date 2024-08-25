7fb7cb48dd1d6d5b836caded690f690bb50aaf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:05:25 +0000

    Add package gptbioinsightor/0.4.0/gptbioinsightor-0.4.0-py3-none-any.whl

---
+++ packages/gptbioinsightor/gptbioinsightor-0.4.0-py3-none-any.whl/gptbioinsightor/constant.py
@@ -0,0 +1,22 @@
+
+
+API_SOURCE = {
+    "openai": "https://api.openai.com/v1/",
+    "meta": "https://api.llama-api.com",
+    "moonshot": "https://api.moonshot.cn/v1",
+    "aliyun": "https://dashscope.aliyuncs.com/compatible-mode/v1"  
+}
+
+LANG_DIC = {
+    'ar': 'Arabic',
+    'de': 'German',
+    'en': 'English',
+    'es': 'Spanish',
+    'fr': 'French',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'ko': 'Korean',
+    'pt': 'Portuguese',
+    'ru': 'Russian',
+    'zh': 'Chinese'
+}
