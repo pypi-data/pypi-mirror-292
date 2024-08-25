d3aca28cfde820febf80c92833fe351cbaeb4590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:00 +0000

    Add package app-tools-zxw/1.0.1/app_tools_zxw-1.0.1-py3-none-any.whl

---
+++ packages/app-tools-zxw/app_tools_zxw-1.0.1-py3-none-any.whl/app_tools_zxw/SDK_微信/SDK_微信支付/__init__.py
@@ -0,0 +1,9 @@
+"""
+# File       : __init__.py.py
+# Time       ：2024/8/22 09:32
+# Author     ：xuewei zhang
+# Email      ：shuiheyangguang@gmail.com
+# version    ：python 3.12
+# Description：
+"""
+from .微信支付SDK二次打包_可直接调用 import 生成预支付单, 查询订单, 申请退款, 查询退款, wxpay_recall
