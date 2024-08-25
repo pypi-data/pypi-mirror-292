a6398422b38181788c5944ce97eadde399b7a7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:05:13 +0000

    Add package tma_flutter/0.1.8/tma_flutter-0.1.8.tar.gz

---
+++ packages/tma_flutter/tma_flutter-0.1.8.tar.gz/tma_flutter-0.1.8/tma_flutter/modules/targets/example/sources/example.py
@@ -0,0 +1,70 @@
+import typer, os
+from typing_extensions import Annotated
+from pathlib import Path
+from tma_flutter.snippets.sources import flutter, template
+
+
+app = typer.Typer()
+
+
+@app.command(name="make")
+def make_target(
+    example_name: Annotated[str, typer.Argument()],
+    dir_name: Annotated[str, typer.Argument()] = "examples",
+):
+    flutter.create_app(
+        app_name=example_name,
+        dir_name=dir_name,
+    )
+
+
+def name(module_name: str) -> str:
+    return module_name + "_" + "example"
+
+
+def copy_template(
+    example_name: str,
+    view_name: str,
+):
+    example_path = _get_example_path()
+    lib_path = example_path.joinpath("lib")
+    test_path = example_path.joinpath("test")
+    template.prepare_copy(lib_path, test_path)
+
+    template_path = Path(__file__).absolute().parent.parent.joinpath("templates")
+    template.copy(
+        copy_path=template_path.joinpath("lib"),
+        copy_file="main.dart",
+        paste_path=lib_path,
+        paste_file="main.dart",
+        template_variables={
+            "example_snake": example_name,
+            "example_pascal": template.pascal_case(example_name),
+        },
+    )
+    template.copy(
+        copy_path=template_path.joinpath("lib"),
+        copy_file="example.dart",
+        paste_path=lib_path,
+        paste_file=f"{example_name}.dart",
+        template_variables={
+            "example_pascal": template.pascal_case(example_name),
+            "view_snake": view_name,
+            "view_pascal": template.pascal_case(view_name),
+        },
+    )
+
+
+def add_dependency(view_name: str):
+    flutter.add_dependencies(
+        dependency_names=[view_name],
+        pubspec_path=_get_example_path(),
+    )
+
+
+def _get_example_path() -> Path:
+    return Path(os.getcwd()).joinpath("examples")
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/tma_flutter/tma_flutter-0.1.8.tar.gz/tma_flutter-0.1.8/tma_flutter/modules/targets/interface/sources/interface.py
@@ -0,0 +1,53 @@
+import typer, os
+from typing_extensions import Annotated
+from pathlib import Path
+from typing import List
+from tma_flutter.snippets.sources import flutter, template
+
+
+app = typer.Typer()
+
+
+@app.command(name="make")
+def make_target(
+    interface_name: Annotated[str, typer.Argument()],
+    dir_name: Annotated[str, typer.Argument()] = "interfaces",
+):
+    flutter.create_package(
+        package_name=interface_name,
+        dir_name=dir_name,
+    )
+
+
+def name(module_name: str) -> str:
+    return module_name + "_" + "interface"
+
+
+def copy_template(interface_name: str):
+    interface_path = _get_interface_path()
+    lib_path = interface_path.joinpath("lib")
+    test_path = interface_path.joinpath("test")
+    template.prepare_copy(lib_path, test_path)
+
+    template_path = Path(__file__).absolute().parent.parent.joinpath("templates")
+    template.copy(
+        copy_path=template_path.joinpath("lib"),
+        copy_file="interface.dart",
+        paste_path=lib_path,
+        paste_file=f"{interface_name}.dart",
+    )
+
+
+def add_dependency(dependency_names: List[str]):
+    flutter.add_dependencies(
+        dependency_names=dependency_names,
+        pubspec_path=_get_interface_path(),
+    )
+
+
+def _get_interface_path() -> Path:
+    return Path(os.getcwd()).joinpath("interfaces")
+
+
+if __name__ == "__main__":
+    app()
