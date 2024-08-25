6f4f154cea4b563270adaedd3d05fee3d519a342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:05 +0000

    Add package pysigma-backend-cortexxdr/0.1.4/pysigma_backend_cortexxdr-0.1.4.tar.gz

---
+++ packages/pysigma-backend-cortexxdr/pysigma_backend_cortexxdr-0.1.4.tar.gz/pysigma_backend_cortexxdr-0.1.4/sigma/pipelines/cortexxdr/__init__.py
@@ -0,0 +1,6 @@
+from .cortexxdr import CortexXDR_pipeline
+# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
+
+pipelines = {
+    "CortexXDR_pipeline": CortexXDR_pipeline,   # TODO: adapt identifier to something approproiate
+}
