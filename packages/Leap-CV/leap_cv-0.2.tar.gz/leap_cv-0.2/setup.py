afed25d40d0d0d056ece2b9b3ef9f0c94283da5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:18 +0000

    Add package Leap-CV/0.2/leap_cv-0.2.tar.gz

---
+++ packages/Leap-CV/leap_cv-0.2.tar.gz/leap_cv-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Leap-CV',
+    version='0.2',
+    author="10 Academy",
+    packages=find_packages(),
+    description="Cv Generation Package",
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'leap=leap.leap:main',  # Ensure this matches your module and function
+        ],
+    },
+    package_data={
+        'leap': ['templates/*.cls' ,# Include all .cls files in the templates directory
+                'fonts/*',  # Include all files in the fonts directory
+                ],
+    },
+    include_package_data=True,
+)
