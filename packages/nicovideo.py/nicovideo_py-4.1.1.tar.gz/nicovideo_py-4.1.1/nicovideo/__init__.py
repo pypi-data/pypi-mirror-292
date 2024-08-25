ac6826770412ce8da3e8750fd1accfbc12fa8b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:05 +0000

    Add package nicovideo.py/4.1.1/nicovideo_py-4.1.1.tar.gz

---
+++ packages/nicovideo.py/nicovideo_py-4.1.1.tar.gz/nicovideo_py-4.1.1/nicovideo/__init__.py
@@ -0,0 +1,5 @@
+""" nicovideo.py: nicovideo API wrapper for Python3. """
+from . import video, errors, user, apirawdicts
+
+__version__ = "4.1.1"
+__all__ = ["video", "user", "errors", "apirawdicts"]
