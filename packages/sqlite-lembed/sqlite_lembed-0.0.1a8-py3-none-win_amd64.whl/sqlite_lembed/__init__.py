4eb1ae06526d06fce4e091f78c972bc7e9956392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:52 +0000

    Add package sqlite-lembed/0.0.1a8/sqlite_lembed-0.0.1a8-py3-none-win_amd64.whl

---
+++ packages/sqlite-lembed/sqlite_lembed-0.0.1a8-py3-none-win_amd64.whl/sqlite_lembed/__init__.py
@@ -0,0 +1,18 @@
+
+from os import path
+import sqlite3
+
+__version__ = "0.0.1a8"
+__version_info__ = tuple(__version__.split("."))
+
+def loadable_path():
+  """ Returns the full path to the sqlite-lembed loadable SQLite extension bundled with this package """
+
+  loadable_path = path.join(path.dirname(__file__), "lembed0")
+  return path.normpath(loadable_path)
+
+def load(conn: sqlite3.Connection)  -> None:
+  """ Load the sqlite-lembed SQLite extension into the given database connection. """
+
+  conn.load_extension(loadable_path())
+
