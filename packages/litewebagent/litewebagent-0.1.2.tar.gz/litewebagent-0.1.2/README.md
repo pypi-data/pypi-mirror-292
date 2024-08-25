187e2a17e538ea3e15a09084136c757814507ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:43 +0000

    Add package litewebagent/0.1.2/litewebagent-0.1.2.tar.gz

---
+++ packages/litewebagent/litewebagent-0.1.2.tar.gz/litewebagent-0.1.2/README.md
@@ -0,0 +1,66 @@
+# LiteWebAgent
+Please note that the LiteWebAgent repository is in development mode. We have open-sourced the repository to foster collaboration between contributors.
+
+<a href='https://danqingz.github.io/blog/2024/08/22/LiteWebAgent.html'><img src='https://img.shields.io/badge/BLOG-181717?logo=github&logoColor=white'></a>
+
+
+## 2. Development mode
+### (1) Installation
+From PyPI: https://pypi.org/project/litewebagent/
+```
+pip install litewebagent 
+```
+Then, a required step is to setup playwright by running
+```
+playwright install chromium
+```
+Set up locally
+First set up virtual environment, and allow your code to be able to see 'litewebagent'
+```bash
+python3 -m venv venv
+. venv/bin/activate
+pip install -e .
+```
+Then please create a .env file, and update your API keys:
+
+```bash
+cp .env.example .env
+```
+
+### (2) QuickStart
+* use web agent to finish some task and save the workflow
+```bash
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.google.com --goal 'search dining table' --plan 'search dining table'
+python -m litewebagent.main --agent_type HighLevelPlanningAgent --starting_url https://www.airbnb.com --goal "set destination as San Francisco, then search the results" --plan "(1) enter the 'San Francisco' as destination, (2) and click search"
+python -m litewebagent.main --agent_type ContextAwarePlanningAgent --starting_url https://www.google.com --goal 'search dining table' --plan 'search dining table'
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.google.com --goal 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"' --plan 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"'
+python -m litewebagent.main --agent_type HighLevelPlanningAgent --starting_url https://www.google.com --goal 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"' --plan 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"'
+python -m litewebagent.main --agent_type ContextAwarePlanningAgent --starting_url https://www.google.com --goal 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"' --plan 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"'
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.google.com --goal 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"' --plan 'Find the pdf of the paper "GPT-4V(ision) is a Generalist Web Agent, if Grounded"'
+```
+* replay the workflow verified by the web agent
+If you haven't used the web agent to try any tests yet, first copy our example.json file.
+```bash
+cp litewebagent/flow/example.json litewebagent/flow/steps.json 
+```
+Now that we have the steps.json file, which contains the workflow of a specific task, we can replay.
+```bash
+python -m litewebagent.replay
+```
+* enable user agent interaction
+```bash
+python -m litewebagent.cli_main --agent_type HighLevelPlanningAgent 
+```
+
+### (3) test different input features
+We use axtree by default. Alternatively, you can provide a comma-separated string listing the desired input feature types.
+```bash
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.airbnb.com --goal 'set destination as San Francisco, then search the results' --plan '(1) enter the "San Francisco" as destination, (2) and click search'
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.airbnb.com --goal 'set destination as San Francisco, then search the results' --plan '(1) enter the "San Francisco" as destination, (2) and click search' --features interactive_elements
+python -m litewebagent.main --agent_type DemoAgent --starting_url https://www.airbnb.com --goal 'set destination as San Francisco, then search the results' --plan '(1) enter the "San Francisco" as destination, (2) and click search' --features axtree,interactive_elements
+```
+
+### (4) Demos
+* https://www.loom.com/share/1018bcc4e21c4a7eb517b60c2931ee3c
+* https://www.loom.com/share/aa48256478714d098faac740239c9013
+* https://www.loom.com/share/89f5fa69b8cb49c8b6a60368ddcba103
---
+++ packages/litewebagent/litewebagent-0.1.2.tar.gz/litewebagent-0.1.2/litewebagent/webagent.py
@@ -0,0 +1,192 @@
+import sys
+import os
+import argparse
+import logging
+from dotenv import load_dotenv
+from openai import OpenAI
+from litewebagent.playwright_manager import get_browser, get_context, get_page, playwright_manager
+from litewebagent.agents.DemoAgent import DemoAgent
+from litewebagent.agents.HighLevelPlanningAgent import HighLevelPlanningAgent
+from litewebagent.agents.ContextAwarePlanningAgent import ContextAwarePlanningAgent
+from litewebagent.functions.functions import navigation, upload_file, scan_page_extract_information, take_action, select_option
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+_ = load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("log.txt", mode="w"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+openai_client = OpenAI()
+
+# Define the default features
+DEFAULT_FEATURES = ['screenshot', 'dom', 'axtree', 'focused_element', 'extra_properties', 'interactive_elements']
+
+
+def create_function_wrapper(func, features, branching_factor):
+    def wrapper(task_description):
+        return func(task_description, features, branching_factor)
+
+    return wrapper
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "navigation",
+            "description": "Perform a web navigation task, including click, search ",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "task_description": {
+                        "type": "string",
+                        "description": "The description of the web navigation task"
+                    },
+                },
+                "required": [
+                    "task_description",
+                ]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "upload_file",
+            "description": "upload file.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "task_description": {
+                        "type": "string",
+                        "description": "The description of the web navigation task"
+                    },
+                },
+                "required": [
+                    "task_description",
+                ]
+            }
+        }
+    },
+    {
+            "type": "function",
+            "function": {
+                "name": "select_option",
+                "description": "Select an option from a dropdown or list.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "task_description": {
+                            "type": "string",
+                            "description": "The description of the option selection task"
+                        }
+                    },
+                    "required": [
+                        "task_description"
+                    ]
+                }
+            }
+        }
+]
+
+
+def setup_web_agent(starting_url, goal, model_name="gpt-4o-mini", agent_type="DemoAgent", features=['axtree'], branching_factor = None):
+    if features is None:
+        features = DEFAULT_FEATURES
+
+
+    available_tools = {
+        "navigation": create_function_wrapper(navigation, features, branching_factor),
+        "upload_file": create_function_wrapper(upload_file, features, branching_factor),
+        "select_option": create_function_wrapper(select_option, features, branching_factor),
+        # "scan_page_extract_information": scan_page_extract_information,
+    }
+
+    messages = [
+        {
+            "role": "system",
+            "content": """You are a web search agent designed to perform specific tasks on web pages as instructed by the user. Your primary objectives are:
+
+    1. Execute ONLY the task explicitly provided by the user.
+    2. Perform the task efficiently and accurately using the available functions.
+    3. If there are errors, retry using a different approach within the scope of the given task.
+    4. Once the current task is completed, stop and wait for further instructions.
+
+    Critical guidelines:
+    - Strictly limit your actions to the current task. Do not attempt additional tasks or next steps.
+    - Use only the functions provided to you. Do not attempt to use functions or methods that are not explicitly available.
+    - For navigation or interaction with page elements, always use the appropriate bid (browser element ID) when required by a function.
+    - Do not try to navigate to external websites or use URLs directly.
+    - If a task cannot be completed with the available functions, report the limitation rather than attempting unsupported actions.
+    - After completing a task, report its completion and await new instructions. Do not suggest or initiate further actions.
+
+    Remember: Your role is to execute the given task precisely as instructed, using only the provided functions and within the confines of the current web page. Do not exceed these boundaries under any circumstances."""
+        }
+    ]
+    file_path = os.path.join('litewebagent', 'flow', 'steps.json')
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    page = get_page()
+    page.goto(starting_url)
+    # Maximize the window on macOS
+    page.set_viewport_size({"width": 1440, "height": 900})
+
+    with open(file_path, 'w') as file:
+        file.write(goal + '\n')
+        file.write(starting_url + '\n')
+
+    if agent_type == "DemoAgent":
+        agent = DemoAgent(model_name=model_name, tools=tools, available_tools=available_tools, messages=messages,
+                          goal=goal)
+    elif agent_type == "HighLevelPlanningAgent":
+        agent = HighLevelPlanningAgent(model_name=model_name, tools=tools, available_tools=available_tools,
+                                       messages=messages, goal=goal)
+    elif agent_type == "ContextAwarePlanningAgent":
+        agent = ContextAwarePlanningAgent(model_name=model_name, tools=tools, available_tools=available_tools,
+                                          messages=messages, goal=goal)
+    else:
+        error_message = f"Unsupported agent type: {agent_type}. Please use 'DemoAgent', 'HighLevelPlanningAgent', or 'ContextAwarePlanningAgent'."
+        logger.error(error_message)
+        return {"error": error_message}
+    return agent
+
+
+def main(args):
+    browser = get_browser()
+    context = get_context()
+    page = get_page()
+    playwright_manager.playwright.selectors.set_test_id_attribute('data-unique-test-id')
+    starting_url = "https://www.airbnb.com"
+    plan = "(1) enter the 'San Francisco' as destination, (2) and click search"
+    goal = "set destination as San Francisco, then search the results"
+
+    # Use the features from command-line arguments
+    features = args.features.split(',') if args.features else None
+    branching_factor = args.branching_factor if args.branching_factor else None
+
+    agent = setup_web_agent(starting_url, goal, model_name=args.model, agent_type=args.agent_type, features=features, branching_factor = branching_factor)
+    response = agent.send_prompt(plan)
+    print(response)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run web automation tasks with different agent types.")
+    parser.add_argument('--agent_type', type=str, default="DemoAgent",
+                        choices=["DemoAgent", "HighLevelPlanningAgent", "ContextAwarePlanningAgent"],
+                        help="Type of agent to use (default: DemoAgent)")
+    parser.add_argument('--model', type=str, default="gpt-4o-mini",
+                        help="Model to use for the agent (default: gpt-4o-mini)")
+    parser.add_argument('--workflow_index', type=int, default=5,
+                        help="Index of the workflow to use from the JSON file (default: 5)")
+    parser.add_argument('--features', type=str, default="axtree",
+                        help="Comma-separated list of features to use (default: None, which uses all features)")
+    parser.add_argument('--branching_factor', type=int, default=None)
+    args = parser.parse_args()
+    main(args)
