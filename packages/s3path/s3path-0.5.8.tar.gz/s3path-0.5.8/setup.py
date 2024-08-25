92c182a0cc7395ae0aca116a566145efca2438fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:21 +0000

    Add package s3path/0.5.8/s3path-0.5.8.tar.gz

---
+++ packages/s3path/s3path-0.5.8.tar.gz/s3path-0.5.8/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='s3path',
+    version='0.5.8',
+    url='https://github.com/liormizr/s3path',
+    author='Lior Mizrahi',
+    author_email='li.mizr@gmail.com',
+    packages=['s3path'],
+    install_requires=[
+        'boto3>=1.16.35',
+        'smart-open>=5.1.0',
+    ],
+    license='Apache 2.0',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    python_requires='>=3.8',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
