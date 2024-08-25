e187d204fa0b1765db50f45480f3b2d712bfdbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:26 +0000

    Add package courou/0.1.2/courou-0.1.2.tar.gz

---
+++ packages/courou/courou-0.1.2.tar.gz/courou-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="courou",  # Remplacez par le nom de votre package
+    version="0.1.2",  # La version de votre package
+    description="is for manage ftn login and mcp",
+    author="the baaguette man",
+    author_email="votre.email@example.com",
+    url="https://github.com/baguettemanpy",  # URL de votre dépôt GitHub si applicable
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Remplacez par la licence appropriée
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12',  # Version minimale de Python requise
+)
