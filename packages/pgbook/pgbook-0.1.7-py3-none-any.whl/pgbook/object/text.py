dfe088bdd837d6e56ca5df53b5127cb79449397e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:04:51 +0000

    Add package pgbook/0.1.7/pgbook-0.1.7-py3-none-any.whl

---
+++ packages/pgbook/pgbook-0.1.7-py3-none-any.whl/pgbook/object/text.py
@@ -0,0 +1,33 @@
+import pygame
+import pkg_resources
+from .object import Position
+from ..eventlist.thinglist import ThingList
+
+class BaseText:
+    def __init__(self,text='Some Text',color=(255,255,255),textsize=24,textfile=None):
+        self.objects_type='BaseButton'
+        self.drawlist=ThingList()
+        self.eventlist=ThingList()
+        
+        self.new_date(text,color,textsize,textfile)
+        
+    def connect_page(self,page):
+        self.pos=Position(0,0,page.screen.width,page.screen.height)
+        page.drawlist.connect(self.drawlist)
+        page.eventlist.connect(self.eventlist)
+        self.drawlist.connect(self.draw,'DrawButton')
+        
+    def draw(self,screen):
+        textRect = self.textbook.get_rect()
+        textRect.center = (300, 200)
+        screen.blit(self.textbook, textRect)
+        
+    def new_date(self,text='Some Text',color=(255,255,255),textsize=24,textfile=None):
+        self.text=text
+        self.textcolor=color
+        if textfile==None:
+            font_path = pkg_resources.resource_filename('pgbook', 'static/fonts/NotoFont.ttf',textsize)
+        else:
+            font_path=textfile
+        self.font= pygame.font.Font(font_path)
+        self.textbook = self.font.render(self.text, True, self.textcolor, background=None)
---
+++ packages/pgbook/pgbook-0.1.7-py3-none-any.whl/pgbook/screen/window.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+import pkg_resources
+from ..eventlist.thinglist import ThingList
+
+class Window:
+    def __init__(self):
+        pygame.init()
+        infoObject = pygame.display.Info()
+        self.max_width = infoObject.current_w
+        self.max_height = infoObject.current_h
+        #self.max_width, self.max_height = pygame.display.get_surface().get_size()
+        
+        self.width = self.max_width*0.6
+        self.height = self.max_height*0.6
+        self.MaxFps = 60
+        
+        self.check = ThingList()
+        self.event = ThingList()
+        
+        @self.event.connect(name='QuitCheckThing')
+        def Quit(event):
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        @self.check.connect(name='UpDateWindows')
+        def Update(window):
+            pygame.display.update()
+    def show(self):
+        self.screen = pygame.display.set_mode((self.width,self.height))
+        self.clock = pygame.time.Clock()
+        while True:
+            self.clock.tick(self.MaxFps)
+            for event in pygame.event.get():
+                self.event.did(event)
+            self.check.did(self.screen)
+            
+    def set_ico(self,path='static/pic/load.ico'):
+        if path==None:
+            ico_path = pkg_resources.resource_filename('pgbook', 'static/pic/load.ico')
+        img = pygame.image.load(ico_path)
+        pygame.display.set_icon(img)
