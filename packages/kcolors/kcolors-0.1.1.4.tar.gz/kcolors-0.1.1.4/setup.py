236063b3ec49b70ce61f0e7fbc6d21f2ee6e9c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:54 +0000

    Add package kcolors/0.1.1.4/kcolors-0.1.1.4.tar.gz

---
+++ packages/kcolors/kcolors-0.1.1.4.tar.gz/kcolors-0.1.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kcolors',
+    version='0.1.1.4',
+    packages=find_packages(),
+    description='Automatic/Static ANSI colors for python',
+    author='tabibitonokaito',
+    author_email='tabibitonokaito@gmail.com',
+    url='https://github.com/tabibitonokaito/kcolors',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+    ],
+)
