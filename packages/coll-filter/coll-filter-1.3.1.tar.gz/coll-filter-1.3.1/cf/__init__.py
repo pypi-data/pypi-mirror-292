23ea1c3be2d35395b572153a86ba862e35fe2c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:23 +0000

    Add package coll-filter/1.3.1/coll-filter-1.3.1.tar.gz

---
+++ packages/coll-filter/coll-filter-1.3.1.tar.gz/coll-filter-1.3.1/cf/__init__.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import math
+from enum import Enum
+from typing import Iterable, Tuple, List, Mapping, Collection, Generic, TypeVar
+
+from .utils import fusion
+
+
+U = TypeVar('U')
+T = TypeVar('T')
+
+
+class CFType(Enum):
+    UCF = 'UserCF'
+    ICF = 'ItemCF'
+
+
+def default_similar_func(items: List, other: List) -> float:
+    """两个item并集数
+
+    以用户相似度为例，遍历item_users，每行用户间拥有共同的item，避免遍历userTtems大量用户间没有共同的item：
+    item1: user1, user2, user3
+
+    user1和user2共同有item1:
+    user1: item1, item2, item3
+    user2: item1, item4, item5
+
+    传入此方法的参数为:
+    items: [item1, item2, item3]
+    other: [item1, item4, item5]
+    """
+    return 1.0 / float(len(set(items + other)))
+
+
+def sqrt_similar_func(items: List, other: List) -> float:
+    """两个item数相乘开根"""
+    return 1.0 / math.sqrt(len(items) * len(other))
+
+
+class CollFilter(Generic[U, T]):
+    """
+    Collaborative filter
+
+    Examples
+    --------
+    >>> from cf import CollFilter
+    >>> data = read_data('file_path')
+    >>> data = pre_process(data)  # return List[(user_id: Any, item_id: Any, rating: float)]
+    >>> cf = CollFilter(data)
+    >>> ucf = cf.user_cf()  # return {user_id: [(item_id, score),],}
+    >>> icf = cf.item_cf()  # return {user_id: [(item_id, score),],}
+    >>> recommend = cf.recommend(user_id, recall_num=5) # return [(item_id, score),]
+    >>> recommends = cf.recommends(user_ids, recall_num=5) # return {user_id: [(item_id, score),],}
+    >>> cf.release()
+    """
+
+    def __init__(self, data: Iterable[Tuple[U, T, float]], parallel_num=2 * os.cpu_count(), row_unique=False,
+                 similar_fn=default_similar_func, cache_similar=False):
+        if parallel_num > 1:
+            from cf.pooling import PoolCollFilter
+            self._coll_filter = PoolCollFilter(data, parallel_num, row_unique, similar_fn, cache_similar)
+        else:
+            from cf.base import BaseCollFilter
+            self._coll_filter = BaseCollFilter(data, row_unique, similar_fn, cache_similar)
+
+    def user_cf(self,
+                recall_num=64,
+                similar_num=256,
+                user_ids: Collection[U] = None,
+                user_similar: Mapping[U, Mapping[U, float]] = None,
+                similar_fn=None
+                ) -> Mapping[U, List[Tuple[T, float]]]:
+        """
+        基于用户的协同过滤
+        @param recall_num  每个用户最大召回个数
+        @param similar_num  每个用户最大相似用户个数
+        @param user_ids  要推荐的用户列表
+        @param user_similar  用户相似矩阵
+        @param similar_fn  相似度计算函数
+        @return {user_id: [(item_id, score),],}
+        """
+        assert recall_num > 0, "'recall_num' should be a positive number."
+        return self._coll_filter.user_cf(recall_num, similar_num, user_ids, user_similar, similar_fn)
+
+    def item_cf(self,
+                recall_num=64,
+                similar_num=256,
+                user_ids: Collection[U] = None,
+                item_similar: Mapping[T, Mapping[T, float]] = None,
+                similar_fn=None
+                ) -> Mapping[U, List[Tuple[T, float]]]:
+        """
+        基于物品的协同过滤
+        @param recall_num  每个用户最大召回个数
+        @param similar_num  每个物品最大相似物品个数
+        @param user_ids  要推荐的用户列表
+        @param item_similar  物品相似矩阵
+        @param similar_fn  相似度计算函数
+        @return {user_id: [(item_id, score),],}
+        """
+        assert recall_num > 0, "'recall_num' should be a positive number."
+        return self._coll_filter.item_cf(recall_num, similar_num, user_ids, item_similar, similar_fn)
+
+    def recommend(self, user_id: U, recall_num=64, similar_num=8, similar_fn=None, ratio=0.5, return_score=False) \
+            -> List[Tuple[T, float]]:
+        """
+        给一个用户推荐
+        @param user_id  要推荐的用户
+        @param recall_num  每个用户最大召回个数
+        @param similar_num  每个物品最大相似物品个数
+        @param similar_fn  相似度计算函数
+        @param ratio  推荐结果中item_cf所占比例
+        @param return_score 是否返回分数
+        @return [item_id,] or [(item_id, score),]
+        """
+        result = self._recommend(user_id, recall_num, similar_num, similar_fn, ratio)
+        return result if return_score else [item[0] for item in result]
+
+    def _recommend(self, user_id: U, recall_num, similar_num, similar_fn, ratio) -> List[Tuple[T, float]]:
+        """
+        给一个用户推荐
+        @param user_id  要推荐的用户
+        @param recall_num  每个用户最大召回个数
+        @param similar_num  每个物品最大相似物品个数
+        @param similar_fn  相似度计算函数
+        @param ratio  推荐结果中item_cf所占比例
+        @return [(item_id, score),]
+        """
+        icf = self.item_cf(recall_num, similar_num, [user_id], None, similar_fn)
+        icf_items = icf[user_id]
+        if recall_num == 1:
+            if icf_items:
+                return icf_items
+            else:
+                return self.user_cf(recall_num, similar_num, [user_id], None, similar_fn)[user_id]
+        else:
+            ucf_items = self.user_cf(recall_num, similar_num, [user_id], None, similar_fn)[user_id]
+            return fusion(icf_items, ucf_items, recall_num, ratio)
+
+    def recommends(self,
+                   user_ids: Collection[U] = None,
+                   recall_num: int = 64,
+                   similar_num: int = 8,
+                   similar_fn=None,
+                   ratio: float = 0.5
+                   ) -> Mapping[U, List[Tuple[T, float]]]:
+        """
+        给一批用户推荐
+        @param user_ids  要推荐的用户列表
+        @param recall_num  每个用户最大召回个数
+        @param similar_num  每个物品最大相似物品个数
+        @param similar_fn  相似度计算函数
+        @param ratio  推荐结果中item_cf所占比例
+        @return {user_id: [(item_id, score),],}
+        """
+        icf = self.item_cf(recall_num, similar_num, user_ids, None, similar_fn)
+        if recall_num == 1:
+            user_similar = self.get_user_similar(similar_num, similar_fn)
+            for user_id, items in icf.items():
+                if not items:
+                    icf[user_id] = self.user_cf(recall_num, similar_num, [user_id], user_similar, similar_fn)[user_id]
+        else:
+            ucf = self.user_cf(recall_num, similar_num, user_ids, None, similar_fn)
+            for user_id, icf_items in icf.items():
+                ucf_items = ucf[user_id]
+                icf[user_id] = fusion(icf_items, ucf_items, recall_num, ratio)
+
+        return icf
+
+    def get_user_similar(self, similar_num=256, similar_fn=None) -> Mapping[U, Mapping[U, float]]:
+        """
+        用户相似矩阵
+        """
+        return self._coll_filter.cal_similar(CFType.UCF, similar_num, similar_fn)
+
+    def get_item_similar(self, similar_num=256, similar_fn=None) -> Mapping[T, Mapping[T, float]]:
+        """
+        物品相似矩阵
+        """
+        return self._coll_filter.cal_similar(CFType.ICF, similar_num, similar_fn)
+
+    def release(self):
+        self._coll_filter.release()
+
---
+++ packages/coll-filter/coll-filter-1.3.1.tar.gz/coll-filter-1.3.1/cf/base.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""base_coll_filter, one process"""
+
+import os
+import time
+import math
+from collections import defaultdict
+from typing import Iterable, Mapping, Tuple, Generic
+
+from . import default_similar_func, CFType, U, T
+from .utils import print_cost_time, sort_similar, logger
+
+
+class CollFilterHelper:
+    @staticmethod
+    def _rating_user_cf(user_item_ratings, similar_dict, user_items_list, recall_num):
+        start_time = time.perf_counter()
+        result = {}
+        for user_id, items in user_items_list:
+            item_score = {}  # {item: score}
+            # {user_id: [(user_id: similar),],}  用户间的相似度
+            user_similar = similar_dict.get(user_id, [])
+            for u2, similar in user_similar:  # 遍历相似度用户列表
+                user_item_rating = user_item_ratings.get(u2, {})
+                for item, rating in user_item_rating.items():  # 逐个获取相似用户的item列表
+                    if item in items:  # item不在用户已消费的列表里
+                        continue
+                    item_score[item] = item_score.get(item, 0.0) + math.sqrt(similar * rating)
+            if len(item_score) > 0:
+                result[user_id] = sorted(item_score.items(), key=lambda x: x[1], reverse=True)[:recall_num]
+            else:
+                result[user_id] = []
+
+        print_cost_time(f"\t\t进程<{os.getpid()}> 处理 {len(user_items_list)} 条记录, 生成 {len(result)} 条记录, 耗时", start_time)
+        return result
+
+    @staticmethod
+    def _rating_item_cf(_user_item_ratings, similar_dict, user_items_list, recall_num):
+        start_time = time.perf_counter()
+        result = {}
+        for user_id, item_ratings in user_items_list:
+            item_score = {}  # {item: score}
+            for item, rating in item_ratings.items():  # 遍历用户已消费的item
+                # {item_id: similar,}
+                item_similar = similar_dict.get(item, [])
+                for item2, similar in item_similar:  # 与用户已消费item相似的item
+                    if item2 in item_ratings:
+                        continue
+                    item_score[item2] = item_score.get(item2, 0.0) + math.sqrt(similar * rating)
+            if len(item_score) > 0:
+                result[user_id] = sorted(item_score.items(), key=lambda x: x[1], reverse=True)[:recall_num]
+            else:
+                result[user_id] = []
+
+        print_cost_time(f"\t\t进程<{os.getpid()}> 处理 {len(user_items_list)} 条记录, 生成 {len(result)} 条记录, 耗时", start_time)
+        return result
+
+
+class BaseCollFilter(CollFilterHelper, Generic[U, T]):
+
+    def __init__(self, data: Iterable[Tuple[U, T, float]], row_unique=False,
+                 similar_fn=default_similar_func, cache_similar=False):
+        self.similar_fn = similar_fn
+        # {user_id: {item_id: rating},}  {item_id: [user_id],}
+        self.user_item_ratings, self.item_users = defaultdict(dict), defaultdict(list)
+        start_time = time.perf_counter()
+        cnt = 0
+        if row_unique:
+            for user_id, item_id, rating in data:
+                cnt += 1
+                self.user_item_ratings[user_id][item_id] = rating
+                self.item_users[item_id].append(user_id)
+        else:
+            for user_id, item_id, rating in data:
+                cnt += 1
+                self.user_item_ratings[user_id][item_id] = self.user_item_ratings[user_id].get(item_id, 0) + rating
+                self.item_users[item_id].append(user_id)
+            self.item_users = {item_id: list(set(users)) for item_id, users in self.item_users.items()}
+            
+        self._cache_similar = cache_similar
+        if cache_similar:
+            self._user_similar_cache, self._item_similar_cache = None, None
+
+        print_cost_time(f"数据处理, 当前进程<{os.getpid()}> 处理 {cnt} 条记录, "
+                        f"user数: {len(self.user_item_ratings)}, item数: {len(self.item_users)}, 耗时", start_time)
+        logger.info("=" * 88)
+        
+    def user_cf(self, recall_num=10, similar_num=256, user_ids=None, user_similar=None, similar_fn=None):
+        """
+        用户协同过滤
+
+        @param recall_num  每个用户推荐结果数目
+        @param similar_num  用户相似矩阵最大个数
+        @param user_ids  要推荐的用户列表
+        @param user_similar  用户相似矩阵
+        @param similar_fn  相似度计算函数
+        @return {user_id: [(item, score),],}
+        """
+        user_similar = user_similar or self.cal_similar(CFType.UCF, similar_num, similar_fn)
+        return self._cf(user_ids, user_similar, recall_num, CFType.UCF)
+
+    def item_cf(self, recall_num=10, similar_num=256, user_ids=None, item_similar=None, similar_fn=None):
+        """
+        物品协同过滤
+
+        @param recall_num  每个用户推荐结果数目
+        @param similar_num  物品相似矩阵最大个数
+        @param user_ids  要推荐的用户列表
+        @param item_similar  物品相似矩阵
+        @param similar_fn  相似度计算函数
+        @return {user_id: [(item, score),],}
+        """
+        item_similar = item_similar or self.cal_similar(CFType.ICF, similar_num, similar_fn)
+        return self._cf(user_ids, item_similar, recall_num, CFType.ICF)
+
+    def cal_similar(self, cf_type: CFType, similar_num=256, similar_fn=None):
+        """
+        计算相似度
+
+        @return dict{:List()}    {user1: {user2: similar}}
+        """
+        if self._cache_similar:
+            if CFType.UCF == cf_type:
+                if not self._user_similar_cache:
+                    self._user_similar_cache = self._cal_similar(cf_type, similar_num, similar_fn)
+                return self._user_similar_cache
+            else:
+                if not self._item_similar_cache:
+                    self._item_similar_cache = self._cal_similar(cf_type, similar_num, similar_fn)
+                return self._item_similar_cache
+        else:
+            return self._cal_similar(cf_type, similar_num, similar_fn)
+
+    def release(self):
+        del self.user_item_ratings
+        del self.item_users
+        if self._cache_similar:
+            del self._user_similar_cache
+            del self._item_similar_cache
+
+    def _cal_similar(self, cf_type: CFType, similar_num, similar_fn):
+        """
+        计算相似度
+
+        @return dict{:List()}    {user1: {user2: similar}}
+        """
+        logger.info(f'开始{cf_type.value}相似度计算, similar_num = {similar_num}')
+        func_start_time = time.perf_counter()
+        dict1, items_list, cal_similar_func = self._get_cal_similar_inputs(cf_type)
+        similar_fn = similar_fn or self.similar_fn
+        similar = cal_similar_func(dict1, items_list, similar_fn)
+        similar = sort_similar(similar, similar_num)
+        print_cost_time(f"完成{cf_type.value}相似度计算, 当前进程<{os.getpid()}> 总生成 {len(similar)} 条记录, 总耗时", func_start_time)
+        logger.info("=" * 88)
+        return similar
+
+    def _get_cal_similar_inputs(self, cf_type: CFType):
+        if cf_type == CFType.UCF:
+            return self.user_item_ratings, self.item_users.values(), self._cal_ucf_similar
+        else:
+            return self.item_users, self.user_item_ratings.values(), self._cal_icf_similar
+
+    @staticmethod
+    def _cal_ucf_similar(dict1: Mapping, items_list: Iterable[Iterable], similar_func):
+        start_time = time.perf_counter()
+        similar = defaultdict(dict)
+
+        for items in items_list:
+            if len(items) <= 1:
+                continue
+
+            for item1 in items:
+                for item2 in items:
+                    if item1 == item2:
+                        continue
+                    # 计算两个item间的相似性
+                    similar[item1][item2] = similar[item1].get(item2, 0.0) + similar_func(list(dict1.get(item1, {}).keys()), list(dict1.get(item2, {}).keys()))
+        print_cost_time(f"\t\t进程<{os.getpid()}> 生成 {len(similar)} 条记录, 耗时", start_time)
+        return similar
+
+    @staticmethod
+    def _cal_icf_similar(dict1: Mapping, items_list: Iterable[Iterable], similar_func):
+        start_time = time.perf_counter()
+        similar = defaultdict(dict)
+
+        for items in items_list:
+            if len(items) <= 1:
+                continue
+
+            for item1 in items:
+                for item2 in items:
+                    if item1 == item2:
+                        continue
+                    # 计算两个item间的相似性
+                    similar[item1][item2] = similar[item1].get(item2, 0.0) + similar_func(dict1.get(item1, []),
+                                                                                          dict1.get(item2, []))
+        print_cost_time(f"\t\t进程<{os.getpid()}> 生成 {len(similar)} 条记录, 耗时", start_time)
+        return similar
+
+    def _cf(self, user_ids, similar_dict, recall_num, cf_type: CFType):
+        logger.info(f'开始{cf_type.value}推理, recall_num = {recall_num}')
+        func_start_time = time.perf_counter()
+        if user_ids:
+            if not set(user_ids).intersection(self.user_item_ratings.keys()):
+                return {user_id: [] for user_id in user_ids}
+
+            user_items_list = [(user_id, self.user_item_ratings.get(user_id, {})) for user_id in user_ids]
+        else:
+            user_items_list = self.user_item_ratings.items()
+
+        if cf_type == CFType.UCF:
+            cf_result = self._rating_user_cf(self.user_item_ratings, similar_dict, user_items_list, recall_num)
+        else:
+            cf_result = self._rating_item_cf(self.user_item_ratings, similar_dict, user_items_list, recall_num)
+        print_cost_time(f"完成{cf_type.value}推理, 当前进程<{os.getpid()}> 生成{len(cf_result)}条记录, 总耗时", func_start_time)
+        logger.info("=" * 88)
+        return cf_result
+
