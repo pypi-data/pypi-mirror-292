f0975fd79ab3081a3a79eb7304c68fde81f43e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:05 +0000

    Add package SPLADERunner/0.1.6/SPLADERunner-0.1.6.tar.gz

---
+++ packages/SPLADERunner/SPLADERunner-0.1.6.tar.gz/SPLADERunner-0.1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SPLADERunner', 
+    version='0.1.6', 
+    packages=find_packages(),
+    install_requires=[
+        'tokenizers',
+        'onnxruntime',
+        'numpy',
+        'requests',
+        'tqdm'
+    ],  
+    author='Prithivi Da',
+    author_email='',
+    description='Ultralight and Fast wrapper for the independent implementation of SPLADE++ models for your search & retrieval pipelines. Models and Library created by Prithivi Da, For PRs and Collaboration to checkout the readme.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PrithivirajDamodaran/SPLADERunner',  
+    license='Apache 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
