07ca52dbfeb41dfef051325089c60557ee4c192e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:15 +0000

    Add package jaraco.abode/6.1.0/jaraco.abode-6.1.0-py3-none-any.whl

---
+++ packages/jaraco.abode/jaraco.abode-6.1.0-py3-none-any.whl/jaraco/abode/config.py
@@ -0,0 +1,18 @@
+import platformdirs
+
+
+class PlatformDirs(platformdirs.PlatformDirs):  # type: ignore[misc, valid-type]
+    """
+    >>> dirs = PlatformDirs(appname='Abode', appauthor=False)
+    >>> alt_udp = getfixture('tmp_path') / 'data' / 'dir'
+    >>> vars(dirs).update(user_data_path=alt_udp)
+    >>> assert dirs.user_data.is_dir()
+    """
+
+    @property
+    def user_data(self):
+        self.user_data_path.mkdir(parents=True, exist_ok=True)
+        return self.user_data_path
+
+
+paths = PlatformDirs(appname='Abode', appauthor=False)
