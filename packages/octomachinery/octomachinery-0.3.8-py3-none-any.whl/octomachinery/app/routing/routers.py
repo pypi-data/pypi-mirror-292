d688f9bb1f1ef7bc9b157aea3157b63c5f4a1186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:50 +0000

    Add package octomachinery/0.3.8/octomachinery-0.3.8-py3-none-any.whl

---
+++ packages/octomachinery/octomachinery-0.3.8-py3-none-any.whl/octomachinery/app/routing/routers.py
@@ -0,0 +1,6 @@
+"""Octomachinery event dispatchers collection proxy."""
+
+# pylint: disable=unused-import
+from ...routing.routers import (  # noqa: F401
+    ConcurrentRouter, GidgetHubRouterBase, NonBlockingConcurrentRouter,
+)
