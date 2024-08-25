8760e2b11f03f213e05b85aa97b8b2a82f85517b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:05:26 +0000

    Add package cstation/0.1.7/cstation-0.1.7.tar.gz

---
+++ packages/cstation/cstation-0.1.7.tar.gz/cstation-0.1.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cstation',
+    version='0.1.7',
+    py_modules=['cstation'],
+    url="https://github.com/ansis-ai/cstation.git",
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cstation = cstation:cli',
+        ],
+    },
+)
