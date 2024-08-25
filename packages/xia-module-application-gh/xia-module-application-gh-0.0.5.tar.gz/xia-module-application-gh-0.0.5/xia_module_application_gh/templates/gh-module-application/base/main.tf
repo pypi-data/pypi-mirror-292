e58bb6c50ec2c46dac18887081f4a054db5d70f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:29 +0000

    Add package xia-module-application-gh/0.0.5/xia-module-application-gh-0.0.5.tar.gz

---
+++ packages/xia-module-application-gh/xia-module-application-gh-0.0.5.tar.gz/xia-module-application-gh-0.0.5/xia_module_application_gh/templates/gh-module-application/base/main.tf
@@ -0,0 +1,15 @@
+provider "github" {
+  owner = lookup(yamldecode(file("../../../config/core/github.yaml")), "github_owner", null)
+}
+
+module "gh_module_application" {
+  source = "../../modules/gh-module-application"
+
+  module_name = "gh-module-application"
+  config_file = "../../../config/core/github.yaml"
+  landscape = local.landscape
+  applications = local.applications
+  environment_dict = local.environment_dict
+  app_env_config = local.app_env_config
+  module_app_to_activate = local.module_app_to_activate
+}
