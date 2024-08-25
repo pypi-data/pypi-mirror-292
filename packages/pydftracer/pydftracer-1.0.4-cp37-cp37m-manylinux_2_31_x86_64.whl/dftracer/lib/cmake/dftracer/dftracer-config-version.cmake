5d66aafc1298e1ebd205e6f5e1f6d1b28ca3b7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:33 +0000

    Add package pydftracer/1.0.4/pydftracer-1.0.4-cp37-cp37m-manylinux_2_31_x86_64.whl

---
+++ packages/pydftracer/pydftracer-1.0.4-cp37-cp37m-manylinux_2_31_x86_64.whl/dftracer/lib/cmake/dftracer/dftracer-config-version.cmake
@@ -0,0 +1,12 @@
+
+#-----------------------------------------------------------------------------
+# Version file for install directory
+#-----------------------------------------------------------------------------
+set(PACKAGE_VERSION 3.0.0)
+
+if ("${PACKAGE_FIND_VERSION_MAJOR}" EQUAL 3)
+    set(PACKAGE_VERSION_COMPATIBLE 1)
+    if ("${PACKAGE_FIND_VERSION_PATCH}" EQUAL 0)
+        set(PACKAGE_VERSION_EXACT 1)
+    endif ()
+endif ()
