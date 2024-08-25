cdc4a9ef07cefb011241986d677b75c539e9450d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:14 +0000

    Add package mathsimplicity/1.0.1/mathsimplicity-1.0.1.tar.gz

---
+++ packages/mathsimplicity/mathsimplicity-1.0.1.tar.gz/mathsimplicity-1.0.1/mathsimplicity/__init__.py
@@ -0,0 +1,17 @@
+from mathsimplicity import (
+    primes,
+    primes_in_range,
+    fraction_to_decimal,
+    decimal_to_fraction,
+    greatest_common_divisor,
+    least_common_multiple
+)
+
+__all__ = [
+    'primes',
+    'primes_in_range',
+    'fraction_to_decimal',
+    'decimal_to_fraction',
+    'greatest_common_divisor',
+    'least_common_multiple'
+]
