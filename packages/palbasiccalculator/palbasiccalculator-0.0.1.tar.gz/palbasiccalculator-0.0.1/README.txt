aa7678d7ae019a237dd5a161dbe798848e741a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:41 +0000

    Add package palbasiccalculator/0.0.1/palbasiccalculator-0.0.1.tar.gz

---
+++ packages/palbasiccalculator/palbasiccalculator-0.0.1.tar.gz/palbasiccalculator-0.0.1/README.txt
@@ -0,0 +1,3 @@
+This is a personal project to test out creating packages in python. Inspiration taken from Joshua Lowe.
+
+This is a very simple calculator which takes two numbers and performs these four operations : Add, Subtract, Multiply and Divice
---
+++ packages/palbasiccalculator/palbasiccalculator-0.0.1.tar.gz/palbasiccalculator-0.0.1/palbasiccalculator/__init__.py
@@ -0,0 +1,12 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num2 - num1
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
