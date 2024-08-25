66ed582225cc2220cf155cfed0925628f981f7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:05:06 +0000

    Add package django-nice-auth/0.1.11/django_nice_auth-0.1.11-py3-none-any.whl

---
+++ packages/django-nice-auth/django_nice_auth-0.1.11-py3-none-any.whl/niceauth/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NiceAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'niceauth'
+    verbose_name = 'NICE Authentication'
