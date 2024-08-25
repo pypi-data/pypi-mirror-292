dd43e08be8f46c511f3336ab3634d3cb4bef5592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:42 +0000

    Add package mtmai/0.3.388/mtmai-0.3.388.tar.gz

---
+++ packages/mtmai/mtmai-0.3.388.tar.gz/mtmai-0.3.388/mtmai/api/serve/codeserver.py
@@ -0,0 +1,38 @@
+import logging
+import threading
+import time
+from pathlib import Path
+
+from fastapi import APIRouter
+from mtmlib.mtutils import bash
+from opentelemetry import trace
+
+# from mtmai.core import coreutils
+# from mtmai.core.config import settings
+
+tracer = trace.get_tracer_provider().get_tracer(__name__)
+logger = logging.getLogger(__name__)
+
+
+router = APIRouter()
+
+
+# 官网： https://coder.com/docs/install
+def start_code_server():
+    config_file = Path.home().joinpath(".config/code-server/config.yaml")
+    if not config_file.exists():
+        logger.warning("code-server 配置文件不存在, 跳过启动: %s", config_file)
+        return
+    # 配置要点：
+    # 1: 明确指定 SHELL 路径，否则在一些受限环境，可能没有默认的shell 变量，导致：“The terminal process "/usr/sbin/nologin" terminated with exit code: 1.”
+    bash(
+        "PORT=8622 PASSWORD=feihuo321 SHELL=/bin/bash code-server --bind-addr=0.0.0.0 &"
+    )
+    time.sleep(2)
+    config_content = config_file.read_text()
+    logger.info("codeserver 配置: %s", config_content)
+
+
+@router.get("/start")
+async def start():
+    threading.Thread(target=start_code_server).start()
