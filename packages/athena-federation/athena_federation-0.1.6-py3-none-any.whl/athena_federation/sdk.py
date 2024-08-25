be7b26991b25749264fae4b147451de1dc1b0968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:27 +0000

    Add package athena-federation/0.1.6/athena_federation-0.1.6-py3-none-any.whl

---
+++ packages/athena-federation/athena_federation-0.1.6-py3-none-any.whl/athena_federation/sdk.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+import athena_federation.models as models
+
+
+class AthenaFederationSDK(ABC):  # pragma: no cover
+    """
+    AthenaFederationSDK is a Python implmementation of the Athena Federated Query SDK.
+
+    All the methods in this class must be implemented for an AWS Lambda function to be able to
+    serve as a Data Connector for Athena.
+
+    The "catalog name" is defined in Athena when you create a data connector and passed to the Lambda function
+    in all requests.
+    "Schema" is interchangeable with database.
+    """
+
+    @abstractmethod
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def PingRequest(self) -> models.PingResponse:
+        """
+        Return metadata about the data connector.
+
+        This is used by Athena to verify the Lambda function is accessible.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def ListSchemasRequest(self) -> models.ListSchemasResponse:
+        """List different available databases for your connector"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def ListTablesRequest(self) -> models.ListTablesResponse:
+        """List available tables in the database"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def GetTableRequest(self) -> models.GetTableResponse:
+        """Get Table metadata"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def GetTableLayoutRequest(self) -> models.GetTableLayoutResponse:
+        """I forget the difference between TableLayout and Splits, but for now we just return a default response."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def GetSplitsRequest(self) -> models.GetSplitsResponse:
+        """The splits don't matter to Athena, it's mostly hints to pass on to ReadRecordsRequest"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def ReadRecordsRequest(
+        self,
+    ) -> Union[models.ReadRecordsResponse, models.RemoteReadRecordsResponse]:
+        """The actual data!"""
+        raise NotImplementedError
