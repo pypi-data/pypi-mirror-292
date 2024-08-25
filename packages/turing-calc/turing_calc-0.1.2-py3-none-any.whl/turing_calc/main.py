fd8de61e7f0db223957a946e73524cc83b25348d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:43 +0000

    Add package turing-calc/0.1.2/turing_calc-0.1.2-py3-none-any.whl

---
+++ packages/turing-calc/turing_calc-0.1.2-py3-none-any.whl/turing_calc/main.py
@@ -0,0 +1,15 @@
+from .calculator import Calculator
+
+
+def main() -> None:
+    """
+    Displays the instructions and starts the calculator.
+
+    :return: None
+    """
+    print("+n, -n, *n, /n, rn=n degree root, m=clear memory, q=quit")
+    Calculator.running()
+
+
+if __name__ == "__main__":
+    main()
