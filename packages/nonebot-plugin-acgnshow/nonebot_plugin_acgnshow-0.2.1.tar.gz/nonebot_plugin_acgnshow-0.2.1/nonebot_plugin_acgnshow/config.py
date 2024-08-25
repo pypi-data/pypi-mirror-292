c8398c360543216e88eb69d7ee02ec58bca4d9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:21 +0000

    Add package nonebot-plugin-acgnshow/0.2.1/nonebot_plugin_acgnshow-0.2.1.tar.gz

---
+++ packages/nonebot-plugin-acgnshow/nonebot_plugin_acgnshow-0.2.1.tar.gz/nonebot_plugin_acgnshow-0.2.1/nonebot_plugin_acgnshow/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from pydantic import BaseModel
+from nonebot import get_plugin_config
+
+RES_PATH = Path(__file__).parent / "res"
+LIST_TEMPLATE_NAME = "template.html"
+DETAILS_TEMPLATE_NAME = "details.html"
+BGIMAGE_PATH = RES_PATH / "bgimage"
+
+
+class ConfigModel(BaseModel):
+    acgnshow_pagesize: int = 8
+    acgnshow_bgimage_path: str = BGIMAGE_PATH
+
+config: ConfigModel = get_plugin_config(ConfigModel)
