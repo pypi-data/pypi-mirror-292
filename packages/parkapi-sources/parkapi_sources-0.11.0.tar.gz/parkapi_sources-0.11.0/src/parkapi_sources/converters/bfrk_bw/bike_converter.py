f535e29b39f95af95b67419ac9ef62043fc56e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:21 +0000

    Add package parkapi-sources/0.11.0/parkapi_sources-0.11.0.tar.gz

---
+++ packages/parkapi-sources/parkapi_sources-0.11.0.tar.gz/parkapi_sources-0.11.0/src/parkapi_sources/converters/bfrk_bw/bike_converter.py
@@ -0,0 +1,22 @@
+"""
+Copyright 2024 binary butterfly GmbH
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt.
+"""
+
+from validataclass.validators import DataclassValidator
+
+from parkapi_sources.models import SourceInfo
+
+from .base_converter import BfrkBasePushConverter
+from .bike_models import BfrkBikeInput
+
+
+class BfrkBwBikePushConverter(BfrkBasePushConverter):
+    source_info = SourceInfo(
+        uid='bfrk_bw_bike',
+        name='Barrierefreie Reisekette Baden-Württemberg: Fahrrad-Parkplätze',
+        public_url='https://www.mobidata-bw.de/dataset/bfrk-barrierefreiheit-an-bw-haltestellen',
+        source_url='https://bfrk-kat-api.efa-bw.de/bfrk_api/fahrradanlagen',
+        has_realtime_data=False,
+    )
+    bfrk_validator = DataclassValidator(BfrkBikeInput)
