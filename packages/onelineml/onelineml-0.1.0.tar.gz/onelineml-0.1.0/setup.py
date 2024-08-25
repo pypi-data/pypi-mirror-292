0b89a222c2333f8615fad6280b1ecbd0bab062ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:26 +0000

    Add package onelineml/0.1.0/onelineml-0.1.0.tar.gz

---
+++ packages/onelineml/onelineml-0.1.0.tar.gz/onelineml-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="onelineml",  # Project name
+    version="0.1.0",
+    description="A tool for creating machine learning pipelines with custom preprocessing and model selection",
+    author="Harivatsa G A",  # Your name
+    author_email="gaharivatsa@gmail.com",  # Your email
+    url="https://github.com/gaharivatsa/easyml",  # GitHub URL
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "joblib"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
