9f8530de55f60239f231eee9b720b446291f0614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:29 +0000

    Add package mlx-llm/1.0.7/mlx_llm-1.0.7-py3-none-any.whl

---
+++ packages/mlx-llm/mlx_llm-1.0.7-py3-none-any.whl/mlx_llm/prompt/__init__.py
@@ -0,0 +1,41 @@
+from ._base import Prompt
+from .gemma import GemmaPrompt
+from .hermes import HermesPrompt
+from .llama import LLaMA2Prompt, LLaMA3Prompt, TinyLLaMAPrompt
+from .mistral import MistralPrompt, StarlingPrompt
+from .openelm import OpenELMPrompt
+from .phi import Phi3Prompt
+
+PROMPT_ENTRYPOINTS = {
+    "llama2": LLaMA2Prompt,
+    "llama3": LLaMA3Prompt,
+    "phi3": Phi3Prompt,
+    "mistral": MistralPrompt,
+    "tinyllama": TinyLLaMAPrompt,
+    "gemma": GemmaPrompt,
+    "openelm": OpenELMPrompt,
+    "starling": StarlingPrompt,
+    "hermes": HermesPrompt,
+}
+
+
+def list_prompts() -> None:
+    """List all available prompts."""
+    print("Available prompts based on model families:")
+    for model_family in list(PROMPT_ENTRYPOINTS.keys()):
+        print(f"\t- {model_family}")
+
+
+def create_prompt(model_family: str, system: str = "") -> Prompt:
+    """Create prompt based on model family
+
+    Args:
+        model_name (str): model name
+        system (str): system prompt
+
+    Returns:
+        Prompt: prompt class
+    """
+
+    assert model_family in PROMPT_ENTRYPOINTS.keys(), f"Model family {model_family} not found in available prompts."
+    return PROMPT_ENTRYPOINTS[model_family](system=system)  # type: ignore
