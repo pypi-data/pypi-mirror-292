9d89acd56978709b8c00a70a5b31056646180540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:05 +0000

    Add package bishograd/0.1.0/bishograd-0.1.0-py3-none-any.whl

---
+++ packages/bishograd/bishograd-0.1.0-py3-none-any.whl/bishograd/engine.py
@@ -0,0 +1,128 @@
+import math
+
+
+class Hako:
+    def __init__(self, data, _children=(), _op=""):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self.grad = 0
+        self._backward = lambda: None
+
+    def __repr__(self) -> str:
+        return f"Hako(data : {self.data:.2f})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Hako) else Hako(other)
+        out = Hako(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Hako) else Hako(other)
+        out = Hako(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Hako(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other ** (-1)
+
+    def __rtruediv__(self, other):
+        return other * self ** (-1)
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def tanh(self):
+        x = self.data
+        tanh_value = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Hako(tanh_value, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - tanh_value**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = Hako(0 if self.data < 0 else self.data, (self,), "ReLU")
+
+        def _backward():
+            self.grad += out.grad if out.data > 0 else 0
+
+        out._backward = _backward
+        return out
+
+    def sigmoid(self):
+        out = Hako(1 / (1 + math.exp(-self.data)), (self,), "Sig")
+
+        def _backward():
+            self.grad += out.data * (1 - out.data) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def linear(self):
+        out = Hako(self.data, (self,), "linear")
+
+        def _backward():
+            self.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Hako(math.exp(x), (self,), "exp")
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        vis = set()
+
+        def build_topo(curr):
+            if curr not in vis:
+                vis.add(curr)
+                for child in curr._prev:
+                    build_topo(child)
+                topo.append(curr)
+
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
---
+++ packages/bishograd/bishograd-0.1.0-py3-none-any.whl/bishograd/nn.py
@@ -0,0 +1,107 @@
+import random
+from .engine import Hako
+
+
+class Neuron:
+    def __init__(self, nin, activation="relu"):
+        self.w = [Hako(random.uniform(-0.5, 0.5)) for _ in range(nin)]
+        self.b = Hako(random.uniform(-0.5, 0.5))
+        self.activation = activation
+
+    def __call__(self, x):
+        if not isinstance(x, (list, tuple)):
+            x = [x]
+        act = sum(wi * xi for wi, xi in zip(self.w, x)) + self.b
+        if self.activation == "relu":
+            out = act.relu()
+        elif self.activation == "sigmoid":
+            out = act.sigmoid()
+        elif self.activation == "tanh":
+            out = act.tanh()
+        elif self.activation == "linear":
+            out = act.linear()
+        else:
+            raise ValueError(f"Unsupported activation function: {self.activation}")
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+
+class Layer:
+    def __init__(self, nin, nout, activation="relu") -> None:
+        self.neurons = [Neuron(nin, activation) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
+
+class MLP:
+    def __init__(self, nin, nouts, activations=None) -> None:
+        sz = [nin] + nouts
+        if activations is None:
+            activations = ["linear"] * (len(nouts) - 1) + ["sigmoid"]
+        self.layers = [
+            Layer(sz[i], sz[i + 1], activation=activations[i])
+            for i in range(len(nouts))
+        ]
+
+    def __call__(self, x):
+        if not isinstance(x, (list, tuple)):
+            x = [x]
+        for layers in self.layers:
+            x = layers(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def zero_grad(self):
+        for param in self.parameters():
+            param.grad = 0.0
+
+    def train(
+        self,
+        epochs: int = 50,
+        lr_rate: float = 0.001,
+        x_input=None,
+        y_output=None,
+        stats: bool = True,
+    ):
+        """
+        Trains the MLP model with given hyperparameters.
+
+        Args:
+            epochs (int) : The number of training epochs. Default is 50
+            lr_rate (float): The learning rate for gradient descent. Default is 0.001.
+            x_input (list): The input data for training. Default is None.
+            y_output (list): The target output data for training. Default is None.
+            stats (bool): Whether to print training statistics. Default is True.
+
+        Raises:
+            ValueError: If x_input or y_output is None.
+
+        Returns:
+            None
+        """
+        if stats:
+            print(f"{'-'*8}Training Model{'-'*8}")
+            print(
+                f"Parameters = {len(self.parameters())}\nEpochs = {epochs}\nLearning Rate: {lr_rate}"
+            )
+            print(f"{'-'*30}")
+        if x_input is None or y_output is None:
+            raise ValueError("Invalid input for training data")
+        for epoch in range(epochs + 1):
+            y_pred = [self(x) for x in x_input]
+            loss = sum((y1 - y2) ** 2 for y1, y2 in zip(y_pred, y_output))
+            self.zero_grad()
+            loss.backward()
+            for param in self.parameters():
+                param.grad = max(min(param.grad, 1), -1)
+                param.data -= lr_rate * param.grad
+            print(f"epoch: {epoch}/{epochs} ---- Loss: {loss.data:.4f}")
