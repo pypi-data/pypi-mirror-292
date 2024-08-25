c88a9a7096015fc0439c59358203105125bda42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 15:04:51 +0000

    Add package ytnoti/2.0.1/ytnoti-2.0.1-py3-none-any.whl

---
+++ packages/ytnoti/ytnoti-2.0.1-py3-none-any.whl/ytnoti/enums.py
@@ -0,0 +1,34 @@
+"""
+This module defines Enum classes used in the package.
+"""
+
+from enum import Enum
+
+__all__ = ["NotificationKind", "ServerMode"]
+
+
+class NotificationKind(Enum):
+    """
+    Enum for the kind of notification
+    """
+
+    UPLOAD = 0
+    """Upload notification"""
+
+    EDIT = 1
+    """Edit notification"""
+
+    ANY = 2
+    """Any notification"""
+
+
+class ServerMode(Enum):
+    """
+    Enum for the server mode
+    """
+
+    RUN = "run"
+    """Create an event loop and run the server"""
+
+    SERVE = "serve"
+    """Serve the server in the existing event loop"""
---
+++ packages/ytnoti/ytnoti-2.0.1-py3-none-any.whl/ytnoti/models/video.py
@@ -0,0 +1,59 @@
+"""
+This module contains the dataclasses for the video model.
+"""
+
+from dataclasses import dataclass
+from datetime import datetime
+
+__all__ = ["Video", "Channel", "Timestamp"]
+
+
+@dataclass
+class Channel:
+    """
+    Represents a YouTube channel
+    """
+
+    id: str
+    """The unique ID of the channel"""
+
+    name: str
+    """The name of the channel"""
+
+    url: str
+    """The URL of the channel"""
+
+
+@dataclass
+class Timestamp:
+    """
+    Represents the timestamps of a video
+    """
+
+    published: datetime
+    """The published time of the video"""
+
+    updated: datetime
+    """The updated time of the video"""
+
+
+@dataclass
+class Video:
+    """
+    Represents a YouTube video
+    """
+
+    id: str
+    """The unique ID of the video"""
+
+    title: str
+    """The title of the video"""
+
+    url: str
+    """The URL of the video"""
+
+    timestamp: Timestamp
+    """The timestamps of the video"""
+
+    channel: Channel
+    """The channel of the video"""
