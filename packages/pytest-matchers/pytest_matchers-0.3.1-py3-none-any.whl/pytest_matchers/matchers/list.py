1b61906e106a0e8599535d19dae919f1724d1bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:57 +0000

    Add package pytest-matchers/0.3.1/pytest_matchers-0.3.1-py3-none-any.whl

---
+++ packages/pytest-matchers/pytest_matchers-0.3.1-py3-none-any.whl/pytest_matchers/matchers/list.py
@@ -0,0 +1,42 @@
+from typing import Any, Callable, Type
+
+from pytest_matchers.matchers import IsInstance, Matcher
+from pytest_matchers.matchers.matcher_factory import matcher
+from pytest_matchers.utils.matcher_utils import (
+    is_instance_matcher,
+    length_matcher,
+    matches_or_none,
+    partial_matches_or_none,
+)
+from pytest_matchers.utils.repr_utils import concat_reprs
+
+
+@matcher
+class List(Matcher):
+    def __init__(
+        self,
+        match_type: Type = None,
+        *,
+        length: int = None,
+        min_length: int = None,
+        max_length: int = None,
+    ):
+        super().__init__()
+        self._is_instance_matcher = is_instance_matcher(match_type)
+        self._length_matcher = length_matcher(length, min_length, max_length)
+
+    def matches(self, value: Any) -> bool:
+        return self._matches_type(value) and matches_or_none(self._length_matcher, value)
+
+    def _matches_type(self, value: Any) -> bool:
+        return IsInstance(list) == value and all(map(self._type_matcher(), value))
+
+    def _type_matcher(self) -> Callable[[Any], bool]:
+        return partial_matches_or_none(self._is_instance_matcher)
+
+    def __repr__(self) -> str:
+        return concat_reprs(
+            "To be a list",
+            self._is_instance_matcher,
+            self._length_matcher,
+        )
