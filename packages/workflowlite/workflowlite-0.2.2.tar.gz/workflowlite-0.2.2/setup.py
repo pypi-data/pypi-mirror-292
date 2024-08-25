07490139c5268e655dfd5369b27c751db7d7c265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:22 +0000

    Add package workflowlite/0.2.2/workflowlite-0.2.2.tar.gz

---
+++ packages/workflowlite/workflowlite-0.2.2.tar.gz/workflowlite-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="workflowlite",
+    version="0.2.2",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Ed Huang",
+    author_email="dongxuhuang@yahoo.com",
+    description="a simple workflow engine",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/c4pt0r/workflowlite",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
