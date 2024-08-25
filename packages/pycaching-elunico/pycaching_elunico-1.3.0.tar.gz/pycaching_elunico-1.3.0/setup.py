1a717022fa23ba3b67ddeb4eaba634e3b368d1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:18 +0000

    Add package pycaching-elunico/1.3.0/pycaching_elunico-1.3.0.tar.gz

---
+++ packages/pycaching-elunico/pycaching_elunico-1.3.0.tar.gz/pycaching_elunico-1.3.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycaching-elunico",
+    version="1.3.0",
+    author="Thomas Povinelli",
+    author_email="tompov227@gmail.com",
+    description="A utility for file system caching",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/elunico/PyCaching",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.11",
+)
