dc98f3cb222742100add870b1e740a95b9048fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:16 +0000

    Add package spaceone-cost-analysis/2.0.dev150/spaceone_cost_analysis-2.0.dev150-py3-none-any.whl

---
+++ packages/spaceone-cost-analysis/spaceone_cost_analysis-2.0.dev150-py3-none-any.whl/spaceone/cost_analysis/plugin/data_source/model/job_response.py
@@ -0,0 +1,24 @@
+from typing import List, Union
+from pydantic import BaseModel
+
+__all__ = ["TasksResponse"]
+
+
+class Task(BaseModel):
+    task_options: dict
+
+
+class Changed(BaseModel):
+    start: str
+    end: Union[str, None] = None
+    filter: Union[dict, None] = None
+
+
+class SyncedAccount(BaseModel):
+    account_id: str
+
+
+class TasksResponse(BaseModel):
+    tasks: List[Task]
+    changed: List[Changed] = []
+    synced_accounts: Union[List[SyncedAccount], None] = None
