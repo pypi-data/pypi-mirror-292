8b57066bbf5f58b7848536b98fe6029085d61651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:52 +0000

    Add package datasette-sqlite-lembed/0.0.1a8/datasette_sqlite_lembed-0.0.1a8-py3-none-any.whl

---
+++ packages/datasette-sqlite-lembed/datasette_sqlite_lembed-0.0.1a8-py3-none-any.whl/datasette_sqlite_lembed/__init__.py
@@ -0,0 +1,12 @@
+
+from datasette import hookimpl
+import sqlite_lembed
+
+__version__ = "0.0.1a8"
+__version_info__ = tuple(__version__.split("."))
+
+@hookimpl
+def prepare_connection(conn):
+  conn.enable_load_extension(True)
+  sqlite_lembed.load(conn)
+  conn.enable_load_extension(False)
