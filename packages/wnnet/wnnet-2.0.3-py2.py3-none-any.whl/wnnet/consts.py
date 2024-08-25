0e5b79a085c50a9d90e4b50494b13269db191792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:07 +0000

    Add package wnnet/2.0.3/wnnet-2.0.3-py2.py3-none-any.whl

---
+++ packages/wnnet/wnnet-2.0.3-py2.py3-none-any.whl/wnnet/consts.py
@@ -0,0 +1,39 @@
+"""This module constants for use in the wnnet package.  The module
+also includes all constants from
+`gslconsts <https://gslconsts.readthedocs.io/en/latest/gslconsts.html#module-gslconsts.consts>`_.
+"""
+
+import numpy as np
+import gslconsts.consts as gc
+
+# Conversions for use with the package.
+
+#: Speed of light in vacuum (cm/s)
+c = gc.GSL_CONST_CGSM_SPEED_OF_LIGHT
+
+#: Boltzmann's constant (ergs/K)
+k_B = gc.GSL_CONST_CGSM_BOLTZMANN
+
+#: Electron mass (g)
+m_e = gc.GSL_CONST_CGSM_MASS_ELECTRON
+
+#: Amu mass (g)
+u = gc.GSL_CONST_CGSM_UNIFIED_ATOMIC_MASS
+
+#: Avogadro's number
+N_A = gc.GSL_CONST_NUM_AVOGADRO
+
+#: Planck's constant divided by 2pi (ergs/s)
+hbar = gc.GSL_CONST_CGSM_PLANCKS_CONSTANT_HBAR
+
+#: Conversion factor from MeVs to ergs
+MeV_to_ergs = 1.0e6 * gc.GSL_CONST_CGSM_ELECTRON_VOLT
+
+#: Conversion factor from ergs to MeVs
+ergs_to_MeV = 1.0 / MeV_to_ergs
+
+#: Electron rest mass (MeV)
+m_e_in_MeV = m_e * np.power(c, 2) * ergs_to_MeV
+
+#: Amu rest mass (MeV)
+m_u_in_MeV = u * np.power(c, 2) * ergs_to_MeV
