d9478e824cd328abfa47116a51663e0ecbcd5bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:50 +0000

    Add package graphbook/0.5.1/graphbook-0.5.1-py3-none-any.whl

---
+++ packages/graphbook/graphbook-0.5.1-py3-none-any.whl/graphbook/resources/base.py
@@ -0,0 +1,39 @@
+class Resource:
+    """
+    The base class for all resources in Graphbook. All resources should be a descendant from this class. Also acts as the resource for string values.
+    """
+    Parameters = {"val": {"type": "string"}}
+    Category = "Util"
+
+    def __init__(self, val=""):
+        self.val = val
+
+    def value(self):
+        return self.val
+
+    def __str__(self):
+        return str(self.val)
+
+class NumberResource(Resource):
+    """
+    The number resource. This will parse the incoming value as a float.
+    """
+    Parameters = {"val": {"type": "number"}}
+    Category = "Util"
+
+    def __init__(self, val):
+        super().__init__(val)
+        
+    def value(self):
+        return float(self.val)
+
+
+class FunctionResource(Resource):
+    """
+    The function resource. This will parse the incoming value as a function.
+    """
+    Parameters = {"val": {"type": "function"}}
+    Category = "Util"
+
+    def __init__(self, val):
+        super().__init__(val)
