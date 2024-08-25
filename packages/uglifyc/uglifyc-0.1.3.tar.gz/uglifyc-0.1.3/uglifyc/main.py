b444e761fb15b5f7b9d6e1f0cddf2b6c1f03c50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:43 +0000

    Add package uglifyc/0.1.3/uglifyc-0.1.3.tar.gz

---
+++ packages/uglifyc/uglifyc-0.1.3.tar.gz/uglifyc-0.1.3/uglifyc/main.py
@@ -0,0 +1,45 @@
+import click
+
+def get_max_width(file):
+    max_width = 0
+    with open(file, "r") as f:
+        for line in f:
+            if len(line) > max_width:
+                max_width = len(line)
+    return max_width
+
+def get_first_normal_char_from_right(s, char_list, space_number):
+    for index in range(len(s) - 1, -1, -1):
+        if s[index] not in char_list:
+            return s[:index+1] + ' '*space_number + s[index+1:]
+    return s
+
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.option('-o', '--output', type=click.Path(), help='Output file for the uglified code.')
+def uglify(input_file, output):
+    """
+    Uglifies a C file.
+    """
+    # Set the output file name
+    if output is None:
+        output = input_file.replace('.c', '_uglified.c')
+
+    char_list = ["}", ";", ")", "\n", " ", "{"]
+
+    max_width = get_max_width(input_file)
+
+    uglified_code = ""
+
+    with open(input_file, "r") as f:
+        for line in f:
+            space_number = max_width - len(line)
+            uglified_code += get_first_normal_char_from_right(line, char_list, space_number + 10)
+
+    with open(output, 'w') as outfile:
+        outfile.write(uglified_code)
+
+    click.echo(f"Uglified code written to {output}")
+
+if __name__ == '__main__':
+    uglify()
