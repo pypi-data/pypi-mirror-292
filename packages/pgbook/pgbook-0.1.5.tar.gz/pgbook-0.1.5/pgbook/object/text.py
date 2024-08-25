a6315cb409ce6ff416d7fc496e51c0fc488787fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 14:04:48 +0000

    Add package pgbook/0.1.5/pgbook-0.1.5.tar.gz

---
+++ packages/pgbook/pgbook-0.1.5.tar.gz/pgbook-0.1.5/pgbook/object/text.py
@@ -0,0 +1,8 @@
+import pygame
+class BaseText:
+    def __init__(self):
+        self.font= pygame.font.Font('NotoFont.ttf')
+        #self.font.size() 
+        
+if __name__=='__main__':
+    import pkg_resources
