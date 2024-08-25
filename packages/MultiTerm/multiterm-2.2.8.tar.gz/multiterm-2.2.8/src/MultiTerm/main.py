2000a3e2be27b5b8f489736b4a4419cfd95cd7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 03:04:16 +0000

    Add package MultiTerm/2.2.8/multiterm-2.2.8.tar.gz

---
+++ packages/MultiTerm/multiterm-2.2.8.tar.gz/multiterm-2.2.8/src/MultiTerm/main.py
@@ -0,0 +1,18 @@
+"""A simple test application for testing new features."""
+from classes import *
+width,height = os.get_terminal_size()
+screen1 = Screen((width//2,height),(0,0))
+screen2 = Screen(((width)//2,height),((width+1)//2,0))
+screens = cluster()
+screens.add_screen(screen1)
+screens.add_screen(screen2)
+cursors = [[0,0],[0,0]]
+screen1.objects.append(Interactible_Object((0,0),"Hello World!"))
+def main(screen:Screen,event,focus:int,screens:cluster):
+    if focus == screens.index(screen):
+        if type(event) == asciimaticsEvent.MouseEvent:
+            screen.fill(f"{Fore.BLUE}{Back.BLUE}█{RESET}")
+    else:
+        screen.fill(f"{Back.BLACK} {RESET}")
+
+screens.loop(main)
