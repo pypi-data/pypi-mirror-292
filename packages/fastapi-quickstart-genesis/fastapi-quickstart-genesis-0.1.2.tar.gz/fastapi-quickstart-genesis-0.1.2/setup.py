a0c4d5222322b44a21e68fc08e1cf2af7bd44da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-25 04:04:16 +0000

    Add package fastapi-quickstart-genesis/0.1.2/fastapi-quickstart-genesis-0.1.2.tar.gz

---
+++ packages/fastapi-quickstart-genesis/fastapi-quickstart-genesis-0.1.2.tar.gz/fastapi-quickstart-genesis-0.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fastapi-quickstart-genesis",
+    version="0.1.2",
+    author="JCN",
+    author_email="ackmanb@gmail.com",
+    description="A quick start tool for FastAPI projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ackmanb/fastapi-quickstart",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"fastapi_quickstart": ["templates/*"]},
+    install_requires=[
+        "fastapi>=0.68.0",
+        "uvicorn>=0.15.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "fastapi-quickstart=fastapi_quickstart.main:main",
+        ],
+    },
+)
