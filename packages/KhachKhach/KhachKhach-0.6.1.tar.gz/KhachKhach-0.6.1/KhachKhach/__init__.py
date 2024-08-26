c134bd0f6d2bfbb8d7f7b55c841b60f57a456d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 16:13:57 +0000

    Add package KhachKhach/0.6.1/KhachKhach-0.6.1.tar.gz

---
+++ packages/KhachKhach/KhachKhach-0.6.1.tar.gz/KhachKhach-0.6.1/KhachKhach/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+from KhachKhach.KhachKhach import BoundingBoxProcessor
+from KhachKhach.KhachKhach import DetectionAnnotation
+from KhachKhach.KhachKhach import ExtendedArrayProcessor
+from KhachKhach.KhachKhach import FileAppender
+from KhachKhach.KhachKhach import FrameExtractor
+from KhachKhach.KhachKhach import KeypointAnnotator
+from KhachKhach.KhachKhach import XYNExtractor
+
+
