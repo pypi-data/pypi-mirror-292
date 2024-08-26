6de2f70de5b470de62e6688edb02020b8f86df6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:15 +0000

    Add package collada2obj/0.0.1/collada2obj-0.0.1-py3-none-any.whl

---
+++ packages/collada2obj/collada2obj-0.0.1-py3-none-any.whl/collada2obj/__init__.py
@@ -0,0 +1,7 @@
+from .MeshConverter import MeshConverter
+from .TechniqueCommon import TechniqueCommon, ParameterDescription
+
+__all__ = [
+    'MeshConverter',
+    'TechniqueCommon', 'ParameterDescription',
+]
