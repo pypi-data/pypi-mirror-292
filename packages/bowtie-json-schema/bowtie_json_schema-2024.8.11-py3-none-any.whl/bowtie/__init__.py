9ebb8052c72c00ffb0cba6ce8d76ba29949a0ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:05 +0000

    Add package bowtie-json-schema/2024.8.11/bowtie_json_schema-2024.8.11-py3-none-any.whl

---
+++ packages/bowtie-json-schema/bowtie_json_schema-2024.8.11-py3-none-any.whl/bowtie/__init__.py
@@ -0,0 +1,12 @@
+"""
+A meta-validator for the JSON Schema specification.
+"""
+
+from url import URL
+
+HOMEPAGE = URL.parse("https://bowtie.report/")
+DOCS = URL.parse("https://docs.bowtie.report/")
+
+GITHUB = URL.parse("https://github.com/")
+ORG = GITHUB / "bowtie-json-schema"
+REPO = ORG / "bowtie"
