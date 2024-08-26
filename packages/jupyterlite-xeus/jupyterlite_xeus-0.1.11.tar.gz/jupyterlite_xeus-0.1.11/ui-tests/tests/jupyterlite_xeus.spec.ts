7b36ea6cbdec374b8d1a587d00d9848f9030db26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 15:07:22 +0000

    Add package jupyterlite-xeus/0.1.11/jupyterlite_xeus-0.1.11.tar.gz

---
+++ packages/jupyterlite-xeus/jupyterlite_xeus-0.1.11.tar.gz/jupyterlite_xeus-0.1.11/ui-tests/tests/jupyterlite_xeus.spec.ts
@@ -0,0 +1,47 @@
+import { test } from '@jupyterlab/galata';
+
+import { expect } from '@playwright/test';
+
+test.describe('General Tests', () => {
+  test.beforeEach(({ page }) => {
+    page.setDefaultTimeout(600000);
+
+    page.on('console', message => {
+      console.log('CONSOLE MSG ---', message.text());
+    });
+  });
+
+  test('Launcher should contain xeus-python and xeus-lua kernels', async ({
+    page
+  }) => {
+    await page.goto('lab/index.html');
+
+    const launcher = page.locator('.jp-Launcher');
+    expect(await launcher.screenshot()).toMatchSnapshot(
+      'jupyter-xeus-launcher.png'
+    );
+  });
+
+  test('xeus-python should execute some code', async ({ page }) => {
+    await page.goto('lab/index.html');
+
+    // Launch a Python notebook
+    const xpython = page.locator('[title="Python 3.11 (XPython)"]').first();
+    await xpython.click();
+
+    // Wait for kernel to be idle
+    await page.locator('#jp-main-statusbar').getByText('Idle').waitFor();
+
+    await page.notebook.addCell('code', 'import bqplot; print("ok")');
+    await page.notebook.runCell(1);
+
+    // Wait for kernel to be idle
+    await page.locator('#jp-main-statusbar').getByText('Idle').waitFor();
+
+    const cell = await page.notebook.getCellOutput(1);
+
+    expect(await cell?.screenshot()).toMatchSnapshot(
+      'jupyter-xeus-execute.png'
+    );
+  });
+});
