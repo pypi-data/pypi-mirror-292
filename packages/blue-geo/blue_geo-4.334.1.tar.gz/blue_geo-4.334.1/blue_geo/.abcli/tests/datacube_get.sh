af9e27fb01ca5a22df05e2101c6f90893f03f6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:37 +0000

    Add package blue-geo/4.334.1/blue_geo-4.334.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.334.1.tar.gz/blue_geo-4.334.1/blue_geo/.abcli/tests/datacube_get.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+function test_blue_geo_datacube_get_catalog() {
+    abcli_assert \
+        $(blue_geo_datacube_get catalog void) \
+        void
+    [[ $? -ne 0 ]] && return 1
+
+    abcli_assert \
+        $(blue_geo_datacube_get catalog datacube-generic) \
+        generic
+    [[ $? -ne 0 ]] && return 1
+
+    abcli_assert \
+        $(blue_geo_datacube_get catalog $BLUE_GEO_TEST_DATACUBE_FIRMS_AREA) \
+        firms
+}
+
+function test_blue_geo_datacube_get_template() {
+    abcli_assert \
+        $(blue_geo_datacube_get template void) \
+        unknown-template
+    [[ $? -ne 0 ]] && return 1
+
+    abcli_assert \
+        $(blue_geo_datacube_get template datacube-generic) \
+        unknown-template
+    [[ $? -ne 0 ]] && return 1
+
+    abcli_assert \
+        $(blue_geo_datacube_get template $BLUE_GEO_TEST_DATACUBE_FIRMS_AREA) \
+        $BLUE_GEO_QGIS_TEMPLATE_FIRMS_AREA
+
+    abcli_assert \
+        $(blue_geo_datacube_get template $BLUE_GEO_TEST_DATACUBE_UKRAINE_TIMEMAP) \
+        $BLUE_GEO_QGIS_TEMPLATE_UKRAINE_TIMEMAP
+}
