cd1fca570d128c5299a550c7fe279cae6b3bbccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:47 +0000

    Add package gai-sdk/0.257/gai_sdk-0.257-py3-none-any.whl

---
+++ packages/gai-sdk/gai_sdk-0.257-py3-none-any.whl/gai/scripts/gai_docker_stop.py
@@ -0,0 +1,8 @@
+import os
+from rich.console import Console
+console=Console()
+from gai.scripts._docker_utils import _docker_stop
+
+def docker_stop(component):
+    console.print(f"Stopping {component}...")
+    _docker_stop(component_name=component)
