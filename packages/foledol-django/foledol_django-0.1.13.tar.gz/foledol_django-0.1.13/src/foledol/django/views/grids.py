00d8c2e036cf71db0a2d5a7d426d6dc4adb1921c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:45 +0000

    Add package foledol-django/0.1.13/foledol_django-0.1.13.tar.gz

---
+++ packages/foledol-django/foledol_django-0.1.13.tar.gz/foledol_django-0.1.13/src/foledol/django/views/grids.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
+from foledol.django.tools.table import TableColumn, TableView
+
+from ..models import Grid
+
+
+class GridTables(TableView):
+    def __init__(self):
+        super().__init__(Grid, [
+            TableColumn('name', "Nom", sortable=True),
+            TableColumn('table', "Table", method="table_as_str")
+        ], path='django/grids', search=True, sort='name_asc')
+        self.update = 'django:grid_update'
+        self.create = 'django:grid_create'
+
+    def select(self, grids, search, order_by):
+        if len(search) > 0:
+            grids = Grid.objects.filter(name__contains=search)
+        if order_by == 'name_asc':
+            return grids.order_by('name')
+        elif order_by == 'name_desc':
+            return grids.order_by('-name')
+        return grids
+
+
+@login_required
+def grid_list(request):
+    if not request.user.is_staff:
+        return HttpResponseForbidden()
+    return GridTables().render(request)
+
+
+def grid_renumber(grid):
+    order = 10
+    for column in grid.column_set.all().order_by('order'):
+        if column.order != order:
+            column.order = order
+            column.save()
+        order += 10
+
+
+
+
+
+
