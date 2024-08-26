b4dbc8ff95686bda9b09a0b1c550503c935474ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:30 +0000

    Add package zyx/0.2.64/zyx-0.2.64.tar.gz

---
+++ packages/zyx/zyx-0.2.64.tar.gz/zyx-0.2.64/zyx/__init__.pyi
@@ -0,0 +1,48 @@
+__all__ = [
+    "BaseModel",
+    "Field",
+    "hf",
+    "logger",
+    "app",
+    "legacy_agents",
+    "cli",
+    "chainofthought",
+    "classify",
+    "completion",
+    "embedding",
+    "image",
+    "code",
+    "delegate",
+    "extract",
+    "function",
+    "generate",
+    "qdrant",
+    "sql",
+    "transcribe",
+    "speak",
+    "zyxModuleLoader",
+]
+
+# --- zyx ----------------------------------------------------------------
+
+from .core.ext import BaseModel, Field, zyxModuleLoader
+from .client.agents import Agents as legacy_agents
+from .client.app import cli, app
+from .client.main import completion
+from .client.fn import (
+    classify,
+    chainofthought,
+    delegate,
+    code,
+    extract,
+    function,
+    generate,
+)
+from .data.qdr import Qdrant as qdrant
+from .data.main import SQLModelVectorDB as sql
+from .multimodal.main import image
+from .multimodal.main import openai_speech_to_text as transcribe
+from .multimodal.main import openai_text_to_speech as speak
+from litellm.main import embedding
+from huggingface_hub.inference._client import InferenceClient as hf
+from loguru import logger
