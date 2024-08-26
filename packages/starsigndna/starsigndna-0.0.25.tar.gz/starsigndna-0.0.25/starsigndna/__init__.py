f693cde25738fefafd8aafdb2f8c352455be18bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 15:07:34 +0000

    Add package starsigndna/0.0.25/starsigndna-0.0.25.tar.gz

---
+++ packages/starsigndna/starsigndna-0.0.25.tar.gz/starsigndna-0.0.25/starsigndna/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Cucumber."""
+
+__author__ = """Christian Bope"""
+__email__ = 'chrisbop@uio.no'
+__version__ = '0.0.25'
+
+
+from .refit import refit
+from .denovo import  denovo
