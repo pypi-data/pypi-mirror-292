1255c7b91ec04d056200f0d5cbc351d38976606b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:02 +0000

    Add package compose-sdk/0.10.0/compose_sdk-0.10.0-py3-none-any.whl

---
+++ packages/compose-sdk/compose_sdk-0.10.0-py3-none-any.whl/compose_sdk/core/ui/components/displayInteraction.py
@@ -0,0 +1,142 @@
+from ..constants import INTERACTION_TYPE, TYPE, Nullable, DISPLAY_UTILS, ComponentReturn
+from ...utils import Utils
+
+
+def display_text(text: str, *, style: Nullable.Style = None) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {"id": id, "style": style, "properties": {"text": text}},
+        "hooks": None,
+        "type": TYPE.DISPLAY_TEXT,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_header(text: str, *, style: Nullable.Style = None) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {"id": id, "style": style, "properties": {"text": text}},
+        "hooks": None,
+        "type": TYPE.DISPLAY_HEADER,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_json(
+    json: DISPLAY_UTILS.Json,
+    *,
+    label: Nullable.Str = None,
+    description: Nullable.Str = None,
+    style: Nullable.Style = None
+) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": style,
+            "properties": {
+                "label": label,
+                "description": description,
+                "json": json,
+            },
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_JSON,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_spinner(
+    *, text: Nullable.Str = None, style: Nullable.Style = None
+) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": style,
+            "properties": {
+                "text": text,
+            },
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_SPINNER,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_code(
+    code: str,
+    *,
+    label: Nullable.Str = None,
+    description: Nullable.Str = None,
+    style: Nullable.Style = None
+) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": style,
+            "properties": {
+                "code": code,
+                "label": label,
+                "description": description,
+            },
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_CODE,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_image(src: str, *, style: Nullable.Style = None) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": style,
+            "properties": {
+                "src": src,
+            },
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_IMAGE,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_markdown(markdown: str, *, style: Nullable.Style = None) -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": style,
+            "properties": {
+                "markdown": markdown,
+            },
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_MARKDOWN,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
+
+
+def display_none() -> ComponentReturn:
+    id = Utils.generate_id()
+
+    return {
+        "model": {
+            "id": id,
+            "style": None,
+            "properties": {},
+        },
+        "hooks": None,
+        "type": TYPE.DISPLAY_NONE,
+        "interactionType": INTERACTION_TYPE.DISPLAY,
+    }
