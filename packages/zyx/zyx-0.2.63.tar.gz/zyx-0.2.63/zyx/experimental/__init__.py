a6fa5e858941e6e471fc655096d4e1da4e65ea2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:29 +0000

    Add package zyx/0.2.63/zyx-0.2.63.tar.gz

---
+++ packages/zyx/zyx-0.2.63.tar.gz/zyx-0.2.63/zyx/experimental/__init__.py
@@ -0,0 +1,9 @@
+from crewai_tools.tools.base_tool import BaseTool
+from langchain_core.tools.convert import tool
+
+from . import tools as tools
+from ..core.ext import zyxModuleLoader
+
+class agents(zyxModuleLoader):
+    pass
+agents.init("zyx.experimental.agents", "Agents")
