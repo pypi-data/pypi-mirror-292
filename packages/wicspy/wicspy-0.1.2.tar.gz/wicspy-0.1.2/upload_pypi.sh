9415276d62b0209d89df07a97142cd68c583656d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 16:13:58 +0000

    Add package wicspy/0.1.2/wicspy-0.1.2.tar.gz

---
+++ packages/wicspy/wicspy-0.1.2.tar.gz/wicspy-0.1.2/upload_pypi.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+###
+ # @Author: wicsp wicspa@gmail.com
+ # @Date: 2024-06-05 17:38:35
+ # @LastEditors: wicsp wicspa@gmail.com
+ # @LastEditTime: 2024-08-26 18:03:00
+ # @FilePath: /wicspy/upload_pypi.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2024 by wicsp, All Rights Reserved. 
+### 
+
+# 检查是否提供了 commit message
+if [ -z "$1" ]; then
+  echo "Missing Commit Message!"
+  exit 1
+fi
+
+COMMIT_MESSAGE=$1
+
+# 删除旧的构建文件
+rm -rf ./dist
+
+# 创建新的构建文件
+# 上传到 PyPI
+rye publish
+
+# 提交到 git
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push
