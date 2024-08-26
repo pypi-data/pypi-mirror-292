ce8dc1c5d33092202458b91b8217343bca5c2306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:56 +0000

    Add package mathsimplicity/1.1.0/mathsimplicity-1.1.0-py3-none-any.whl

---
+++ packages/mathsimplicity/mathsimplicity-1.1.0-py3-none-any.whl/mathsimplicity/__init__.py
@@ -0,0 +1,9 @@
+from .mathsimplicity import (
+    primes,
+    primes_in_range,
+    fraction_to_decimal,
+    decimal_to_fraction,
+    greatest_common_divisor,
+    least_common_multiple,
+    arithmetic_operations
+)
