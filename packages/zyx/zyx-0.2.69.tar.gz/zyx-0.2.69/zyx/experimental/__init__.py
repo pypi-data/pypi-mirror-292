3c685c8b1d906a25466ca4f08bbb88b96b561686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:35 +0000

    Add package zyx/0.2.69/zyx-0.2.69.tar.gz

---
+++ packages/zyx/zyx-0.2.69.tar.gz/zyx-0.2.69/zyx/experimental/__init__.py
@@ -0,0 +1,28 @@
+__all__ = ["Agents", "BaseTool", "tool", "tools"]
+
+
+from . import tools
+from ..core.ext import zyxModuleLoader
+
+
+class Agents(zyxModuleLoader):
+    pass
+
+
+Agents.init("zyx.agents.main", "Agents")
+
+
+
+class BaseTool(zyxModuleLoader):
+    pass
+
+
+BaseTool.init("crewai_tools.tools.base_tool", "BaseTool")
+
+
+
+class tool(zyxModuleLoader):
+    pass
+
+
+tool.init("langchain_core.tools.convert", "tool")
