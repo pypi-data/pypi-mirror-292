4b3107c050b256e55e1eaf4e9e69d6954af89f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:58 +0000

    Add package parsett/1.2.0/parsett-1.2.0-py3-none-any.whl

---
+++ packages/parsett/parsett-1.2.0-py3-none-any.whl/PTT/__init__.py
@@ -0,0 +1,17 @@
+from .handlers import add_defaults
+from .parse import Parser
+
+_parser = Parser()
+add_defaults(_parser)
+
+
+def parse_title(raw_title: str) -> dict:
+    """
+    Parse the given input string using the initialized parser instance.
+    :param raw_title: The input raw torrent title to parse.
+    :return: A dictionary with the parsed results.
+    """
+    return _parser.parse(raw_title)
+
+
+__all__ = ["Parser", "add_defaults", "parse", "parse_title", "handlers", "transformers"]
