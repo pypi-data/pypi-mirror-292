66bdb9e919dffa2ba7dad2cd6a6eeac46c024904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:10 +0000

    Add package brigid/0.8.1/brigid-0.8.1.tar.gz

---
+++ packages/brigid/brigid-0.8.1.tar.gz/brigid-0.8.1/brigid/theme/default_translations.py
@@ -0,0 +1,28 @@
+translations = {
+    "ru": {
+        "read_more": "Далее",
+        "no_posts_found": "Ничего не найдено.",
+        "pagination_first_page": "к новым",
+        "pagination_last_page": "к старым",
+        "tags_filter_header": "Нашли постов:",
+        "article_similar_header": "Похожее",
+        "last_posts_header": "Последние посты",
+        "similarity_score_tooltip": "Уровень похожести",
+        "license": "лицензия",
+        "content_repository": "исходники постов",
+        "site_is_created_with": "сайт создан с помощью",
+    },
+    "en": {
+        "read_more": "Read more",
+        "no_posts_found": "Nothing found.",
+        "pagination_first_page": "to new",
+        "pagination_last_page": "to old",
+        "tags_filter_header": "Found posts:",
+        "article_similar_header": "Similar",
+        "last_posts_header": "Last posts",
+        "similarity_score_tooltip": "Similarity score",
+        "license": "license",
+        "content_repository": "content repository",
+        "site_is_created_with": "site is created with",
+    },
+}
