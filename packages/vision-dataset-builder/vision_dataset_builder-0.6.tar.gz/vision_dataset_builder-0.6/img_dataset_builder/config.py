72e165852c82b827fc338115a2c483f2a79996c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:31 +0000

    Add package vision-dataset-builder/0.6/vision_dataset_builder-0.6.tar.gz

---
+++ packages/vision-dataset-builder/vision_dataset_builder-0.6.tar.gz/vision_dataset_builder-0.6/img_dataset_builder/config.py
@@ -0,0 +1,22 @@
+import yaml
+import os
+
+
+def load_config(config_file=None, overrides=None):
+    # Set the path to config.yaml relative to the package root
+    if config_file is None:
+        config_file = os.path.join(os.path.dirname(__file__), '..', 'config.yaml')
+    
+    if not os.path.exists(config_file):
+        raise FileNotFoundError(f"Configuration file {config_file} not found.")
+    
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+
+    if overrides:
+        config.update(overrides)
+    
+    return config
+
+# Initialize the configuration
+config = load_config()
