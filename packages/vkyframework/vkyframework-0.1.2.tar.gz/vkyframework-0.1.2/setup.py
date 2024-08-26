8bf3aeb2baa3d24f13b1aa14d260c8bbafa760d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:46 +0000

    Add package vkyframework/0.1.2/vkyframework-0.1.2.tar.gz

---
+++ packages/vkyframework/vkyframework-0.1.2.tar.gz/vkyframework-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vkyframework",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description="vkyframework for python3 rest api",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Vivek Gajbhiye",
+    author_email="vivekgajbhiye024@gmail.com",
+    url="https://github.com/Automatex-testai/vkyframework",  # Replace with your actual URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.12.3',
+)
