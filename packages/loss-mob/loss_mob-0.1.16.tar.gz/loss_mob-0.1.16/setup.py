cc05bb99fdedf4a37e5831538253770013417539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:08 +0000

    Add package loss-mob/0.1.16/loss_mob-0.1.16.tar.gz

---
+++ packages/loss-mob/loss_mob-0.1.16.tar.gz/loss_mob-0.1.16/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+ 
+setuptools.setup(
+  name = "loss_mob",
+  version = "0.1.16",
+  author = "WenSui Liu",
+  author_email = "liuwensui@gmail.com",
+  description = "Monotonic Optimal Binning for Loss Models",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/statcompute/loss_mob",
+  packages = setuptools.find_packages(),
+  install_requires = ['numpy', 'scipy', 'scikit-learn', 'tabulate', 'cytoolz', 'dcor'], 
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
