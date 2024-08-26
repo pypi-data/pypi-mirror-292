0e2b8466fffb0bc9e74780e3e9c7a3d8f77d1e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:16 +0000

    Add package ServiceDiscovery/0.4.1/ServiceDiscovery-0.4.1-py3-none-any.whl

---
+++ packages/ServiceDiscovery/ServiceDiscovery-0.4.1-py3-none-any.whl/ServiceDiscovery/ServiceDiscoveryD.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python3
+# 
+# This file is part of the ServiceDiscovery distribution.
+# Copyright (c) 2023 Javier Moreno Garcia.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import argparse
+import ServiceDiscovery
+import sys
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Service discovery damemon")
+    parser.add_argument(
+        "service_name",
+        nargs=1,
+        help="Service name",
+        type=str)
+    args = parser.parse_args(sys.argv[1:])
+
+
+    try:
+        daemon = ServiceDiscovery.daemon(args.service_name[0])
+        daemon.run()
+
+    except KeyboardInterrupt:
+        pass
+
+
+# Main execution
+if __name__ == '__main__':
+    main()
