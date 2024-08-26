5556c5a9e55a7308f8a24a67dc8230628025d479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:34 +0000

    Add package DjangoEssentials/0.1.6/DjangoEssentials-0.1.6.tar.gz

---
+++ packages/DjangoEssentials/DjangoEssentials-0.1.6.tar.gz/DjangoEssentials-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DjangoEssentials",
+    version="0.1.6",
+    author="Coder Mungan",
+    author_email="codermungan@gmail.com",
+    description="Reusable Django models, utils and storages",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/CoderMungan/DjangoEssentials",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=3.2",
+        "django-storages[boto3]>=1.11",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
