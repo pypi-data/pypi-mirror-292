47602ba53abd5c64533742bdf6b16118812d2648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 05:04:26 +0000

    Add package crashvb-logging-utilities/0.1.1/crashvb_logging_utilities-0.1.1-py3-none-any.whl

---
+++ packages/crashvb-logging-utilities/crashvb_logging_utilities-0.1.1-py3-none-any.whl/crashvb_logging_utilities/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Consolidation of logging utilities."""
+
+from .click import *
+from .logging import *
+from .utils import *
+
+__version__ = "0.1.1"
---
+++ packages/crashvb-logging-utilities/crashvb_logging_utilities-0.1.1-py3-none-any.whl/crashvb_logging_utilities/utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""Helper utilities."""
+
+
+def ellipsis(string: str, max_length: int = 40) -> str:
+    ellipsis
+    """
+    Reduces the length of a given string, if it is over a certain length, by inserting ellipsis.
+
+    Args:
+        string: The string to be reduced.
+        max_length: The maximum length of the string.
+
+    Returns:
+        A string with a maximum length of :param:max_length.
+    """
+    if len(string) <= max_length:
+        return string
+    n_2 = int(max_length) / 2 - 3
+    n_1 = max_length - n_2 - 3
+
+    return f"{string[:int(n_1)]}...{string[-int(n_2):]}"
