e0623ae72a0eb978453c455f2c47c95a3749d14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:44 +0000

    Add package ScrollableContainers/2.1.0/scrollablecontainers-2.1.0.tar.gz

---
+++ packages/ScrollableContainers/scrollablecontainers-2.1.0.tar.gz/scrollablecontainers-2.1.0/src/ScrollableContainers/_wx.py
@@ -0,0 +1,22 @@
+__all__ = ["ScrollablePanelWx"]
+
+import wx
+from wx.lib.scrolledpanel import ScrolledPanel
+
+
+class ScrollablePanelWx(ScrolledPanel):
+    """
+    Container with horizontal and vertical scrolling capabilities. Widgets must
+    be added to its ``panel`` attribute. Constructor arguments are passed to
+    the parent constructor.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # According to the documentation, a sizer is required to calculate the
+        # minimum virtual size of the panel.
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.panel = wx.Panel(self)
+        vbox.Add(self.panel, flag=wx.ALIGN_CENTRE)
+        self.SetSizer(vbox)
