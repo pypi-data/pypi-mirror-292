515737dd7d15548186504ff8d6a8489a1024d316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:36 +0000

    Add package gradientify/2.0.8/gradientify-2.0.8.tar.gz

---
+++ packages/gradientify/gradientify-2.0.8.tar.gz/gradientify-2.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='gradientify',
+    version='2.0.8',
+    author='virtual',
+    description='A Python package for gradient colors',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://vanishnet.netlify.app/',
+    packages=find_packages(),
+    python_requires='>=3.11',
+)
