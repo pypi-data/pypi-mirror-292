eba58fcd3aa2b75c1ad9160b2801e0b5ae77e330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 05:04:24 +0000

    Add package coherent.test/0.7.1/coherent_test-0.7.1-py3-none-any.whl

---
+++ packages/coherent.test/coherent_test-0.7.1-py3-none-any.whl/coherent/test/__init__.py
@@ -0,0 +1,13 @@
+"""
+A trivial doctest to cause something to be tested.
+
+>>> __name__
+'coherent.test'
+"""
+
+__requires__ = [
+    'pip-run',
+    'pytest',
+    'coherent.build>=0.10',
+    'jaraco.compat>=4.2',
+]
