5b421af236c6fb2b07415267b398b79482936eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:13 +0000

    Add package darklock/0.1.6/darklock-0.1.6-py3-none-any.whl

---
+++ packages/darklock/darklock-0.1.6-py3-none-any.whl/darklock/__init__.py
@@ -0,0 +1,28 @@
+from darklock.restrict_os_access import RestrictOSAccess
+from darklock.restrict_network_access import RestrictNetworkAccess
+
+
+network = RestrictNetworkAccess()
+os = RestrictOSAccess()
+
+def activate(
+    whitelisted_operations: list = None,
+    whitelisted_filenames: list = None,
+    whitelisted_imports: list = None,
+    blacklisted_filenames: list = None,
+    whitelisted_directories: list = None,
+):
+    # network.activate(
+    #     allowed_port=4222
+    # )
+    os.activate(
+        whitelisted_operations,
+        whitelisted_filenames,
+        whitelisted_imports,
+        blacklisted_filenames,
+        whitelisted_directories,
+    )
+
+def deactivate():
+    network.deactivate()
+    os.deactivate()
