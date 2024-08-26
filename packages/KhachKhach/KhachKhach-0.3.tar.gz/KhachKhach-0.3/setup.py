519ac1fae59ea50061e2ed59aca94b9ec605416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:16 +0000

    Add package KhachKhach/0.3/KhachKhach-0.3.tar.gz

---
+++ packages/KhachKhach/KhachKhach-0.3.tar.gz/KhachKhach-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KhachKhach',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'ultralytics',
+    ],
+    author='pratapdevs11',
+    author_email='divyapratap360@gmail.com',
+    description='A package for processing video frames, annotating keypoints, and more.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pratapsdev11/KhachKhach/tree/main',  # Update with your URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
