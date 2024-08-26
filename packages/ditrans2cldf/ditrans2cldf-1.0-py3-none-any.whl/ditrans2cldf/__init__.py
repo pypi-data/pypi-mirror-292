137322c8f42e5d0c11e1b39fbf5144176b626c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 16:13:56 +0000

    Add package ditrans2cldf/1.0/ditrans2cldf-1.0-py3-none-any.whl

---
+++ packages/ditrans2cldf/ditrans2cldf-1.0-py3-none-any.whl/ditrans2cldf/__init__.py
@@ -0,0 +1,6 @@
+from ditrans2cldf.bibliography import make_bibliography  # noqa: F401
+from ditrans2cldf.config import load_config_file  # noqa: F401
+from ditrans2cldf.excel_input import load_excel_data  # noqa: F401
+from ditrans2cldf.csv_input import load_csv_data  # noqa: F401
+from ditrans2cldf.process import make_cldf_tables  # noqa: F401
+from ditrans2cldf.scaffold import add_custom_columns, cldfspec  # noqa: F401
