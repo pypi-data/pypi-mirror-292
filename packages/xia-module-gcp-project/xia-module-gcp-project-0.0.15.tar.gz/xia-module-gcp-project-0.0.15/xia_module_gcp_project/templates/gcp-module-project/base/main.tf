8d0a1ef077e828669d5c40cc7a29c77d48c75f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:06:03 +0000

    Add package xia-module-gcp-project/0.0.15/xia-module-gcp-project-0.0.15.tar.gz

---
+++ packages/xia-module-gcp-project/xia-module-gcp-project-0.0.15.tar.gz/xia-module-gcp-project-0.0.15/xia_module_gcp_project/templates/gcp-module-project/base/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  alias = "google-gcp-project"
+}
+
+provider "github" {
+  alias = "github-gcp-project"
+}
+
+module "gcp_module_project" {
+  providers = {
+    google = google.google-gcp-project
+    github = github.github-gcp-project
+  }
+
+  source = "../../modules/gcp-module-project"
+
+  module_name = "gcp-module-project"
+  config_file = "../../../config/platform/gcp-project.yaml"
+  landscape = local.landscape
+  applications = local.applications
+  modules = local.modules
+  environment_dict = local.environment_dict
+  app_env_config = local.app_env_config
+  module_app_to_activate = local.module_app_to_activate
+  github_config = module.gh_module_application.github_config
+
+  depends_on = [module.gh_module_application]
+}
