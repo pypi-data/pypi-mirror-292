21829e4a029f9018b5d42195a8556832cfbb45f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:46 +0000

    Add package fsd/0.0.3/fsd-0.0.3.tar.gz

---
+++ packages/fsd/fsd-0.0.3.tar.gz/fsd-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_namespace_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
+setup(
+    name='fsd',
+    version='0.0.3',
+    author='Zinley',
+    author_email='dev@zinley.com',
+    description='A brief description of your library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Zinley-dev/fsd_v2_universal',  # Replace with your library's URL
+    packages=find_namespace_packages(include=['fsd','fsd.*']),  # Automatically find package directories
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the Python version required
+    install_requires=required,
+    include_package_data=True,  # Include files listed in MANIFEST.in
+)
