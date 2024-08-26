fbfca0c78e656cac73526a35dad032079168fd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:23 +0000

    Add package GT-1000PILOT/0.1.1/gt_1000pilot-0.1.1-py3-none-any.whl

---
+++ packages/GT-1000PILOT/gt_1000pilot-0.1.1-py3-none-any.whl/gt1000pilot/pages/chorus.py
@@ -0,0 +1,31 @@
+import dash
+from dash import html, dcc, ctx, Input, Output, callback, get_app
+from datetime import datetime
+from time import sleep
+
+from gt1000pilot.shared import gt1000, off_color, on_color, logger
+from gt1000pilot.pages.pages_common import (
+    send_fx_state_command,
+    register_callbacks,
+    refresh_all_effects,
+    generate_buttons,
+    serve_layout,
+    callbacks_registered,
+)
+
+dash.register_page(__name__, path="/chorus")
+
+state_key = "chorus"
+callbacks_registered[state_key] = False
+
+
+@callback(
+    Output(f"{state_key}_buttons", "children"),
+    Input("interval-component", "n_intervals"),
+)
+def update_metrics(n):
+    refresh_all_effects(state_key)
+    return generate_buttons(state_key)
+
+
+layout = serve_layout(state_key)
