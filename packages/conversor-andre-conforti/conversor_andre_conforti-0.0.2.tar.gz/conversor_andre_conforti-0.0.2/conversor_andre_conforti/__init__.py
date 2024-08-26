21374f02a8e1e1aa7bf72289abd6f7686c59a431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:46 +0000

    Add package conversor-andre-conforti/0.0.2/conversor_andre_conforti-0.0.2.tar.gz

---
+++ packages/conversor-andre-conforti/conversor_andre_conforti-0.0.2.tar.gz/conversor_andre_conforti-0.0.2/conversor_andre_conforti/__init__.py
@@ -0,0 +1,9 @@
+from .temperatura import celsius_para_fahrenheit, fahrenheit_para_celsius
+from .comprimento import metros_para_centimetros, centimetros_para_metros
+
+__all__ = [
+    'celsius_para_fahrenheit',
+    'fahrenheit_para_celsius',
+    'metros_para_centimetros',
+    'centimetros_para_metros',
+]
