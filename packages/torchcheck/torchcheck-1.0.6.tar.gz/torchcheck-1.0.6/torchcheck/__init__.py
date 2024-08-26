a33a29d7b50a1f68d8006574cbf9102a787bc65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:02 +0000

    Add package torchcheck/1.0.6/torchcheck-1.0.6.tar.gz

---
+++ packages/torchcheck/torchcheck-1.0.6.tar.gz/torchcheck-1.0.6/torchcheck/__init__.py
@@ -0,0 +1,17 @@
+from .checks import (
+    assert_dim,
+    assert_dtype,
+    assert_eq,
+    assert_true,
+    assert_shape,
+)
+from .torchcheck import (
+    batched_index_gen,
+    batched_index_padded,
+    batched_masked_select,
+)
+
+__all__ = [
+    "assert_dim", "assert_dtype", "assert_eq", "assert_true", "assert_shape",
+    "batched_index_gen", "batched_index_padded", "batched_masked_select"
+]
