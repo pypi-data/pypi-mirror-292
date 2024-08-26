13a532531d5197c71f4cc4d13ced37e1cbd738ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 15:07:46 +0000

    Add package nhmail/1.0.5/nhmail-1.0.5-py3-none-any.whl

---
+++ packages/nhmail/nhmail-1.0.5-py3-none-any.whl/nhmail/validators.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields, validate
+
+
+class SendEmailSchema(Schema):
+    _email_validator = validate.Email()
+    _name_validator = validate.Length(min=1)
+    to = fields.Str(required=True, validate=_email_validator)
+    template_id = fields.Str(required=True)
+    template_data = fields.Nested(
+        {
+            "redirect_link": fields.Str(required=True),
+            "user_name": fields.Str(required=True, validate=_name_validator),
+            "password": fields.Str(required=False),
+        },
+        required=True,
+    )
