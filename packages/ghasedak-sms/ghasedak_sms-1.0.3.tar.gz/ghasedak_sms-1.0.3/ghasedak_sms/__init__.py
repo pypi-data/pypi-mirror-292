0d92513cd1ecee61a535bee5dd60621450739cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:48 +0000

    Add package ghasedak-sms/1.0.3/ghasedak_sms-1.0.3.tar.gz

---
+++ packages/ghasedak-sms/ghasedak_sms-1.0.3.tar.gz/ghasedak_sms-1.0.3/ghasedak_sms/__init__.py
@@ -0,0 +1,7 @@
+from ghasedak_sms.dto import AccountInformationResponse, CheckOtpTemplateResponse, CheckSmsStatusInput, \
+    GetOtpParametersInput, GetReceivedSmsInput, GetReceivedSmsPagingInput, ReceivedSmsesPagingResponse, \
+    ReceivedSmsesResponse, ResponseDto, ResponseDtoWithData, SendBulkInput, SendBulkResponse, SendOldOtpInput, \
+    SendOtpReceptorDto, SendOtpInput, SendOtpResponse, SendPairToPairInput, SendPairToPairResponse, SendSingleResponse, \
+    SendSingleSmsInput, SmsStatusResponseItems
+from ghasedak_sms.enums.message_id_type import MessageIdType
+from ghasedak_sms.ghasedak import Ghasedak
---
+++ packages/ghasedak-sms/ghasedak_sms-1.0.3.tar.gz/ghasedak_sms-1.0.3/ghasedak_sms/test_ghasedak.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestGhasedak(TestCase):
+    def test_check_sms_status(self):
+        self.fail()
+
+
+class TestGhasedak(TestCase):
+    def test_get_account_information(self):
+        self.fail()
