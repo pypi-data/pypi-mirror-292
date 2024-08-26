e07972dea06e0b5c69565b5e28ba2317d8e934e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:26 +0000

    Add package conversor-andre-conforti/0.0.1/conversor_andre_conforti-0.0.1-py3-none-any.whl

---
+++ packages/conversor-andre-conforti/conversor_andre_conforti-0.0.1-py3-none-any.whl/conversor_andre_conforti/comprimento.py
@@ -0,0 +1,7 @@
+def metros_para_centimetros(metros):
+    """Converte metros para centímetros."""
+    return metros * 100
+
+def centimetros_para_metros(centimetros):
+    """Converte centímetros para metros."""
+    return centimetros / 100
