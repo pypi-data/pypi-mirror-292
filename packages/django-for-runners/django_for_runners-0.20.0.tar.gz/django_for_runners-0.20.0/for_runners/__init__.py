d8e0502a4b169dc75313f7480e8255acd0922099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:34 +0000

    Add package django-for-runners/0.20.0/django_for_runners-0.20.0.tar.gz

---
+++ packages/django-for-runners/django_for_runners-0.20.0.tar.gz/django_for_runners-0.20.0/for_runners/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Django-ForRunners
+    Store your GPX tracks of your running (or other sports activity) in django.
+"""
+
+__version__ = '0.20.0'
+__author__ = 'Jens Diemer <django-for-runners@jensdiemer.de>'
