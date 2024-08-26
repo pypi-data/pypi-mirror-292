2d543e626e9841c2c3e6366fd15c7925155f0f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:05:34 +0000

    Add package homeassistant-stubs/2024.8.3/homeassistant_stubs-2024.8.3-py3-none-any.whl

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2024.8.3-py3-none-any.whl/homeassistant-stubs/components/lawn_mower/const.pyi
@@ -0,0 +1,17 @@
+from enum import IntFlag, StrEnum
+
+class LawnMowerActivity(StrEnum):
+    ERROR = 'error'
+    PAUSED = 'paused'
+    MOWING = 'mowing'
+    DOCKED = 'docked'
+
+class LawnMowerEntityFeature(IntFlag):
+    START_MOWING = 1
+    PAUSE = 2
+    DOCK = 4
+
+DOMAIN: str
+SERVICE_START_MOWING: str
+SERVICE_PAUSE: str
+SERVICE_DOCK: str
