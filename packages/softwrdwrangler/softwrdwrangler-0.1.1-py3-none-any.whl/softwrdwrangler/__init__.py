d4547ab47c9b555457b2af3537f17fc4e60eced1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:06:18 +0000

    Add package softwrdwrangler/0.1.1/softwrdwrangler-0.1.1-py3-none-any.whl

---
+++ packages/softwrdwrangler/softwrdwrangler-0.1.1-py3-none-any.whl/softwrdwrangler/__init__.py
@@ -0,0 +1,16 @@
+from .s3 import (
+    read_csv,
+    write_csv,
+    read_geojson,
+    write_geojson,
+    read_json,
+    to_json,
+    read_pickle,
+    write_pickle,
+    get_latest_ds_nodash,
+    list_files,
+    delete_file,
+    upload_file,
+    download_file,
+    generate_presigned_url
+)
