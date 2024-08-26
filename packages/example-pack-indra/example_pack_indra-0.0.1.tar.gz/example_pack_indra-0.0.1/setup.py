bcdd824b6545a39f455be5baa74d4a6a725bd946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 15:07:15 +0000

    Add package example-pack-indra/0.0.1/example_pack_indra-0.0.1.tar.gz

---
+++ packages/example-pack-indra/example_pack_indra-0.0.1.tar.gz/example_pack_indra-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='example_pack_indra',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version>"3.10"',
+    ],
+)
