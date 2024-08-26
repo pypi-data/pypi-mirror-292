0c6d38430a0e7c6286a2174469caf37e8af41892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:58 +0000

    Add package nopasaran/0.2.30/nopasaran-0.2.30.tar.gz

---
+++ packages/nopasaran/nopasaran-0.2.30.tar.gz/nopasaran-0.2.30/nopasaran/definitions/events.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class EventNames(Enum):
+    """
+    Enum representing event names.
+    
+    This enum represents different event names.
+    """
+
+    STARTED = 0
+    SYNC = 1
+    PACKET_SENT = 2
+    PACKET_AVAILABLE = 3
+    DONE = 4
+    ERROR = 5
+    TIMEOUT = 6
+    READY = 7
+    SYNC_SENT = 8
+    SYNC_AVAILABLE = 9
+    CONNECTION_ENDING = 10
+    REQUEST_RECEIVED = 11
+    RESPONSE_RECEIVED = 12
+    REQUEST_ERROR = 13
