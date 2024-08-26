aa61eee8c783cfb56d98545d4396cfc98e409953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:14 +0000

    Add package notter/0.2.3/notter-0.2.3.tar.gz

---
+++ packages/notter/notter-0.2.3.tar.gz/notter-0.2.3/src/notter/explorers/base.py
@@ -0,0 +1,89 @@
+import asyncio
+import os
+from pathlib import Path
+from typing import Dict, List
+
+import aiofiles
+
+import notter.constants as ncons
+from notter.explorers.registry import registry
+from notter.model import Comment, NoteType
+from notter.notter import Notter
+
+
+class BaseExplorer:
+    def __init__(self, notter: Notter):
+        raise NotImplementedError
+
+    async def discover(self, tags: List[str]) -> List[Comment]:
+        raise NotImplementedError
+
+
+class LexicalExplorer(BaseExplorer):
+    def __init__(self, notter: Notter) -> None:
+        self.notter = notter
+        self.source_path = notter.get_config(ncons.SRC_PATH)
+
+    async def discover(self, tags: List[str]) -> List[Comment]:
+        comments = []
+        tags = [tag.lower() for tag in tags]
+
+        files_per_ext = LexicalExplorer._find_files_with_extensions(
+            str(Path(self.source_path)), ncons.SUPPORTED_EXTENSIONS
+        )
+
+        for ext, files in files_per_ext.items():
+            explorer_class = registry.get(ext)
+
+            # Skip if unrecognized file format
+            if not explorer_class:
+                continue
+
+            explorer = explorer_class(self.notter)
+
+            # Read and process files chunk by chunk
+            read_file_calls = [LexicalExplorer._read_file_async(file) for file in files]
+            results = dict(zip(files, await asyncio.gather(*read_file_calls, return_exceptions=True)))
+            file_contents: Dict[str, str] = {
+                file: content for file, content in results.items() if isinstance(content, str)
+            }
+
+            # Discover comments in files
+            for file, content in file_contents.items():
+                discovered_comments = explorer._discover_todos_in_file(file, content, tags)
+                comments.extend(discovered_comments)
+
+        return comments
+
+    @staticmethod
+    async def _read_file_async(filepath: str) -> str:
+        async with aiofiles.open(filepath, "r", encoding="utf-8") as file:
+            file_content = await file.read()
+        return file_content
+
+    @staticmethod
+    def _find_files_with_extensions(source_path: str, extensions: List[str]) -> Dict[str, List[str]]:
+        found_files: Dict[str, List[str]] = {ext: [] for ext in extensions}
+
+        for root, dirs, files in os.walk(source_path):
+            for file in files:
+                file_ext = os.path.splitext(file)[-1].lower()
+                if file_ext in extensions:
+                    found_files[file_ext].append(os.path.join(root, file))
+
+        return found_files
+
+    @classmethod
+    def _discover_comments_in_file(cls, filepath: str, file_content: str, tags: List[str]) -> List[Comment]:
+        raise NotImplementedError
+
+    @classmethod
+    def _discover_todos_in_file(cls, filepath: str, file_content: str, tags: List[str]) -> List[Comment]:
+        comments: List[Comment] = cls._discover_comments_in_file(filepath, file_content, tags)
+        return [comment for comment in comments if comment.type == NoteType.TODO]
+
+    @staticmethod
+    def determine_note_type(text: str, tags: List[str]) -> NoteType:
+        text = text.lower().strip()
+        tags = [tag.lower() for tag in tags]
+        return NoteType.TODO if any(tag in text for tag in tags) else NoteType.NOTE
---
+++ packages/notter/notter-0.2.3.tar.gz/notter-0.2.3/src/notter/model.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+
+class NoteType(str, Enum):
+    NOTE = "NOTE"
+    TODO = "TODO"
+
+
+# TODO: Add input validation, max number of characters for each field
+@dataclass
+class Note:
+    id: str
+    username: str
+    email: str
+    filepath: str
+    line: int
+    type: NoteType = NoteType.NOTE
+    created_at: Optional[str] = datetime.now().isoformat()
+    updated_at: Optional[str] = datetime.now().isoformat()
+
+
+@dataclass
+class Content:
+    text: str
+
+
+@dataclass
+class NoteWithContent:
+    note: Note
+    content: Content
+
+    def __str__(self) -> str:
+        return f"{self.content.text} - {self.note.username} / {self.note.updated_at}"
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.note.id,
+            "username": self.note.username,
+            "email": self.note.email,
+            "filepath": self.note.filepath,
+            "line": self.note.line,
+            "type": self.note.type.value,
+            "created_at": self.note.created_at,
+            "updated_at": self.note.updated_at,
+            "content": self.content.text,
+        }
+
+    # TODO: This is too ugly, find a better way to do this
+    @staticmethod
+    def from_db_row(row: tuple) -> "NoteWithContent":
+        note = Note(
+            id=row[0],
+            username=row[1],
+            email=row[2],
+            filepath=row[3],
+            line=row[4],
+            type=NoteType(row[5]),
+            created_at=row[6],
+            updated_at=row[7],
+        )
+        content = Content(text=row[8])
+        return NoteWithContent(note, content)
+
+    def to_db_row(self) -> tuple:
+        return (
+            self.note.id,
+            self.note.username,
+            self.note.email,
+            self.note.filepath,
+            self.note.line,
+            self.note.type.value,
+            self.note.created_at,
+            self.note.updated_at,
+            self.content.text,
+        )
+
+
+@dataclass
+class Comment:
+    filepath: str
+    text: str
+    line: int
+    type: NoteType
+    multiline: bool = False
+
+    def __str__(self) -> str:
+        return f"{self.filepath}:{self.line} - {self.text}"
