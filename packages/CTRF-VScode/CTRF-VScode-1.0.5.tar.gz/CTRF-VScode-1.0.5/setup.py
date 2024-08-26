825497439741cd796a3cbf18127a18d52169cc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:04:49 +0000

    Add package CTRF-VScode/1.0.5/CTRF-VScode-1.0.5.tar.gz

---
+++ packages/CTRF-VScode/CTRF-VScode-1.0.5.tar.gz/CTRF-VScode-1.0.5/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import glob
+
+setup(
+    name="CTRF-VScode",
+    version="1.0.5",
+    author="Srinivas M",
+    author_email="maddimsetti34@gmail.com",
+    description="Please add the appropriate description",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hemanth2410/CTRF-VScode",
+    
+    # Adjusted to find packages in the 'CTRF-VScode' directory
+    packages=find_packages(where="CTRF-VScode"),
+    package_dir={"": "CTRF-VScode"},
+
+    # Standalone Python modules
+    py_modules=[
+        "CTRF-VScode.basic_functions",
+        "CTRF-VScode.bds",
+        "CTRF-VScode.eobds",
+        "CTRF-VScode.gmm_mml",
+        "CTRF-VScode.hellof",
+        "CTRF-VScode.IG_func",
+        "CTRF-VScode.obds",
+        "CTRF-VScode.predictf",
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "pyeda",
+    ],
+    include_package_data=True,
+    
+    package_data={
+        'CTRF-VScode': [
+            'CTRF-VScode/TrainingFiles/*',
+            'CTRF-VScode/Output/*'
+        ],
+    },    
+)
+
