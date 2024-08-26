bf8e8bd49ef1100d60663b5ac9102a8db00ae0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:06:02 +0000

    Add package cmdline-add-args/1.0.0/cmdline_add_args-1.0.0-py3-none-any.whl

---
+++ packages/cmdline-add-args/cmdline_add_args-1.0.0-py3-none-any.whl/cmdline_add_args/plugin.py
@@ -0,0 +1,18 @@
+import pytest
+
+from config.env import ALLURE_REPORT_PATH, CREATE_ALLURE_REPORT
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_load_initial_conftests(args):
+    print("pytest_load_initial_conftests is called")
+
+    if CREATE_ALLURE_REPORT:
+        new_args = ["-v", f"--alluredir={ALLURE_REPORT_PATH}", "--clean-alluredir"]
+        for arg in new_args:
+            if arg not in args:
+                if args[-1].split(".")[-1] == "py":
+                    args.insert(-1, arg)
+                else:
+                    args.append(arg)
+        print(f"Modified pytest args: {args}")
