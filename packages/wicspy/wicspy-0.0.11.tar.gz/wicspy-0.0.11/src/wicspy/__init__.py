17cef047bd16a1962406bbd36d82be57c835f8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 16:13:57 +0000

    Add package wicspy/0.0.11/wicspy-0.0.11.tar.gz

---
+++ packages/wicspy/wicspy-0.0.11.tar.gz/wicspy-0.0.11/src/wicspy/__init__.py
@@ -0,0 +1,23 @@
+"""
+Author: wicsp wicspa@gmail.com
+Date: 2024-08-26 17:01:30
+LastEditors: wicsp wicspa@gmail.com
+LastEditTime: 2024-08-26 18:19:56
+FilePath: /wicspy/src/wicspy/__init__.py
+Description:
+
+
+Copyright (c) 2024 by wicsp, All Rights Reserved.
+"""
+
+
+def hello() -> str:
+    return "Hello from wicsp!"
+
+
+from .bark import bark
+from .timer_tool import timing
+
+
+__all__ = ["bark", "timing"]
+__version__ = "0.0.11"
