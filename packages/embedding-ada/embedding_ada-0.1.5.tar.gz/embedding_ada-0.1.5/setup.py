908a815a1f0b7a710738c62f8f401db365e93735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 03:05:13 +0000

    Add package embedding-ada/0.1.5/embedding_ada-0.1.5.tar.gz

---
+++ packages/embedding-ada/embedding_ada-0.1.5.tar.gz/embedding_ada-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+setup(
+    name='embedding_ada',
+    version='0.1.5',
+    author='Pavan Kumar Medarametla',
+    author_email='pavankumarchowdary35@gmail.com',
+    description='Python Package to Train Embedding adapter on top of any Embedding models from Hugging face and OpenAI',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
