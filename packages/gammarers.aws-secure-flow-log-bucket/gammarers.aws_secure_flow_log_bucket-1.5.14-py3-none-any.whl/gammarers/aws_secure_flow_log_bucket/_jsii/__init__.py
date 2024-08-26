4fda07ef08438b684a97ee315e4df70faa30bc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:06:03 +0000

    Add package gammarers.aws-secure-flow-log-bucket/1.5.14/gammarers.aws_secure_flow_log_bucket-1.5.14-py3-none-any.whl

---
+++ packages/gammarers.aws-secure-flow-log-bucket/gammarers.aws_secure_flow_log_bucket-1.5.14-py3-none-any.whl/gammarers/aws_secure_flow_log_bucket/_jsii/__init__.py
@@ -0,0 +1,32 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import gammarers.aws_secure_bucket._jsii
+import gammarers.aws_secure_log_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@gammarers/aws-secure-flow-log-bucket",
+    "1.5.14",
+    __name__[0:-6],
+    "aws-secure-flow-log-bucket@1.5.14.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
