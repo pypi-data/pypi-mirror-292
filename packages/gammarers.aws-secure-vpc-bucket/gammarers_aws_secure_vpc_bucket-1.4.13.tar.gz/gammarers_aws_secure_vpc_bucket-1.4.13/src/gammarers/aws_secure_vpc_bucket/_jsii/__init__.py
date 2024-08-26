b9a4f42b513b6bb4df12fe03f86f0679a8f2c9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:43 +0000

    Add package gammarers.aws-secure-vpc-bucket/1.4.13/gammarers_aws_secure_vpc_bucket-1.4.13.tar.gz

---
+++ packages/gammarers.aws-secure-vpc-bucket/gammarers_aws_secure_vpc_bucket-1.4.13.tar.gz/gammarers_aws_secure_vpc_bucket-1.4.13/src/gammarers/aws_secure_vpc_bucket/_jsii/__init__.py
@@ -0,0 +1,31 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import gammarers.aws_secure_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@gammarers/aws-secure-vpc-bucket",
+    "1.4.13",
+    __name__[0:-6],
+    "aws-secure-vpc-bucket@1.4.13.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
