9b108c0aef08ec37007d8a8cffaaa25226ea1f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 04:05:20 +0000

    Add package yxmath/0.0.5/yxmath-0.0.5-py3-none-any.whl

---
+++ packages/yxmath/yxmath-0.0.5-py3-none-any.whl/yxmath/versions.py
@@ -0,0 +1,43 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.5",
+        "features": [
+            "1. add jaccord_index",
+        ],
+    },
+    {
+        "number": "0.0.4",
+        "features": [
+            "1. add uniqify function to set module",
+        ],
+    },
+    {
+        "number": "0.0.3.1",
+        "features": [
+            "1. debug",
+            "2. add histogram example",
+        ],
+    },
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. add histogram_ploter function",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add plot module",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]
