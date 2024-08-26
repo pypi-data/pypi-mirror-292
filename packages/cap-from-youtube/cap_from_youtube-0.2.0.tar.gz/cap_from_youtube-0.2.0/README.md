04e0d1af544fd3724ec97969b45d44c92991f533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-26 14:04:43 +0000

    Add package cap-from-youtube/0.2.0/cap_from_youtube-0.2.0.tar.gz

---
+++ packages/cap-from-youtube/cap_from_youtube-0.2.0.tar.gz/cap_from_youtube-0.2.0/README.md
@@ -0,0 +1,79 @@
+# cap_from_youtube
+ Get an OpenCV video capture from an YouTube video URL
+
+[![PyPI](https://img.shields.io/pypi/v/cap-from-youtube?color=2BAF2B)](https://pypi.org/project/cap-from-youtube/)
+<p align="center">
+  <img src="https://raw.githubusercontent.com/ibaiGorordo/cap_from_youtube/main/doc/img/cap_from_youtube_logo.png" />
+</p>
+
+# Why
+- pafy is widely used to get the video URL from a YouTube video URL, but since it uses youtube-dl which has not been updated recently, it suffers from some issues (dislike_count not found...).
+- This repository is a simplified version of what pafy does, by just getting the url of the video and creating an OpenCV video capture from it.
+- It uses YT-DLP (https://github.com/yt-dlp/yt-dlp), which is a fork of youtube-dl that is updated frequently.
+
+# Requirement
+* YT-DLP
+* OpenCV
+* NumPy
+ 
+# Installation
+- The easiest way is to install it with pip:
+
+```bash
+pip install cap_from_youtube
+```
+- You can also install it from GitHub:
+
+```bash
+pip install git+https://github.com/ibaiGorordo/cap_from_youtube
+```
+
+# Usage
+
+## cap_from_youtube()
+- `cap_from_youtube()` is the main function to obtain a video capture from a YouTube video URL. 
+- It requires the video URL as input and returns a `cv2.VideoCapture` object.
+- By default, it returns the video with the highest resolution available.
+- You can specify the resolution you want to get with the `resolution` parameter.
+  - Available resolutions: '144p', '240p', '360p', '480p', '720p', '720p60', '1080p', '1080p60', 'best'
+- Example:
+
+```python
+from datetime import timedelta
+import cv2
+from cap_from_youtube import cap_from_youtube
+
+youtube_url = 'https://youtu.be/LXb3EKWsInQ'
+start_time = timedelta(seconds=5)
+cap = cap_from_youtube(youtube_url, 'best', start=start_time)
+
+cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow('video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+```
+
+## list_video_streams()
+- You can also use the `list_video_streams()` function to get the list of available video streams.
+- It requires the video URL as input and returns two values: 
+  - streams: a list of VideoStream with the information of the available video streams.
+  - resolutions: a NumPy array with the available resolutions.
+- Example:
+```python
+from cap_from_youtube import list_video_streams
+
+youtube_url = 'https://youtu.be/LXb3EKWsInQ'
+streams, resolutions = list_video_streams(youtube_url)
+
+for stream in streams:
+    print(stream)
+```
+ 
+# References
+- pafy: https://github.com/mps-youtube/pafy
+- yt-dlp: https://github.com/yt-dlp/yt-dlp
+- imread_from_url: https://github.com/Kazuhito00/imread_from_url
---
+++ packages/cap-from-youtube/cap_from_youtube-0.2.0.tar.gz/cap_from_youtube-0.2.0/cap_from_youtube/cap_from_youtube.py
@@ -0,0 +1,86 @@
+from datetime import timedelta
+from dataclasses import dataclass
+import yt_dlp
+import numpy as np
+import cv2
+
+valid_resolutions = ['144p', '240p', '360p', '480p', '720p', '720p60', '1080p', '1080p60']
+
+@dataclass
+class VideoStream:
+    url: str = None
+    resolution: str = None
+    height: int = 0
+    width: int = 0
+
+    def __init__(self, video_format: dict):
+        self.url = video_format['url']
+        self.resolution = video_format['format_note']
+        self.height = video_format['height']
+        self.width = video_format['width']
+
+    def __str__(self):
+        return f'{self.resolution} ({self.height}x{self.width}): {self.url}'
+
+
+def list_video_streams(url: str) -> tuple[list[VideoStream], np.ndarray]:
+    # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+        filter = lambda x: x['vcodec'] != 'none' and 'format_note' in x and x['format_note'] in valid_resolutions
+        streams = [VideoStream(format) for format in info['formats'][::-1] if filter(format)]
+        _, unique_indices = np.unique(np.array([stream.resolution
+                                                for stream in streams]), return_index=True)
+        streams = [streams[index] for index in np.sort(unique_indices)]
+        resolutions = np.array([stream.resolution for stream in streams])
+        return streams[::-1], resolutions[::-1]
+
+
+def cap_from_youtube(url: str,
+                     resolution: str = 'best',
+                     start: timedelta = timedelta(seconds=0)) -> cv2.VideoCapture:
+
+    streams, resolutions = list_video_streams(url)
+
+    if resolution == 'best':
+        resolution = resolutions[-1]
+
+    if resolution not in resolutions:
+        raise ValueError(f'Resolution {resolution} not available')
+
+    res_index = np.where(resolutions == resolution)[0][0]
+    cap = cv2.VideoCapture(streams[res_index].url)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    start_frame = int(start.total_seconds() * fps)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    return cap
+
+
+if __name__ == '__main__':
+    import time
+
+    def test_video(cap, vid_res):
+        num_frames = 100
+        for i in range(num_frames):
+            start_time = time.perf_counter()
+            ret, frame = cap.read()
+            # print(f'Frame process time: {time.perf_counter() - start_time}s')
+            if not ret:
+                break
+            cv2.imshow(f'{vid_res}', frame)
+            cv2.waitKey(1)
+        cap.release()
+
+
+    youtube_url = 'https://youtu.be/XqZsoesa55w'
+
+    _, resolutions = list_video_streams(youtube_url)
+    resolutions = np.append(resolutions, 'best')
+
+    print(resolutions)
+    for vid_res in resolutions:
+        cap = cap_from_youtube(youtube_url, vid_res)
+        test_video(cap, vid_res)
