add160a3c59dfa02bb46d4af2e1b488bc420ed27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:53 +0000

    Add package querysource/3.11.23/querysource-3.11.23-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/querysource/querysource-3.11.23-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/querysource/queries/multi/transformations/__init__.py
@@ -0,0 +1,16 @@
+from .crosstab import crosstab
+from .correlation import correlation
+from .google import GoogleMaps
+from .forecast import Forecast
+from .rmap import Map
+from .pivot import pivot
+
+
+__all__ = (
+    'crosstab',
+    'correlation',
+    'GoogleMaps',
+    'Forecast',
+    'Map',
+    'pivot'
+)
