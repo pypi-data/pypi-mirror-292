23a23dd70b2ee9ba2ac3e8ba641b978e185ab310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:05:20 +0000

    Add package argument-mining-framework/0.0.4/argument_mining_framework-0.0.4.tar.gz

---
+++ packages/argument-mining-framework/argument_mining_framework-0.0.4.tar.gz/argument_mining_framework-0.0.4/src/__init__.py
@@ -0,0 +1,9 @@
+from .amf.scheme.predictor import SchemePredictor
+from .amf.argument_relation.predictor import ArgumentRelationPredictor
+from .amf.hypothesis.predictor import HypothesisPredictor
+
+__all__ = [
+    'SchemePredictor',
+    'ArgumentRelationPredictor',
+    'HypothesisPredictor'
+]
