5914cc8972966d1ded4adcdc934598a83fe0f3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:05:31 +0000

    Add package worker-automate-hub/0.4.8/worker_automate_hub-0.4.8-py3-none-any.whl

---
+++ packages/worker-automate-hub/worker_automate_hub-0.4.8-py3-none-any.whl/worker_automate_hub/tasks/jobs/conexao_rdp.py
@@ -0,0 +1,86 @@
+import subprocess
+import pyautogui
+import asyncio
+import os
+from rich.console import Console
+from worker_automate_hub.utils.logger import logger
+import time
+
+console = Console()
+
+# Função para tirar screenshots
+def tirar_screenshot(nome_etapa):
+    caminho_screenshot = f"{nome_etapa}_{int(time.time())}.png"
+    pyautogui.screenshot(caminho_screenshot)
+    console.print(f"Screenshot tirada: {caminho_screenshot}")
+    return caminho_screenshot
+
+# Função para deletar screenshots
+def deletar_screenshots(caminhos_screenshots):
+    for caminho in caminhos_screenshots:
+        try:
+            os.remove(caminho)
+            console.print(f"Screenshot deletada: {caminho}")
+        except OSError as e:
+            console.print(f"Erro ao deletar screenshot {caminho}: {e}")
+
+# Função principal para a conexão RDP
+async def conexao_rdp(task):
+    caminhos_screenshots = []
+    try:
+        ip = task["configEntrada"].get("ip", "")
+        user = task["configEntrada"].get("user", "")
+        password = task["configEntrada"].get("password", "")
+
+        # Abrir o Remote Desktop Connection
+        subprocess.Popen('mstsc')
+        await asyncio.sleep(5)
+
+        # Inserir o endereço IP
+        caminhos_screenshots.append(tirar_screenshot("antes_de_inserir_ip"))
+        pyautogui.write(ip)
+        pyautogui.press('tab')
+        await asyncio.sleep(2)
+        caminhos_screenshots.append(tirar_screenshot("depois_de_inserir_ip"))
+
+        # Avançar para a próxima etapa
+        pyautogui.press('space')
+        await asyncio.sleep(2)
+
+        pyautogui.press('tab')
+        await asyncio.sleep(2)
+        pyautogui.write(user)
+        pyautogui.press('enter')
+        await asyncio.sleep(5)
+        caminhos_screenshots.append(tirar_screenshot("depois_de_inserir_usuario"))
+
+        # Inserir a senha
+        pyautogui.write(password)
+        pyautogui.press('enter')
+        await asyncio.sleep(9)
+        caminhos_screenshots.append(tirar_screenshot("depois_de_inserir_senha"))
+
+        # Lidar com o pop-up de certificado clicando em "Yes"
+        pyautogui.press('left')
+        await asyncio.sleep(2)
+        pyautogui.press('enter')
+        await asyncio.sleep(20)
+        caminhos_screenshots.append(tirar_screenshot("depois_do_certificado"))
+
+        # Deletar screenshots após o sucesso
+        deletar_screenshots(caminhos_screenshots)
+
+        return {"sucesso": True, "retorno": "Processo de conexão ao RDP executado com sucesso."}
+
+    except Exception as ex:
+        err_msg = f"Erro ao executar conexao_rdp: {ex}"
+        logger.error(err_msg)
+        console.print(err_msg, style="bold red")
+
+        # Tirar screenshot do erro e adicionar à lista de caminhos para deletar posteriormente
+        caminhos_screenshots.append(tirar_screenshot("erro"))
+
+        # Deletar screenshots mesmo em caso de erro
+        deletar_screenshots(caminhos_screenshots)
+
+        return {"sucesso": False, "retorno": err_msg}
