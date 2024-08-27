99ac4c0e8708dd42be002b26f18575c87c686fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:06:21 +0000

    Add package arcade/3.0.0.dev33/arcade-3.0.0.dev33-py3-none-any.whl

---
+++ packages/arcade/arcade-3.0.0.dev33-py3-none-any.whl/arcade/gui/experimental/password_input.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from arcade.gui import Surface, UIEvent, UIInputText, UITextInputEvent
+
+
+class UIPasswordInput(UIInputText):
+    """A password input field. The text is hidden with asterisks."""
+
+    def on_event(self, event: UIEvent) -> Optional[bool]:
+        """Remove new lines from the input, which are not allowed in passwords."""
+        if isinstance(event, UITextInputEvent):
+            event.text = event.text.replace("\n", "").replace("\r", "")
+        return super().on_event(event)
+
+    def do_render(self, surface: Surface):
+        """Override to render the text as asterisks."""
+        self.layout.begin_update()
+        position = self.caret.position
+        text = self.text
+        self.text = "*" * len(self.text)
+        super().do_render(surface)
+        self.text = text
+        self.caret.position = position
+        self.layout.end_update()
