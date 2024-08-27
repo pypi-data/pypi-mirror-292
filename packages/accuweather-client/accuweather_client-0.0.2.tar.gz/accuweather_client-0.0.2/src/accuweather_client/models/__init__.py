07a03562f142223ead89c76f9658858e954e4064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:05:02 +0000

    Add package accuweather-client/0.0.2/accuweather_client-0.0.2.tar.gz

---
+++ packages/accuweather-client/accuweather_client-0.0.2.tar.gz/accuweather_client-0.0.2/src/accuweather_client/models/__init__.py
@@ -0,0 +1,6 @@
+from .location import LocationModel, LocationModelCity  # noqa: F401
+from ._base import TokenValidation  # noqa: F401
+from .weather import (
+    ForecastModel5Days,  # noqa: F401
+    CurrentConditionsModel,  # noqa: F401
+)
