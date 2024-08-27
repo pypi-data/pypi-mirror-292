d27e44487b7ef5782b61bf1ca20e67ff38a259c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:49 +0000

    Add package docta-http-client/0.1.18/docta-http-client-0.1.18.tar.gz

---
+++ packages/docta-http-client/docta-http-client-0.1.18.tar.gz/docta-http-client-0.1.18/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docta-http-client",
+    version="0.1.18",
+    author="Tongzhou Jiang @ docta.ai",
+    author_email="tojiang@docta.ai",
+    description="A simple HTTP client for the Docta API.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/docta/docta-http-client",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
