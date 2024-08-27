94d90f467d227ca732f6cc56e8b830e9f2f4f459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:31 +0000

    Add package sammhelper/0.3.6/sammhelper-0.3.6.tar.gz

---
+++ packages/sammhelper/sammhelper-0.3.6.tar.gz/sammhelper-0.3.6/src/sammhelper/version.py
@@ -0,0 +1,26 @@
+import importlib.metadata
+import requests
+
+def version():
+
+    def get_installed_version():
+        installed_version = importlib.metadata.version("sammhelper")
+        return installed_version
+
+    def check_for_updates():
+        installed_version = get_installed_version()
+    
+        # Fetch the latest version from PyPI
+        try:
+            response = requests.get("https://pypi.org/pypi/sammhelper/json")
+            response.raise_for_status()  # Raise an error for bad responses (e.g., 404, 500)
+            latest_version = response.json()["info"]["version"]
+        except requests.RequestException:
+            print("Error: Unable to fetch the latest version of sammhelper from PyPI.")
+        
+        if installed_version == latest_version:
+            print(f"The newest version of sammhelper ({installed_version}) is already installed.")
+        else:
+            print (
+                f"A new version of sammhelper is available ({latest_version}).\nIn order to update sammhelper, open your command window and enter: pip install --upgrade sammhelper"
+            )
