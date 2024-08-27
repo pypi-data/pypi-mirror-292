14745000d83b60628ac8cf42a508e8b9a2ae9409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:45 +0000

    Add package lexcube/0.4.19/lexcube-0.4.19-py3-none-any.whl

---
+++ packages/lexcube/lexcube-0.4.19-py3-none-any.whl/lexcube-0.4.19.data/data/share/jupyter/labextensions/lexcube/static/lib_plugin_js.f72d709339a65f5c4f8c.js
@@ -0,0 +1,83 @@
+"use strict";
+(self["webpackChunklexcube"] = self["webpackChunklexcube"] || []).push([["lib_plugin_js"],{
+
+/***/ "./lib/plugin.js":
+/*!***********************!*\
+  !*** ./lib/plugin.js ***!
+  \***********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+/*
+    Lexcube - Interactive 3D Data Cube Visualization
+    Copyright (C) 2022 Maximilian Söchting <maximilian.soechting@uni-leipzig.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const base_1 = __webpack_require__(/*! @jupyter-widgets/base */ "webpack/sharing/consume/default/@jupyter-widgets/base");
+const widgetExports = __importStar(__webpack_require__(/*! ./widget */ "./lib/widget.js"));
+const version_1 = __webpack_require__(/*! ./version */ "./lib/version.js");
+const EXTENSION_ID = 'lexcube:plugin';
+/**
+ * The example plugin.
+ */
+const examplePlugin = {
+    id: EXTENSION_ID,
+    requires: [base_1.IJupyterWidgetRegistry],
+    activate: activateWidgetExtension,
+    autoStart: true,
+};
+// the "as unknown as ..." typecast above is solely to support JupyterLab 1
+// and 2 in the same codebase and should be removed when we migrate to Lumino.
+exports["default"] = examplePlugin;
+/**
+ * Activate the widget extension.
+ */
+function activateWidgetExtension(app, registry) {
+    registry.registerWidget({
+        name: version_1.MODULE_NAME,
+        version: version_1.MODULE_VERSION,
+        exports: widgetExports,
+    });
+}
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_plugin_js.f72d709339a65f5c4f8c.js.map
