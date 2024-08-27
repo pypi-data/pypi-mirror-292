fad82b652442bde83603b6501702410567d2c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:14 +0000

    Add package dsrag-sdk/0.0.4/dsrag_sdk-0.0.4.tar.gz

---
+++ packages/dsrag-sdk/dsrag_sdk-0.0.4.tar.gz/dsrag_sdk-0.0.4/dsrag_sdk/__init__.py
@@ -0,0 +1,48 @@
+from dsrag_sdk.main import (
+    set_base_url,
+)
+
+from dsrag_sdk.knowledge_bases import (
+    create_knowledge_base,
+    update_knowledge_base,
+    list_knowledge_bases,
+    get_knowledge_base,
+    delete_knowledge_base,
+)
+
+from dsrag_sdk.documents import (
+    create_document_via_text,
+    list_documents,
+    get_document,
+    delete_document,
+)
+
+from dsrag_sdk.chat import (
+    create_chat_thread,
+    list_chat_threads,
+    get_chat_thread,
+    delete_chat_thread,
+    list_thread_interactions,
+    get_chat_response
+)
+
+
+__all__ = [
+    'set_base_url',
+    'create_knowledge_base',
+    'update_knowledge_base',
+    'list_knowledge_bases',
+    'get_knowledge_base',
+    'delete_knowledge_base',
+    'create_document_via_text',
+    'list_documents',
+    'get_document',
+    'delete_document',
+    'create_chat_thread',
+    'list_chat_threads',
+    'get_chat_thread',
+    'delete_chat_thread',
+    'list_thread_interactions',
+    'get_chat_response'
+]
+
