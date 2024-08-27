c4a38efb5894ff84d1c85434b4db110c5e4c036f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:05:11 +0000

    Add package StochasticGhost/0.2.0/stochasticghost-0.2.0.tar.gz

---
+++ packages/StochasticGhost/stochasticghost-0.2.0.tar.gz/stochasticghost-0.2.0/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+setup(
+    name='StochasticGhost',
+    version='0.2.0',
+    description='Empirical Risk minimization with constraints',
+    author='Vyacheslav Kungrutsev',
+    author_email='kunguvya@fel.cvut.cz',
+    packages=find_packages(),
+    python_requires='==3.10.*',
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'pot==0.9.3',
+        'argparse',
+        'scipy==1.11.4',
+        'qpsolvers==4.2.0',
+        'scikit-learn==1.3.0',
+        'matplotlib',
+        'autoray==0.6.7',
+    ],
+)
