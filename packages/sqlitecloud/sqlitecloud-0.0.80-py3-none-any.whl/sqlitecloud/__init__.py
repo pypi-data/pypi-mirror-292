67f775e1001f10964d7c3fd21e9a72e81494c87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:05:19 +0000

    Add package sqlitecloud/0.0.80/sqlitecloud-0.0.80-py3-none-any.whl

---
+++ packages/sqlitecloud/sqlitecloud-0.0.80-py3-none-any.whl/sqlitecloud/__init__.py
@@ -0,0 +1,33 @@
+# To replicate the public interface of sqlite3, we need to import
+# the classes and functions from the dbapi2 module.
+# eg:  sqlite3.connect() -> sqlitecloud.connect()
+#
+
+from .dbapi2 import (
+    PARSE_COLNAMES,
+    PARSE_DECLTYPES,
+    Connection,
+    Cursor,
+    Row,
+    adapters,
+    connect,
+    converters,
+    register_adapter,
+    register_converter,
+)
+
+__all__ = [
+    "VERSION",
+    "Connection",
+    "Cursor",
+    "connect",
+    "register_adapter",
+    "register_converter",
+    "PARSE_DECLTYPES",
+    "PARSE_COLNAMES",
+    "adapters",
+    "converters",
+    "Row",
+]
+
+VERSION = "0.0.80"
