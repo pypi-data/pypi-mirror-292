79c26da8cc4da747738e63128a4df6854fc95ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:54 +0000

    Add package zinley-cli/0.0.12/zinley_cli-0.0.12.tar.gz

---
+++ packages/zinley-cli/zinley_cli-0.0.12.tar.gz/zinley_cli-0.0.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_namespace_packages
+
+# read requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="zinley-cli",
+    version="0.0.12",
+    packages=find_namespace_packages(include=['zinley', 'zinley.*']),
+    include_package_data=True,
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'zinley = zinley.__main__:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
