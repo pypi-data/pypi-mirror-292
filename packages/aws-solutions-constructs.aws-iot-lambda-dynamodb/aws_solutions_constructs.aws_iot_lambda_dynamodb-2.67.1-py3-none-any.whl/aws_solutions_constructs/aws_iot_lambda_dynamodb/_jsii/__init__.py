638f96ae627f28a5945ef8423aa44118763cfc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:06:11 +0000

    Add package aws-solutions-constructs.aws-iot-lambda-dynamodb/2.67.1/aws_solutions_constructs.aws_iot_lambda_dynamodb-2.67.1-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-iot-lambda-dynamodb/aws_solutions_constructs.aws_iot_lambda_dynamodb-2.67.1-py3-none-any.whl/aws_solutions_constructs/aws_iot_lambda_dynamodb/_jsii/__init__.py
@@ -0,0 +1,34 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.integ_tests_alpha._jsii
+import aws_solutions_constructs.aws_iot_lambda._jsii
+import aws_solutions_constructs.aws_lambda_dynamodb._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-iot-lambda-dynamodb",
+    "2.67.1",
+    __name__[0:-6],
+    "aws-iot-lambda-dynamodb@2.67.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
