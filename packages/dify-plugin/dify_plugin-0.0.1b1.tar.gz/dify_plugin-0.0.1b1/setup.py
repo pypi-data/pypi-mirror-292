04c2987d0ae8021c98a360542daf4222730e02a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:40 +0000

    Add package dify-plugin/0.0.1b1/dify_plugin-0.0.1b1.tar.gz

---
+++ packages/dify-plugin/dify_plugin-0.0.1b1.tar.gz/dify_plugin-0.0.1b1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="dify_plugin",
+    version="0.0.1-beta1",
+    author="langgenius",
+    long_description_content_type="text/markdown",
+    url="https://github.com/langgenius/dify-plugin-sdks.git",
+    description="Dify Plugin SDK",
+    long_description=open("README.md", encoding="utf-8").read(),
+    packages=["dify_plugin"],
+    keywords=["dify", "plugin", "sdk"],
+    install_requires=[
+        "pydantic~=2.8.2",
+        "pydantic_settings~=2.3.4",
+        "pyyaml~=6.0.1",
+        "transformers~=4.42.4",
+        "Flask~=3.0.3",
+        "gevent~=24.2.1",
+        "Werkzeug~=3.0.3",
+        "dpkt~=1.9.8",
+        "awslambdaric~=2.1.0",
+        "yarl~=1.9.4",
+    ],
+    python_requires='>=3.12'
+)
