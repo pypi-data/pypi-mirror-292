74ece6ae5233cef88b1df86eafa4a3c92bc766e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:04:54 +0000

    Add package azure-ai-evaluation/0.0.0b0/azure_ai_evaluation-0.0.0b0.tar.gz

---
+++ packages/azure-ai-evaluation/azure_ai_evaluation-0.0.0b0.tar.gz/azure_ai_evaluation-0.0.0b0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='azure-ai-evaluation',
+    version='0.0.0b0',
+    description='',
+    long_description='',
+    url='https://learn.microsoft.com/en-us/python/api/overview/azure/ai?view=azure-python-preview',
+    author='Microsoft Corporation',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
