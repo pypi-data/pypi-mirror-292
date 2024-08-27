e350bb26076e73f304279fa46e9beb182cd5bdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:47 +0000

    Add package OpenForest/0.3/openforest-0.3.tar.gz

---
+++ packages/OpenForest/openforest-0.3.tar.gz/openforest-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenForest',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, if any
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of the OpenForest library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/OpenForest',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
