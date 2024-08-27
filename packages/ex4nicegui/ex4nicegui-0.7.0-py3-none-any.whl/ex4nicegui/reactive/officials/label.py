7fe926a81ad4f1412d630719a67e60e3d4ea94cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:30 +0000

    Add package ex4nicegui/0.7.0/ex4nicegui-0.7.0-py3-none-any.whl

---
+++ packages/ex4nicegui/ex4nicegui-0.7.0-py3-none-any.whl/ex4nicegui/reactive/officials/label.py
@@ -0,0 +1,45 @@
+from typing import Any
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    to_value,
+    _TMaybeRef as TMaybeRef,
+)
+from nicegui import ui
+from .base import BindableUi
+from ex4nicegui.reactive.mixins.textColor import HtmlTextColorableMixin
+
+
+class LabelBindableUi(BindableUi[ui.label], HtmlTextColorableMixin):
+    def __init__(
+        self,
+        text: TMaybeRef[Any] = "",
+    ) -> None:
+        pc = ParameterClassifier(locals(), maybeRefs=["text"], events=[])
+
+        element = ui.label(**pc.get_values_kws())
+        super().__init__(element)
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    @property
+    def text(self):
+        return self.element.text
+
+    def bind_prop(self, prop: str, value: TGetterOrReadonlyRef):
+        if prop == "text":
+            return self.bind_text(value)
+
+        if prop == "color":
+            return self.bind_color(value)
+
+        return super().bind_prop(prop, value)
+
+    def bind_text(self, text: TGetterOrReadonlyRef):
+        @self._ui_effect
+        def _():
+            self.element.set_text(str(to_value(text)))
+            self.element.update()
+
+        return self
