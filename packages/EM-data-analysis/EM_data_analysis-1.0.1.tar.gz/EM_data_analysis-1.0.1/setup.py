8bf576c2d3c86453aec0cb6705ef6136cf36b853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:11 +0000

    Add package EM-data-analysis/1.0.1/EM_data_analysis-1.0.1.tar.gz

---
+++ packages/EM-data-analysis/EM_data_analysis-1.0.1.tar.gz/EM_data_analysis-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EM_data_analysis',  
+    version='1.0.1',  
+    packages=find_packages(),  
+    install_requires=[
+        'opencv-python',       
+        'numpy',               
+        'porespy',             
+        'matplotlib',          
+        'openpnm',             
+        'scipy',              
+        'pandas',              
+        'dataframe-image',     
+        'moviepy',             
+        'IPython',             
+        'scikit-image',        
+    ],
+    description='A Python package for EM data analysis',
+    author='Mattia Lizzano',
+    author_email='mattializzano@gmail.com',
+    url='https://github.com/Nomeutente1/EM_data_analysis',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+)
+
