5ee7ee823ad5e090d760fbc6a4fab5541a927593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:31 +0000

    Add package old-doc/0.0.1/old-doc-0.0.1.tar.gz

---
+++ packages/old-doc/old-doc-0.0.1.tar.gz/old-doc-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="old-doc",
+    version="0.0.1",
+    description="Easily create synthetic data for HTR and OCR",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="WJB Mattingly",
+    url="https://github.com/wjbmattingly/bagpipes-spacy",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "Pillow",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.8',
+)
