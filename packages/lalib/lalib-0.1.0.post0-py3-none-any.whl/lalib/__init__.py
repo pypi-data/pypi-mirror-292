86ad6d2259b084928d85424188015fabb85016fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:54 +0000

    Add package lalib/0.1.0.post0/lalib-0.1.0.post0-py3-none-any.whl

---
+++ packages/lalib/lalib-0.1.0.post0-py3-none-any.whl/lalib/__init__.py
@@ -0,0 +1,23 @@
+"""A Python library to study linear algebra.
+
+Example:
+    >>> import lalib
+    >>> lalib.__version__ != '0.0.0'
+    True
+"""
+
+from importlib import metadata as _metadata
+
+
+try:
+    _pkg_info = _metadata.metadata(__name__)
+
+except _metadata.PackageNotFoundError:  # pragma: no cover
+    __author__ = "unknown"
+    __pkg_name__ = "unknown"
+    __version__ = "unknown"
+
+else:
+    __author__ = _pkg_info["author"]
+    __pkg_name__ = _pkg_info["name"]
+    __version__ = _pkg_info["version"]
