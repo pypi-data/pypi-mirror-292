3ce06ddaff361a5825b93da891e397754ded5aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:32 +0000

    Add package ImgToolsGG/0.1.23/ImgToolsGG-0.1.23.tar.gz

---
+++ packages/ImgToolsGG/ImgToolsGG-0.1.23.tar.gz/ImgToolsGG-0.1.23/README.md
@@ -0,0 +1,16 @@
+# test!!! ImgToolsGG
+
+
+### Installation
+
+> pip install ImgToolsGG
+
+### Get started
+How to use this package:
+
+```
+import os
+
+print("ok")
+
+```
---
+++ packages/ImgToolsGG/ImgToolsGG-0.1.23.tar.gz/ImgToolsGG-0.1.23/src/App.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class ImageUtils:
+    def __init__(self, image_name):
+        self.image_name = image_name
+    def get_image_name(self):
+
+        return self.image_name
+
