331cd7fb7ed0320ea6f56779bb93126098476aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:50 +0000

    Add package omakase-media-tools/0.1.0/omakase_media_tools-0.1.0-py3-none-any.whl

---
+++ packages/omakase-media-tools/omakase_media_tools-0.1.0-py3-none-any.whl/omakase_media_tools/mediaconvert/tears-of-steel_sdr_24_BITC.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+aws mediaconvert create-job --endpoint-url https://q25wbt2lc.mediaconvert.us-east-1.amazonaws.com --cli-input-json file://./tears-of-steel_sdr_24_BITC.json --profile omakase
