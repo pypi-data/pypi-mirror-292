8ea7579203fb16b8a7e1ec003b86fdb14bbf3d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:05:01 +0000

    Add package XJTU-TGA/0.1.3/XJTU_TGA-0.1.3-py3-none-any.whl

---
+++ packages/XJTU-TGA/XJTU_TGA-0.1.3-py3-none-any.whl/XJTU_TGA/run.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/8/27 2:25
+# @Author : DanYang
+# @File : run.py
+# @Software : PyCharm
+import os
+import shutil
+
+WORK_PATH = os.path.dirname(__file__)
+DIRS = ["dataset", "plot", "template", "user_data"]
+FILES = ["config.json"]
+
+for dirs in DIRS:
+    if not os.path.exists(dirs):
+        shutil.copytree(os.path.join(WORK_PATH, dirs), dirs)
+
+for file in FILES:
+    if not os.path.exists(file):
+        print("HHH")
+        shutil.copy(os.path.join(WORK_PATH, file), file)
+
+
+from .TGAanalyser.DataPloter import run as run
