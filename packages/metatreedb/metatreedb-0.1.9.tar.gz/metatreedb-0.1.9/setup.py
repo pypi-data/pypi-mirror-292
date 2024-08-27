27e5a2e97abb6685f721c2f7ab7e6b58a4caea17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:31 +0000

    Add package metatreedb/0.1.9/metatreedb-0.1.9.tar.gz

---
+++ packages/metatreedb/metatreedb-0.1.9.tar.gz/metatreedb-0.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metatreedb",
+    version="0.1.9",
+    author="oboki",
+    author_email="oboki@kakao.com",
+    description="Metatree is a DBMS that uses the filesystem itself as a tree-structured database.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/oboki/metatreedb",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "hdfs",
+        "pyyaml",
+        "requests",
+    ],
+    extras_require={"dev": ["pytest"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
