e2bc0ee35814cf529b554ebff9b44e45111c33d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:08 +0000

    Add package dtmapi/0.0.6/dtmapi-0.0.6.tar.gz

---
+++ packages/dtmapi/dtmapi-0.0.6.tar.gz/dtmapi-0.0.6/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import find_packages, setup
+
+setup(
+    name="dtmapi",
+    version="0.0.6",
+    packages=find_packages(),
+    install_requires=["requests", "pandas"],
+    author="Luong Bang Tran",
+    author_email="lutran@iom.int",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Github": "https://github.com/Displacement-Tracking-Matrix/dtmapi",
+        "Documentation": "https://pydtm.readthedocs.io/en/latest/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
