41e09488d337b7090caaed71b7881b412a59f21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:11 +0000

    Add package ticketsdk/2.3.3/ticketsdk-2.3.3-py3-none-any.whl

---
+++ packages/ticketsdk/ticketsdk-2.3.3-py3-none-any.whl/ticketsdk/constants.py
@@ -0,0 +1,3 @@
+LAMBDA_URI = "https://faas-sgp1-18bc02ac.doserverless.co"
+LAMBDA_ENDPOINT_FOR_TICKET = "api/v1/namespaces/fn-ddfd7283-a591-4b18-8f95-7bcb178e6951/actions/ticket/api?blocking=true&result=false"
+LAMBDA_AUTHEN = "Basic MDk4Y2QwNTYtYTRmZS00YTJmLWE4ZTYtYmJlMTY0ZWQ4MjdmOjVxa2RXR2JtWEh3bnBMZHl2bWg3TlQzam5ZalJpWElyRjFqVnczeklsUWZMQnBiZVpKbGdMRHFRSjZ2NXMwQ0k="
