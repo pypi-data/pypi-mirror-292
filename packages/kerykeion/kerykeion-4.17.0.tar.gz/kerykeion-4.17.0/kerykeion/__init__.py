a599d73f3b37a30e00f071a2f8cc234bb91eeec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:04:30 +0000

    Add package kerykeion/4.17.0/kerykeion-4.17.0.tar.gz

---
+++ packages/kerykeion/kerykeion-4.17.0.tar.gz/kerykeion-4.17.0/kerykeion/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+This is part of Kerykeion (C) 2024 Giacomo Battaglia
+
+.. include:: ../README.md
+"""
+
+# Local
+from .astrological_subject import AstrologicalSubject
+from .charts.kerykeion_chart_svg import KerykeionChartSVG
+from .kr_types import *
+from .relationship_score import RelationshipScore
+from .aspects import SynastryAspects, NatalAspects
+from .report import Report
+from .settings import KerykeionSettingsModel, get_settings
+from .enums import Planets, Aspects, Signs
+from .ephemeris_data import EphemerisDataFactory
