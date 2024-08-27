86c18f1189092a45868483baf71002905a4ecbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:25 +0000

    Add package spectran/0.2.0/spectran-0.2.0-py3-none-any.whl

---
+++ packages/spectran/spectran-0.2.0-py3-none-any.whl/spectran/daq/daq.py
@@ -0,0 +1,115 @@
+"""This module contains all functionality regarding communication with the DAQ device.
+"""
+
+from .. import log, ureg
+from enum import Enum
+
+from abc import ABC, abstractmethod
+import numpy as np
+from PySide6.QtCore import Signal
+
+class DAQ(ABC):
+    
+    connected_device: str = None
+    
+    @abstractmethod
+    def list_devices(self) -> list[str]:
+        """List devices picked up by driver
+
+        Returns:
+            list[str]: list of names of devices
+        """
+
+    @abstractmethod
+    def list_ports(self) -> list[str]:
+        """List ports picked up by self.connected_device
+
+        Returns:
+            list[str]: list of names of ports
+        """
+
+    @abstractmethod
+    def list_term_configs(self) -> tuple[Enum, int]:
+        """List of Terminal configurations and the default element"""
+    
+    def connect_device(self, resource_name):
+        """Connect to the device
+
+        Args:
+            resource_name (str): name of the device to connect to
+        """
+        self.connected_device = resource_name
+        return self.connected_device
+    
+    @abstractmethod
+    def get_properties(self) -> dict:
+        """Returns properties of self.connected_device in a dict"""
+    
+    @abstractmethod
+    def get_sequence(self, data_holder:np.ndarray, 
+                     average_index:int,
+                     config:dict,
+                     main_window,
+                     plotting_signal:Signal) -> np.ndarray:
+        """Get data from DAQ device
+
+        Args:
+            data_holder (np.ndarray): array to store data
+            average_index (int): index of average
+            config (dict): configuration dictionary
+            plotting_signal (Signal): signal to emit when to plot. 
+                    This is filled automatically by the Worker class 
+
+        Returns:
+            np.ndarray: data_holder with data
+        """
+
+import time
+class DummyDAQ(DAQ):
+     
+    def list_devices(self):
+        return ["Dev1", "Dev2"]
+    
+    def list_ports(self):
+        return ["ai1", "ai2", "ai3"]
+    
+    def list_term_configs(self):
+        return Enum("Test", names='RED GREEN BLUE'), 1
+    
+    def get_properties(self):
+        return super().get_properties()
+    
+    def get_sequence(self, data_holder:np.ndarray,
+                     average_index:int, 
+                     config:dict,
+                     main_window,
+                     plotting_signal:Signal):
+        
+        duration = config["duration"].to(ureg.second).magnitude
+        sample_rate = config["sample_rate"].to(ureg.Hz).magnitude
+        averages = config["averages"]
+        
+        start_time = time.time()
+        # this is where the data is acquired
+        data_holder[average_index] = self.acquire(duration, sample_rate)
+        
+        if average_index % 1 == 0:
+            log.info(f"Emit at {average_index+1}/{averages} - {(time.time()-start_time)*1e3:.2f} ms")
+            plotting_signal.emit(average_index, data_holder)
+        else:
+            log.info(f"Updating at {average_index+1}/{averages} - {(time.time()-start_time)*1e3:.2f} ms")
+
+        
+    def acquire(self, duration, sample_rate) -> np.ndarray:
+        """A wrapper function to simulate data acquisition. 
+        You don't need to use such a function in your implementation.
+        """
+        
+        t = np.linspace(0, duration, int(duration * sample_rate))
+        num_freqs = 100
+        rand = 1e6 * np.sort(np.random.random(num_freqs))
+        decrease = np.logspace(10, 0.1, num_freqs)
+        s = np.random.normal(0, 0.1, len(t))
+        s += np.sum(decrease[:, np.newaxis] * np.sin(2 * np.pi * rand[:, np.newaxis] * t), axis=0)
+       
+        return s
---
+++ packages/spectran/spectran-0.2.0-py3-none-any.whl/spectran/data_handler.py
@@ -0,0 +1,90 @@
+"""This class should contain all data related functionality."""
+from . import log, ureg
+from scipy.signal import welch, periodogram
+import numpy as np 
+from PySide6.QtWidgets import QFileDialog
+from pathlib import Path
+
+class DataHandler():
+
+    voltage_data = None
+    data_has_changed = False
+    time_seq = None
+    frequencies = None
+    psd = None
+    _config = None
+
+    def __init__(self, main_window) -> None:
+        self.main_window = main_window
+
+    # config setter and getter    
+    @property
+    def config(self):
+        return self._config
+    
+    @config.setter
+    def config(self, value):
+        self._config = value
+
+        # calculate time sequence
+        self.time_seq = np.linspace(0, 
+            self._config["duration"].to(ureg.second).magnitude, 
+            int(self._config["duration"].to(ureg.second).magnitude * self._config["sample_rate"].to(ureg.Hz).magnitude))
+
+    def calculate_psd(self, index):
+            if index is None:
+                voltages = self.voltage_data
+            else:
+                voltages = self.voltage_data[:index+1]
+            # self.frequencies, psds = welch(voltages,
+            #                         fs=self._config["sample_rate"].to(ureg.Hz).magnitude)
+            self.frequencies, psds = periodogram(voltages, 
+                                                 fs=self._config["sample_rate"].to(ureg.Hz).magnitude)
+            self.psd = np.mean(psds, axis=0)
+            log.debug("PSD calculated until index {}".format(index))
+            
+            return self.frequencies, self.psd
+        
+    def calculate_data(self, data, index, progress_callback):
+        if data is not None:
+            self.voltage_data = data
+            self.data_has_changed = True
+            self.calculate_psd(index)
+
+    def save_file(self):
+        
+        filename = self.save_file_dialog()
+        if filename is None:
+            return
+        
+        self.file_path = Path(filename)
+        header_text = (f"Measurement with Driver:{self._config['driver']} on Device:{self._config['device']}\n"
+            + f"Date: {self._config['start_time']}\n"
+            + f"Input Channel: {self._config['input_channel']} with {self._config['terminal_config']}\n"
+            + f"Duration: {self._config['duration']}\n"
+            + f"Sample Rate: {self._config['sample_rate_real']}\n"
+            + f"Averages: {self._config['averages']}\n"
+            + f"Unit for Data: {self._config['unit']}\n"
+            + f"Signal Range: {self._config['signal_range_min_real']}, {self._config['signal_range_max_real']}"
+            )
+
+        np.savetxt(self.file_path, self.voltage_data.T, 
+                   delimiter="\t",
+                   header=header_text)
+        
+        # self.main_window.statusBar().showMessage(f"Data saved to {self.file_path}")
+        log.info("Data saved to {}".format(self.file_path))
+        self.main_window.save_window.close()
+
+        # raise NotImplementedError("Saving data not implemented yet")
+
+    
+    def save_file_dialog(
+        self, file_name="output.dat", extensions="Data-File (*.dat *.txt);;All Files (*)"
+    ):
+        filename, _ = QFileDialog.getSaveFileName(
+            self.main_window.main_ui, "Save", file_name, extensions
+        )
+        if filename:
+            return filename
+        
