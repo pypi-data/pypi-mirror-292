728dca27ccb3eb40c2b77c4acbeb96cb88f8b2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:05:51 +0000

    Add package shamsi-datetime/0.1.2/shamsi_datetime-0.1.2.tar.gz

---
+++ packages/shamsi-datetime/shamsi_datetime-0.1.2.tar.gz/shamsi_datetime-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shamsi_datetime",
+    version="0.1.2", 
+    description="A Shamsi (Jalali) datetime class for NumPy integration",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ali Razmjooei",
+    author_email="your.email@example.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["numpy", "jdatetime"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
