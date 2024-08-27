cb7601cc28116b75f98bc093e23018b838eee1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:17 +0000

    Add package dvc-pandas/0.2.0/dvc_pandas-0.2.0.tar.gz

---
+++ packages/dvc-pandas/dvc_pandas-0.2.0.tar.gz/dvc_pandas-0.2.0/src/dvc_pandas/__init__.py
@@ -0,0 +1,9 @@
+from .dataset import Dataset, DatasetMeta
+from .repository import Repository, StaticRepository
+
+__all__ = [
+    'Dataset',
+    'DatasetMeta',
+    'Repository',
+    'StaticRepository',
+]
