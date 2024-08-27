37eed6ac8b7bda6ede9186df35cec80b416904c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:55 +0000

    Add package group2p/0.2.0/group2p-0.2.0.tar.gz

---
+++ packages/group2p/group2p-0.2.0.tar.gz/group2p-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="group2p",
+    version="0.2.0",
+    packages=find_packages(),
+    requires=[
+        "requests"
+        ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
