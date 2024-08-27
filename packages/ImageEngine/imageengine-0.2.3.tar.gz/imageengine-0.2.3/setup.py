35db01d2542beb760237e8c391e0e1a46ce6ff88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:05:02 +0000

    Add package ImageEngine/0.2.3/imageengine-0.2.3.tar.gz

---
+++ packages/ImageEngine/imageengine-0.2.3.tar.gz/imageengine-0.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='ImageEngine',
+    version='0.2.3',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'search-ddg=ImageEngine.search_duckduckgo:search_ddg_entry',
+            'search-google=ImageEngine.search_google:search_google_entry',
+            'search-bing=ImageEngine.search_bing:search_bing_entry',
+            'search-web=ImageEngine.search_web:search_web_entry',
+        ],
+    },
+    install_requires=[
+        "fastai",
+        "duckduckgo_search",
+        "icrawler",
+        "requests",
+        "Pillow",
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
