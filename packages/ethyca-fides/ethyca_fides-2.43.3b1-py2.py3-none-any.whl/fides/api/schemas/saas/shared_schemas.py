de34d625f1f38101b3156ec83057e11e24906646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:26 +0000

    Add package ethyca-fides/2.43.3b1/ethyca_fides-2.43.3b1-py2.py3-none-any.whl

---
+++ packages/ethyca-fides/ethyca_fides-2.43.3b1-py2.py3-none-any.whl/fides/api/schemas/saas/shared_schemas.py
@@ -0,0 +1,51 @@
+from enum import Enum
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class HTTPMethod(Enum):
+    """Enum to represent HTTP Methods"""
+
+    GET = "GET"
+    POST = "POST"
+    PATCH = "PATCH"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
+
+
+class SaaSRequestParams(BaseModel):
+    """
+    Holds the method, path, headers, query, and body params to build a SaaS HTTP request.
+    """
+
+    method: HTTPMethod
+    path: str
+    headers: Dict[str, Any] = {}
+    query_params: Dict[str, Any] = {}
+    body: Optional[str] = None
+    model_config = ConfigDict(use_enum_values=True)
+
+
+class ConnectorParamRef(BaseModel):
+    """A reference to a value in the connector params (by name)"""
+
+    connector_param: Any = None
+
+
+class IdentityParamRef(BaseModel):
+    """A reference to the identity type in the filter Post Processor Config"""
+
+    identity: str
+
+
+class ConsentPropagationStatus(Enum):
+    """
+    An enum for the different statuses that can be returned from a consent propagation request.
+    """
+
+    executed = "executed"
+    no_update_needed = "no_update_needed"
+    missing_data = "missing_data"
