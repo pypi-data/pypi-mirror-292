441c79389e5cdf677a82150158f6a3c021b4a063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:52 +0000

    Add package dashinfer/1.3.0/dashinfer-1.3.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/dashinfer/dashinfer-1.3.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/dashinfer/helper/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) Alibaba, Inc. and its affiliates.
+# @file    __init__.py
+#
+from .helper import EngineHelper, ConfigManager
+
+__all__ = [
+    "EngineHelper",
+    "ConfigManager"
+]
