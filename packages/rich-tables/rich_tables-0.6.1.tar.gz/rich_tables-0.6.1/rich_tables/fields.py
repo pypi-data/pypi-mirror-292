ded1353a6b3c42933a679da85968449f7ac91b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:38 +0000

    Add package rich-tables/0.6.1/rich_tables-0.6.1.tar.gz

---
+++ packages/rich-tables/rich_tables-0.6.1.tar.gz/rich_tables-0.6.1/rich_tables/fields.py
@@ -0,0 +1,314 @@
+from __future__ import annotations
+
+import re
+from collections import defaultdict
+from datetime import datetime
+from functools import singledispatch
+from itertools import islice
+from typing import TYPE_CHECKING, Any, Callable, Iterable, MutableMapping
+
+from rich.text import Text
+
+from .utils import (
+    BOLD_GREEN,
+    BOLD_RED,
+    JSONDict,
+    border_panel,
+    duration2human,
+    fmt_time,
+    format_string,
+    format_with_color,
+    format_with_color_on_black,
+    get_country,
+    human_dt,
+    md_panel,
+    new_table,
+    pretty_diff,
+    progress_bar,
+    simple_panel,
+    split_with_color,
+    sql_syntax,
+    syntax,
+    timestamp2timestr,
+    wrap,
+)
+
+if TYPE_CHECKING:
+    from rich.console import RenderableType
+    from rich.table import Table
+
+
+MATCH_COUNT_HEADER = re.compile(r"duration|(_sum$|_?count$)")
+
+
+def counts_table(data: list[JSONDict]) -> Table:
+    count_header = ""
+    subcount_header = None
+    ordered_headers = []
+    for key in data[0]:
+        if key.endswith("_subcount"):
+            subcount_header = key
+        elif MATCH_COUNT_HEADER.search(key):
+            if count_header:
+                ordered_headers.append(key)
+            else:
+                count_header = key
+        else:
+            ordered_headers.append(key)
+
+    all_counts = [float(i[count_header]) for i in data]
+    num_type = int if len({c % 1 for c in all_counts}) == 1 else float
+    max_value = max(all_counts)
+
+    if subcount_header:
+        count_header = f"{subcount_header}/{count_header}".replace(
+            "_count", ""
+        ).replace("_subcount", "")
+
+    table = new_table(*ordered_headers, count_header, count_header, expand=True)
+    for item, count in zip(data, all_counts):
+        subcount = None
+        inverse = False
+        count_val = str(count)
+        if subcount_header:
+            subcount = float(item[subcount_header])
+            count_val = f"{num_type(subcount)}/{num_type(count)}"
+        elif "duration" in count_header:
+            inverse = True
+            if num_type is int:
+                count_val = duration2human(count)
+        else:
+            count_val = str(num_type(count))
+
+        table.add_row(
+            *(get_val(item, h) for h in ordered_headers),
+            count_val,
+            progress_bar(end=subcount, width=max_value, size=count, inverse=inverse),
+        )
+    if count_header in {"duration", "total_duration"}:
+        table.caption = "Total " + duration2human(float(sum(all_counts)))
+        table.caption_justify = "left"
+    return table
+
+
+FIELDS_MAP: MutableMapping[str, Callable[..., RenderableType]] = defaultdict(
+    lambda: str,
+    diff=lambda x: pretty_diff(*x),
+    albumtypes=lambda x: " ".join(
+        map(
+            format_with_color,
+            (
+                ("; ".join(x) if isinstance(x, list) else x)
+                .replace("compilation", "comp")
+                .replace("dj-mix; broadcast", "dj-mix")
+                .replace("broadcast; dj-mix", "dj-mix")
+            ).split("; "),
+        )
+    ),
+    author=format_with_color_on_black,
+    labels=lambda x: (
+        wrap("    ".join(wrap(y["name"].upper(), f"#{y['color']}") for y in x), "b")
+        if isinstance(x, list)
+        else format_with_color(x.upper())
+    ),
+    avg_last_played=lambda x: human_dt(x, acc=2),
+    since=lambda x: (
+        x
+        if isinstance(x, str)
+        else datetime.fromtimestamp(float(x)).strftime("%F %H:%M")
+    ),
+    dt=lambda x: human_dt(x, acc=5),
+    wait_per_play=lambda x: wrap(
+        " ".join(islice(fmt_time(int(float(x))), 1)), BOLD_GREEN
+    ),
+    bpm=lambda x: (
+        wrap(
+            str(x),
+            (
+                BOLD_GREEN
+                if x < 135
+                else BOLD_RED
+                if x > 230
+                else "red"
+                if x > 165
+                else "yellow"
+            ),
+        )
+        if isinstance(x, int)
+        else x
+    ),
+    length=timestamp2timestr,
+    tracktotal=lambda x: (
+        (wrap("{}", "b cyan") + "/" + wrap("{}", "b cyan")).format(*x)
+        if isinstance(x, Iterable) and not isinstance(x, str)
+        else str(x)
+    ),
+    category=lambda x: "/".join(map(format_with_color, x.split("/"))),
+    country=get_country,
+    helicopta=lambda x: ":fire: " if x and int(x) else "",
+    hidden=lambda x: ":shit: " if x and int(x) else "",
+    keywords=format_with_color_on_black,
+    ingr=lambda x: simple_panel(format_with_color(x)),
+    comments=lambda x: md_panel(
+        x.replace("\n0", "\n* 0").replace("\n[", "\n* ["), title="comments"
+    ),
+    released=lambda x: x.replace("-00", "") if isinstance(x, str) else str(x),
+    duration=lambda x: duration2human(x) if isinstance(x, (int, float)) else x,
+    plays=lambda x: wrap(x, BOLD_GREEN),
+    skips=lambda x: wrap(x, BOLD_RED),
+    new=lambda x: (
+        wrap(":heavy_check_mark: ", BOLD_GREEN)
+        if x
+        else wrap(":cross_mark_button: ", BOLD_RED)
+    ),
+    link=lambda name: (
+        wrap(f" {name} ", "b black on red")
+        if name == "blocks"
+        else wrap(name, BOLD_RED)
+        if name == "is blocked by"
+        else name
+    ),
+    code=lambda x: syntax(x, "python"),
+    context=lambda x: syntax(x, "python"),
+    python=lambda x: syntax(x, "python"),
+    CreatedBy=lambda x: syntax(x.replace(";", "\n"), "sh"),
+    file=lambda x: "/".join(map(format_with_color, x.split("/"))),
+    field=lambda x: ".".join(map(format_with_color, x.split("."))),
+    unified_diff=lambda x: syntax(x, "diff"),
+    diffHunk=lambda x: syntax(x, "diff"),
+    snippet=lambda x: border_panel(syntax(x, "python", indent_guides=True)),
+    query=lambda x: Text(x, style="bold"),
+    sql=lambda x: border_panel(sql_syntax(x.replace(r"\[", "["))),
+)
+fields_by_func: dict[Callable[..., RenderableType], Iterable[str]] = {
+    format_with_color: (
+        "__typename",
+        "album",
+        "albumtype",
+        "app",
+        "area",
+        "assignee",
+        "brand",
+        "calendar",
+        "catalognum",
+        "categories",
+        "Category",
+        "data_source",
+        "default_start_time",
+        "default_end_time",
+        "Description",
+        "endpoint",
+        "entity",
+        "environment",
+        "epic_key",
+        "epic_priority",
+        "epic_status",
+        "event",
+        "from",
+        "full_name",
+        "group_source",
+        "Interests",
+        "issuetype",
+        "key",
+        "kind",
+        "label",
+        "mastering",
+        "media",
+        "module",
+        "operation",
+        "primary",
+        "priority",
+        "project",
+        "short_name",
+        "source",
+        "status",
+        "style",
+        "subtask_assignee",
+        "subtask_key",
+        "subtask_priority",
+        "subtask_status",
+        "symbol",
+        "table",
+        "to",
+        "tables",
+        "test",
+        "type_name",
+        "user",
+    ),
+    split_with_color: ("genre", "genres"),
+    human_dt: (
+        "added",
+        "committedDate",
+        "due",
+        "start",
+        "end",
+        "entry",
+        "first_active",
+        "last_active",
+        "last_played",
+        "mtime",
+        "updated",
+        "providerPublishTime",
+    ),
+    md_panel: (
+        "answer",
+        "benefits",
+        "bodyHTML",
+        "creditText",
+        "description",
+        "desc",
+        # "instructions",
+        "interview",
+        "notes",
+        "text",
+    ),
+}
+for func, fields in fields_by_func.items():
+    for field in fields:
+        FIELDS_MAP[field] = func
+
+DISPLAY_HEADER: dict[str, str] = {
+    "track": "#",
+    "bpm": "🚀",
+    "last_played": ":timer_clock: ",
+    "mtime": "updated",
+    "data_source": "source",
+    "helicopta": ":helicopter: ",
+    "hidden": ":no_entry: ",
+    "track_alt": ":cd: ",
+    "catalognum": ":pen: ",
+    "plays": wrap(":play_button: ", BOLD_GREEN),
+    "skips": wrap(":stop_button: ", BOLD_RED),
+    "albumtypes": "types",
+}
+
+
+def _get_val(value: Any, field: str) -> Any:
+    if value is None:
+        return "None"
+
+    if isinstance(value, str):
+        value = format_string(value)
+
+    if isinstance(value, (int, float)):
+        value = str(value)
+
+    if field not in FIELDS_MAP and field.endswith("_group") and isinstance(value, list):
+        return format_with_color(value)
+
+    return FIELDS_MAP[field](value)
+
+
+@singledispatch
+def get_val(obj: JSONDict | object, field: str) -> Any:
+    """Definition of a generic get_val function."""
+
+
+@get_val.register
+def _(obj: dict, field: str) -> Any:
+    return _get_val(obj.get(field), field)
+
+
+@get_val.register
+def _(obj: object, field: str) -> Any:
+    return _get_val(getattr(obj, field, None), field)
---
+++ packages/rich-tables/rich_tables-0.6.1.tar.gz/rich_tables-0.6.1/rich_tables/utils.py
@@ -0,0 +1,476 @@
+from __future__ import annotations
+
+import random
+import re
+import time
+from datetime import datetime, timedelta, timezone
+from difflib import SequenceMatcher
+from itertools import groupby, islice, starmap, zip_longest
+from math import copysign
+from pprint import pformat
+from string import ascii_uppercase, punctuation
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Match,
+    Optional,
+    Protocol,
+    Sequence,
+    SupportsFloat,
+    Tuple,
+    TypeVar,
+    Union,
+)
+
+import platformdirs
+import sqlparse
+from multimethod import multimethod
+from rich import box
+from rich.align import Align
+from rich.bar import Bar
+from rich.console import Console, RenderableType
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich.table import Table
+from rich.text import Text
+from rich.theme import Theme
+from rich.tree import Tree
+
+JSONDict = Dict[str, Any]
+SPLIT_PAT = re.compile(r"[;,] ?")
+PRED_COLOR_PAT = re.compile(r"(pred color)\]([^\[]+)")
+HTML_PARAGRAPH = re.compile(r"</?p>")
+
+
+BOLD_GREEN = "b green"
+BOLD_RED = "b red"
+SECONDS_PER_DAY = 86400
+CONSECUTIVE_SPACE = re.compile("(?:^ +)|(?: +$)")
+
+
+_T_contra = TypeVar("_T_contra", contravariant=True)
+
+
+class SupportsDunderLT(Protocol[_T_contra]):
+    def __lt__(self, __other: _T_contra) -> bool:
+        pass
+
+
+T = TypeVar("T")
+K = TypeVar("K", bound=SupportsDunderLT[Any])
+
+
+def group_by(iterable: Iterable[T], key: Callable[[T], K]) -> List[Tuple[K, List[T]]]:
+    return [(k, list(g)) for k, g in groupby(sorted(iterable, key=key), key)]
+
+
+def format_string(text: str) -> str:
+    if "pred color]" in text:
+        return PRED_COLOR_PAT.sub(fmt_pred_color, text)
+    if "[" in text and r"\[" not in text and "[/" not in text:
+        return text.replace("[", r"\[")
+
+    return text
+
+
+def wrap(text: str, tag: str) -> str:
+    return f"[{tag}]{format_string(str(text))}[/]"
+
+
+def format_space(string: str) -> str:
+    return CONSECUTIVE_SPACE.sub(r"[u]\g<0>[/]", string)
+
+
+def format_new(string: str) -> str:
+    return wrap(format_space(string), BOLD_GREEN)
+
+
+def format_old(string: str) -> str:
+    return wrap(wrap(string, BOLD_RED), "s")
+
+
+def fmtdiff(change: str, before: str, after: str) -> str:
+    if change == "insert":
+        return format_new(after)
+    if change == "delete":
+        return format_old(before)
+    if change == "replace":
+        return format_old(before) + format_new(after)
+
+    return wrap(before, "dim")
+
+
+def make_difftext(
+    before: str,
+    after: str,
+    junk: str = "".join(
+        sorted((set(punctuation) - {"_", "-", ":"}) | set(ascii_uppercase))
+    ),
+) -> str:
+    matcher = SequenceMatcher(
+        lambda x: x not in junk, autojunk=False, a=before, b=after
+    )
+    diff = ""
+    for code, a1, a2, b1, b2 in matcher.get_opcodes():
+        diff += fmtdiff(code, before[a1:a2], after[b1:b2]) or ""
+    return diff
+
+
+def duration2human(duration: SupportsFloat) -> str:
+    diff = timedelta(seconds=float(duration))
+    days = f"{diff.days}d " if diff.days else ""
+    time_parts = [diff.seconds // 3600, diff.seconds % 3600 // 60, diff.seconds % 60]
+    return "{:>12}".format(days + ":".join(map("{0:0>2}".format, time_parts)))
+
+
+def fmt_time(seconds: int) -> Iterable[str]:
+    abs_seconds = abs(seconds)
+    return (
+        f"{int(copysign(num, seconds)):>3}{unit}"
+        for num, unit in (
+            (abs_seconds // 86400, "d"),
+            (abs_seconds // 3600, "h"),
+            (abs_seconds % 3600 // 60, "m"),
+            (abs_seconds % 60, "s"),
+        )
+        if num
+    )
+
+
+def get_theme() -> Optional[Theme]:
+    config_path = platformdirs.user_config_path("rich") / "config.ini"
+    if config_path.exists():
+        return Theme.read(str(config_path))
+    return None
+
+
+def make_console(**kwargs: Any) -> Console:
+    return Console(
+        theme=get_theme(),
+        force_terminal=True,
+        force_interactive=False,
+        emoji=True,
+        **kwargs,
+    )
+
+
+console = make_console()
+print = console.print
+
+
+class NewTable(Table):
+    def __init__(self, *args: str, **kwargs: Any) -> None:
+        ckwargs = {
+            "overflow": kwargs.pop("overflow", "fold"),
+            "justify": kwargs.pop("justify", "left"),
+            "vertical": kwargs.pop("vertical", "middle"),
+        }
+        super().__init__(**kwargs)
+        for arg in args:
+            self.add_column(arg, **ckwargs)
+
+    def add_rows(self, rows: Iterable[Iterable[RenderableType]]) -> None:
+        """Add multiple rows to the table."""
+        for row in rows:
+            self.add_row(*row)
+
+    @property
+    def colnames(self) -> List[str]:
+        """Provide a mapping between columns names / ids and columns."""
+        return [str(c.header) for c in self.columns]
+
+    def add_dict_item(
+        self,
+        item: JSONDict,
+        transform: Callable[[Any, str], Any] = lambda x, _: x,
+        **kwargs: Any,
+    ) -> None:
+        """Take the required columns / keys from the given dictionary item."""
+        vals = (transform(item.get(c, ""), c) for c in self.colnames)
+        self.add_row(*vals, **kwargs)
+
+
+def new_table(*headers: str, **kwargs: Any) -> NewTable:
+    default = {
+        "show_edge": False,
+        "show_header": False,
+        "pad_edge": False,
+        "highlight": True,
+        "row_styles": ["white"],
+        "expand": False,
+        "title_justify": "left",
+        "style": "black",
+        "border_style": "black",
+        "box": box.ROUNDED,
+    }
+    if headers:
+        default.update(
+            header_style="bold misty_rose1", box=box.SIMPLE_HEAVY, show_header=True
+        )
+    rows = kwargs.pop("rows", [])
+    table = NewTable(*headers, **{**default, **kwargs})
+    if rows:
+        table.add_rows(rows)
+    return table
+
+
+def list_table(items: Iterable[Any], **kwargs: Any) -> NewTable:
+    return new_table(rows=[[i] for i in items], **kwargs)
+
+
+def _randint() -> int:
+    return random.randint(50, 205)
+
+
+def predictably_random_color(string: str) -> str:
+    random.seed(string.strip())
+
+    return f"#{_randint():02X}{_randint():02X}{_randint():02X}"
+
+
+def _format_with_color(string: str, on: Optional[str] = None) -> str:
+    color = f"b {predictably_random_color(string)}"
+    if on:
+        color += f" on {on}"
+    return wrap(string, color)
+
+
+def split_with_color(text: str) -> str:
+    return " ".join(_format_with_color(str(x)) for x in sorted(SPLIT_PAT.split(text)))
+
+
+def format_with_color(items: str | Sequence[str]) -> str:
+    if isinstance(items, str):
+        items = [items]
+
+    return " ".join(_format_with_color(str(x)) for x in items)
+
+
+def format_with_color_on_black(items: Union[str, Iterable[str]]) -> str:
+    if not (isinstance(items, Iterable) and not isinstance(items, str)):
+        items = sorted(SPLIT_PAT.split(str(items)))
+
+    sep = wrap("a", "#000000 on #000000")
+    return " ".join(
+        sep + _format_with_color(str(item), on="#000000") + sep for item in items
+    )
+
+
+def fmt_pred_color(m: Match[str]) -> str:
+    return f"{predictably_random_color(m.group(2))}]{m.group(2)}"
+
+
+def simple_panel(content: RenderableType, **kwargs: Any) -> Panel:
+    default: JSONDict = {
+        "title_align": "left",
+        "subtitle_align": "right",
+        "box": box.SIMPLE,
+        "expand": False,
+        "border_style": "red",
+    }
+    if "title" in kwargs:
+        kwargs["title"] = wrap(kwargs["title"], "b")
+    if kwargs.pop("align", "") == "center":
+        content = Align.center(content, vertical="middle")
+    return Panel(content, **{**default, **kwargs})
+
+
+def border_panel(content: RenderableType, **kwargs: Any) -> Panel:
+    return simple_panel(
+        content, **{"box": box.ROUNDED, "border_style": "dim", **kwargs}
+    )
+
+
+def md_panel(content: str, **kwargs: Any) -> Panel:
+    return border_panel(
+        Markdown(
+            HTML_PARAGRAPH.sub("", content),
+            inline_code_theme="nord-darker",
+            code_theme="nord-darker",
+            justify=kwargs.pop("justify", "left"),
+        ),
+        **kwargs,
+    )
+
+
+def new_tree(
+    values: Iterable[RenderableType] = [], title: str = "", **kwargs: Any
+) -> Tree:
+    color = predictably_random_color(title or str(values))
+    default: JSONDict = {"guide_style": color, "highlight": True}
+    tree = Tree(title, **{**default, **kwargs})
+
+    for val in values:
+        tree.add(val)
+    return tree
+
+
+def get_country(code: str) -> str:
+    return format_with_color(code)
+
+
+def progress_bar(
+    size: float, width: float, end: Optional[float] = None, inverse: bool = False
+) -> Bar:
+    if end is None:
+        end = size
+        size = width
+        bgcolor = "black"
+    else:
+        bgcolor = "#252c3a"
+    ratio = end / size if size else 1
+    if inverse:
+        ratio = 1 - ratio
+
+    random.seed(str(width))
+
+    def norm() -> int:
+        return round(_randint() * ratio)
+
+    color = f"#{norm():0>2X}{norm():0>2X}{norm():0>2X}"
+    return Bar(
+        size=size, begin=0, width=int(width), end=end, color=color, bgcolor=bgcolor
+    )
+
+
+def timestamp2datetime(timestamp: Union[str, int, float, None]) -> datetime:
+    if isinstance(timestamp, str):
+        timestamp = re.sub(r"[.]\d+", "", timestamp.strip("'"))
+        formats = [
+            "%Y-%m-%dT%H:%M:%SZ",
+            "%Y%m%dT%H%M%SZ",
+            "%Y-%m-%d %H:%M:%S",
+            "%Y-%m-%dT%H:%M:%S%z",
+        ]
+        for fmt in formats:
+            try:
+                return datetime.strptime(timestamp, fmt)
+            except ValueError:
+                pass
+    return datetime.fromtimestamp(int(float(timestamp or 0)), tz=timezone.utc)
+
+
+def timestamp2timestr(timestamp: Union[str, int, float, None]) -> str:
+    return timestamp2datetime(timestamp).strftime("%T")
+
+
+def human_dt(timestamp: Union[int, str, float], acc: int = 1) -> str:
+    try:
+        datetime = timestamp2datetime(timestamp)
+    except ValueError:
+        return str(timestamp)
+
+    diff = datetime.timestamp() - time.time()
+    fmted = " ".join(islice(fmt_time(int(diff)), acc))
+
+    return wrap(fmted, BOLD_RED if diff < 0 else BOLD_GREEN)
+
+
+def diff_dt(timestamp: Union[int, str, float], acc: int = 2) -> str:
+    try:
+        datetime = timestamp2datetime(timestamp)
+    except ValueError:
+        return str(timestamp)
+
+    diff = datetime.timestamp() - time.time()
+    fmted = " ".join(islice(fmt_time(int(diff)), acc))
+
+    strtime = datetime.strftime("%F" if abs(diff) > SECONDS_PER_DAY else "%T")
+
+    return wrap(fmted, BOLD_RED if diff < 0 else BOLD_GREEN) + " " + strtime
+
+
+def syntax(*args: Any, **kwargs: Any) -> Syntax:
+    default = {
+        "theme": "paraiso-dark",
+        "background_color": "black",
+        "word_wrap": True,
+    }
+    return Syntax(*args, **{**default, **kwargs})
+
+
+def sql_syntax(sql_string: str) -> Syntax:
+    return Syntax(
+        sqlparse.format(
+            sql_string,
+            indent_columns=True,
+            strip_whitespace=True,
+            strip_comments=True,
+            reindent=True,
+            reindent_aligned=False,
+        ),
+        "sql",
+        theme="gruvbox-dark",
+        background_color="black",
+        word_wrap=True,
+    )
+
+
+def diff_serialize(value: Any) -> str:
+    if value is None:
+        return "null"
+    if value == "":
+        return '""'
+    return str(value)
+
+
+@multimethod
+def diff(before: str, after: str) -> Any:
+    return make_difftext(before, after)
+
+
+@diff.register
+def _(before: Any, after: Any) -> Any:
+    return diff(diff_serialize(before), diff_serialize(after))
+
+
+@diff.register
+def _(before: List[Any], after: List[Any]) -> Any:
+    return list(starmap(diff, zip_longest(before, after)))
+
+
+@diff.register
+def _(before: List[str], after: List[str]) -> Any:
+    before_set, after_set = set(before), set(after)
+    common = before_set & after_set
+    common_list = list(common)
+    return [
+        *list(starmap(diff, zip(common_list, common_list))),
+        *list(
+            starmap(
+                diff,
+                zip_longest(list(before_set - common), list(after_set - common)),
+            )
+        ),
+    ]
+
+
+@diff.register
+def _(before: dict, after: dict) -> Any:
+    data = {}
+    keys = sorted(before.keys() | after.keys())
+    for key in keys:
+        if key not in before:
+            data[wrap(key, BOLD_GREEN)] = wrap(diff_serialize(after[key]), BOLD_GREEN)
+        elif key not in after:
+            data[wrap(key, BOLD_RED)] = wrap(diff_serialize(before[key]), BOLD_RED)
+        else:
+            data[key] = diff(before[key], after[key])
+
+    return data
+
+
+def pretty_diff(before: Any, after: Any) -> Text:
+    result = diff(before, after)
+    if not isinstance(result, str):
+        result = (
+            pformat(result, indent=2, width=300, sort_dicts=False)
+            .replace("'", "")
+            .replace("\\\\", "\\")
+        )
+
+    return Text.from_markup(result)
