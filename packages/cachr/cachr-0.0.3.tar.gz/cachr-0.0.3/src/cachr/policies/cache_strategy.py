fa511da2682aa85b603e514f484dc5628960e37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:36 +0000

    Add package cachr/0.0.3/cachr-0.0.3.tar.gz

---
+++ packages/cachr/cachr-0.0.3.tar.gz/cachr-0.0.3/src/cachr/policies/cache_strategy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class CacheStrategy(ABC):
+    @abstractmethod
+    def on_access(self, cache: "Cache", key: Any) -> None:
+        """ Method that is called before a key is accessed in the cache."""
+        pass
+
+    @abstractmethod
+    def on_insert(self, cache: "Cache", key: Any) -> None:
+        """Method that is called before a new key-value pair is inserted into the cache."""
+        pass
+
+    @abstractmethod
+    def evict(self, cache: "Cache") -> None:
+        """Method that evicts an item based. """
+        pass
