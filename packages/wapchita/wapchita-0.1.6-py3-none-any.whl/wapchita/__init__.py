1002e020843e4821329156c3bb4a4f02ea1818d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 06:05:49 +0000

    Add package wapchita/0.1.6/wapchita-0.1.6-py3-none-any.whl

---
+++ packages/wapchita/wapchita-0.1.6-py3-none-any.whl/wapchita/__init__.py
@@ -0,0 +1,7 @@
+from wapchita.client import Wapchita, T_Wapchita
+from wapchita.request_wap.request_wap import RequestWap
+
+from wapchita.models.user import WapUser
+from wapchita.models.chats import WapChat, WapChats
+from wapchita.models.device import WapDevice
+from wapchita.models.webhook import WapWebhookBody
