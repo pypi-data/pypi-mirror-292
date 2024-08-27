6c21e1aec9fe4099c5b4e395430c7fe51849aaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:29 +0000

    Add package echosms/0.1.6/echosms-0.1.6.tar.gz

---
+++ packages/echosms/echosms-0.1.6.tar.gz/echosms-0.1.6/src/echosms/utils.py
@@ -0,0 +1,154 @@
+"""Miscellaneous utility functions."""
+import numpy as np
+import pandas as pd
+import xarray as xr
+from scipy.special import spherical_jn, spherical_yn
+from collections.abc import Iterable
+
+
+def as_dataframe(params: dict) -> pd.DataFrame:
+    """Convert model parameters from dict form to a Pandas DataFrame.
+
+    Parameters
+    ----------
+    params :
+        A dictionary containing model parameters.
+
+    Returns
+    -------
+    :
+        Returns a Pandas DataFrame generated from the Cartesian product of all items in the
+        input dict. DataFrame column names are obtained from the dict keys.
+
+    """
+    # Use meshgrid to do the Cartesian product then create a Pandas DataFrame from that, having
+    # flattened the multidimensional arrays and using a dict to provide column names.
+    # This preserves the differing dtypes in each column compared to other ways of
+    # constructing the DataFrame).
+    return pd.DataFrame({k: t.flatten()
+                         for k, t in zip(params.keys(), np.meshgrid(*tuple(params.values())))})
+
+
+def as_dataarray(params: dict) -> xr.DataArray:
+    """Convert model parameters from dict form to a Xarray DataArray.
+
+    Parameters
+    ----------
+    params :
+        A dictionary containing model parameters.
+
+    Returns
+    -------
+    :
+        Returns a multi-dimensional DataArray generated from the Cartesian product of all items
+        in the input dict. The array is named `ts`, the values are initialised to `nan`, the
+        dimension names are the dict keys, and the coordinate variables are the dict values.
+
+    """
+    # Convert scalars to iterables so xarray is happy
+    for k, v in params.items():
+        if not isinstance(v, Iterable) or isinstance(v, str):
+            params[k] = [v]
+
+    sz = [len(v) for k, v in params.items()]
+    return xr.DataArray(data=np.full(sz, np.nan), coords=params, name='ts')
+
+
+def eta(m: int) -> int:
+    """Neumann number.
+
+    Parameters
+    ----------
+    m :
+        The input integer.
+
+    Returns
+    -------
+    :
+        The Neumann number.
+    """
+    if m == 0:
+        return 1
+    else:
+        return 2
+
+
+def k(c: float, f: float) -> float:
+    """Calculate the acoustic wavenumber.
+
+    Parameters
+    ----------
+    c :
+        Sound speed [m/s]
+
+    f :
+        Frequency [Hz]
+
+    Returns
+    -------
+    :
+        The acoustic wavenumber [m⁻¹].
+    """
+    return 2*np.pi*f/c
+
+
+def h1(n: int, z: float, derivative=False) -> complex:
+    """Spherical Hankel function of the first kind or its' derivative.
+
+    Parameters
+    ----------
+    n :
+        Order (n ≥ 0).
+    z :
+        Argument of the Hankel function.
+    derivative :
+        if True, the value of the derivative (rather than the function itself) is returned.
+
+    Returns
+    -------
+    :
+        Value of the spherical Hankel function
+
+    Raises
+    ------
+    ValueError
+        For negative n values.
+
+    Notes
+    -----
+    The value of the Hankel function is calculated from spherical Bessel functions [1].
+
+    The derivative is computed from spherical Hankel functions [2].
+
+    References
+    ----------
+    [1] <https://dlmf.nist.gov/10.47.E10>
+
+    [2] <https://dlmf.nist.gov/10.51.E2>
+    """
+    if n < 0:
+        raise ValueError('Negative n values are not supported for spherical Hankel functions.')
+
+    if not derivative:
+        return spherical_jn(n, z) + 1j*spherical_yn(n, z)
+    else:
+        return -h1(n+1, z) + (n/z) * h1(n, z)
+
+
+def spherical_jnpp(n: int, z: float) -> float:
+    """Second derivative of the spherical Bessel function.
+
+    Parameters
+    ----------
+    n :
+        Order (n ≥ 0)
+    z :
+        Argument of the Bessel function.
+
+    Returns
+    -------
+    :
+        The second derivative of the spherical Bessel function.
+
+    """
+    return 1./z**2 * ((n**2-n-z**2)*spherical_jn(n, z) + 2.*z*spherical_jn(n+1, z))
---
+++ packages/echosms/echosms-0.1.6.tar.gz/echosms-0.1.6/src/example_code.py
@@ -0,0 +1,250 @@
+# %%
+"""Examples of using the echoSMs code to estimate scatter from objects."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from echosms import MSSModel, PSMSModel, DCMModel, ESModel, PTDWBAModel
+from echosms import BenchmarkData
+from echosms import ReferenceModels
+from echosms import as_dataframe, as_dataarray
+
+# Load the reference model defintiions
+rm = ReferenceModels()
+print('Available reference models are:\n')
+print('\n'.join(rm.names()))
+
+# Load the benchmark data (from Jech et al., 2015)
+bm = BenchmarkData()
+bmf = bm.freq_dataset
+bmt = bm.angle_dataset
+
+# %% ###############################################################################################
+# Run the benchmark models and compare to the frequency-varying benchmark results.
+
+# This is the mapping between model name from ReferenceModels and the appropriate column of data in
+# BenchMarkData and which model to use.
+models = [('weakly scattering sphere', 'Sphere_WeaklyScattering'),
+          ('fixed rigid sphere', 'Sphere_Rigid'),
+          ('pressure release sphere', 'Sphere_PressureRelease'),
+          ('gas filled sphere', 'Sphere_Gas'),
+          ('spherical fluid shell with pressure release interior', 'ShellSphere_PressureRelease'),
+          ('spherical fluid shell with gas interior', 'ShellSphere_Gas'),
+          ('spherical fluid shell with weakly scattering interior',
+           'ShellSphere_WeaklyScattering'),
+          ('fixed rigid finite cylinder', 'Cylinder_Rigid'),
+          ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
+          ('gas filled finite cylinder', 'Cylinder_Gas'),
+          ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')]
+# Remove PSMS ones until the PSMS model works
+# ('fixed rigid prolate spheroid', 'ProlateSpheroid_Rigid'),
+# ('pressure release prolate spheroid', 'ProlateSpheroid_PressureRelease'),
+# ('gas filled prolate spheroid', 'ProlateSpheroid_Gas'),
+# ('weakly scattering prolate spheroid', 'ProlateSpheroid_WeaklyScattering')]
+
+for name, bm_name in models:
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
+
+    # create the appropriate model instance
+    match s['benchmark_model']:
+        case 'mss':
+            mod = MSSModel()
+        case 'psms':
+            mod = PSMSModel()
+        case 'dcm':
+            mod = DCMModel()
+        case _:
+            pass
+
+    # Add frequencies and angle to the model parameters
+    m['f'] = bm.freq_dataset['Frequency_kHz']*1e3  # [Hz]
+    m['theta'] = 90.0
+
+    # and run these
+    ts = mod.calculate_ts(m)
+
+    jech_index = np.mean(np.abs(ts - bmf[bm_name]))
+
+    # Plot the mss model and benchmark results
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    axs[0].plot(m['f']/1e3, ts, label='echoSMs')
+    axs[0].plot(bmf['Frequency_kHz'], bmf[bm_name], label='Benchmark')
+    axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
+    axs[0].legend(frameon=False, fontsize=6)
+
+    # Plot difference between benchmark values and newly calculated mss model values
+    axs[1].plot(m['f']*1e-3, ts-bmf[bm_name], color='black')
+    axs[1].set_xlabel('Frequency [kHz]')
+    axs[1].set_ylabel(r'$\Delta$ TS [dB]')
+    axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
+                    backgroundcolor=[.8, .8, .8])
+    plt.suptitle(name)
+    plt.show()
+
+# %% ###############################################################################################
+# Run the benchmark models and compare to the angle-varying benchmark results.
+models = [('fixed rigid finite cylinder', 'Cylinder_Rigid'),
+          ('pressure release finite cylinder', 'Cylinder_PressureRelease'),
+          ('gas filled finite cylinder', 'Cylinder_Gas'),
+          ('weakly scattering finite cylinder', 'Cylinder_WeaklyScattering')]
+
+for name, bm_name in models:
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
+
+    # create the appropriate model instance
+    match s['benchmark_model']:
+        case 'mss':
+            mod = MSSModel()
+        case 'psms':
+            mod = PSMSModel()
+        case 'dcm':
+            mod = DCMModel()
+        case _:
+            pass
+    # Get the model parameters used in Jech et al. (2015) for a particular model.
+    s = rm.specification(name)
+    m = rm.parameters(name)
+
+    # Add frequencies and angle to the model parameters
+    m['f'] = 38000  # [Hz]
+    m['theta'] = bmt['Angle_deg']
+
+    # and run these
+    ts = mod.calculate_ts(m)
+
+    jech_index = np.mean(np.abs(ts - bmt[bm_name]))
+
+    # Plot the mss model and benchmark results
+    fig, axs = plt.subplots(2, 1, sharex=True)
+    axs[0].plot(m['theta'], ts, label='echoSMs')
+    axs[0].plot(bmt['Angle_deg'], bmt[bm_name], label='Benchmark')
+    axs[0].set_ylabel('TS re 1 m$^2$ [dB]')
+    axs[0].legend(frameon=False, fontsize=6)
+
+    # Plot difference between benchmark values and newly calculated mss model values
+    axs[1].plot(m['theta'], ts-bmt[bm_name], color='black')
+    axs[1].set_xlabel('Angle (°)')
+    axs[1].set_ylabel(r'$\Delta$ TS [dB]')
+    axs[1].annotate(f'{jech_index:.2f} dB', (0.05, 0.80), xycoords='axes fraction',
+                    backgroundcolor=[.8, .8, .8])
+    plt.suptitle(name)
+    plt.show()
+
+# %% ###############################################################################################
+# Use the ES model on a calibration sphere
+name = 'WC38.1 calibration sphere'
+p = rm.parameters(name)
+p['f'] = np.arange(10, 100, 0.05) * 1e3  # [kHz]
+
+es = ESModel()
+ts = es.calculate_ts(p)
+
+plt.plot(p['f']*1e-3, ts)
+plt.xlabel('Freq [kHz]')
+plt.ylabel('TS re 1 m$^2$ [dB] ')
+plt.title(name)
+plt.show()
+
+# Can readily modify the parameters for a different sphere
+p['a'] = 0.012/2
+ts = es.calculate_ts(p)
+plt.plot(p['f']*1e-3, ts)
+plt.xlabel('Freq [kHz]')
+plt.ylabel('TS re 1 m$^2$ [dB] ')
+plt.title('WC12 calibration sphere')
+plt.show()
+
+# %% ###############################################################################################
+# Some other ways to run models.
+
+mss = MSSModel()
+
+# Can add more variations to the model parameters
+m = rm.parameters('weakly scattering sphere')
+m['f'] = np.linspace(12, 200, num=800) * 1e3  # [Hz]
+m['target_rho'] = np.arange(1020, 1030, 1)  # [kg/m^3]
+m['theta'] = [0, 90.0, 180.0]
+# can convert this to a dataframe
+models_df = as_dataframe(m)
+# could also make a DataFrame of parameters that are not just the combination of all input
+# parameters. This offers a way to specify a more tailored set of model parameters.
+
+print(f'Running {len(models_df)} models')
+# and run. This will return a Series
+ts = mss.calculate_ts(models_df, multiprocess=True)
+models_df['ts'] = ts
+
+# Alternatively, the ts results can be added to the dataframe that is passed in:
+# ts = mss.calculate_ts(models_df, multiprocess=True, result_type='expand')
+
+# plot some of the results
+for rho in m['target_rho']:
+    r = models_df.query('target_rho == @rho and theta==90')
+    plt.plot(r['f']/1e3, r['ts'], label=f'{rho:.0f}')
+
+plt.xlabel('Freq [kHz]')
+plt.ylabel('TS re 1 m$^2$ [dB] ')
+plt.legend(title='Density [kg m$^{-3}$]')
+plt.show()
+
+# %% ###############################################################################################
+# Example of model parameters not from the benchmarks
+params = {'medium_rho': [1000, 1250, 1500],
+          'medium_c': np.arange(1400, 1600, 100),
+          'f': np.linspace(12, 100, num=400) * 1000,
+          'theta': np.arange(0, 180, 1),
+          'a': 0.07,
+          'boundary_type': 'fluid filled',
+          'target_c': 1450,
+          'target_rho': 1250}
+
+# Instead of converting those to a dataframe, an xarray can be used.
+params_xa = as_dataarray(params)
+
+# how many models runs would that be?
+print(f'Running {np.prod(params_xa.shape)} models!')
+
+# and is called the same way as for the dataframe
+if False:  # cause it takes a long time to run (as multiprocess is not enabled internally)
+    # When called with a dataarray, the values in that dataarray are overwritten with the ts, so
+    # it is not necessary to get the return value (i.e., there is an implicit inplace=True)
+    mss.calculate_ts(params_xa, multiprocess=True)
+
+# Xarray selections and dimenions names can then be used
+plt.plot(params_xa.f, params_xa.sel(theta=90, medium_rho=1000, medium_c=1600))
+
+# %% ###############################################################################################
+# Example of PT-DWBA model
+
+# A simple, minimal example
+p = {
+     'f': 38000,
+     'theta': 45,
+     'phi': 0,
+     'voxel_size': [0.0005, 0.0005, 0.0005],
+     'target_rho': [1024, 1025, 1026],
+     'target_c': [1480, 1490, 1500],
+     'volume': np.array([[[0, 0, 0, 0],
+                          [0, 1, 2, 0],
+                          [0, 0, 0, 0]],
+                         [[0, 0, 0, 0],
+                          [0, 1, 0, 2],
+                          [0, 0, 0, 0]],
+                         [[0, 0, 0, 0],
+                          [0, 1, 0, 2],
+                          [0, 0, 0, 0]],
+                         [[0, 0, 0, 0],
+                          [0, 1, 0, 1],
+                          [0, 0, 0, 0]],
+                         [[0, 0, 0, 0],
+                          [0, 1, 0, 1],
+                          [0, 0, 0, 0]]])}
+
+pt = PTDWBAModel()
+pt.calculate_ts_single(**p)
+
+# Note that PTDWBAModel() doesn't yet support calling via the calculate_ts() call.
