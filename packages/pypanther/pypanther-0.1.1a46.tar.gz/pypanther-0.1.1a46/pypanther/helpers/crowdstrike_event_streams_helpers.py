e052dfafbd5cb3afad327fc5f45c14a6199cfaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:27 +0000

    Add package pypanther/0.1.1a46/pypanther-0.1.1a46.tar.gz

---
+++ packages/pypanther/pypanther-0.1.1a46.tar.gz/pypanther-0.1.1a46/pypanther/helpers/crowdstrike_event_streams_helpers.py
@@ -0,0 +1,9 @@
+"""
+Helpers for Crowdstrike Event Streams detections.
+"""
+
+from pypanther.helpers.panther_base_helpers import key_value_list_to_dict
+
+
+def cs_alert_context(event):
+    return key_value_list_to_dict(event.deep_get("event", "AuditKeyValues", default=[]), "Key", "ValueString")
