bc2530afdda07e0e3e3bbe5cd4ad044cec581ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:40 +0000

    Add package digitaltwin-on-fhir/1.1.1/digitaltwin_on_fhir-1.1.1.tar.gz

---
+++ packages/digitaltwin-on-fhir/digitaltwin_on_fhir-1.1.1.tar.gz/digitaltwin_on_fhir-1.1.1/digitaltwin_on_fhir/__init__.py
@@ -0,0 +1,10 @@
+from .core import Adapter
+
+__title__ = "digitaltwin-on-fhir"
+__version__ = "1.1.1"
+__author__ = "Linkun Gao"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright 2024 ABI"
+
+# Version synonym
+VERSION = __version__
