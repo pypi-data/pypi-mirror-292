a7fb84af7128e743d330b9740cf1609a19653b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 06:06:18 +0000

    Add package pyrematch/1.0.3/pyrematch-1.0.3-cp310-cp310-win32.whl

---
+++ packages/pyrematch/pyrematch-1.0.3-cp310-cp310-win32.whl/pyrematch/__init__.py
@@ -0,0 +1,39 @@
+from .pyrematch import Flags, reql
+
+__version__ = "1.0.3"
+
+from ._pyrematch import (
+    AnchorInsideCaptureException,
+    ComplexQueryException,
+    EmptyWordCaptureException,
+    InvalidCharacterException,
+    InvalidEscapeException,
+    InvalidRangeException,
+    MemoryLimitExceededException,
+    QuerySyntaxException,
+    SameNestedVariableException,
+    UnhandledExpressionException,
+    VariableLimitExceededException,
+    VariableNotFoundException,
+    VariableNotFoundInCatalogException,
+    MultiSpannersNotAllowedException,
+)
+
+__all__ = [
+    "reql",
+    "Flags",
+    "QuerySyntaxException",
+    "AnchorInsideCaptureException",
+    "ComplexQueryException",
+    "EmptyWordCaptureException",
+    "InvalidCharacterException",
+    "InvalidEscapeException",
+    "InvalidRangeException",
+    "MemoryLimitExceededException",
+    "SameNestedVariableException",
+    "UnhandledExpressionException",
+    "VariableLimitExceededException",
+    "VariableNotFoundException",
+    "VariableNotFoundInCatalogException",
+    "MultiSpannersNotAllowedException",
+]
