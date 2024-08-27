4cacfd92d6f0b23dd48c8bea61f6e0b5c7c1d0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:57 +0000

    Add package starSwimmer/0.0.2/starSwimmer-0.0.2.tar.gz

---
+++ packages/starSwimmer/starSwimmer-0.0.2.tar.gz/starSwimmer-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = '3D Star Swimmer package'
+LONG_DESCRIPTION = '3D Star-Swimmer simulation tool package'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="starSwimmer", 
+        version=VERSION,
+        author="Alireza Astane",
+        author_email="<apoorhass@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['Swimmer'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
---
+++ packages/starSwimmer/starSwimmer-0.0.2.tar.gz/starSwimmer-0.0.2/starSwimmer/__init__.py
@@ -0,0 +1,1 @@
+from starSwimmer import Swimmer
