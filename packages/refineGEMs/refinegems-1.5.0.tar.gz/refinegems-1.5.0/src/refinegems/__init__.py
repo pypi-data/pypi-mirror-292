0444aca8817335080f6f8584a8131a2530629cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:06:02 +0000

    Add package refineGEMs/1.5.0/refinegems-1.5.0.tar.gz

---
+++ packages/refineGEMs/refinegems-1.5.0.tar.gz/refinegems-1.5.0/src/refinegems/__init__.py
@@ -0,0 +1,22 @@
+# declares public API for this module
+# loads functions from subscripts which are needed in main.py
+import refinegems.databases
+import refinegems.biomass
+import refinegems.growth
+import refinegems.modelseed
+import refinegems.sboann
+import refinegems.charges
+import refinegems.pathways
+import refinegems.curate
+import refinegems.polish
+import refinegems.cvterms
+import refinegems.analysis_db
+import refinegems.analysis_biocyc
+import refinegems.analysis_kegg
+import refinegems.entities
+import refinegems.gapfill
+import refinegems.comparison
+import refinegems.investigate
+import refinegems.io
+
+__author__ = "Famke Baeuerle"
