1b3db667daeaabe8ca8b5d490cf15e610437dfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 14:05:15 +0000

    Add package cdk-secret-manager-wrapper-layer/1.0.704/cdk_secret_manager_wrapper_layer-1.0.704-py3-none-any.whl

---
+++ packages/cdk-secret-manager-wrapper-layer/cdk_secret_manager_wrapper_layer-1.0.704-py3-none-any.whl/cdk_secret_manager_wrapper_layer/_jsii/__init__.py
@@ -0,0 +1,34 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.assertions._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-secret-manager-wrapper-layer",
+    "1.0.704",
+    __name__[0:-6],
+    "cdk-secret-manager-wrapper-layer@1.0.704.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
