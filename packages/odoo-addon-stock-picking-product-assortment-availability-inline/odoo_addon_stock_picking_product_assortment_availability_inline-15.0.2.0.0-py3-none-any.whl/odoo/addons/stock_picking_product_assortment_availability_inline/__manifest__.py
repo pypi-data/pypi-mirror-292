67ffd02ea6eb19109d3de5a48eacddc26b71a678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 16:06:43 +0000

    Add package odoo-addon-stock-picking-product-assortment-availability-inline/15.0.2.0.0/odoo_addon_stock_picking_product_assortment_availability_inline-15.0.2.0.0-py3-none-any.whl

---
+++ packages/odoo-addon-stock-picking-product-assortment-availability-inline/odoo_addon_stock_picking_product_assortment_availability_inline-15.0.2.0.0-py3-none-any.whl/odoo/addons/stock_picking_product_assortment_availability_inline/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Stock Picking Product Assortment Availability Inline",
+    "summary": "Glue module to display stock available when an assortment is defined "
+    "for a partner",
+    "version": "15.0.2.0.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["Sergio-teruel"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_product_assortment",
+        "stock_picking_product_availability_inline",
+    ],
+    "auto_install": True,
+}
