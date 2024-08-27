75867b96415340a1af1b7c9394e419ce318e5efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:43 +0000

    Add package bitsightpy/0.1.7/bitsightpy-0.1.7-py3-none-any.whl

---
+++ packages/bitsightpy/bitsightpy-0.1.7-py3-none-any.whl/bitsightpy/alerts/__init__.py
@@ -0,0 +1,11 @@
+"""
+Automate how you monitor changes to risk vector grades and rating changes in your portfolio.
+"""
+
+from .calls import (
+    get_alerts,
+    get_4th_party_disclosures,
+    get_latest_alerts,
+    get_percent_change_details,
+    get_vuln_alert_details,
+)
