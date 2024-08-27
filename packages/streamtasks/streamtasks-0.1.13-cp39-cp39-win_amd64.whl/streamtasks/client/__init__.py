71ae530feee77306c1c51738ee80fe7f8725cb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 15:05:56 +0000

    Add package streamtasks/0.1.13/streamtasks-0.1.13-cp39-cp39-win_amd64.whl

---
+++ packages/streamtasks/streamtasks-0.1.13-cp39-cp39-win_amd64.whl/streamtasks/client/__init__.py
@@ -0,0 +1,142 @@
+from typing import Iterable, Optional, Any, Union
+import asyncio
+from streamtasks.client.discovery import request_addresses
+from streamtasks.client.receiver import Receiver
+from streamtasks.client.topic import InTopic, InTopicSynchronizer, OutTopic, InTopicsContext, OutTopicsContext, SynchronizedInTopic
+from streamtasks.client.discovery import DiscoveryConstants, GenerateTopicsRequestBody, GenerateTopicsResponseBody, ResolveAddressRequestBody, ResolveAddressResonseBody
+from streamtasks.utils import IdGenerator, IdTracker, AwaitableIdTracker
+from streamtasks.net.serialization import RawData
+from streamtasks.net import Endpoint, EndpointOrAddress, Link, endpoint_or_address_to_endpoint
+from streamtasks.net.helpers import ids_to_priced_ids
+from streamtasks.net.messages import AddressedMessage, AddressesChangedMessage, InTopicsChangedMessage, OutTopicsChangedMessage, TopicControlData, TopicDataMessage, TopicMessage
+from streamtasks.services.constants import NetworkAddresses, NetworkPorts
+from streamtasks.client.fetch import FetchError, FetchReponseReceiver, FetchRequestMessage, FetchResponseMessage
+
+
+class Client:
+  def __init__(self, link: Link):
+    self._link = link
+    self._started_event = asyncio.Event()
+    self._receivers: list[Receiver] = []
+    self._receive_task: Optional[asyncio.Task] = None
+    self._address: Optional[int] = None
+    self._address_resolver_cache: dict[str, int] = {}
+    self._port_generator = IdGenerator(NetworkPorts.DYNAMIC_START, 0xffffffffffffffff)
+
+    self._subscribed_provided_topics = AwaitableIdTracker()
+    self._in_topics = IdTracker()
+    self._out_topics = IdTracker()
+
+  @property
+  def address(self): return self._address
+
+  def out_topic(self, topic: int): return OutTopic(self, topic)
+  def in_topic(self, topic: int): return InTopic(self, topic)
+  def sync_in_topic(self, topic: int, sync: InTopicSynchronizer): return SynchronizedInTopic(self, topic, sync)
+
+  def start(self): self._started_event.set()
+  async def stop_wait(self):
+    self._started_event.clear()
+    if self._receive_task is not None:
+      self._receive_task.cancel("stopped receiving!")
+      try: await self._receive_task
+      except asyncio.CancelledError: pass
+
+  def get_free_port(self): return self._port_generator.next()
+
+  async def send_to(self, endpoint: Endpoint, data: RawData):
+    await self._link.send(AddressedMessage(
+      await self._get_address(endpoint[0]),
+      endpoint[1],
+      data
+    ))
+  async def send_stream_control(self, topic: int, control_data: TopicControlData): await self._link.send(control_data.to_message(topic))
+  async def send_stream_data(self, topic: int, data: RawData): await self._link.send(TopicDataMessage(topic, data))
+  async def resolve_address_name(self, name: str) -> Optional[int]:
+    if name in self._address_resolver_cache: return self._address_resolver_cache[name]
+    raw_res = await self.fetch(NetworkAddresses.ID_DISCOVERY, DiscoveryConstants.FD_RESOLVE_ADDRESS, ResolveAddressRequestBody(address_name=name).model_dump())
+    res: ResolveAddressResonseBody = ResolveAddressResonseBody.model_validate(raw_res)
+    if res.address is not None: self._address_resolver_cache[name] = res.address
+    return res.address
+
+  async def request_address(self):
+    addresses = await request_addresses(self, 1)
+    new_address = next(iter(addresses))
+    assert self._address is None, "there cant be an address already present, when requesting one"
+    await self.set_address(new_address)
+    return new_address
+
+  async def request_topic_ids(self, count: int, apply: bool = False) -> list[int]:
+    raw_res = await self.fetch(NetworkAddresses.ID_DISCOVERY, DiscoveryConstants.FD_REQUEST_TOPICS, GenerateTopicsRequestBody(count=count).model_dump())
+    res = GenerateTopicsResponseBody.model_validate(raw_res)
+    if len(res.topics) != count: raise Exception("The fetch request returned an invalid number of topics")
+    if apply: await self.register_out_topics(res.topics)
+    return res.topics
+
+  async def set_address(self, address: Optional[int]):
+    new_addresses = set() if address is None else set([ address ])
+    old_addresses = set() if self._address is None else set([ self._address ])
+    add = new_addresses - old_addresses
+    remove = old_addresses - new_addresses
+    if len(add) > 0 or len(remove) > 0:
+      await self._link.send(AddressesChangedMessage(ids_to_priced_ids(add), remove))
+    self._address = address
+
+  def out_topics_context(self, topics: Iterable[int]): return OutTopicsContext(self, topics)
+  def in_topics_context(self, topics: Iterable[int]): return InTopicsContext(self, topics)
+
+  async def register_out_topics(self, topics: Iterable[int]):
+    actually_added = self._out_topics.add_many(topics)
+    if len(actually_added) > 0: await self._link.send(OutTopicsChangedMessage(ids_to_priced_ids(actually_added), set()))
+  async def unregister_out_topics(self, topics: Iterable[int], force: bool = False):
+    actually_removed = self._out_topics.remove_many(topics, force=force)
+    if len(actually_removed) > 0: await self._link.send(OutTopicsChangedMessage(set(), set(actually_removed)))
+  async def register_in_topics(self, topics: Iterable[int]):
+    actually_added = self._in_topics.add_many(topics)
+    if len(actually_added) > 0: await self._link.send(InTopicsChangedMessage(set(actually_added), set()))
+  async def unregister_in_topics(self, topics: Iterable[int], force: bool = False):
+    actually_removed = self._in_topics.remove_many(topics, force=force)
+    if len(actually_removed) > 0: await self._link.send(InTopicsChangedMessage(set(), set(actually_removed)))
+
+  async def fetch(self, endpoint: EndpointOrAddress, descriptor: str, body: Any):
+    if self.address is None: raise Exception("No local address")
+    return_port = self.get_free_port()
+    async with FetchReponseReceiver(self, return_port) as receiver:
+      await self.send_to(endpoint_or_address_to_endpoint(endpoint, NetworkPorts.FETCH), RawData(FetchRequestMessage(
+        return_address=self.address,
+        return_port=return_port,
+        descriptor=descriptor,
+        body=body).model_dump()))
+      response_data: FetchResponseMessage = await receiver.get()
+    if response_data.error:
+      raise FetchError(response_data.body)
+    return response_data.body
+
+  async def enable_receiver(self, receiver: Receiver):
+    self._receivers.append(receiver)
+    self._receive_task = self._receive_task or asyncio.create_task(self._task_receive())
+  async def disable_receiver(self, receiver: Receiver):
+    self._receivers.remove(receiver)
+    if len(self._receivers) == 0 and self._receive_task is not None:
+      self._receive_task.cancel()
+      try: await self._receive_task
+      except asyncio.CancelledError: pass
+
+  def set_address_name(self, name: str, address: Optional[int]):
+    if address is None: self._address_resolver_cache.pop(name, None)
+    else: self._address_resolver_cache[name] = address
+
+  async def _get_address(self, address: Union[int, str]) -> int: return await self.resolve_address_name(address) if isinstance(address, str) else address
+
+  async def _task_receive(self):
+    try:
+      while len(self._receivers) > 0:
+        await self._started_event.wait()
+        message = await self._link.recv()
+        await self._started_event.wait()
+        if isinstance(message, InTopicsChangedMessage): self._subscribed_provided_topics.change_many(message.add, message.remove)
+        if isinstance(message, TopicMessage) and message.topic not in self._in_topics: continue
+        for receiver in self._receivers:
+          receiver.on_message(message)
+    finally:
+      self._receive_task = None
---
+++ packages/streamtasks/streamtasks-0.1.13-cp39-cp39-win_amd64.whl/streamtasks/system/tasks/repeater.py
@@ -0,0 +1,87 @@
+import asyncio
+import time
+from typing import Any
+from pydantic import BaseModel
+from streamtasks.net.serialization import RawData
+from streamtasks.net.messages import TopicControlData
+from streamtasks.message.utils import get_timestamp_from_message, set_timestamp_on_message
+from streamtasks.system.configurators import EditorFields, static_configurator
+from streamtasks.utils import AsyncTrigger, TimeSynchronizer, hertz_to_fintervall
+from streamtasks.system.task import Task, TaskHost
+from streamtasks.client import Client
+
+class RepeaterConfigBase(BaseModel):
+  rate: float | int = 10
+  fail_closed: bool = False
+
+class RepeaterConfig(RepeaterConfigBase):
+  out_topic: int
+  in_topic: int
+
+class RepeaterTask(Task):
+  def __init__(self, client: Client, config: RepeaterConfig):
+    super().__init__(client)
+    self.out_topic = self.client.out_topic(config.out_topic)
+    self.in_topic = self.client.in_topic(config.in_topic)
+    self.config = config
+    self.current_data: RawData | None = None
+    self.data_trigger = AsyncTrigger()
+    self.time_sync = TimeSynchronizer()
+
+  async def run(self):
+    async with self.out_topic, self.out_topic.RegisterContext(), self.in_topic, self.in_topic.RegisterContext():
+      self.client.start()
+      await asyncio.gather(self._run_receiver(), self._run_sender())
+
+  async def _run_receiver(self):
+    while True:
+      data = await self.in_topic.recv_data_control()
+      if isinstance(data, TopicControlData):
+        await self.out_topic.set_paused(data.paused)
+      else:
+        try:
+          msg_time = get_timestamp_from_message(data)
+          self.time_sync.update(msg_time)
+          self.current_data = data.shallow_copy()
+          self.data_trigger.trigger()
+        except ValueError:
+          if not self.config.fail_closed: await self.out_topic.send(data)
+          else: self.current_data = None
+
+  async def _run_sender(self):
+    frame_counter = 0
+    t0: float | None = None
+    delay = hertz_to_fintervall(self.config.rate)
+
+    while True:
+      if self.current_data is None:
+        await self.data_trigger.wait()
+        frame_counter = 0
+        t0 = time.time()
+      if not self.out_topic.is_paused and self.current_data is not None:
+        try:
+          msg = self.current_data.shallow_copy()
+          set_timestamp_on_message(msg, self.time_sync.time)
+          await self.out_topic.send(msg)
+        except ValueError: pass
+      frame_counter += 1
+      wait_time = float(t0 + frame_counter * delay) - time.time()
+      if wait_time > 0: await asyncio.sleep(wait_time)
+
+class RepeaterTaskHost(TaskHost):
+  @property
+  def metadata(self): return static_configurator(
+    label="repeater",
+    inputs=[{ "label": "input", "type": "ts", "key": "in_topic" }],
+    outputs=[{ "label": "output", "type": "ts", "key": "out_topic" }],
+    default_config=RepeaterConfigBase().model_dump(),
+    config_to_output_map=[{ "rate": "rate" }],
+    io_mirror=[("in_topic", 0)],
+    io_mirror_ignore=["rate"],
+    editor_fields=[
+      EditorFields.number(key="rate", min_value=0.001, unit="hz"),
+      EditorFields.boolean(key="fail_closed"),
+    ]
+  )
+  async def create_task(self, config: Any, topic_space_id: int | None):
+    return RepeaterTask(await self.create_client(topic_space_id), RepeaterConfig.model_validate(config))
