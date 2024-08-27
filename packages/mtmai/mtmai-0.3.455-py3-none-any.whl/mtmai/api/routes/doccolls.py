63843abfa33151f43451bac1e33533892cde7471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:32 +0000

    Add package mtmai/0.3.455/mtmai-0.3.455-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.455-py3-none-any.whl/mtmai/api/routes/doccolls.py
@@ -0,0 +1,26 @@
+import logging
+
+from fastapi import APIRouter
+from sqlmodel import select
+
+from mtmai.api.deps import CurrentUser, SessionDep
+from mtmai.models import DocColl, DocCollPublic
+
+router = APIRouter()
+
+logger = logging.getLogger()
+
+
+################################################################################################################
+# 知识库集
+@router.get("/doccolls", response_model=DocCollPublic)
+async def items(
+    db: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
+):
+    docs = db.exec(
+        select(DocColl)
+        # .order_by(Document.embedding.l2_distance(result[0]))
+        .skip(skip)
+        .limit(limit)
+    ).all()
+    return DocCollPublic(data=docs, count=len(docs))
