bdfe6652b23d8c4ca58faa16096a744748be1eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:06:11 +0000

    Add package mkdocs-bootstrap-elements-plugin/0.1.1/mkdocs-bootstrap-elements-plugin-0.1.1.tar.gz

---
+++ packages/mkdocs-bootstrap-elements-plugin/mkdocs-bootstrap-elements-plugin-0.1.1.tar.gz/mkdocs-bootstrap-elements-plugin-0.1.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-bootstrap-elements-plugin',
+    version='0.1.1',
+    description='A MkDocs plugin that adds Bootstrap accordions, modals, and cards.',
+    long_description='This plugin allows you to easily add Bootstrap accordions, modals, and cards to your MkDocs site using custom Markdown syntax.',
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown bootstrap',
+    url='https://github.com/kdkiss/mkdocs-bootstrap-elements-plugin',
+    author='Kolja Kiss',
+    author_email='kolja.kiss@outlook.com',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'mkdocs>=1.0.4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'mkdocs_bootstrap_elements_plugin': [
+            'css/*.css',
+            'js/*.js'
+        ],
+    },
+    entry_points={
+        'mkdocs.plugins': [
+            'bootstrap_elements = mkdocs_bootstrap_elements_plugin:BootstrapElementsPlugin',
+        ]
+    },
+    zip_safe=False
+)
