ec019fea767515394b768cc38ddeebc5a42c1744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:04:55 +0000

    Add package valor-client/0.29.5/valor_client-0.29.5-py3-none-any.whl

---
+++ packages/valor-client/valor_client-0.29.5-py3-none-any.whl/valor/schemas/evaluation.py
@@ -0,0 +1,89 @@
+from dataclasses import asdict, dataclass, field
+from typing import List, Optional, Union
+
+from valor.enums import AnnotationType, MetricType, TaskType
+from valor.schemas.filters import Filter
+
+
+@dataclass
+class EvaluationParameters:
+    """
+    Defines parameters for evaluation methods.
+
+    Attributes
+    ----------
+    task_type: TaskType
+        The task type of a given evaluation.
+    label_map: Optional[List[List[List[str]]]]
+        Optional mapping of individual labels to a grouper label. Useful when you need to evaluate performance using labels that differ across datasets and models.
+    metrics: List[str], optional
+        The list of metrics to compute, store, and return to the user.
+    convert_annotations_to_type: AnnotationType | None = None
+        The type to convert all annotations to.
+    iou_thresholds_to_compute: List[float], optional
+        A list of floats describing which Intersection over Unions (IoUs) to use when calculating metrics (i.e., mAP).
+    iou_thresholds_to_return: List[float], optional
+        A list of floats describing which Intersection over Union (IoUs) thresholds to calculate a metric for. Must be a subset of `iou_thresholds_to_compute`.
+    recall_score_threshold: float, default=0
+        The confidence score threshold for use when determining whether to count a prediction as a true positive or not while calculating Average Recall.
+    pr_curve_iou_threshold: float, optional
+            The IOU threshold to use when calculating precision-recall curves for object detection tasks. Defaults to 0.5.
+    pr_curve_max_examples: int
+        The maximum number of datum examples to store when calculating PR curves.
+    """
+
+    task_type: TaskType
+    label_map: Optional[List[List[List[str]]]] = None
+    metrics_to_return: Optional[List[MetricType]] = None
+
+    convert_annotations_to_type: Optional[AnnotationType] = None
+    iou_thresholds_to_compute: Optional[List[float]] = None
+    iou_thresholds_to_return: Optional[List[float]] = None
+    recall_score_threshold: float = 0
+    pr_curve_iou_threshold: float = 0.5
+    pr_curve_max_examples: int = 1
+
+
+@dataclass
+class EvaluationRequest:
+    """
+    An evaluation request.
+
+    Defines important attributes of the API's `EvaluationRequest`.
+
+    Attributes
+    ----------
+    dataset_names : List[str]
+        The list of datasets we want to evaluate by name.
+    model_names : List[str]
+        The list of models we want to evaluate by name.
+    filters : dict
+        The filter object used to define what the model(s) is evaluating against.
+    parameters : EvaluationParameters
+        Any parameters that are used to modify an evaluation method.
+    """
+
+    dataset_names: Union[str, List[str]]
+    model_names: Union[str, List[str]]
+    parameters: EvaluationParameters
+    filters: Filter = field(default_factory=Filter)
+
+    def __post_init__(self):
+        if isinstance(self.filters, dict):
+            self.filters = Filter(**self.filters)
+        elif self.filters is None:
+            self.filters = Filter()
+
+        if isinstance(self.parameters, dict):
+            self.parameters = EvaluationParameters(**self.parameters)
+
+    def to_dict(self) -> dict:
+        """
+        Converts the request into a JSON-compatible dictionary.
+        """
+        return {
+            "dataset_names": self.dataset_names,
+            "model_names": self.model_names,
+            "parameters": asdict(self.parameters),
+            "filters": self.filters.to_dict(),
+        }
---
+++ packages/valor-client/valor_client-0.29.5-py3-none-any.whl/valor/schemas/filters.py
@@ -0,0 +1,70 @@
+from dataclasses import asdict, dataclass
+from typing import Optional, Union
+
+from valor.schemas.symbolic.operators import (
+    And,
+    Contains,
+    Eq,
+    FunctionType,
+    Gt,
+    Gte,
+    Inside,
+    Intersects,
+    IsNotNull,
+    IsNull,
+    Lt,
+    Lte,
+    Ne,
+    Not,
+    Or,
+    Outside,
+)
+
+FunctionTypeTuple = (
+    And,
+    Or,
+    Not,
+    IsNull,
+    IsNotNull,
+    Eq,
+    Ne,
+    Gt,
+    Gte,
+    Lt,
+    Lte,
+    Intersects,
+    Inside,
+    Outside,
+    Contains,
+)
+
+
+@dataclass
+class Filter:
+    datasets: Optional[Union[dict, FunctionType]] = None
+    models: Optional[Union[dict, FunctionType]] = None
+    datums: Optional[Union[dict, FunctionType]] = None
+    annotations: Optional[Union[dict, FunctionType]] = None
+    groundtruths: Optional[Union[dict, FunctionType]] = None
+    predictions: Optional[Union[dict, FunctionType]] = None
+    labels: Optional[Union[dict, FunctionType]] = None
+    embeddings: Optional[Union[dict, FunctionType]] = None
+
+    def to_dict(self) -> dict:
+        if isinstance(self.datasets, FunctionTypeTuple):
+            self.datasets = self.datasets.to_dict()
+        if isinstance(self.models, FunctionTypeTuple):
+            self.models = self.models.to_dict()
+        if isinstance(self.datums, FunctionTypeTuple):
+            self.datums = self.datums.to_dict()
+        if isinstance(self.annotations, FunctionTypeTuple):
+            self.annotations = self.annotations.to_dict()
+        if isinstance(self.groundtruths, FunctionTypeTuple):
+            self.groundtruths = self.groundtruths.to_dict()
+        if isinstance(self.predictions, FunctionTypeTuple):
+            self.predictions = self.predictions.to_dict()
+        if isinstance(self.labels, FunctionTypeTuple):
+            self.labels = self.labels.to_dict()
+        if isinstance(self.embeddings, FunctionTypeTuple):
+            self.embeddings = self.embeddings.to_dict()
+        return asdict(self)
