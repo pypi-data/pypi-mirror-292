ba1b63556ed8142f97241469b181b73a0d414719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 04:05:18 +0000

    Add package sc-surelog/1.84.1/sc_surelog-1.84.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/sc-surelog/sc_surelog-1.84.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/surelog/__init__.py
@@ -0,0 +1,24 @@
+import pathlib
+import shutil
+import sys
+
+
+__version__ = "1.84.1"
+
+
+def has_system_surelog():
+    return shutil.which(get_bin()) is not None
+
+
+def get_path():
+    return pathlib.Path(__file__).parent / "bin"
+
+
+def get_bin(platform=None):
+    exe = "surelog"
+    if not platform:
+        platform = sys.platform
+    if platform.startswith("win32"):
+        exe = f"{exe}.exe"
+
+    return exe
