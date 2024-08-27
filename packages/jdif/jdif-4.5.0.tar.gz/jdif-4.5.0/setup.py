3f848913bd26e2feebd6675caa33b91903bb8c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 05:06:06 +0000

    Add package jdif/4.5.0/jdif-4.5.0.tar.gz

---
+++ packages/jdif/jdif-4.5.0.tar.gz/jdif-4.5.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    desc=f.read()
+
+setup(
+    name="jdif",
+    version="4.5.0",
+    description="To find json differences on CLI",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    author="shivani",
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'jdif = jdif:start',
+        ]
+    },
+    project_urls={
+        "Source": "https://github.com/Shivani-20/json-diff-cli"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Developers",
+        "Topic :: File Formats :: JSON",
+        "Topic :: File Formats :: JSON :: JSON Schema",
+        "Topic :: Software Development :: Debuggers"
+    ],
+    package_dir={"": "src"},
+    packages=["jdif"],
+    python_requires='>=3.12',
+    install_requires=[
+        'termcolor>=2.4.0',
+        'deepdiff>=7.0.1',
+        'charade>=1.0.3',
+        'pyparsing>=3.1.2'
+    ])
