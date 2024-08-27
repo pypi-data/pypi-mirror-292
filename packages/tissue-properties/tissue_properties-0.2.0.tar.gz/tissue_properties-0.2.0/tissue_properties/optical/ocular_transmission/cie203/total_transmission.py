ba9eb32185b59103cd8539d420061954cdb5c63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:06:07 +0000

    Add package tissue-properties/0.2.0/tissue_properties-0.2.0.tar.gz

---
+++ packages/tissue-properties/tissue_properties-0.2.0.tar.gz/tissue_properties-0.2.0/tissue_properties/optical/ocular_transmission/cie203/total_transmission.py
@@ -0,0 +1,20 @@
+import importlib.resources
+
+from ....base_classes import InterpolatedDataModel
+from ....units import Q_
+from .reference import CIEModel203
+
+
+class TotalTransmission(InterpolatedDataModel, CIEModel203):
+    def __init__(self):
+        datafile = importlib.resources.path(
+            __package__, "total_transmission-human_eye-cie203_2012.txt"
+        )
+        super().__init__("total ocular transmission", "", "wavelength", "um", datafile)
+
+    # We are overriding the call operator so that we can name the argument
+    # The CLI uses the inspect module to find all of the models we have implemented
+    # and then get a list of their inputs. If we don't override this method, the
+    # argument name will be "arg" because that is what InterpolatedDataModel declares.
+    def __call__(self, wavelength: str | Q_):
+        return super().__call__(wavelength)
