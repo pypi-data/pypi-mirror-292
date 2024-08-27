87848798ab2773008aa4daa629e68f5e62a5fd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 06:04:54 +0000

    Add package TDTNex/0.4.1/TDTNex-0.4.1-py3-none-any.whl

---
+++ packages/TDTNex/TDTNex-0.4.1-py3-none-any.whl/TDTNex/__init__.py
@@ -0,0 +1,5 @@
+name = "TDTNex"
+from TDTNex.TDTNexReader import TDTNex
+from TDTNex.TDTNexReader import get_avg_fps_float, get_avg_fps_frac, get_r_fps, get_movie_dur, count_frames
+from TDTNex.TDTNexReader import rle, trig_signal_avgsem, descritized_spike_train, BalloonProgram
+from TDTNex.TDTNexReader import get_lsr_stim_blocks, tdt_ts_to_mov_ts, DeMultiPlex, sec_to_time_stamp, time_stamp_to_sec
