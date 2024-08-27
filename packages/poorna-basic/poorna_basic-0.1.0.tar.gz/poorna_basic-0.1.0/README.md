f8147fd25b72d4798e460c6bd2b87a00a839bd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:06:24 +0000

    Add package poorna-basic/0.1.0/poorna_basic-0.1.0.tar.gz

---
+++ packages/poorna-basic/poorna_basic-0.1.0.tar.gz/poorna_basic-0.1.0/README.md
@@ -0,0 +1,10 @@
+my module contains the two class 
+1)Calculator
+  ----> add(a,b)
+  ----> subtract(a,b)
+  ----> multiply(a,b)
+  ----> division(a,b)
+2)list_lib
+  ----> flatten_list(list_of_lists)
+3)string_op_lib
+  ----> count_char_frequency(string)
---
+++ packages/poorna-basic/poorna_basic-0.1.0.tar.gz/poorna_basic-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="poorna_basic",
+    version="0.1.0",
+    author="K_poorna_chandra_reddy",
+    author_email="kottepoornachandrareddy@gmail.com",
+    description="I created this package to reduse my work",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Poorna2411/machine_learning2411",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
