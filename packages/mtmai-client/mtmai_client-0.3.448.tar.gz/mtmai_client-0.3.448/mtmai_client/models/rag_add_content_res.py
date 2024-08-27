f472861ff9b1339fc1bf5e5965e3388455da01ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-27 03:05:18 +0000

    Add package mtmai-client/0.3.448/mtmai_client-0.3.448.tar.gz

---
+++ packages/mtmai-client/mtmai_client-0.3.448.tar.gz/mtmai_client-0.3.448/mtmai_client/models/rag_add_content_res.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="RagAddContentRes")
+
+
+@_attrs_define
+class RagAddContentRes:
+    """
+    Attributes:
+        success (Union[Unset, bool]):  Default: True.
+    """
+
+    success: Union[Unset, bool] = True
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        success = self.success
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if success is not UNSET:
+            field_dict["success"] = success
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        success = d.pop("success", UNSET)
+
+        rag_add_content_res = cls(
+            success=success,
+        )
+
+        rag_add_content_res.additional_properties = d
+        return rag_add_content_res
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
