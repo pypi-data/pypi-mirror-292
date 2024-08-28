6d4547b27f4d1855f5bd6d6918ad710b0f5188a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:05:48 +0000

    Add package Kumar-Hello-World-Project/0.1/Kumar_Hello_World_Project-0.1-py3-none-any.whl

---
+++ packages/Kumar-Hello-World-Project/Kumar_Hello_World_Project-0.1-py3-none-any.whl/hello.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "ef1f389b-4441-4c67-a976-aaedef8a9651",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello, World!\n"
+     ]
+    }
+   ],
+   "source": [
+    "class HelloWorld:\n",
+    "    def say_hello(self):\n",
+    "        print(\"Hello, World! From Kumar Deepak\")\n",
+    "\n",
+    "if __name__ == \"__main__\":\n",
+    "    hello = HelloWorld()\n",
+    "    hello.say_hello()\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7b854680-5585-4f05-aeae-0ac8068acb42",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  },
+  "widgets": {
+   "application/vnd.jupyter.widget-state+json": {
+    "state": {},
+    "version_major": 2,
+    "version_minor": 0
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
