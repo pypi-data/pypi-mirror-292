d84edd8dec155cba712023d47683e9f1d5bdf60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:07:17 +0000

    Add package DataProFileExt/0.1.4/dataprofileext-0.1.4.tar.gz

---
+++ packages/DataProFileExt/dataprofileext-0.1.4.tar.gz/dataprofileext-0.1.4/README.md
@@ -0,0 +1,19 @@
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## What is this?
+
+This is a script to handle .DataPro files.
+
+## Whats the point of this?
+
+A really easy to use file ext
+
+## How do i use this?
+
+Check here: https://github.com/BlueScriptsAndDies/DataPro-File-Extension
+
+## Id rather use json
+
+Json is way better than this, however this is alot easier to use and to setup.
---
+++ packages/DataProFileExt/dataprofileext-0.1.4.tar.gz/dataprofileext-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="DataProFileExt",  # Your package name
+    version="0.1.4",  # Initial version
+    author="Justin Degrechie",  # Your name
+    author_email="justin.doodle3845@gmail.com",  # Your email
+    description="A package for .DataPro file extensions. .DataPro is like json but worse, however its really easy to use.",
+    long_description=open("README.md").read(),  # Read the long description from README.md
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlueScriptsAndDies/DataPro-File-Extension",  # Your project repository URL
+    packages=find_packages(),  # Automatically find all packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Change if using a different license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Minimum Python version
+    install_requires=[],  # List dependencies here if any
+)
