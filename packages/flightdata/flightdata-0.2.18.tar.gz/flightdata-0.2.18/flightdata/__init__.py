7e7553e140ce7681bf6f4a316fb612485efc2a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:07:46 +0000

    Add package flightdata/0.2.18/flightdata-0.2.18.tar.gz

---
+++ packages/flightdata/flightdata-0.2.18.tar.gz/flightdata-0.2.18/flightdata/__init__.py
@@ -0,0 +1,8 @@
+from .base import *
+from .origin import Origin
+from .flight import *
+from .environment import *
+from .coefficients import Coefficients
+from .flow import Flow, Attack
+from .state import State
+from .model import *
