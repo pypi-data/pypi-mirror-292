45d4e35379e54b36fafacd4846e4e65ecb7ea1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:05:52 +0000

    Add package Font/1.1/Font-1.1.tar.gz

---
+++ packages/Font/Font-1.1.tar.gz/Font-1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    required = f.read().split("\n")
+
+with open("README.md", "r") as f:
+    desc = f.read()
+
+setup(
+    name='Font',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=required,
+    description='A custom text rendering library using custom fonts for opencv',
+    author='Ivan Raphel Jaison',
+    author_email='ivanrj7j@gmail.com',
+    url='https://github.com/ivanrj7j/Font',
+    license='Apache 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    long_description_content_type='text/markdown',
+    long_description=desc
+)
