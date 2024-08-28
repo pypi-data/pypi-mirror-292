0d79e736a1c22f9ceda2bbbfe663049e9a520d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:07:36 +0000

    Add package lastversion/3.5.5/lastversion-3.5.5.tar.gz

---
+++ packages/lastversion/lastversion-3.5.5.tar.gz/lastversion-3.5.5/tests/test_hg.py
@@ -0,0 +1,27 @@
+"""Test Mercurial repositories."""
+import os
+
+from packaging import version
+
+from lastversion.lastversion import latest
+
+# change dir to tests directory to make relative paths possible
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+def test_hg_nginx():
+    """Test NGINX."""
+    repo = "https://nginx.org/"
+
+    output = latest(repo, "version")
+
+    assert output >= version.parse("1.18.0")
+
+
+def test_hg_nginx_source_url():
+    """Test NGINX."""
+    repo = "https://nginx.org/"
+
+    result = latest(repo, "source", major="1.18.0")
+
+    assert result == 'https://nginx.org/download/nginx-1.18.0.tar.gz'
