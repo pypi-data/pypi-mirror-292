04ffccc839096a54c0a2c7b75890e7f620a100d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 06:07:11 +0000

    Add package ray/2.35.0/ray-2.35.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/ray/ray-2.35.0-cp312-cp312-macosx_11_0_arm64.whl/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "ba24879ff81c8c5fd77acae435ee4a1d0b3d9e0c"
+version = "2.35.0"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))
