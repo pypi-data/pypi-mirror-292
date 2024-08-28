78badf1187ee4c8f1c8f01150676189a13879295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:05:37 +0000

    Add package noxii/0.1.4/noxii-0.1.4.tar.gz

---
+++ packages/noxii/noxii-0.1.4.tar.gz/noxii-0.1.4/noxii/NoxiiAPI.py
@@ -0,0 +1,8 @@
+class NoxiiAPI():
+    def __init__(self, api_key):
+        if not api_key:
+            print("No API Key found! Try NoxiiAPI(api_key=YOUR_API_KEY_HERE)")
+        self.api_key = api_key
+
+    def test(self):
+        print("Success!")
