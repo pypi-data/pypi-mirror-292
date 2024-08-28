f00dcde1162c1e79f709ffa115a56c0b57c4ff73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:07:00 +0000

    Add package mtmai/0.3.495/mtmai-0.3.495.tar.gz

---
+++ packages/mtmai/mtmai-0.3.495.tar.gz/mtmai-0.3.495/mtmai/models/agent.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class AgentMeta(BaseModel):
+    name: str
+    chat_url: str | None = None
+    can_chat: bool = (False,)
+    agent_type: str | None = None
+    graph_image: str | None = None
+    label: str | None = None
+    description: str | None = None
