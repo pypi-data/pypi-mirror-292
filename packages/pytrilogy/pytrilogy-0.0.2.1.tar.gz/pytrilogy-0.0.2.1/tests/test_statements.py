2d4aef6aa8c0f10d0107278604ef27f1c640eebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:06:42 +0000

    Add package pytrilogy/0.0.2.1/pytrilogy-0.0.2.1.tar.gz

---
+++ packages/pytrilogy/pytrilogy-0.0.2.1.tar.gz/pytrilogy-0.0.2.1/tests/test_statements.py
@@ -0,0 +1,43 @@
+from trilogy.parser import parse
+
+# from trilogy.compiler import compile
+
+
+def test_declarations():
+    declarations = """key user_id int metadata(description="the description");
+property user_id.display_name string metadata(description="The display name ");
+property user_id.about_me string metadata(description="User provided description");
+key post_id int;
+    """
+    parse(declarations)
+
+
+def test_duplicate_declarations():
+    declarations = """key user_id int metadata(description="the description");
+property user_id.display_name string metadata(description="The display name ");
+property user_id.about_me string metadata(description="User provided description");
+key post_id int;
+key post_id int;
+    """
+    try:
+        parse(declarations)
+    except Exception as e:
+        assert "line 5" in str(e)
+
+
+def test_datasource():
+    text = """key user_id int metadata(description="the description");
+property user_id.display_name string metadata(description="The display name ");
+property user_id.about_me string metadata(description="User provided description");
+key post_id int;
+
+
+datasource posts (
+    user_id: user_id,
+    id: post_id
+    )
+    grain (post_id)
+    address bigquery-public-data.stackoverflow.post_history
+;
+"""
+    parse(text)
