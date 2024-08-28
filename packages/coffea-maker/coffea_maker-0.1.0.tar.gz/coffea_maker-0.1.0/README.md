715403139c45b02b060225c8c35a74a2bb24104c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:06:13 +0000

    Add package coffea-maker/0.1.0/coffea_maker-0.1.0.tar.gz

---
+++ packages/coffea-maker/coffea_maker-0.1.0.tar.gz/coffea_maker-0.1.0/README.md
@@ -0,0 +1,3 @@
+# coffea-maker
+
+... is going to be great ☕️.
---
+++ packages/coffea-maker/coffea_maker-0.1.0.tar.gz/coffea_maker-0.1.0/coffea_maker/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+"""
+Law automation for coffea-based workflows.
+"""
+
+from __future__ import annotations
+
+__author__ = "Marcel Rieger"
+__copyright__ = "Copyright 2024, Marcel Rieger"
+__credits__ = ["Marcel Rieger"]
+__contact__ = "https://github.com/riga/coffea-maker"
+__license__ = "BSD-3-Clause"
+__status__ = "Production/Stable"
+__version__ = "0.1.0"
+__all__ = []
