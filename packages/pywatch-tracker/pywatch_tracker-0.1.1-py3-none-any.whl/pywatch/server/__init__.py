3a402e3bc0bc37e935de34ecba224bd77bd92f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:06:53 +0000

    Add package pywatch-tracker/0.1.1/pywatch_tracker-0.1.1-py3-none-any.whl

---
+++ packages/pywatch-tracker/pywatch_tracker-0.1.1-py3-none-any.whl/pywatch/server/__init__.py
@@ -0,0 +1,12 @@
+import importlib
+from types import ModuleType
+
+import pywatch.server.main as main
+from pywatch.parse_setup import parse_module
+
+
+def start_webapp(path: str) -> None:
+    module_ = parse_module("Setup_Module", path)
+    main.setup_mod = module_
+
+    main.app.run()
---
+++ packages/pywatch-tracker/pywatch_tracker-0.1.1-py3-none-any.whl/pywatch/server/detector_logic_calculation/__init__.py
@@ -0,0 +1,3 @@
+from .detector import detector, Detector, line_in_cuboid, Line
+from .vector import euler_order, Vector
+from .coincidences import Coincidences, calculate_coincidences, parse_from_json
