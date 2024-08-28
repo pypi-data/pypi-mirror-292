171bc6beb47984b542e8aa0a14ece1e0b78c2713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 06:06:39 +0000

    Add package taxi-data/0.0.7/taxi_data-0.0.7.tar.gz

---
+++ packages/taxi-data/taxi_data-0.0.7.tar.gz/taxi_data-0.0.7/src/taxi_data/get_job_list.py
@@ -0,0 +1,94 @@
+from typing import List
+from taxi_data.classes import Job
+
+def get_job_list(shift_date: str | None = None) -> List[Job]:
+
+    from datetime import date, datetime
+    import taxi_data.automate_bwc_web_site as BwcSite
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from bs4 import BeautifulSoup
+    from taxi_data.constants import BwcWebSiteConstants as Constants
+
+    if shift_date is not None:
+        shift_date = datetime.strptime(shift_date, str('%d/%m/%Y'))
+    else:
+        shift_date = date.today()
+    shift_date = datetime.strftime(shift_date, str('%m/%d/%Y'))
+
+    driver = BwcSite.login()
+
+    BwcSite.close_last_login_window(driver)
+
+    BwcSite.nav_to_vehicles_for_operator(driver)
+
+    BwcSite.select_operator_from_drop_down(driver)
+
+    BwcSite.click_on_car(driver)
+
+    BwcSite.shifts_for_vehicle_set_date_range(driver=driver, from_date=shift_date, to_date=shift_date)
+
+    try:
+       
+        # Iterate over each shift and extract job data
+        jobs_data = []
+        shift_rows = driver.find_elements(By.XPATH, Constants.XPATH_SHIFT_ROW)
+
+        for i in range(0, len(shift_rows)):
+            # Click on the driver ID link for each shift
+            shift_rows[i].find_element(By.TAG_NAME, Constants.TAG_ANCHOR).click()
+
+            # Wait for job list to load
+            WebDriverWait(driver, Constants.TIMEOUT).until(EC.presence_of_element_located((By.ID, Constants.ID_JOBS_FOR_SHIFT)))
+
+            # Extract the job data
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            jobs_table = soup.find('table', {'id': Constants.ID_JOBS_FOR_SHIFT})
+
+            for row in jobs_table.find_all('tr')[1:-1]:  # Skip header  & Footer row
+                cols = row.find_all('td')
+                job = Job(booking_id=int(cols[0].text.strip()),
+                        driver=int(cols[1].text.strip()),
+                        status=cols[2].text.strip(),
+                        accepted=cols[3].text.strip(),
+                        meter_on=cols[4].text.strip(),
+                        meter_off=cols[5].text.strip(),
+                        pick_up_suburb=cols[6].text.strip(),
+                        destination_suburb=cols[7].text.strip(),
+                        fare=float(str.replace(cols[8].text.strip(), '$', '')),
+                        toll=float(str.replace(cols[9].text.strip(), '$', '')),
+                        account=cols[10].text.strip())
+                jobs_data.append(job)
+
+            # Go back to the shift list page to process the next shift
+            driver.back()
+            WebDriverWait(driver, Constants.TIMEOUT).until(EC.presence_of_element_located((By.ID, Constants.ID_SHIFTS_FOR_VEHICLE)))
+            shift_rows = driver.find_elements(By.XPATH, Constants.XPATH_SHIFT_ROW)
+    
+    except Exception as e:
+        raise e
+    
+    finally:
+
+        driver.quit()
+        
+    return jobs_data
+
+def main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Retrives job listing from BWC web portal')
+    parser.add_argument('-d',type=str,required=False,help="date to pull records from",default=None)
+    args, unknown = parser.parse_known_args()
+
+    if args.d is None:
+        shift_date = str(input("Enter date (Leave blank for today): "))
+    else:
+        shift_date = args.d
+
+    jobs_list = get_job_list(shift_date)
+    print(len(jobs_list))
+
+if __name__ == "__main__":
+    main()
---
+++ packages/taxi-data/taxi_data-0.0.7.tar.gz/taxi_data-0.0.7/src/taxi_data/get_kml_file.py
@@ -0,0 +1,97 @@
+import os, pydantic, pathlib
+from typing import List, Tuple
+from datetime import datetime, time, timedelta, date
+from taxi_data.classes import RawEvent, ProcessedEvent, GpsTrackerEvent, PlaybackSpeed
+from taxi_data.constants import GpsTrackerWebSiteConstants as Constants
+import taxi_data.automate_gps_tracker_web_site as WebSite
+
+def process_raw_events(raw_events: RawEvent) -> List[ProcessedEvent]:
+    
+    processed_events: List[ProcessedEvent] = []
+
+    for raw_event in raw_events:
+        match raw_event.event_type:
+            case "Stay": 
+                event_type: GpsTrackerEvent = GpsTrackerEvent.STAY  
+        
+        shift_date: date = datetime.date(datetime.strptime(raw_event.from_time, '%Y-%m-%d %H:%M:%S'))
+        from_time: time = datetime.time(datetime.strptime(raw_event.from_time, '%Y-%m-%d %H:%M:%S'))
+        to_time: time = datetime.time(datetime.strptime(raw_event.to_time, '%Y-%m-%d %H:%M:%S'))
+        duration: timedelta = datetime.combine(shift_date, to_time) - datetime.combine(shift_date, from_time)
+
+        processed_event: ProcessedEvent = ProcessedEvent(event_type=event_type,
+                                         from_time=from_time,
+                                         to_time=to_time,
+                                         duration=duration)
+        processed_events.append(processed_event)
+    
+    
+    return processed_events
+
+def process_raw_info_text(raw_text):
+    # Step 1: Split the string into lines
+    lines = raw_text.splitlines()
+
+    # Step 2: Extract the second line
+    second_line = lines[1]
+
+    # Step 3: Split the second line to get the time part
+    date, start_time = second_line.split()  # This splits "2024-08-16 07:46:37" into date and time
+    return start_time
+
+def get_kml_file(shift_date: str | None) -> Tuple[pathlib.Path, ProcessedEvent]:
+
+    if shift_date is not None:
+        shift_date = datetime.strptime(shift_date, str('%d/%m/%Y'))
+    else:
+        shift_date = date.today()
+    shift_date = datetime.strftime(shift_date, str('%Y-%m-%d'))
+
+    try:
+        driver = WebSite.login()
+        driver.switch_to.default_content()
+        WebSite.switch_to_main_box_iframe(driver)
+        tracker = WebSite.click_on_tracker(driver)
+        WebSite.nav_to_tracking_report(driver, tracker)
+        WebSite.set_tracking_report_date_and_go(driver, date=shift_date)
+        driver.switch_to.default_content()
+        WebSite.open_playback(driver, tracker)
+        WebSite.set_playback_date(driver, date=shift_date)
+        WebSite.set_plaback_speed(driver, speed=PlaybackSpeed.SLOW)
+        playback_buttons = WebSite.get_playback_buttons(driver)
+        playback_buttons.Play.click()
+        raw_text = WebSite.get_info_pane_text(driver, playback_buttons)
+        WebSite.set_plaback_speed(driver, PlaybackSpeed.FAST)
+        WebSite.play_to_end(driver, playback_buttons)
+        raw_events: RawEvent = WebSite.get_raw_events(driver)
+    finally:
+        driver.quit()
+
+    events: ProcessedEvent = process_raw_events(raw_events)
+    start_time = process_raw_info_text(raw_text)
+
+    # Rename the KML file
+    old_file = os.path.join(Constants.DOWNLOADS_FOLDER,f"{Constants.FILE_NAME_STRING}{shift_date}.kml")
+    new_file = os.path.join(Constants.DOWNLOADS_FOLDER,f"{Constants.FILE_NAME_STRING}{shift_date}-{start_time}.kml")
+    os.rename(old_file, new_file)
+        
+    file_path: pydantic.FilePath = pathlib.Path(f"{Constants.DOWNLOADS_FOLDER}{Constants.FILE_NAME_STRING}{shift_date}-{start_time}.kml")
+    return file_path, events
+
+def main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Retrives GPS trace from web portal')
+    parser.add_argument('-d',type=str,required=False,help="date to pull records from",default=None)
+    args, unknown = parser.parse_known_args()
+
+    if args.d is None:
+        shift_date = str(input("Enter date (Leave blank for today): "))
+    else:
+        shift_date = args.d
+
+    path, events = get_kml_file(shift_date)
+    print(f"Downloaded to '{path}'")    
+
+if __name__ == "__main__":
+    main()
