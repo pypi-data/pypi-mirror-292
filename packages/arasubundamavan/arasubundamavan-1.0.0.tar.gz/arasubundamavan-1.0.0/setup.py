0280f479c49c1540266fef4f64a2318df4823e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:06:36 +0000

    Add package arasubundamavan/1.0.0/arasubundamavan-1.0.0.tar.gz

---
+++ packages/arasubundamavan/arasubundamavan-1.0.0.tar.gz/arasubundamavan-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='arasubundamavan',
+    version='1.0.0',
+    packages=['arasubundamavan'],
+    install_requires=[],
+    author='raghul',
+    author_email='raghulares@gmail.com'
+)
