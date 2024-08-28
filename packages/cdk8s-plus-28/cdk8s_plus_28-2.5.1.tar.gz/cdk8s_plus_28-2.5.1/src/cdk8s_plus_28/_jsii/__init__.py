d7808d49f2bd37e0ab99ec6b31a6bf97c07a8b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:06:16 +0000

    Add package cdk8s-plus-28/2.5.1/cdk8s_plus_28-2.5.1.tar.gz

---
+++ packages/cdk8s-plus-28/cdk8s_plus_28-2.5.1.tar.gz/cdk8s_plus_28-2.5.1/src/cdk8s_plus_28/_jsii/__init__.py
@@ -0,0 +1,27 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import cdk8s._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk8s-plus-28", "2.5.1", __name__[0:-6], "cdk8s-plus-28@2.5.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
