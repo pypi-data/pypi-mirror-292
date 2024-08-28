aeea09d7bbf281a9e3db563936b1fe94c77932d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 06:06:42 +0000

    Add package exchars/0.0.2/exchars-0.0.2.tar.gz

---
+++ packages/exchars/exchars-0.0.2.tar.gz/exchars-0.0.2/src/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="exchars",
+    version="1.1",
+    packages=find_packages(),
+    description="A package for generating and finding cyclic patterns",
+    author="Brian M",
+    author_email="DONOTEMAIL@mail.ru",
+    url="https://github.com//exchars",  # Optional
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
