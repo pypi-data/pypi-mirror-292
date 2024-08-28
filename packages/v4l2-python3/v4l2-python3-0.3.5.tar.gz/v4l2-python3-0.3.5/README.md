833372d849f133c95c6c05c06bbead1c8003af68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 03:06:45 +0000

    Add package v4l2-python3/0.3.5/v4l2-python3-0.3.5.tar.gz

---
+++ packages/v4l2-python3/v4l2-python3-0.3.5.tar.gz/v4l2-python3-0.3.5/README.md
@@ -0,0 +1,24 @@
+# python-v4l2
+
+Provides Python bindings for Video4Linux2.
+
+Based on the work of [Jinlei Zheng](https://github.com/AlexJinlei/python-v4l2),
+which was based in turn on [python-v4l2](https://pypi.org/project/v4l2/).
+
+Additions have been made for the H264 format and for Python3.
+
+## Installation
+
+Can be installed via:
+
+```
+pip3 install v4l2-python3
+```
+
+Or alternatively on Raspberry Pi OS (bullseye):
+
+```
+sudo apt install python3-v4l2
+```
+
+See the [Wiki](https://github.com/RaspberryPiFoundation/python-v4l2/wiki) for PyPI publishing instructions.
---
+++ packages/v4l2-python3/v4l2-python3-0.3.5.tar.gz/v4l2-python3-0.3.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='v4l2-python3',
+    version='0.3.5',
+    license='GPLv2',
+    requires=('ctypes',),
+    py_modules=('v4l2',),
+
+    maintainer='Raspberry Pi Foundation',
+    maintainer_email='web@raspberrypi.org',
+    url='https://pypi.org/project/v4l2-python3/',
+    keywords='v4l2 video4linux video4linux2 binding ctypes',
+    description='Python bindings for the v4l2 userspace api.',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Video :: Capture',
+    ],
+)
