1e8e77318ff893349c728f44d8fa39e722b7edd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:07:27 +0000

    Add package ghastoolkit/0.14.0/ghastoolkit-0.14.0-py3-none-any.whl

---
+++ packages/ghastoolkit/ghastoolkit-0.14.0-py3-none-any.whl/ghastoolkit/octokit/graphql/__init__.py
@@ -0,0 +1,97 @@
+DEPENDENCY_GRAPH_STATUS = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        hasVulnerabilityAlertsEnabled
+    }
+}
+"""
+
+DEPENDENCY_GRAPH_ALERTS = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        vulnerabilityAlerts(first: 100, states: [OPEN], $cursor) {
+            totalCount
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            edges {
+                node {
+                    number
+                    state
+                    createdAt
+                    dismissReason
+                    securityVulnerability {
+                        package {
+                            ecosystem
+                            name
+                        }
+                    }
+                    securityAdvisory {
+                        ghsaId
+                        severity
+                        cwes(first: 100) {
+                            edges {
+                                node {
+                                    cweId
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+DEPENDENCY_GRAPH_INFO = """\
+{
+    repository(owner: "$owner", name: "$repo") {
+        name
+        licenseInfo {
+            name
+        }
+        dependencyGraphManifests {
+            totalCount
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            edges {
+                node {
+                    filename
+                    dependencies {
+                        edges {
+                            node {
+                                packageName
+                                packageManager
+                                requirements
+                                repository {
+                                    nameWithOwner
+                                    isArchived
+                                    isDisabled
+                                    isEmpty
+                                    isFork
+                                    isSecurityPolicyEnabled
+                                    isInOrganization
+                                    licenseInfo {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
+
+QUERIES = {
+    "GetDependencyStatus": DEPENDENCY_GRAPH_STATUS,
+    "GetDependencyAlerts": DEPENDENCY_GRAPH_ALERTS,
+    "GetDependencyInfo": DEPENDENCY_GRAPH_INFO,
+}
