8c6888df5a323cdd878a9bed023fd942178ba330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:06:18 +0000

    Add package egpy/0.1.1724769939000/egpy-0.1.1724769939000.tar.gz

---
+++ packages/egpy/egpy-0.1.1724769939000.tar.gz/egpy-0.1.1724769939000/setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+  name="egpy",
+  version=os.environ.get("EGPY_VERSION", "0.0.0"),
+  packages=find_packages(where=["egpy"]),
+  install_requires=["grpcio", "grpcio-tools", "uuid7"],
+)
