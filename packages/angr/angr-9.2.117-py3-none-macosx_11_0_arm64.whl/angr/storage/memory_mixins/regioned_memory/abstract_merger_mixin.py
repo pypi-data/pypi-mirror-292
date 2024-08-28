ee707a57abeb008a06f527b207f951288b69462b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:06:26 +0000

    Add package angr/9.2.117/angr-9.2.117-py3-none-macosx_11_0_arm64.whl

---
+++ packages/angr/angr-9.2.117-py3-none-macosx_11_0_arm64.whl/angr/storage/memory_mixins/regioned_memory/abstract_merger_mixin.py
@@ -0,0 +1,45 @@
+import logging
+from collections.abc import Iterable
+from typing import Any
+
+import claripy
+
+from .. import MemoryMixin
+
+l = logging.getLogger(name=__name__)
+
+
+class AbstractMergerMixin(MemoryMixin):
+    def _merge_values(self, values: Iterable[tuple[Any, Any]], merged_size: int, **kwargs):
+        # if self.category == 'reg' and self.state.arch.register_endness == 'Iend_LE':
+        #     should_reverse = True
+        # elif self.state.arch.memory_endness == 'Iend_LE':
+        #     should_reverse = True
+        # else:
+        #     should_reverse = False
+
+        values = list(values)
+        merged_val = values[0][0]
+
+        # if should_reverse: merged_val = merged_val.reversed
+
+        for tm, _ in values[1:]:
+            # if should_reverse: tm = tm.reversed
+
+            if self._is_uninitialized(tm):
+                continue
+            l.info("Merging %s %s...", merged_val, tm)
+            merged_val = merged_val.union(tm)
+            l.info("... Merged to %s", merged_val)
+
+        # if should_reverse:
+        #     merged_val = merged_val.reversed
+
+        if not values[0][0].uninitialized and claripy.backends.vsa.identical(merged_val, values[0][0]):
+            return None
+
+        return merged_val
+
+    @staticmethod
+    def _is_uninitialized(a):
+        return getattr(claripy.backends.vsa.convert(a), "uninitialized", False)
