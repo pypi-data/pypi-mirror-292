bf6e961373fd25c2c23c56eac4323ba1a456baa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:06:35 +0000

    Add package manim-recorder/0.1.2/manim_recorder-0.1.2-py3-none-any.whl

---
+++ packages/manim-recorder/manim_recorder-0.1.2-py3-none-any.whl/manim_recorder/multimedia.py
@@ -0,0 +1,42 @@
+from pydub import AudioSegment
+from pydub.playback import play
+import platform
+import os
+
+
+def Check_OS():
+    os_name = platform.system()
+    match os_name:
+        case "Linux":
+            if 'TERMUX_VERSION' in os.environ::
+                return "Termux (Android)"
+            else:
+                return "Linux"
+        case "Darwin":
+            return "macOS"
+        case "Windows":
+            return "Windows"
+        case _:
+            return "Unknown OS"
+
+
+class Multimedia:
+    def __init__(self, path):
+        self.path = path
+
+    def Play(self):
+        match Check_OS():
+            case "Termux (Android)":
+                self.Termux_Media_Play()
+                input("[ Stop ]: Press any key to listen to the recording....")
+                self.Termux_Media_Stop()
+            case "Linux" | "macOS" | "Windows":
+                audio = AudioSegment.from_file(self.path)
+                play(audio)
+
+    
+    def Termux_Media_Play(self):
+        subprocess.run(["termux-media-player", "play", str(self.path)])
+
+    def Termux_Media_Stop(self):
+        subprocess.run(["termux-media-player", "stop"])
