9dd3a812a6a6ce16caa7ed274676ab359c8e82d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:07:39 +0000

    Add package nafas/0.7/nafas-0.7.tar.gz

---
+++ packages/nafas/nafas-0.7.tar.gz/nafas-0.7/nafas/__main__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""nafas main."""
+
+import argparse
+from nafas.functions import description_print, get_input_standard, input_filter
+from nafas.functions import get_program_data, program_description_print, run
+from nafas.params import NAFAS_VERSION, EXIT_MESSAGE
+from art import tprint
+
+
+def main():
+    """
+    CLI main function.
+
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', help='version', nargs="?", const=1)
+    args = parser.parse_args()
+    if args.version:
+        print(NAFAS_VERSION)
+    else:
+        tprint("Nafas")
+        tprint("v" + str(NAFAS_VERSION))
+        description_print()
+        EXIT_FLAG = False
+        while not EXIT_FLAG:
+            input_data = get_input_standard()
+            filtered_data = input_filter(input_data)
+            program_name, level, program_data = get_program_data(filtered_data)
+            program_description_print(program_name, level, program_data)
+            run(program_data)
+            INPUTINDEX = str(
+                input("Press [R] to restart or any other key to exit."))
+            if INPUTINDEX.upper() != "R":
+                EXIT_FLAG = True
+                print(EXIT_MESSAGE)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n" + EXIT_MESSAGE)
