d6f22738fe708bd2c09734ed48a3de8474351885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 06:06:48 +0000

    Add package slack-message-blocks-simplified/0.0.5/slack_message_blocks_simplified-0.0.5.tar.gz

---
+++ packages/slack-message-blocks-simplified/slack_message_blocks_simplified-0.0.5.tar.gz/slack_message_blocks_simplified-0.0.5/src/slack_message_blocks_simplified/__init__.py
@@ -0,0 +1,2 @@
+from .slack_blocks import SlackBlock, HeaderBlock, DividerBlock, SectionBlock, ImageBlock, RichTextBlock, ContextBlock # type: ignore
+from .slack_client import SlackClient # type: ignore
