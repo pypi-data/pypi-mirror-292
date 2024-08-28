b112ea263e53d795f2d50be2d6c3c31f265acf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:07:31 +0000

    Add package jsonz/0.0.7/jsonz-0.0.7-py3-none-any.whl

---
+++ packages/jsonz/jsonz-0.0.7-py3-none-any.whl/jsonz/__init__.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+import os, sys, json
+
+"""
+https://github.com/python/cpython/blob/main/Lib/json/__init__.py
+"""
+
+def load_file(fp):
+    if not os.path.exists(fp):
+        return None
+    with open(fp, 'r') as reader:
+        return reader.readlines()
+
+class jsonc(object):
+    @staticmethod
+    def load(fp):
+        return json.loads("\n".join([
+            x
+            for x in load_file(fp)
+            if not x.startswith("//")
+        ]))
+class jsonl(object):
+    @staticmethod
+    def load(fp):
+        output = []
+        for content_line in load_file(fp):
+            if not content_line.strip().startswith("//"):
+                try:
+                    output += [
+                        json.loads(content_line)
+                    ]
+                except Exception as e:
+                    print(e)
+        return output
+
+def load(file:str):
+    if not os.path.exists(file):
+        return None
+    if file.endswith(".jsonc"):
+        return jsonc.load(file)
+    elif file.endswith(".jsonl"):
+        return jsonl.load(file)
+    if file.endswith(".json"):
+        with open(file, "r") as reader:
+            return json.load(reader)
+    return None
