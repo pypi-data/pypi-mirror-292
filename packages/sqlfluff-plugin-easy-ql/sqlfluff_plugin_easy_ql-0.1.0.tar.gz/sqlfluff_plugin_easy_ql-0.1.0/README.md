21742513a26655f9bf0dfa29351eab4f40d4ad89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:06:00 +0000

    Add package sqlfluff-plugin-easy-ql/0.1.0/sqlfluff_plugin_easy_ql-0.1.0.tar.gz

---
+++ packages/sqlfluff-plugin-easy-ql/sqlfluff_plugin_easy_ql-0.1.0.tar.gz/sqlfluff_plugin_easy_ql-0.1.0/README.md
@@ -0,0 +1,11 @@
+# Easy QL
+
+EasyQL is a SQLFluff plugin
+
+## Installation
+
+Before the plugin is released as a PyPi package, to use it locally you can download the code in the repo and run the following command from the main directory:
+
+```sh
+pip install -e .
+```
---
+++ packages/sqlfluff-plugin-easy-ql/sqlfluff_plugin_easy_ql-0.1.0.tar.gz/sqlfluff_plugin_easy_ql-0.1.0/src/sqlfluff_easy_ql/__init__.py
@@ -0,0 +1,41 @@
+from typing import List, Type
+
+from sqlfluff.core.config import ConfigLoader
+from sqlfluff.core.plugin import hookimpl
+from sqlfluff.core.rules import BaseRule
+
+@hookimpl
+def get_rules() -> List[Type[BaseRule]]:
+    """Get plugin rules.
+
+    NOTE: It is much better that we only import the rule on demand.
+    The root module of the plugin (i.e. this file which contains
+    all of the hook implementations) should have fully loaded before
+    we try and import the rules. This is partly for performance
+    reasons - but more because the definition of a BaseRule requires
+    that all of the get_configs_info() methods have both been
+    defined _and have run_ before so all the validation information
+    is available for the validation steps in the meta class.
+    """
+    # i.e. we DO recommend importing here:
+    from sqlfluff_easy_ql.rules import Rule_EasyQL_L001, Rule_EasyQL_L002  # noqa: F811
+
+    return [Rule_EasyQL_L001, Rule_EasyQL_L002]
+
+
+@hookimpl
+def load_default_config() -> dict:
+    """Loads the default configuration for the plugin."""
+    return ConfigLoader.get_global().load_config_resource(
+        package="sqlfluff_easy_ql",
+        file_name="easy_ql_default_config.cfg",
+    )
+
+
+@hookimpl
+def get_configs_info() -> dict:
+    """Get rule config validations and descriptions."""
+    return {
+        "forbidden_columns": {"definition": "A list of column to forbid"},
+        "join_on_same_line": {"definition": "Whether this rule is on or not"}
+    }
