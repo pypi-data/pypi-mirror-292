351afed7d1baedb5967e0e23ea733c0b1f21cd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 05:07:25 +0000

    Add package snowman-py/0.0.28/snowman_py-0.0.28-py3-none-win32.whl

---
+++ packages/snowman-py/snowman_py-0.0.28-py3-none-win32.whl/snowman/query/__init__.py
@@ -0,0 +1,4 @@
+from .builder import delete as delete
+from .builder import insert as insert
+from .builder import truncate as truncate
+from .builder import update as update
---
+++ packages/snowman-py/snowman_py-0.0.28-py3-none-win32.whl/snowman/query/builder/_builder.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import Any, NamedTuple
+
+from snowman._cursor import Cursor
+from snowman._features import USE_TURU, TuruSnowflakeCursor
+
+
+class QueryWithParams(NamedTuple):
+    query: str
+    params: tuple[Any, ...]
+
+
+class QueryBuilder(ABC):
+    @abstractmethod
+    def build(self) -> QueryWithParams: ...
+
+    @abstractmethod
+    def execute(self, cursor: Cursor, /) -> None: ...
+
+
+def execute_with_tag(
+    tag,
+    cursor: Cursor,
+    query: str,
+    params: tuple[Any, ...],
+):
+    if USE_TURU and isinstance(cursor, TuruSnowflakeCursor):
+        cursor.execute_with_tag(tag, query, params)
+
+    else:
+        cursor.execute(query, params)
+
+
+def executemany_with_tag(
+    tag,
+    cursor: Cursor,
+    query: str,
+    params: tuple[Any, ...],
+):
+    if USE_TURU and isinstance(cursor, TuruSnowflakeCursor):
+        cursor.executemany_with_tag(tag, query, params)
+
+    else:
+        cursor.executemany(query, params)
