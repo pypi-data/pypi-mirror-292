ea4a17a73aaf0665c1c42d20a3f09e7244c8b7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:06:37 +0000

    Add package py-ev3-my-library/0.0.2/py_ev3_my_library-0.0.2.tar.gz

---
+++ packages/py-ev3-my-library/py_ev3_my_library-0.0.2.tar.gz/py_ev3_my_library-0.0.2/README.md
@@ -0,0 +1,4 @@
+# Example Package
+
+This is a package for lego ev3 micropython 2.0
+naurobot
---
+++ packages/py-ev3-my-library/py_ev3_my_library-0.0.2.tar.gz/py_ev3_my_library-0.0.2/src/py_ev3_my_library/example_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+ev3 = EV3Brick()
+
+
+
+def bpp():
+    ev3.speaker.beep()
+    wait(500)
+    ev3.speaker.beep()
