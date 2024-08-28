5cc43d66f21236640740f32a3b573e1161087b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 06:06:33 +0000

    Add package netbox-contract/2.2.4/netbox_contract-2.2.4.tar.gz

---
+++ packages/netbox-contract/netbox_contract-2.2.4.tar.gz/netbox_contract-2.2.4/src/netbox_contract/filtersets.py
@@ -0,0 +1,74 @@
+from django.db.models import Q
+from netbox.filtersets import NetBoxModelFilterSet
+
+from .models import (
+    AccountingDimension,
+    Contract,
+    ContractAssignment,
+    Invoice,
+    InvoiceLine,
+    ServiceProvider,
+)
+
+
+class ContractFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = Contract
+        fields = ('id', 'internal_partie', 'status', 'parent')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value)
+            | Q(external_reference__icontains=value)
+            | Q(comments__icontains=value),
+            Q(status__iexact='Active'),
+        )
+
+
+class InvoiceFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = Invoice
+        fields = ('id', 'contracts')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(
+            Q(number__icontains=value) | Q(contracts__name__icontains=value)
+        )
+
+
+class ServiceProviderFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = ServiceProvider
+        fields = ('id', 'name')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(name__icontains=value)
+
+
+class ContractAssignmentFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = ContractAssignment
+        fields = ('id', 'contract')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(Q(contract__name__icontains=value))
+
+
+class InvoiceLineFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = InvoiceLine
+        fields = ('id', 'invoice')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(
+            Q(comments__icontains=value) | Q(invoice__number__icontains=value)
+        )
+
+
+class AccountingDimensionFilterSet(NetBoxModelFilterSet):
+    class Meta:
+        model = AccountingDimension
+        fields = ('name', 'value')
+
+    def search(self, queryset, name, value):
+        return queryset.filter(Q(comments__icontains=value) | Q(name__icontains=value))
