5faeccc40bdbe37f4e383bd3d4466673c2696a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 14:06:43 +0000

    Add package labelsmith/0.0.1b8/labelsmith-0.0.1b8.tar.gz

---
+++ packages/labelsmith/labelsmith-0.0.1b8.tar.gz/labelsmith-0.0.1b8/src/labelsmith/shyft/utils/log_config.py
@@ -0,0 +1,39 @@
+import logging
+from pathlib import Path
+from labelsmith.shyft.constants import (
+    APP_NAME, APP_AUTHOR, APP_DATA_DIR,
+    CONFIG_FILE, DATA_FILE_PATH, LOGS_DIR
+)
+
+def configure_logging(debug=False):
+    # Create logs directory if it doesn't exist
+    LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Create a custom logger
+    logger = logging.getLogger('labelsmith')
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+
+    # Create handlers
+    file_handler = logging.FileHandler(LOGS_DIR / "app.log")
+    file_handler.setLevel(logging.DEBUG)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+
+    # Create formatters and add it to handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    # Prevent logger from passing messages to the root logger
+    logger.propagate = False
+
+    # Reduce logging level for some overly verbose libraries
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
+    logging.getLogger('PIL').setLevel(logging.WARNING)
+
+    return logger
