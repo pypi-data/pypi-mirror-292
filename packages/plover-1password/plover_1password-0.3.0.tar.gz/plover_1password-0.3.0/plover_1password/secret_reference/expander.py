4a1a552f7309ee30826a128d795feb778790d100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-28 04:05:33 +0000

    Add package plover-1password/0.3.0/plover_1password-0.3.0.tar.gz

---
+++ packages/plover-1password/plover_1password-0.3.0.tar.gz/plover_1password-0.3.0/plover_1password/secret_reference/expander.py
@@ -0,0 +1,33 @@
+"""
+Expander - a module for dealing with expansion of ENV vars in a secret
+reference URI.
+"""
+import os
+from typing import Optional
+
+
+_ENV_VAR_SYNTAX = "$"
+_POWERSHELL_COMMAND = "echo $ExecutionContext.InvokeCommand.ExpandString({0})"
+_SHELL_COMMAND = "{0} -ic 'echo {1}'"
+
+def expand_env_vars(
+    system: str,
+    shell: Optional[str],
+    secret_reference: str
+) -> str:
+    """
+    Expands env vars in a secret reference. Returns immediately if no env vars
+    contained in secret reference string.
+    """
+    if not _ENV_VAR_SYNTAX in secret_reference:
+        return secret_reference
+
+    command: str
+    if system == "Windows":
+        command = _POWERSHELL_COMMAND.format(secret_reference)
+    else:
+        command = _SHELL_COMMAND.format(shell, secret_reference)
+
+    expanded: str = os.popen(command).read().strip()
+
+    return expanded
